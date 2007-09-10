9248b76f091a8e351f2cee7bf314a6f8eabb5159
Felix Meschberger <fmeschbe@apache.org>
2007-09-10 08:50:41 +0000

    Import initial Sling source
    git-svn-id: https://svn.apache.org/repos/asf/incubator/sling/trunk/jackrabbit-server@574179 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/main/java/org/apache/sling/jcr/server/SlingServerRepository.java
@@ -0,0 +1,245 @@
+/*
+ * Copyright 2007 The Apache Software Foundation.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at 
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.sling.jcr.server;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
+import java.util.Dictionary;
+
+import javax.jcr.Repository;
+import javax.jcr.RepositoryException;
+
+import org.apache.jackrabbit.core.RepositoryImpl;
+import org.apache.jackrabbit.core.config.RepositoryConfig;
+import org.apache.sling.jcr.AbstractSlingRepository;
+import org.apache.sling.jcr.SlingRepository;
+import org.osgi.framework.Bundle;
+import org.osgi.service.component.ComponentContext;
+import org.osgi.service.log.LogService;
+
+/**
+ * The <code>RepositorySPIImpl</code> TODO
+ * 
+ * @scr.component label="%repository.name" description="%repository.description"
+ *          factory="org.apache.sling.jcr.server.SlingServerRepositoryFactory"
+ * 
+ * @scr.property name="service.vendor" value="The Apache Software Foundation"
+ * @scr.property name="service.description"
+ *      value="Factory for embeded Jackrabbit Repository Instances"
+ *      
+ * @scr.service
+ * 
+ * @scr.property value="default" name="defaultWorkspace"
+ * @scr.property value="anonymous" name="anonymous.name"
+ * @scr.property value="anonymous" name="anonymous.password"
+ * @scr.property value="admin" name="admin.name"
+ * @scr.property value="admin" name="admin.password"
+ * @scr.property value="-1" type="Integer" name="pool.maxActive"
+ * @scr.property value="10" type="Integer" name="pool.maxIdle"
+ * @scr.property value="1" type="Integer" name="pool.maxActiveWait"
+ * 
+ * @author fmeschbe
+ */
+public class SlingServerRepository extends AbstractSlingRepository
+        implements Repository, SlingRepository {
+
+    /**
+     * @scr.property value="true" type="Boolean"
+     */
+    public static final String PROPERTY_REPOSITORY_AUTOSTART = "autostart";
+
+    /**
+     * The name of the configuration property defining the URL to the
+     * repository configuration file (value is
+     * "com.day.sling.repository.config").
+     * <p>
+     * If the configuration file is located in the local file system, the
+     * "file:" scheme must still be specified.
+     * <p>
+     * This parameter is mandatory for this activator to start the repository.
+     * 
+     * @scr.property value=""
+     */
+    public static final String REPOSITORY_CONFIG_URL = "config";
+
+    /**
+     * The name of the configuration property defining the file system
+     * directory where the repository files are located (value is
+     * "com.day.sling.repository.home").
+     * <p>
+     * This parameter is mandatory for this activator to start the repository.
+     * 
+     * @scr.property value=""
+     */
+    public static final String REPOSITORY_HOME_DIR = "home";
+
+    /**
+     * @scr.property value=""
+     */
+    public static final String REPOSITORY_REGISTRATION_NAME = "name";
+
+    /**
+     * @scr.reference
+     */
+    private LogService log;
+
+    private RepositoryImpl delegatee;
+    
+    //---------- AbstractSlingRepository methods ------------------------------
+    
+    protected Repository getDelegatee() throws RepositoryException {
+        if (delegatee == null) {
+            try {
+                delegatee = (RepositoryImpl) getRepository();
+            } catch (IOException ioe) {
+                throw new RepositoryException(ioe.getMessage(), ioe);
+            }
+        }
+        
+        return delegatee;
+    }
+    
+    protected LogService getLog() {
+        return log;
+    }
+    
+    //---------- SCR integration ----------------------------------------------
+    
+    // activate this service
+    protected void activate(ComponentContext componentContext) throws Exception {
+        // set up the base class (session pooling etc)
+        super.activate(componentContext);
+        
+        // setup the repository from descriptor
+        Object autoStart = componentContext.getProperties().get(PROPERTY_REPOSITORY_AUTOSTART);
+        if (autoStart instanceof Boolean && ((Boolean) autoStart).booleanValue()) {
+            // have the exception thrown go up the chain ...
+            getDelegatee();
+        }
+    }
+    
+    // deactivate this service
+    protected void deactivate(ComponentContext componentContext) {
+        if (delegatee != null) {
+            try {
+                delegatee.shutdown();
+            } catch (Throwable t) {
+                log(LogService.LOG_ERROR, "Unexpected problem shutting down repository", t);
+            }
+        }
+        
+        // deactivate the base class (session pooling etc.)
+        super.deactivate(componentContext);
+    }
+    
+    // set logger
+    protected void bindLogService(LogService log) {
+        this.log = log;
+    }
+
+    // remove logger
+    protected void unbindLogService(LogService log) {
+        this.log = null;
+    }
+    
+    //---------- Repository Publication ---------------------------------------
+    
+    private Repository getRepository() throws RepositoryException, IOException {
+
+        Dictionary environment = getComponentContext().getProperties();
+        
+        String configURLObj = (String) environment.get(REPOSITORY_CONFIG_URL);
+        String home = (String) environment.get(REPOSITORY_HOME_DIR);
+
+        InputStream ins = null;
+        
+        // check whether the URL is a file path
+        File configFile = new File(configURLObj);
+        if (configFile.canRead()) {
+            ins = new FileInputStream(configFile);
+        } else {
+            URL configURL = new URL(configURLObj);
+            ins = configURL.openStream();
+        }
+        
+        try {
+            RepositoryConfig crc = RepositoryConfig.create(ins, home);
+            return RepositoryImpl.create(crc);
+        } finally {
+            if (ins != null) {
+                try {
+                    ins.close();
+                } catch (IOException ioe) {
+                    // ignore
+                }
+            }
+        }
+    }
+    
+    
+    //---------- Helper -------------------------------------------------------
+    
+    static void copyFile(Bundle bundle, String entryPath, File destFile) throws FileNotFoundException, IOException { 
+        if (destFile.canRead()) {
+            // nothing to do, file exists
+            return;
+        }
+        
+        // copy from property
+        URL entryURL = bundle.getEntry(entryPath);
+        if (entryURL == null) {
+            throw new FileNotFoundException(entryPath);
+        }
+        
+        // check for a file property
+        InputStream source = entryURL.openStream();
+        
+        OutputStream dest = null;
+        try {
+
+            // ensure path to parent folder of licFile
+            destFile.getParentFile().mkdirs();
+            
+            dest = new FileOutputStream(destFile);
+            byte[] buf = new byte[2048];
+            int rd;
+            while ( (rd = source.read(buf)) >= 0) {
+                dest.write(buf, 0, rd);
+            }
+
+
+        } finally {
+            if (dest != null) {
+                try {
+                    dest.close();
+                } catch (IOException ignore) {
+                }
+            }
+            
+            // licSource is not null
+            try {
+                source.close();
+            } catch (IOException ignore) {
+            }
+        }
+    }
+}
