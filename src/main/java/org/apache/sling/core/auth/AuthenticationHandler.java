81d372c1bbcfbbbdfa311f45d925ec7ea46a1627
Carsten Ziegeler <cziegeler@apache.org>
2007-09-11 12:26:35 +0000

    Refactor core classes part two: move api to core package
    git-svn-id: https://svn.apache.org/repos/asf/incubator/sling/trunk/core@574568 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/sling/core/auth/AuthenticationHandler.java
+++ src/main/java/org/apache/sling/core/auth/AuthenticationHandler.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.sling.auth;
+package org.apache.sling.core.auth;
 
 import javax.jcr.Credentials;
 
