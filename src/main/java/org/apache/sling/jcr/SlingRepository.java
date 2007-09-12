79c1ff264bef968a932744f515b7b8268b61ed69
Carsten Ziegeler <cziegeler@apache.org>
2007-09-12 05:54:54 +0000

    Fix license headers.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/sling/trunk/jackrabbit-api@574792 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/sling/jcr/SlingRepository.java
+++ src/main/java/org/apache/sling/jcr/SlingRepository.java
@@ -1,17 +1,20 @@
 /*
- * Copyright 2007 The Apache Software Foundation.
- * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at 
- * 
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
 package org.apache.sling.jcr;
 
@@ -50,7 +53,7 @@ public interface SlingRepository extends Repository {
      * {@link #loginAnonymous()} or a regular authenticated session retrieved
      * through {@link #login(Credentials, String)} or
      * {@link #impersonate(Session, String)} must be used.</i></b>
-     * 
+     *
      * @param workspace The name of the workspace to which to get an
      *            administrative session. If <code>null</code> the
      *            {@link #getDefaultWorkspace()} default workspace is assumed.
