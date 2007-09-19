8c7b3eec67bf80c7486ec3564f15d1eba0991c2d
Felix Meschberger <fmeschbe@apache.org>
2007-09-19 05:37:32 +0000

    Remove the superfluous null check again ....
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/sling/trunk@577162 13f79535-47bb-0310-9956-ffa450edef68

--- sling-app/src/main/java/org/apache/sling/launcher/Sling.java
+++ sling-app/src/main/java/org/apache/sling/launcher/Sling.java
@@ -874,7 +874,7 @@ public class Sling implements BundleActivator {
                 try {
                     if (is == null && slingHome != null) {
                         File resFile = new File(file);
-                        if (!resFile.isAbsolute()  && slingHome != null) {
+                        if (!resFile.isAbsolute()) {
                             resFile = new File(slingHome, file);
                         }
                         if (resFile.canRead()) {
