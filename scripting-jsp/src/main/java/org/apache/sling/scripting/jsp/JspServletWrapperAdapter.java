d3efe0cea0948aabfe3cd4e9298a98135b5e3f41
Felix Meschberger <fmeschbe@apache.org>
2007-09-23 17:36:33 +0000

    SLING-19 stackoverflow trying to run jsp scripting
    git-svn-id: https://svn.apache.org/repos/asf/incubator/sling/trunk@578588 13f79535-47bb-0310-9956-ffa450edef68

--- scripting-jsp/src/main/java/org/apache/sling/scripting/jsp/JspServletWrapperAdapter.java
+++ scripting-jsp/src/main/java/org/apache/sling/scripting/jsp/JspServletWrapperAdapter.java
@@ -21,93 +21,87 @@ import java.io.IOException;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 import org.apache.jasper.Constants;
 import org.apache.jasper.JasperException;
 import org.apache.jasper.Options;
 import org.apache.jasper.compiler.JspRuntimeContext;
 import org.apache.jasper.servlet.JspServletWrapper;
-import org.apache.sling.scripting.HttpServletAdapter;
+import org.apache.sling.component.ComponentException;
+import org.apache.sling.component.ComponentRequest;
+import org.apache.sling.component.ComponentResponse;
+import org.apache.sling.scripting.ComponentRenderer;
 
 /**
  * The <code>JspServletWrapperAdapter</code> TODO
- *
+ * 
  * @author fmeschbe
  * @version $Rev:23741 $, $Date:2006-12-01 16:24:05 +0100 (Fr, 01 Dez 2006) $
  */
-public class JspServletWrapperAdapter extends JspServletWrapper {
-
-    private HttpServletAdapter httpServletAdapter;
+public class JspServletWrapperAdapter extends JspServletWrapper implements
+        ComponentRenderer {
 
     JspServletWrapperAdapter(ServletConfig config, Options options,
-        String jspUri, boolean isErrorPage, JspRuntimeContext rctxt)
+            String jspUri, boolean isErrorPage, JspRuntimeContext rctxt)
             throws JasperException {
         super(config, options, jspUri, isErrorPage, rctxt);
     }
 
-    HttpServletAdapter getServletAdapter() {
-        if (this.httpServletAdapter == null) {
-            this.httpServletAdapter = new JspHttpServletAdapter();
+    public void service(ComponentRequest request, ComponentResponse response)
+            throws IOException, ComponentException {
+        try {
+            service(request, response, preCompile(request));
+        } catch (ComponentException ce) {
+            // just rethrow
+            throw ce;
+        } catch (ServletException se) {
+            // convert to ComponentException
+            throw new ComponentException(se.getMessage(), se);
         }
-
-        return this.httpServletAdapter;
     }
 
-    private class JspHttpServletAdapter extends HttpServletAdapter {
+    /**
+     * <p>
+     * Look for a <em>precompilation request</em> as described in Section
+     * 8.4.2 of the JSP 1.2 Specification. <strong>WARNING</strong> - we cannot
+     * use <code>request.getParameter()</code> for this, because that will
+     * trigger parsing all of the request parameters, and not give a servlet the
+     * opportunity to call <code>request.setCharacterEncoding()</code> first.
+     * </p>
+     * 
+     * @param request The servlet requset we are processing
+     * @exception ServletException if an invalid parameter value for the
+     *                <code>jsp_precompile</code> parameter name is specified
+     */
+    boolean preCompile(HttpServletRequest request) throws ServletException {
 
-        protected void service(HttpServletRequest request,
-                HttpServletResponse response) throws IOException,
-                ServletException {
-            JspServletWrapperAdapter.this.service(request, response,
-                this.preCompile(request));
+        // assume it is ok to access the parameters here, as we are not a
+        // toplevel servlet
+        String jspPrecompile = request.getParameter(Constants.PRECOMPILE);
+        if (jspPrecompile == null) {
+            return false;
         }
 
-        /**
-         * <p>
-         * Look for a <em>precompilation request</em> as described in Section
-         * 8.4.2 of the JSP 1.2 Specification. <strong>WARNING</strong> - we
-         * cannot use <code>request.getParameter()</code> for this, because
-         * that will trigger parsing all of the request parameters, and not give
-         * a servlet the opportunity to call
-         * <code>request.setCharacterEncoding()</code> first.
-         * </p>
-         *
-         * @param request The servlet requset we are processing
-         * @exception ServletException if an invalid parameter value for the
-         *                <code>jsp_precompile</code> parameter name is
-         *                specified
-         */
-        boolean preCompile(HttpServletRequest request) throws ServletException {
-
-            // assume it is ok to access the parameters here, as we are not a
-            // toplevel servlet
-            String jspPrecompile = request.getParameter(Constants.PRECOMPILE);
-            if (jspPrecompile == null) {
-                return false;
-            }
-
-            if (jspPrecompile.length() == 0) {
-                return true; // ?jsp_precompile
-            }
-
-            if (jspPrecompile.equals("true")) {
-                return true; // ?jsp_precompile=true
-            }
-
-            if (jspPrecompile.equals("false")) {
-                // Spec says if jsp_precompile=false, the request should not
-                // be delivered to the JSP page; the easiest way to implement
-                // this is to set the flag to true, and precompile the page
-                // anyway.
-                // This still conforms to the spec, since it says the
-                // precompilation request can be ignored.
-                return true; // ?jsp_precompile=false
-            }
-
-            // unexpected value, fail
-            throw new ServletException("Cannot have request parameter "
-                + Constants.PRECOMPILE + " set to " + jspPrecompile);
+        if (jspPrecompile.length() == 0) {
+            return true; // ?jsp_precompile
         }
+
+        if (jspPrecompile.equals("true")) {
+            return true; // ?jsp_precompile=true
+        }
+
+        if (jspPrecompile.equals("false")) {
+            // Spec says if jsp_precompile=false, the request should not
+            // be delivered to the JSP page; the easiest way to implement
+            // this is to set the flag to true, and precompile the page
+            // anyway.
+            // This still conforms to the spec, since it says the
+            // precompilation request can be ignored.
+            return true; // ?jsp_precompile=false
+        }
+
+        // unexpected value, fail
+        throw new ServletException("Cannot have request parameter "
+            + Constants.PRECOMPILE + " set to " + jspPrecompile);
     }
 }
