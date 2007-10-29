2fef5bca9c6d461127ca19e8c6d8f15af8c1fb42
Carsten Ziegeler <cziegeler@apache.org>
2007-10-29 16:22:18 +0000

    More javadoc fixes.
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/sling/trunk@589722 13f79535-47bb-0310-9956-ffa450edef68

--- sling-api/src/main/java/org/apache/sling/api/request/RequestParameter.java
+++ sling-api/src/main/java/org/apache/sling/api/request/RequestParameter.java
@@ -25,20 +25,20 @@ import java.io.UnsupportedEncodingException;
 /**
  * The <code>RequestParameter</code> class represents a single parameter sent
  * with the client request. Instances of this class are returned by the
- * {@link SlingHttpServletRequest#getRequestParameter(String)},
- * {@link SlingHttpServletRequest#getRequestParameters(String)} and
- * {@link SlingHttpServletRequest#getRequestParameterMap()} method.
- * 
- * @see SlingHttpServletRequest#getRequestParameter(String)
- * @see SlingHttpServletRequest#getRequestParameters(String)
- * @see SlingHttpServletRequest#getRequestParameterMap()
+ * {@link org.apache.sling.api.SlingHttpServletRequest#getRequestParameter(String)},
+ * {@link org.apache.sling.api.SlingHttpServletRequest#getRequestParameters(String)} and
+ * {@link org.apache.sling.api.SlingHttpServletRequest#getRequestParameterMap()} method.
+ *
+ * @see org.apache.sling.api.SlingHttpServletRequest#getRequestParameter(String)
+ * @see org.apache.sling.api.SlingHttpServletRequest#getRequestParameters(String)
+ * @see org.apache.sling.api.SlingHttpServletRequest#getRequestParameterMap()
  */
 public interface RequestParameter {
 
     /**
      * Determines whether or not this instance represents a simple form field or
      * an uploaded file.
-     * 
+     *
      * @return <code>true</code> if the instance represents a simple form
      *         field; <code>false</code> if it represents an uploaded file.
      */
@@ -47,7 +47,7 @@ public interface RequestParameter {
     /**
      * Returns the content type passed by the browser or <code>null</code> if
      * not defined.
-     * 
+     *
      * @return The content type passed by the browser or <code>null</code> if
      *         not defined.
      */
@@ -55,14 +55,14 @@ public interface RequestParameter {
 
     /**
      * Returns the size in bytes of the parameter.
-     * 
+     *
      * @return The size in bytes of the parameter.
      */
     long getSize();
 
     /**
      * Returns the contents of the parameter as an array of bytes.
-     * 
+     *
      * @return The contents of the parameter as an array of bytes.
      */
     byte[] get();
@@ -70,7 +70,7 @@ public interface RequestParameter {
     /**
      * Returns an InputStream that can be used to retrieve the contents of the
      * file.
-     * 
+     *
      * @return An InputStream that can be used to retrieve the contents of the
      *         file.
      * @throws IOException if an error occurs.
@@ -82,7 +82,7 @@ public interface RequestParameter {
      * the browser (or other client software). In most cases, this will be the
      * base file name, without path information. However, some clients, such as
      * the Opera browser, do include path information.
-     * 
+     *
      * @return The original filename in the client's filesystem.
      */
     String getFileName();
@@ -91,7 +91,7 @@ public interface RequestParameter {
      * Returns the contents of the parameter as a String, using the default
      * character encoding. This method uses {@link #get()} to retrieve the
      * contents of the item.
-     * 
+     *
      * @return The contents of the parameter, as a string.
      */
     String getString();
@@ -100,7 +100,7 @@ public interface RequestParameter {
      * Returns the contents of the parameter as a String, using the specified
      * encoding. This method uses link {@link #get()} to retrieve the contents
      * of the item.
-     * 
+     *
      * @param encoding The character encoding to use.
      * @return The contents of the parameter, as a string.
      * @throws UnsupportedEncodingException if the requested character encoding
