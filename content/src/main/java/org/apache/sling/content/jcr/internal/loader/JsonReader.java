a83840ed764434150badfa84ffa0d21c3d820d96
Carsten Ziegeler <cziegeler@apache.org>
2007-11-01 12:43:13 +0000

    Use new sling-json library and remove own copies.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/sling/trunk@590982 13f79535-47bb-0310-9956-ffa450edef68

--- content/src/main/java/org/apache/sling/content/jcr/internal/loader/JsonReader.java
+++ content/src/main/java/org/apache/sling/content/jcr/internal/loader/JsonReader.java
@@ -25,9 +25,9 @@ import java.util.Iterator;
 import javax.jcr.PropertyType;
 
 import org.apache.commons.io.IOUtils;
-import org.apache.sling.content.jcr.internal.json.JSONArray;
-import org.apache.sling.content.jcr.internal.json.JSONException;
-import org.apache.sling.content.jcr.internal.json.JSONObject;
+import org.apache.sling.json.JSONArray;
+import org.apache.sling.json.JSONException;
+import org.apache.sling.json.JSONObject;
 
 
 /**
