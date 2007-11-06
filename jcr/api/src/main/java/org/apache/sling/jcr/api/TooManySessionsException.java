071f6da966e043c154a947f55378a09141c5d4e6
Felix Meschberger <fmeschbe@apache.org>
2007-11-06 12:10:47 +0000

    SLING-83 Fix SVN location, project name and java packages
    git-svn-id: https://svn.apache.org/repos/asf/incubator/sling/trunk@592398 13f79535-47bb-0310-9956-ffa450edef68

--- jcr/api/src/main/java/org/apache/sling/jcr/api/TooManySessionsException.java
+++ jcr/api/src/main/java/org/apache/sling/jcr/api/TooManySessionsException.java
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package org.apache.sling.jcr;
+package org.apache.sling.jcr.api;
 
 import javax.jcr.LoginException;
 
--- jcr/api/src/main/java/org/apache/sling/jcr/api/internal/NamespaceMapper.java
+++ jcr/api/src/main/java/org/apache/sling/jcr/api/internal/NamespaceMapper.java
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package org.apache.sling.jcr.internal;
+package org.apache.sling.jcr.api.internal;
 
 import javax.jcr.RepositoryException;
 import javax.jcr.Session;
