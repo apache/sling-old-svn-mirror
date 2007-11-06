5ef10be8b1d7331acc35cb57aa471f03a47eb83c
Felix Meschberger <fmeschbe@apache.org>
2007-11-06 12:20:13 +0000

    SLING-83 Fix SVN location, project name and java packages
    git-svn-id: https://svn.apache.org/repos/asf/incubator/sling/trunk/jcr/jackrabbit.server@592404 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/sling/jcr/jackrabbit/server/JndiRegistrationSupport.java
+++ src/main/java/org/apache/sling/jcr/jackrabbit/server/JndiRegistrationSupport.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.sling.jcr.server;
+package org.apache.sling.jcr.jackrabbit.server;
 
 import java.security.AccessController;
 import java.security.PrivilegedActionException;
