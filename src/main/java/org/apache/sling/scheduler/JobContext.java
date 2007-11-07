013fc0a6cfdebca3170982065e8014596d5595de
Carsten Ziegeler <cziegeler@apache.org>
2007-11-07 10:25:58 +0000

    Use new service locator interface.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/sling/trunk/sling/scheduler@592688 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/sling/scheduler/JobContext.java
+++ src/main/java/org/apache/sling/scheduler/JobContext.java
@@ -19,7 +19,7 @@ package org.apache.sling.scheduler;
 import java.io.Serializable;
 import java.util.Map;
 
-import org.apache.sling.core.ServiceLocator;
+import org.apache.sling.api.services.ServiceLocator;
 
 public interface JobContext {
 
