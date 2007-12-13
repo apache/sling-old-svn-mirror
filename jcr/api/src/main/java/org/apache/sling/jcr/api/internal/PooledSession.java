c672c9d601ca5b09251e82bf5f1a20dde2df867b
Carsten Ziegeler <cziegeler@apache.org>
2007-12-13 10:55:41 +0000

    Make constructor protected to be able to subclass.
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/sling/trunk@603879 13f79535-47bb-0310-9956-ffa450edef68

--- jcr/api/src/main/java/org/apache/sling/jcr/api/internal/PooledSession.java
+++ jcr/api/src/main/java/org/apache/sling/jcr/api/internal/PooledSession.java
@@ -77,7 +77,7 @@ public class PooledSession implements Session {
      * @param delegatee The <code>Session</code> to which all calls are
      *          delegated.
      */
-    PooledSession(SessionPool sessionPool, Session delegatee) {
+    protected PooledSession(SessionPool sessionPool, Session delegatee) {
         this.sessionPool = sessionPool;
         this.delegatee = delegatee;
     }
