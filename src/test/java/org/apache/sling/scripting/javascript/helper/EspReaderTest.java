7b6f225c886b29a1595abc4974f53141a7c1f947
Felix Meschberger <fmeschbe@apache.org>
2007-12-20 10:33:39 +0000

    Move EspReaderTest to the helper package to match the package of the tested EspReader class.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/sling/trunk/scripting/javascript@605877 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/sling/scripting/javascript/helper/EspReaderTest.java
+++ src/test/java/org/apache/sling/scripting/javascript/helper/EspReaderTest.java
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package org.apache.sling.scripting.javascript;
+package org.apache.sling.scripting.javascript.helper;
 
 import java.io.IOException;
 import java.io.Reader;
