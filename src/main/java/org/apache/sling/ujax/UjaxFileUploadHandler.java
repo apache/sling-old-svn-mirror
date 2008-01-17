d69051fe6c32f4e4dce7eb3738fd4bb481d72b5d
Bertrand Delacretaz <bdelacretaz@apache.org>
2008-01-17 15:18:10 +0000

    SLING-168, file upload support for UjaxPostServlet, contributed by Tobias Bocanegra, thanks!
    git-svn-id: https://svn.apache.org/repos/asf/incubator/sling/trunk/usling/usling-servlets@612842 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/main/java/org/apache/sling/ujax/UjaxFileUploadHandler.java
@@ -0,0 +1,188 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.sling.ujax;
+
+import org.apache.sling.api.request.RequestParameter;
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.commons.mime.MimeTypeService;
+import org.apache.jackrabbit.util.Text;
+
+import java.util.Calendar;
+import java.io.IOException;
+
+import javax.jcr.Node;
+import javax.jcr.RepositoryException;
+import javax.jcr.nodetype.NodeTypeManager;
+import javax.jcr.nodetype.NodeType;
+
+/**
+ * Handles file uploads.
+ * <p/>
+ *
+ * Simple example:
+ * <xmp>
+ *   <form action="/home/admin" method="POST" enctype="multipart/form-data">
+ *     <input type="file" name="./portrait" />
+ *   </form>
+ * </xmp>
+ *
+ * this will create a nt:file node below "/home/admin" if the node type of
+ * "admin" is (derived from) nt:folder, a nt:resource node otherwise.
+ * <p/>
+ *
+ * Filename example:
+ * <xmp>
+ *   <form action="/home/admin" method="POST" enctype="multipart/form-data">
+ *     <input type="file" name="./*" />
+ *   </form>
+ * </xmp>
+ *
+ * same as above, but uses the filename of the uploaded file as name for the
+ * new node.
+ * <p/>
+ *
+ * Type hint example:
+ * <xmp>
+ *   <form action="/home/admin" method="POST" enctype="multipart/form-data">
+ *     <input type="file" name="./portrait" />
+ *     <input type="hidden" name="./portrait@TypeHint" value="my:file" />
+ *   </form>
+ * </xmp>
+ *
+ * this will create a new node with the type my:file below admin. if the hinted
+ * type extends from nt:file an intermediate file node is created otherwise
+ * directly a resource node.
+ * 
+ * @version $Rev$, $Date$
+ */
+public class UjaxFileUploadHandler {
+
+    /**
+     * The CVS/SVN id
+     */
+    static final String CVS_ID = "$URL$ $Rev$ $Date$";
+
+    // nodetype name string constants
+    public static final String NT_FOLDER = "nt:folder";
+    public static final String NT_FILE = "nt:file";
+    public static final String NT_RESOURCE = "nt:resource";
+
+    // item name string constants
+    public static final String JCR_CONTENT = "jcr:content";
+    public static final String JCR_LASTMODIFIED = "jcr:lastModified";
+    public static final String JCR_MIMETYPE = "jcr:mimeType";
+    public static final String JCR_ENCODING = "jcr:encoding";
+    public static final String JCR_DATA = "jcr:data";
+
+    /**
+     * Uses the file(s) in the request parameter for creation of new nodes.
+     * if the parent node is a nt:folder a new nt:file is created. otherwise
+     * just a nt:resource. if the <code>name</code> is '*', the filename of
+     * the uploaded file is used.
+     *
+     * @param request the servlet request
+     * @param parent the parent node
+     * @param name the name of the file/resource node or '*'
+     * @param values the request values
+     * @param typeHint a hint for the node type
+     * @throws RepositoryException if an error occurs
+     */
+    void setFile(SlingHttpServletRequest request, Node parent, String name,
+                 RequestParameter[] values, String typeHint)
+            throws RepositoryException {
+        RequestParameter value = values[0];
+        assert !value.isFormField();
+
+        // ignore if empty
+        if (value.getSize() <= 0) {
+            return;
+        }
+
+        // get node name
+        if (name.equals("*")) {
+            name = value.getFileName();
+            // strip of possible path (some browsers include the entire path)
+            name = name.substring(name.lastIndexOf('/') + 1);
+            name = name.substring(name.lastIndexOf('\\') + 1);
+        }
+        name = Text.escapeIllegalJcrChars(name);
+
+        // check type hint. if the type is ok and extends from nt:file,
+        // create an nt:file with that type. if it's invalid, drop it and let
+        // the parent node type decide.
+        boolean createNtFile = parent.isNodeType(NT_FOLDER);
+        if (typeHint != null) {
+            try {
+                NodeTypeManager ntMgr = parent.getSession().getWorkspace().getNodeTypeManager();
+                NodeType nt = ntMgr.getNodeType(typeHint);
+                createNtFile = nt.isNodeType(NT_FILE);
+            } catch (RepositoryException e) {
+                // assuming type not valid.
+                typeHint = null;
+            }
+        }
+
+        // set empty type
+        if (typeHint == null) {
+            typeHint = createNtFile ? NT_FILE : NT_RESOURCE;
+        }
+
+        // remove node
+        if (parent.hasNode(name)) {
+            parent.getNode(name).remove();
+        }
+
+        // create nt:file node if needed
+        if (createNtFile) {
+            // create nt:file
+            parent = parent.addNode(name, typeHint);
+            name = JCR_CONTENT;
+            typeHint = NT_RESOURCE;
+        }
+        
+        // create resource node
+        Node res = parent.addNode(name, typeHint);
+
+        // get content type
+        String contentType = value.getContentType();
+        if (contentType != null) {
+            int idx = contentType.indexOf(';');
+            if (idx > 0) {
+                contentType = contentType.substring(0, idx);
+            }
+        }
+        if (contentType == null || contentType.equals("application/octet-stream")) {
+            // try to find a better content type
+            MimeTypeService svc = request.getServiceLocator().getService(MimeTypeService.class);
+            if (svc != null) {
+                contentType = svc.getMimeType(value.getFileName());
+            }
+            if (contentType == null || contentType.equals("application/octet-stream")) {
+                contentType = "application/octet-stream";
+            }
+        }
+
+        // set properties
+        res.setProperty(JCR_LASTMODIFIED, Calendar.getInstance());
+        res.setProperty(JCR_MIMETYPE, contentType);
+        try {
+            res.setProperty(JCR_DATA, value.getInputStream());
+        } catch (IOException e) {
+            throw new RepositoryException("Error while retrieving inputstream from parameter value.", e);
+        }
+    }
+}
