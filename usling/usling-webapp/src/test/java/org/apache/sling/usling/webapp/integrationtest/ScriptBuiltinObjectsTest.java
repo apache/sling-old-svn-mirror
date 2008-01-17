85e84bfabcd4384a575ba99010514bd44e3b900b
Bertrand Delacretaz <bdelacretaz@apache.org>
2008-01-17 14:41:57 +0000

    SLING-149 - merge usling into Sling - integration tests 'ported' from microsling, tests which don't pass yet disabled using TODO_FAILS function name prefix
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/sling/trunk@612830 13f79535-47bb-0310-9956-ffa450edef68

---
+++ usling/usling-webapp/src/test/java/org/apache/sling/usling/webapp/integrationtest/ScriptBuiltinObjectsTest.java
@@ -0,0 +1,48 @@
+package org.apache.sling.usling.webapp.integrationtest;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ScriptBuiltinObjectsTest extends RenderingTestBase {
+    
+    private String slingResourceType;
+    
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+
+        // set test values
+        slingResourceType = "integration-test/srt." + System.currentTimeMillis();
+        testText = "This is a test " + System.currentTimeMillis();
+
+        // create the test node, under a path that's specific to this class to allow collisions
+        final String url = HTTP_BASE_URL + "/" + getClass().getSimpleName() + "/" + System.currentTimeMillis() + "/UJAX_create";
+        final Map<String,String> props = new HashMap<String,String>();
+        props.put("sling:resourceType", slingResourceType);
+        props.put("text", testText);
+        displayUrl = testClient.createNode(url, props);
+
+        // the rendering script goes under /apps in the repository
+        scriptPath = "/apps/" + slingResourceType;
+        testClient.mkdirs(WEBDAV_BASE_URL, scriptPath);
+    }
+
+    public void testNothing() {
+        // TODO remove this all TODO_FAILS_ are gone 
+    }
+ 
+    public void TODO_FAILS_testEspBuiltinObjects() throws IOException {
+        final String toDelete = uploadTestScript("builtin-objects.esp","html.esp");
+        try {
+            final String content = getContent(displayUrl + ".html", CONTENT_TYPE_HTML);
+            assertTrue("Content includes ESP marker",content.contains("ESP template"));
+            assertTrue("Content includes test text", content.contains(testText));
+            assertTrue("Content includes ServletContext data",content.contains("ServletContext:text/plain"));
+            assertTrue("Content includes response data",content.contains("SlingHttpServletResponse:false"));
+        } finally {
+            testClient.delete(toDelete);
+        }
+    }
+    
+}
