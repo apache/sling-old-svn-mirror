b9e5a272f6689b5dd2e9f53cc9439e50f8ee2e44
Bertrand Delacretaz <bdelacretaz@apache.org>
2008-01-24 12:53:09 +0000

    SLING-154 - temporary fix so that launcher-webapp tests pass, but JCR Node methods are inaccessible for now
    git-svn-id: https://svn.apache.org/repos/asf/incubator/sling/trunk@614865 13f79535-47bb-0310-9956-ffa450edef68

--- scripting/javascript/src/main/java/org/apache/sling/scripting/javascript/wrapper/ScriptableNode.java
+++ scripting/javascript/src/main/java/org/apache/sling/scripting/javascript/wrapper/ScriptableNode.java
@@ -16,15 +16,26 @@
  */
 package org.apache.sling.scripting.javascript.wrapper;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.Iterator;
+import java.util.List;
 
 import javax.jcr.Node;
+import javax.jcr.NodeIterator;
+import javax.jcr.Property;
+import javax.jcr.PropertyIterator;
+import javax.jcr.PropertyType;
 import javax.jcr.RepositoryException;
+import javax.jcr.Value;
+import javax.jcr.ValueFormatException;
 import javax.jcr.nodetype.NodeType;
 
 import org.apache.sling.scripting.javascript.helper.SlingWrapper;
+import org.mozilla.javascript.NativeArray;
 import org.mozilla.javascript.ScriptRuntime;
+import org.mozilla.javascript.Scriptable;
 import org.mozilla.javascript.ScriptableObject;
 import org.mozilla.javascript.Undefined;
 import org.slf4j.Logger;
@@ -224,6 +235,103 @@ public class ScriptableNode extends ScriptableObject implements SlingWrapper {
 
     public boolean jsGet_modified() {
         return node.isModified();
+    }
+    
+    /**
+     * Gets the value of a (Javascript) property or child node. If there is a single single-value
+     * JCR property of this node, return its string value. If there are multiple properties
+     * of the same name or child nodes of the same name, return an array.
+     */
+    @Override
+    public Object get(String name, Scriptable start) {
+        List<Scriptable> items = new ArrayList<Scriptable>();
+
+        // add all matching nodes
+        try {
+            NodeIterator it = node.getNodes(name);
+            while (it.hasNext()) {
+                items.add(new ScriptableNode(it.nextNode()));
+            }
+        } catch (RepositoryException e) {}
+
+        // add all matching properies
+        try {
+            PropertyIterator it = node.getProperties(name);
+            while (it.hasNext()) {
+                Property prop = it.nextProperty();
+                int type = prop.getType();
+                if (prop.getDefinition().isMultiple()) {
+                    Value[] values = prop.getValues();
+                    for (int i=0;i<values.length;i++) {
+                        items.add(wrap(values[i], type));
+                    }
+                } else {
+                    if (type==PropertyType.REFERENCE) {
+                        items.add(new ScriptableNode(prop.getNode()));
+                    } else {
+                        items.add(wrap(prop.getValue(), type));
+                    }
+                }
+            }
+        } catch (RepositoryException e) {}
+
+        if (items.size()==0) {
+            return Undefined.instance;
+        } else if (items.size()==1) {
+            return items.iterator().next();
+        } else {
+            //TODO: add write support
+            NativeArray result = new NativeArray(items.toArray());
+            ScriptRuntime.setObjectProtoAndParent(result, this);
+            return result;
+        }
+    }
+
+    /** Wrap JCR Values in a simple way */
+    private Scriptable wrap(Value value, int type) throws ValueFormatException, IllegalStateException, RepositoryException {
+        Object valObj;
+        if (type==PropertyType.BINARY) {
+            valObj = value.getBoolean();
+        } else if (type==PropertyType.DOUBLE) {
+            valObj = value.getDouble();
+        } else if (type==PropertyType.LONG) {
+            valObj = value.getLong();
+        } else {
+            valObj = value.getString();
+        }
+
+        return ScriptRuntime.toObject(this, valObj);
+    }
+
+    @Override
+    public Object[] getIds() {
+        Collection<String> ids = new ArrayList<String>();
+        try {
+            PropertyIterator pit = node.getProperties();
+            while (pit.hasNext()) {
+                ids.add(pit.nextProperty().getName());
+            }
+        } catch (RepositoryException e) {
+            //do nothing, just do not list properties
+        }
+        try {
+            NodeIterator nit = node.getNodes();
+            while (nit.hasNext()) {
+                ids.add(nit.nextNode().getName());
+            }
+        } catch (RepositoryException e) {
+            //do nothing, just do not list child nodes
+        }
+        return ids.toArray();
+    }
+
+    @Override
+    public boolean has(String name, Scriptable start) {
+        try {
+            return node.hasProperty(name) || node.hasNode(name);
+        } catch (RepositoryException e) {
+            return false;
+        }
     }
 
     @Override
