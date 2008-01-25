00c507bc79bf392ad36e19a0eb43a4fbe7857aa2
Felix Meschberger <fmeschbe@apache.org>
2008-01-25 12:47:20 +0000

    SLING-197 Merge resource integration concept into trunk
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/sling/trunk/sling/servlet-resolver@615211 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/sling/servlet/resolver/ServletResolverConstants.java
+++ src/main/java/org/apache/sling/servlet/resolver/ServletResolverConstants.java
@@ -22,19 +22,77 @@ public class ServletResolverConstants {
 
     /**
      * The name of the service registration property of a Servlet registered as
-     * a service containing the resource type(s) supported by the servlet (value
-     * is "sling.core.resourceTypes"). The type of this property is a String or
-     * String[] (array of strings) denoting the resource types. If this property
-     * is missing or empty the Servlet is ignored.
+     * a service providing the absolute paths under which the servlet is
+     * accessible as a Resource (value is "sling.servlet.paths")
+     * <p>
+     * The type of this property is a String or String[] (array of strings)
+     * denoting the resource types.
      */
-    public static final String SLING_RESOURCE_TYPES = "sling.core.resourceTypes";
+    public static final String SLING_SERVLET_PATHS = "sling.servlet.paths";
 
     /**
-     * The name of the registered servlet used as the default servlet if no
-     * other servlet or script could be selected (value is
-     * "sling.core.servlet.default"). If no servlet is registered under this
-     * name, the {@link org.apache.sling.servlet.resolver.defaults.DefaultServlet} is used.
+     * The name of the service registration property of a Servlet registered as
+     * a service containing the resource type(s) supported by the servlet (value
+     * is "sling.servlet.resourceTypes").
+     * <p>
+     * The type of this property is a String or String[] (array of strings)
+     * denoting the resource types. This property is ignored if the
+     * {@link #SLING_SERVLET_PATHS} property is set. Otherwise this property
+     * must be set or the servlet is ignored.
      */
-    public static final String DEFAULT_SERVLET_NAME = "sling.core.servlet.default";
+    public static final String SLING_SERVLET_RESOURCE_TYPES = "sling.servlet.resourceTypes";
+
+    /**
+     * The name of the service registration property of a Servlet registered as
+     * a service containing the request URL selectors supported by the servlet
+     * (value is "sling.servlet.selectors"). The selectors must be configured as
+     * they would be specified in the URL that is as a list of dot-separated
+     * strings such as <em>print.a4</em>.
+     * <p>
+     * The type of this property is a String or String[] (array of strings)
+     * denoting the resource types. This property is ignored if the
+     * {@link #SLING_SERVLET_PATHS} property is set. Otherwise this property is
+     * optional and ignored if not set.
+     */
+    public static final String SLING_SERVLET_SELECTORS = "sling.servlet.selectors";
+
+    /**
+     * The name of the service registration property of a Servlet registered as
+     * a service containing the request URL extensions supported by the servlet
+     * for GET requests (value is "sling.servlet.extensions").
+     * <p>
+     * The type of this property is a String or String[] (array of strings)
+     * denoting the resource types. This property is ignored if the
+     * {@link #SLING_SERVLET_PATHS} property is set. Otherwise this property or
+     * the {@link #SLING_SERVLET_METHODS} is optional and ignored if not set.
+     */
+    public static final String SLING_SERVLET_EXTENSIONS = "sling.servlet.extensions";
+
+    /**
+     * The name of the service registration property of a Servlet registered as
+     * a service containing the request methods supported by the servlet (value
+     * is "sling.servlet.methods").
+     * <p>
+     * The type of this property is a String or String[] (array of strings)
+     * denoting the resource types. This property is ignored if the
+     * {@link #SLING_SERVLET_PATHS} property is set. Otherwise this property or
+     * the {@link #SLING_SERVLET_EXTENSIONS} is optional and ignored if not set.
+     */
+    public static final String SLING_SERVLET_METHODS = "sling.servlet.methods";
+
+    /**
+     * The resource type of a registered servlet used as the default servlet if
+     * no other servlet or script could be selected (value is
+     * "sling/servlet/default"). If no servlet is registered under this name,
+     * the {@link org.apache.sling.servlet.resolver.defaults.DefaultServlet} is
+     * used.
+     */
+    public static final String DEFAULT_SERVLET_NAME = "sling/servlet/default";
+
+    /**
+     * The resource used to select error handlers (value is
+     * "sling/servlet/errorhandler").
+     */
+    public static final String ERROR_HANDLER_PATH = "sling/servlet/errorhandler";
 
 }
---
+++ src/main/java/org/apache/sling/servlet/resolver/resource/ServletResource.java
@@ -0,0 +1,74 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.sling.servlet.resolver.resource;
+
+import javax.servlet.Servlet;
+
+import org.apache.sling.adapter.SlingAdaptable;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ResourceMetadata;
+import org.apache.sling.api.resource.ResourceProvider;
+
+public class ServletResource extends SlingAdaptable implements Resource {
+
+    private final ResourceProvider resourceProvider;
+
+    private final Servlet servlet;
+
+    private final String path;
+
+    private final ResourceMetadata metadata;
+
+    public ServletResource(ResourceProvider resourceProvider, Servlet servlet,
+            String path) {
+        this.resourceProvider = resourceProvider;
+        this.servlet = servlet;
+        this.path = path;
+
+        this.metadata = new ResourceMetadata();
+        metadata.put(ResourceMetadata.RESOLUTION_PATH, path);
+    }
+
+    public ResourceMetadata getResourceMetadata() {
+        return metadata;
+    }
+
+    public ResourceProvider getResourceProvider() {
+        return resourceProvider;
+    }
+
+    public String getResourceType() {
+        // the resource type of a servlet is the servlet's path
+        return path;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    @SuppressWarnings("unchecked")
+    public <AdapterType> AdapterType adaptTo(Class<AdapterType> type) {
+        if (type == Servlet.class) {
+            return (AdapterType) servlet; // unchecked cast
+        }
+
+        return super.adaptTo(type);
+    }
+
+}
