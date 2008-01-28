5b8f7682f629286bdc7b13d42cf54a7b5ef020bc
Bertrand Delacretaz <bdelacretaz@apache.org>
2008-01-28 16:53:28 +0000

    SLING-149 - ujax:sessionInfo works, tests reactivated
    git-svn-id: https://svn.apache.org/repos/asf/incubator/sling/trunk/launchpad/launchpad-webapp@615947 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/sling/launchpad/webapp/integrationtest/ujax/UjaxSessionInfoTest.java
+++ src/test/java/org/apache/sling/launchpad/webapp/integrationtest/ujax/UjaxSessionInfoTest.java
@@ -26,16 +26,12 @@ import org.apache.sling.launchpad.webapp.integrationtest.HttpTestBase;
 
 public class UjaxSessionInfoTest extends HttpTestBase {
     
-    public void testNothing() {
-        // TODO remove this all TODO_FAILS_ are gone 
-    }
-
-    public void TODO_FAILS_testSessionInfo() throws IOException {
+    public void testSessionInfo() throws IOException {
         final String content = getContent(HTTP_BASE_URL + "/ujax:sessionInfo.json", CONTENT_TYPE_JSON);
         assertJavascript("admin.default", content, "out.println(data.userID + '.' + data.workspace)");
     }
     
-    public void TODO_FAILS_testNonexistentUjaxUrl() throws IOException {
+    public void testNonexistentUjaxUrl() throws IOException {
         assertHttpStatus(HTTP_BASE_URL + "/ujax:nothing", HttpServletResponse.SC_NOT_FOUND);
     }
 }
