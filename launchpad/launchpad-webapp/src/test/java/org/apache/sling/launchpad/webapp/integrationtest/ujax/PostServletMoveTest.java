4f974d6608cb66a346bab8b5cfa3f5fc6c2332bc
Carsten Ziegeler <cziegeler@apache.org>
2008-02-13 07:54:03 +0000

    SLING-244: Apply patch from Tobias Bocanegra for fixing order of execution for moves.
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/sling/trunk@627300 13f79535-47bb-0310-9956-ffa450edef68

---
+++ launchpad/launchpad-webapp/src/test/java/org/apache/sling/launchpad/webapp/integrationtest/ujax/PostServletMoveTest.java
@@ -0,0 +1,77 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.sling.launchpad.webapp.integrationtest.ujax;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.HashMap;
+
+import org.apache.sling.launchpad.webapp.integrationtest.HttpTestBase;
+
+/** Test node move via the MicrojaxPostServlet */
+public class PostServletMoveTest extends HttpTestBase {
+    public static final String TEST_BASE_PATH = "/ujax-tests";
+    private String testPath;
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        testPath = TEST_BASE_PATH + "/" + System.currentTimeMillis();
+    }
+
+    public void testMoveNodeAbsolute() throws IOException {
+        Map<String, String> props = new HashMap<String, String>();
+        props.put("text", "Hello");
+        testClient.createNode(HTTP_BASE_URL + testPath + "/src", props);
+
+        props.clear();
+        props.put("ujax:moveSrc", testPath + "/src");
+        props.put("ujax:moveDest", testPath + "/dest");
+        testClient.createNode(HTTP_BASE_URL + testPath, props);
+        String content = getContent(HTTP_BASE_URL + testPath + "/dest.json", CONTENT_TYPE_JSON);
+        assertJavascript("Hello", content, "out.println(data.text)");
+    }
+
+    public void testMoveNodeRelative() throws IOException {
+        Map<String, String> props = new HashMap<String, String>();
+        props.put("text", "Hello");
+        testClient.createNode(HTTP_BASE_URL + testPath + "/src", props);
+
+        props.clear();
+        props.put("ujax:moveSrc", "src");
+        props.put("ujax:moveDest", "dest");
+        testClient.createNode(HTTP_BASE_URL + testPath, props);
+        String content = getContent(HTTP_BASE_URL + testPath + "/dest.json", CONTENT_TYPE_JSON);
+        assertJavascript("Hello", content, "out.println(data.text)");
+    }
+
+    public void testMoveNodeNew() throws IOException {
+        Map<String, String> props = new HashMap<String, String>();
+        props.put("text", "Hello");
+        testClient.createNode(HTTP_BASE_URL + testPath + "/src", props);
+
+        props.clear();
+        props.put("ujax:moveSrc", testPath + "/src");
+        props.put("ujax:moveDest", "new");
+        // special case - need force creation of the new node 
+        props.put("jcr:created", "");
+        String newNode = testClient.createNode(HTTP_BASE_URL + testPath + "/*", props);
+        String content = getContent(newNode + "/new.json", CONTENT_TYPE_JSON);
+        assertJavascript("Hello", content, "out.println(data.text)");
+    }
+
+ }
