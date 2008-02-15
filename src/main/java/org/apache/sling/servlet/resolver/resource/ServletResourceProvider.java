4f51aab5b94cee5066a189a04c9046be10ac886d
Felix Meschberger <fmeschbe@apache.org>
2008-02-15 10:52:02 +0000

    SLING-246 replace Resource.getResourceProvider() by Resource.getResourceResolver() and adapt uses and test cases
    git-svn-id: https://svn.apache.org/repos/asf/incubator/sling/trunk/sling/servlet-resolver@628008 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/sling/servlet/resolver/resource/ServletResourceProvider.java
+++ src/main/java/org/apache/sling/servlet/resolver/resource/ServletResourceProvider.java
@@ -25,23 +25,29 @@ import static org.apache.sling.servlet.resolver.ServletResolverConstants.SLING_S
 import static org.apache.sling.servlet.resolver.ServletResolverConstants.SLING_SERVLET_SELECTORS;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
+import java.util.NoSuchElementException;
+import java.util.Set;
 
 import javax.servlet.Servlet;
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.api.resource.ResourceProvider;
+import org.apache.sling.api.resource.ResourceResolver;
 import org.apache.sling.osgi.commons.OsgiUtil;
 import org.apache.sling.servlet.resolver.helper.PathSupport;
 import org.osgi.framework.ServiceReference;
 
 public class ServletResourceProvider implements ResourceProvider {
 
-    private Map<String, ServletResource> servletMap;
+    private final Servlet servlet;
+
+    private Set<String> resourcePaths;
 
     public static ServletResourceProvider create(ServiceReference ref,
             Servlet servlet, String servletRoot) {
@@ -49,7 +55,7 @@ public class ServletResourceProvider implements ResourceProvider {
         // check whether explicit paths are set
         String[] paths = OsgiUtil.toStringArray(ref.getProperty(SLING_SERVLET_PATHS));
         if (paths != null && paths.length > 0) {
-            for (int i=0; i < paths.length; i++) {
+            for (int i = 0; i < paths.length; i++) {
                 if (!paths[i].startsWith("/")) {
                     paths[i] = servletRoot + paths[i];
                 }
@@ -68,29 +74,29 @@ public class ServletResourceProvider implements ResourceProvider {
         // check for selectors
         String[] selectors = OsgiUtil.toStringArray(ref.getProperty(SLING_SERVLET_SELECTORS));
         if (selectors == null) {
-            selectors = new String[]{ null };
+            selectors = new String[] { null };
         }
-        
+
         // we have types and expect extensions and/or methods
         String[] extensions = OsgiUtil.toStringArray(ref.getProperty(SLING_SERVLET_EXTENSIONS));
         String[] methods = OsgiUtil.toStringArray(ref.getProperty(SLING_SERVLET_METHODS));
-//        if ((extensions == null || extensions.length == 0)
-//            && (methods == null || methods.length == 0)) {
-//            // TODO: should log, why we ignore this servlet
-//            return null;
-//        }
+        // if ((extensions == null || extensions.length == 0)
+        // && (methods == null || methods.length == 0)) {
+        // // TODO: should log, why we ignore this servlet
+        // return null;
+        // }
 
         List<String> pathList = new ArrayList<String>();
         for (String type : types) {
-            
+
             // ensure namespace prefixes are converted to slashes
             type = PathSupport.toPath(type);
-            
+
             // make absolute if relative
             if (!type.startsWith("/")) {
                 type = servletRoot + type;
             }
-            
+
             // ensure trailing slash for full path building
             if (!type.endsWith("/")) {
                 type += "/";
@@ -98,14 +104,14 @@ public class ServletResourceProvider implements ResourceProvider {
 
             // add entries for each selector combined with each ext and method
             for (String selector : selectors) {
-                
+
                 String selPath = type;
                 if (selector != null && selector.length() > 0) {
                     selPath += selector.replace('.', '/') + "/";
                 }
 
                 boolean pathAdded = false;
-                
+
                 // create paths with extensions
                 if (extensions != null) {
                     for (String ext : extensions) {
@@ -121,10 +127,10 @@ public class ServletResourceProvider implements ResourceProvider {
                         pathAdded = true;
                     }
                 }
-                
+
                 // if neither methods nore extensions were added
                 if (!pathAdded) {
-                    pathList.add(selPath.substring(0, selPath.length()-1));
+                    pathList.add(selPath.substring(0, selPath.length() - 1));
                 }
             }
         }
@@ -134,28 +140,73 @@ public class ServletResourceProvider implements ResourceProvider {
     }
 
     private ServletResourceProvider(String[] paths, Servlet servlet) {
-        servletMap = new HashMap<String, ServletResource>();
-        for (String path : paths) {
-            ServletResource res = new ServletResource(this, servlet, path);
-            servletMap.put(path, res);
+        this.servlet = servlet;
+        this.resourcePaths = new HashSet<String>(Arrays.asList(paths));
+    }
+
+    public Resource getResource(ResourceResolver resourceResolver,
+            HttpServletRequest request, String path) {
+        return getResource(resourceResolver, path);
+    }
+
+    public Resource getResource(ResourceResolver resourceResolver, String path) {
+        if (resourcePaths.contains(path)) {
+            return new ServletResource(resourceResolver, servlet, path);
         }
-    }
 
-    public Resource getResource(HttpServletRequest request, String path) {
-        return getResource(path);
-    }
-
-    public Resource getResource(String path) {
-        return servletMap.get(path);
-    }
-
-    public Iterator<Resource> listChildren(Resource parent) {
-        // the resource provider only has exact paths no children
         return null;
     }
 
+    public Iterator<Resource> listChildren(final Resource parent) {
+        return new Iterator<Resource>() {
+
+            private Iterator<String> pathIter;
+
+            private String parentPath;
+
+            private Resource next;
+
+            {
+                pathIter = resourcePaths.iterator();
+                parentPath = parent.getPath() + "/";
+                next = seek();
+            }
+
+            public boolean hasNext() {
+                return next != null;
+            }
+
+            public Resource next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+
+                Resource result = next;
+                next = seek();
+                return result;
+            }
+
+            public void remove() {
+                throw new UnsupportedOperationException("remove");
+            }
+
+            private Resource seek() {
+                while (pathIter.hasNext()) {
+                    String path = pathIter.next();
+                    if (path.startsWith(parentPath)
+                        && path.indexOf('/', parentPath.length()) < 0) {
+                        return new ServletResource(
+                            parent.getResourceResolver(), servlet, path);
+                    }
+                }
+
+                return null;
+            }
+
+        };
+    }
+
     public String[] getSerlvetPaths() {
-        return servletMap.keySet().toArray(
-            new String[servletMap.keySet().size()]);
+        return resourcePaths.toArray(new String[resourcePaths.size()]);
     }
 }
