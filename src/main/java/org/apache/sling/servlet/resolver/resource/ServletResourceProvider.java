5658eadd0b0b61de62cc47a57daa0b763de77517
Carsten Ziegeler <cziegeler@apache.org>
2008-02-18 14:27:36 +0000

    Clean up code.
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/sling/trunk/sling/servlet-resolver@628753 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/sling/servlet/resolver/resource/ServletResourceProvider.java
+++ src/main/java/org/apache/sling/servlet/resolver/resource/ServletResourceProvider.java
@@ -26,7 +26,6 @@ import static org.apache.sling.servlet.resolver.ServletResolverConstants.SLING_S
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
