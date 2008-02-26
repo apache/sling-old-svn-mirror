2e07de52b60fb5bc5ae26d24662c2bab32ff6961
Felix Meschberger <fmeschbe@apache.org>
2008-02-26 10:28:36 +0000

    SLING-268 Add former launchpad servlets to new servlets-default project SLING-277 Add support for redirection to another resource
    git-svn-id: https://svn.apache.org/repos/asf/incubator/sling/trunk/sling/servlets-default@631164 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/main/java/org/apache/sling/servlets/JsonRendererServlet.java
@@ -0,0 +1,162 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.sling.servlets;
+
+import java.io.IOException;
+
+import javax.jcr.Node;
+import javax.jcr.Property;
+import javax.jcr.RepositoryException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.sling.api.SlingException;
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.SlingHttpServletResponse;
+import org.apache.sling.api.resource.NonExistingResource;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ResourceNotFoundException;
+import org.apache.sling.api.servlets.SlingSafeMethodsServlet;
+import org.apache.sling.commons.json.JSONException;
+import org.apache.sling.commons.json.jcr.JsonItemWriter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * A SlingSafeMethodsServlet that renders the current Resource as a JSON data
+ * block
+ * 
+ * @scr.service
+ *  interface="javax.servlet.Servlet"
+ *  
+ * @scr.component 
+ *  immediate="true" 
+ *  metatype="false"
+ *  
+ * @scr.property 
+ *  name="service.description"
+ *  value="Default JSON Renderer Servlet"
+ *  
+ * @scr.property 
+ *  name="service.vendor" 
+ *  value="The Apache Software Foundation"
+ *
+ * Use this as the default servlet for POST requests for Sling 
+ * @scr.property 
+ *  name="sling.servlet.resourceTypes" 
+ *  values.0="sling/servlet/default"
+ *  values.1="sling:redirect"
+ * @scr.property
+ *  name="sling.servlet.extensions"
+ *  value="json"
+ */
+public class JsonRendererServlet extends SlingSafeMethodsServlet {
+
+    private static final Logger log = LoggerFactory.getLogger(JsonRendererServlet.class);
+
+    private static final long serialVersionUID = 5577121546674133317L;
+
+    private static final String responseContentType = "application/json";
+
+    private final JsonItemWriter itemWriter;
+
+    /** Recursion level selector that means "all levels" */
+    public static final String INFINITY = "infinity";
+
+    public JsonRendererServlet() {
+        itemWriter = new JsonItemWriter(null);
+    }
+
+    @Override
+    protected void doGet(SlingHttpServletRequest req,
+            SlingHttpServletResponse resp) throws IOException {
+        // Access and check our data
+        final Resource r = req.getResource();
+        if (r instanceof NonExistingResource) {
+            throw new ResourceNotFoundException("No data to dump");
+        }
+
+        // Do we have a Property?
+        final Property p = r.adaptTo(Property.class);
+        if (p != null) {
+            try {
+                renderProperty(p, resp);
+            } catch (JSONException je) {
+                reportException(je);
+            } catch (RepositoryException re) {
+                reportException(re);
+            }
+            return;
+        }
+
+        // Do we have a Node?
+        final Node n = r.adaptTo(Node.class);
+        if (n == null) {
+            resp.sendError(HttpServletResponse.SC_NOT_IMPLEMENTED,
+                "Can only dump nodes");
+            return;
+        }
+
+        // SLING-167: the last selector, if present, gives the number of
+        // recursion levels, 0 being the default
+        int maxRecursionLevels = 0;
+        final String[] selectors = req.getRequestPathInfo().getSelectors();
+        if (selectors != null && selectors.length > 0) {
+            String level = selectors[selectors.length - 1];
+            if (INFINITY.equals(level)) {
+                maxRecursionLevels = -1;
+            } else {
+                try {
+                    maxRecursionLevels = Integer.parseInt(level);
+                } catch (NumberFormatException nfe) {
+                    resp.sendError(HttpServletResponse.SC_BAD_REQUEST,
+                        "Invalid recursion selector value '" + level + "'");
+                    return;
+                }
+            }
+        }
+
+        resp.setContentType(responseContentType);
+        resp.setCharacterEncoding("UTF-8");
+
+        // do the dump
+        try {
+            itemWriter.dump(n, resp.getWriter(), maxRecursionLevels);
+        } catch (JSONException je) {
+            reportException(je);
+        } catch (RepositoryException re) {
+            reportException(re);
+        }
+    }
+
+    /** Render a Property by dumping its String value */
+    private void renderProperty(Property p, SlingHttpServletResponse resp)
+            throws JSONException, RepositoryException, IOException {
+        resp.setContentType(responseContentType);
+        resp.setCharacterEncoding("UTF-8");
+        new JsonItemWriter(null).dump(p, resp.getWriter());
+    }
+
+    /**
+     * @param e
+     * @throws SlingException wrapping the given exception
+     */
+    private void reportException(Exception e) {
+        log.warn("Error in JsonRendererServlet: " + e.toString(), e);
+        throw new SlingException(e.toString(), e);
+    }
+}
---
+++ src/test/java/org/apache/sling/servlets/RedirectServletTest.java
@@ -0,0 +1,149 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.sling.servlets;
+
+import org.apache.sling.api.SlingHttpServletRequest;
+
+import junit.framework.TestCase;
+
+public class RedirectServletTest extends TestCase {
+
+    public void testSameParent() {
+        String base = "/a";
+        String target = "/b";
+        assertEquals("b", toRedirect(base, target));
+
+        base = "/";
+        target = "/a";
+        assertEquals("a", toRedirect(base, target));
+
+        base = "/a/b/c";
+        target = "/a/b/d";
+        assertEquals("d", toRedirect(base, target));
+    }
+
+    public void testCommonAncestor() {
+        String base = "/a/b/c/d";
+        String target = "/a/b/x/y";
+        assertEquals("../x/y", toRedirect(base, target));
+    }
+
+    public void testUnCommon() {
+        String base = "/a/b/c/d";
+        String target = "/w/x/y/z";
+        assertEquals("../../../w/x/y/z", toRedirect(base, target));
+    }
+
+    public void testSelectorsEtc() {
+        String base = "/a/b/c";
+        String target = "/a/b/d";
+        String expected = "d";
+
+        String selectors = null;
+        String extension = null;
+        String suffix = null;
+        String queryString = null;
+        assertEquals(expected, base, selectors, extension, suffix, queryString,
+            target);
+
+        selectors = null;
+        extension = "html";
+        suffix = null;
+        queryString = null;
+        assertEquals(expected, base, selectors, extension, suffix, queryString,
+            target);
+
+        selectors = "print";
+        extension = "html";
+        suffix = null;
+        queryString = null;
+        assertEquals(expected, base, selectors, extension, suffix, queryString,
+            target);
+
+        selectors = "print.a4";
+        extension = "html";
+        suffix = null;
+        queryString = null;
+        assertEquals(expected, base, selectors, extension, suffix, queryString,
+            target);
+
+        selectors = null;
+        extension = "html";
+        suffix = "/suffix.pdf";
+        queryString = null;
+        assertEquals("../" + expected, base, selectors, extension, suffix,
+            queryString, target);
+
+        selectors = null;
+        extension = "html";
+        suffix = null;
+        queryString = "xy=1";
+        assertEquals(expected, base, selectors, extension, suffix, queryString,
+            target);
+
+        selectors = null;
+        extension = "html";
+        suffix = "/suffix.pdf";
+        queryString = "xy=1";
+        assertEquals("../" + expected, base, selectors, extension, suffix,
+            queryString, target);
+
+        selectors = "print.a4";
+        extension = "html";
+        suffix = "/suffix.pdf";
+        queryString = "xy=1";
+        assertEquals("../" + expected, base, selectors, extension, suffix,
+            queryString, target);
+    }
+
+    private void assertEquals(String expected, String basePath,
+            String selectors, String extension, String suffix,
+            String queryString, String targetPath) {
+
+        if (selectors != null) {
+            expected += "." + selectors;
+        }
+        if (extension != null) {
+            expected += "." + extension;
+        }
+        if (suffix != null) {
+            expected += suffix;
+        }
+        if (queryString != null) {
+            expected += "?" + queryString;
+        }
+
+        String actual = toRedirect(basePath, selectors, extension, suffix,
+            queryString, targetPath);
+
+        assertEquals(expected, actual);
+    }
+
+    private String toRedirect(String basePath, String targetPath) {
+        return toRedirect(basePath, null, null, null, null, targetPath);
+    }
+
+    private String toRedirect(String basePath, String selectors,
+            String extension, String suffix, String queryString,
+            String targetPath) {
+        SlingHttpServletRequest request = new MockSlingHttpServletRequest(
+            basePath, selectors, extension, suffix, queryString);
+        return RedirectServlet.toRedirectPath(targetPath, request);
+    }
+}
