f53b7a9b6624f37a84109eb106375edf899ed9a1
Felix Meschberger <fmeschbe@apache.org>
2008-02-26 10:28:36 +0000

    SLING-268 Add former launchpad servlets to new servlets-default project SLING-277 Add support for redirection to another resource
    git-svn-id: https://svn.apache.org/repos/asf/incubator/sling/trunk@631164 13f79535-47bb-0310-9956-ffa450edef68

---
+++ sling/servlets-default/src/main/java/org/apache/sling/servlets/DefaultHtmlRendererServlet.java
@@ -0,0 +1,105 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.sling.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.jcr.Node;
+import javax.jcr.Property;
+import javax.jcr.RepositoryException;
+import javax.servlet.ServletException;
+
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.SlingHttpServletResponse;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.servlets.SlingSafeMethodsServlet;
+
+/**
+ * A SlingSafeMethodsServlet that renders the current Resource as simple HTML
+ * 
+ * @scr.service
+ *  interface="javax.servlet.Servlet"
+ *  
+ * @scr.component 
+ *  immediate="true" 
+ *  metatype="false"
+ *  
+ * @scr.property 
+ *  name="service.description"
+ *  value="Default HTML Renderer Servlet"
+ *  
+ * @scr.property 
+ *  name="service.vendor" 
+ *  value="The Apache Software Foundation"
+ *
+ * Use this as the default servlet for POST requests for Sling 
+ * @scr.property 
+ *  name="sling.servlet.resourceTypes" 
+ *  value="sling/servlet/default"
+ * @scr.property
+ *  name="sling.servlet.extensions"
+ *  value="html"
+ */
+public class DefaultHtmlRendererServlet extends SlingSafeMethodsServlet {
+
+    private static final long serialVersionUID = -5815904221043005085L;
+
+    private static final String responseContentType = "text/html";
+
+    private final DefaultHtmlRenderer renderer;
+
+    public DefaultHtmlRendererServlet() {
+        this.renderer = new DefaultHtmlRenderer();
+    }
+
+    @Override
+    protected void doGet(SlingHttpServletRequest req,
+            SlingHttpServletResponse resp) throws ServletException, IOException {
+        final Resource r = req.getResource();
+
+        resp.setContentType(responseContentType);
+        resp.setCharacterEncoding("UTF-8");
+
+        final PrintWriter pw = resp.getWriter();
+
+        final Node node = r.adaptTo(Node.class);
+        /*
+         * TODO final SyntheticResourceData srd =
+         * r.adaptTo(SyntheticResourceData.class);
+         */
+        final Property p = r.adaptTo(Property.class);
+
+        try {
+            /*
+             * TODO if(srd != null) { renderer.render(pw, r, srd); } else
+             */
+            if (node != null) {
+                pw.println("<html><body>");
+                renderer.render(pw, r, node);
+                pw.println("</body></html>");
+            } else {
+                // for properties, we just output the String value
+                renderer.render(pw, r, p);
+            }
+
+        } catch (RepositoryException re) {
+            throw new ServletException("Cannot dump contents of "
+                + req.getResource().getPath(), re);
+        }
+    }
+}
