5aba4f485b26c84fa421cff6e874495dd6a66458
Carsten Ziegeler <cziegeler@apache.org>
2008-03-12 14:34:47 +0000

    Clean up code a little bit.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/sling/trunk/jcr/resource@636342 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/sling/jcr/resource/internal/JcrResourceResolverFactoryImpl.java
+++ src/main/java/org/apache/sling/jcr/resource/internal/JcrResourceResolverFactoryImpl.java
@@ -28,7 +28,6 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
-import javax.jcr.RepositoryException;
 import javax.jcr.Session;
 
 import org.apache.commons.collections.BidiMap;
