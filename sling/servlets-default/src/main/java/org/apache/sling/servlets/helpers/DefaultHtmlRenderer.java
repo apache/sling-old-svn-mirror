80fd429cbed2266f73739c74fceffcb26fd5bce5
Bertrand Delacretaz <bdelacretaz@apache.org>
2008-03-12 16:05:53 +0000

    SLING-192 - move DefaultHtmlRenderer to its own package to make it reusable
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/sling/trunk@636383 13f79535-47bb-0310-9956-ffa450edef68

--- sling/servlets-default/src/main/java/org/apache/sling/servlets/helpers/DefaultHtmlRenderer.java
+++ sling/servlets-default/src/main/java/org/apache/sling/servlets/helpers/DefaultHtmlRenderer.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.sling.servlets;
+package org.apache.sling.servlets.helpers;
 
 import java.io.PrintWriter;
 
