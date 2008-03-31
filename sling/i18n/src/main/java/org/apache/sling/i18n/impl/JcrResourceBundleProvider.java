dd7719239c3f49473ff43477d236ccbb7cde3321
Felix Meschberger <fmeschbe@apache.org>
2008-03-31 08:10:33 +0000

    Two Enhancements: (1) Only require the ResourceResolverFactory if the ResourceBundle is not available in the cache and (2) reorder methods an add JavaDoc.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/sling/trunk@642918 13f79535-47bb-0310-9956-ffa450edef68

--- sling/i18n/src/main/java/org/apache/sling/i18n/impl/JcrResourceBundleProvider.java
+++ sling/i18n/src/main/java/org/apache/sling/i18n/impl/JcrResourceBundleProvider.java
@@ -41,11 +41,13 @@ import org.osgi.service.component.ComponentContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-
 /**
- * The <code>JcrResourceBundleProvider</code> TODO
+ * The <code>JcrResourceBundleProvider</code> implements the
+ * <code>ResourceBundleProvider</code> interface creating
+ * <code>ResourceBundle</code> instances from resources stored in the
+ * repository.
  * 
- * @scr.component immediate="true"  label="%provider.name"
+ * @scr.component immediate="true" label="%provider.name"
  *                description="%provider.description"
  * @scr.service interface="org.apache.sling.i18n.ResourceBundleProvider"
  */
@@ -54,7 +56,7 @@ public class JcrResourceBundleProvider implements ResourceBundleProvider,
 
     /** default log */
     private final Logger log = LoggerFactory.getLogger(getClass());
-    
+
     /** @scr.property value="" */
     private static final String PROP_USER = "user";
 
@@ -73,21 +75,179 @@ public class JcrResourceBundleProvider implements ResourceBundleProvider,
     private JcrResourceResolverFactory resourceResolverFactory;
 
     private Locale defaultLocale = Locale.ENGLISH;
-    
+
     private Credentials repoCredentials;
 
     private ResourceResolver resourceResolver;
 
     private Map<Locale, ResourceBundle> resourceBundleCache = new HashMap<Locale, ResourceBundle>();
 
+    // ---------- ResourceBundleProvider ---------------------------------------
+
+    /**
+     * Returns the configured default <code>Locale</code> which is used as a
+     * fallback for {@link #getResourceBundle(Locale)} and also as the basis for
+     * any messages requested from resource bundles.
+     */
     public Locale getDefaultLocale() {
         return defaultLocale;
     }
-    
+
+    /**
+     * Returns the <code>ResourceBundle</code> for the given
+     * <code>locale</code>.
+     * 
+     * @param locale The <code>Locale</code> for which to return the resource
+     *            bundle. If this is <code>null</code> the configured
+     *            {@link #getDefaultLocale() default locale} is assumed.
+     * @return The <code>ResourceBundle</code> for the given locale.
+     * @throws MissingResourceException If the <code>ResourceResolver</code>
+     *             is not available to access the resources.
+     */
     public ResourceBundle getResourceBundle(Locale locale) {
         if (locale == null) {
-            locale = Locale.getDefault();
+            locale = defaultLocale;
         }
+
+        return getResourceBundleInternal(locale);
+    }
+
+    // ---------- EventListener ------------------------------------------------
+
+    /**
+     * Called whenever something is changed inside of <code>jcr:language</code>
+     * or <code>sling:message</code> nodes. We just removed all cached
+     * resource bundles in this case to force reloading them.
+     * <p>
+     * This is much simpler than analyzing the events and trying to be clever
+     * about which exact resource bundles to remove from the cache and at the
+     * same time care for any resource bundle dependencies.
+     * 
+     * @param events The actual JCR events are ignored by this implementation.
+     */
+    public void onEvent(EventIterator events) {
+        log.debug("onEvent: Resource changes, removing cached ResourceBundles");
+        synchronized (resourceBundleCache) {
+            resourceBundleCache.clear();
+        }
+    }
+
+    // ---------- SCR Integration ----------------------------------------------
+
+    /**
+     * Activates and configures this component with the repository access
+     * details and the default locale to use
+     */
+    protected void activate(ComponentContext context) {
+        Dictionary<?, ?> props = context.getProperties();
+
+        String user = (String) props.get(PROP_USER);
+        if (user == null || user.length() == 0) {
+            repoCredentials = null;
+        } else {
+            String pass = (String) props.get(PROP_PASS);
+            char[] pwd = (pass == null) ? new char[0] : pass.toCharArray();
+            repoCredentials = new SimpleCredentials(user, pwd);
+        }
+
+        String localeString = (String) props.get(PAR_DEFAULT_LOCALE);
+        this.defaultLocale = toLocale(localeString);
+    }
+
+    /**
+     * Binds the new <code>respository</code>. If this new repository
+     * replaces and already bound repository, that latter one is released, that
+     * is the session we have on it is loggged out.
+     */
+    protected void bindRepository(SlingRepository repository) {
+        if (this.repository != null) {
+            releaseRepository();
+        }
+        this.repository = repository;
+    }
+
+    /**
+     * Unbinds the given <code>repository</code>. If this is the same
+     * repository we are bound to, we release it by logging out the session we
+     * may have to it.
+     */
+    protected void unbindRepository(SlingRepository repository) {
+        if (this.repository == repository) {
+            releaseRepository();
+            this.repository = null;
+        }
+    }
+
+    /**
+     * Binds a new <code>ResourceResolverFactory</code>. If we are already
+     * bound to another factory, we release that latter one first.
+     */
+    protected void bindResourceResolverFactory(
+            JcrResourceResolverFactory resourceResolverFactory) {
+        if (this.resourceResolverFactory != null) {
+            releaseRepository();
+        }
+        this.resourceResolverFactory = resourceResolverFactory;
+    }
+
+    /**
+     * Unbinds the <code>ResourceResolverFactory</code>. If we are bound to
+     * this factory, we release it.
+     */
+    protected void unbindResourceResolverFactory(
+            JcrResourceResolverFactory resourceResolverFactory) {
+        if (this.resourceResolverFactory == resourceResolverFactory) {
+            releaseRepository();
+            this.resourceResolverFactory = null;
+        }
+    }
+
+    // ---------- internal -----------------------------------------------------
+
+    /**
+     * Internal implementation of the {@link #getResourceBundle(Locale)} method
+     * employing the cache of resource bundles. Creates the bundle if not
+     * already cached.
+     * 
+     * @throws MissingResourceException If the resource bundles needs to be
+     *             created and the <code>ResourceResolver</code> is not
+     *             available to access the resources.
+     */
+    private ResourceBundle getResourceBundleInternal(Locale locale) {
+        ResourceBundle resourceBundle;
+
+        synchronized (resourceBundleCache) {
+            resourceBundle = resourceBundleCache.get(locale);
+        }
+
+        if (resourceBundle == null) {
+            resourceBundle = createResourceBundle(locale);
+
+            synchronized (resourceBundleCache) {
+                // while creating the resource bundle, another thread may
+                // have created the same and already stored it in the cache.
+                // in this case we don't use the one we just created but use
+                // the bundle from the cache. Otherwise, we store our bundle
+                // in the cache and keep using it.
+                if (resourceBundleCache.containsKey(locale)) {
+                    resourceBundle = resourceBundleCache.get(locale);
+                } else {
+                    resourceBundleCache.put(locale, resourceBundle);
+                }
+            }
+
+        }
+
+        return resourceBundle;
+    }
+
+    /**
+     * Creates the resource bundle for the give locale.
+     * 
+     * @throws MissingResourceException If the <code>ResourceResolver</code>
+     *             is not available to access the resources.
+     */
+    private ResourceBundle createResourceBundle(Locale locale) {
 
         ResourceResolver resolver = getResourceResolver();
         if (resolver == null) {
@@ -95,40 +255,39 @@ public class JcrResourceBundleProvider implements ResourceBundleProvider,
             throw new MissingResourceException(
                 "ResourceResolver not available", getClass().getName(), "");
         }
-        
-        return getResourceBundleInternal(resolver, locale);
-    }
-    
-    private ResourceBundle getResourceBundleInternal(ResourceResolver resolver, Locale locale) {
-        ResourceBundle resourceBundle = resourceBundleCache.get(locale);
-        if (resourceBundle == null) {
-            resourceBundle = createResourceBundle(resolver, locale);
-            resourceBundleCache.put(locale, resourceBundle);
-        }
-
-        return resourceBundle;
-    }
-
-    private ResourceBundle createResourceBundle(ResourceResolver resolver, Locale locale) {
 
         JcrResourceBundle bundle = new JcrResourceBundle(locale, resolver);
 
         // set parent resource bundle
-        Locale parent = getParentLocale(locale);
-        if (parent != null) {
-            bundle.setParent(getResourceBundleInternal(resolver, parent));
+        Locale parentLocale = getParentLocale(locale);
+        if (parentLocale != null) {
+            bundle.setParent(getResourceBundleInternal(parentLocale));
         }
 
         return bundle;
     }
 
+    /**
+     * Returns the parent locale of the given locale. The parent locale is the
+     * locale of a locale is defined as follows:
+     * <ol>
+     * <li>If the locale has an variant, the parent locale is the locale with
+     * the same language and country without the variant.</li>
+     * <li>If the locale has no variant but a country, the parent locale is the
+     * locale with the same language but neither country nor variant.</li>
+     * <li>If the locale has no country and not variant and whose language is
+     * different from the language of the the configured default locale, the
+     * parent locale is the configured default locale.</li>
+     * <li>Otherwise there is no parent locale and <code>null</code> is
+     * returned.</li>
+     * </ol>
+     */
     private Locale getParentLocale(Locale locale) {
         if (locale.getVariant().length() != 0) {
             return new Locale(locale.getLanguage(), locale.getCountry());
         } else if (locale.getCountry().length() != 0) {
             return new Locale(locale.getLanguage());
-        } else if (!locale.getLanguage().equals(
-            defaultLocale.getLanguage())) {
+        } else if (!locale.getLanguage().equals(defaultLocale.getLanguage())) {
             return defaultLocale;
         }
 
@@ -136,11 +295,30 @@ public class JcrResourceBundleProvider implements ResourceBundleProvider,
         return null;
     }
 
+    /**
+     * Returns the resource resolver to access messages. This method logs into
+     * the repository and registers with the observation manager if not already
+     * done so. If unable to connect to the repository, <code>null</code> is
+     * returned.
+     * 
+     * @return The <code>ResourceResolver</code> or <code>null</code> if
+     *         unable to login to the repository. <code>null</code> is also
+     *         returned if no <code>ResourceResolverFactory</code> or no
+     *         <code>Repository</code> is available.
+     */
     private ResourceResolver getResourceResolver() {
         if (resourceResolver == null) {
             SlingRepository repo = this.repository;
             JcrResourceResolverFactory fac = this.resourceResolverFactory;
-            if (repo != null && fac != null) {
+            if (repo == null) {
+
+                log.error("getResourceResolver: SlingRepository missing. Cannot login to create ResourceResolver");
+
+            } else if (fac == null) {
+
+                log.error("getResourceResolver: ResourceResolverFactory is missing. Cannot create ResourceResolver");
+
+            } else {
                 Session s = null;
                 try {
                     if (repoCredentials == null) {
@@ -151,14 +329,18 @@ public class JcrResourceBundleProvider implements ResourceBundleProvider,
 
                     ObservationManager om = s.getWorkspace().getObservationManager();
                     om.addEventListener(this, 255, "/", true, null,
-                        new String[] { "mix:language" }, true);
-                    
+                        new String[] { "mix:language", "sling:message" }, true);
+
                     resourceResolver = fac.getResourceResolver(s);
-                    
+
                 } catch (RepositoryException re) {
-                    // TODO log
+                    log.error(
+                        "getResourceResolver: Problem setting up ResourceResolver with Session",
+                        re);
+
                 } finally {
-                    // drop session if we can login but not get the resource resolver
+                    // drop session if we can login but not get the resource
+                    // resolver
                     if (resourceResolver == null && s != null) {
                         s.logout();
                     }
@@ -170,14 +352,20 @@ public class JcrResourceBundleProvider implements ResourceBundleProvider,
         return resourceResolver;
     }
 
+    /**
+     * Logs out from the repository and clears the resource bundle cache.
+     */
     private void releaseRepository() {
         ResourceResolver resolver = this.resourceResolver;
 
         this.resourceResolver = null;
-        this.resourceBundleCache.clear();
+
+        synchronized (resourceBundleCache) {
+            this.resourceBundleCache.clear();
+        }
 
         if (resolver != null) {
-            
+
             Session s = resolver.adaptTo(Session.class);
             if (s != null) {
 
@@ -185,18 +373,33 @@ public class JcrResourceBundleProvider implements ResourceBundleProvider,
                     ObservationManager om = s.getWorkspace().getObservationManager();
                     om.removeEventListener(this);
                 } catch (Throwable t) {
-                    // TODO log
+                    log.info(
+                        "releaseRepository: Problem unregistering as event listener",
+                        t);
                 }
 
                 try {
                     s.logout();
                 } catch (Throwable t) {
-                    // TODO log
+                    log.info(
+                        "releaseRepository: Unexpected problem logging out from the repository",
+                        t);
                 }
             }
         }
     }
 
+    /**
+     * Converts the given <code>localeString</code> to valid
+     * <code>java.util.Locale</code>. If the locale string is
+     * <code>null</code> or empty, the platform default locale is assumed. If
+     * the localeString matches any locale available per default on the
+     * platform, that platform locale is returned. Otherwise the localeString is
+     * parsed and the language and country parts are compared against the
+     * languages and countries provided by the platform. Any unsupported
+     * language or country is replaced by the platform default language and
+     * country.
+     */
     private Locale toLocale(String localeString) {
         if (localeString == null || localeString.length() == 0) {
             return Locale.getDefault();
@@ -239,7 +442,7 @@ public class JcrResourceBundleProvider implements ResourceBundleProvider,
         String[] countries = Locale.getISOCountries();
         for (int i = 0; i < countries.length; i++) {
             if (countries[i].equals(lang)) {
-                lang = null; // signal ok
+                country = null; // signal ok
                 break;
             }
         }
@@ -256,59 +459,4 @@ public class JcrResourceBundleProvider implements ResourceBundleProvider,
         return new Locale(parts[0], parts[1], parts[2]);
     }
 
-    // ---------- EventListener ------------------------------------------------
-
-    public void onEvent(EventIterator events) {
-        // don't care for the concrete events, just drop the cache
-        log.info("onEvent: Got " + events.getSize() + " events");
-        resourceBundleCache.clear();
-    }
-
-    // ---------- SCR Integration ----------------------------------------------
-
-    protected void activate(ComponentContext context) {
-        Dictionary<?, ?> props = context.getProperties();
-
-        String user = (String) props.get(PROP_USER);
-        if (user == null || user.length() == 0) {
-            repoCredentials = null;
-        } else {
-            String pass = (String) props.get(PROP_PASS);
-            char[] pwd = (pass == null) ? new char[0] : pass.toCharArray();
-            repoCredentials = new SimpleCredentials(user, pwd);
-        }
-
-        String localeString = (String) props.get(PAR_DEFAULT_LOCALE);
-        this.defaultLocale = toLocale(localeString);
-    }
-
-    protected void bindRepository(SlingRepository repository) {
-        if (this.repository != null) {
-            releaseRepository();
-        }
-        this.repository = repository;
-    }
-
-    protected void unbindRepository(SlingRepository repository) {
-        if (this.repository == repository) {
-            releaseRepository();
-            this.repository = null;
-        }
-    }
-
-    protected void bindResourceResolverFactory(
-            JcrResourceResolverFactory resourceResolverFactory) {
-        if (this.resourceResolverFactory != null) {
-            releaseRepository();
-        }
-        this.resourceResolverFactory = resourceResolverFactory;
-    }
-
-    protected void unbindResourceResolverFactory(
-            JcrResourceResolverFactory resourceResolverFactory) {
-        if (this.resourceResolverFactory == resourceResolverFactory) {
-            releaseRepository();
-            this.resourceResolverFactory = null;
-        }
-    }
 }
