18caa5fabcc17cfc0b4239f70a017445254b0db7
Felix Meschberger <fmeschbe@apache.org>
2008-04-01 13:04:33 +0000

    SLING-349 BundleResource.listChildren(Resource) should also be implemented by BundleResourceIterator if the Resource is not a BundleResource. Also fix a glitch in the BundleResource.createResource method which creates a file resource if the resource in fact is a folder resource and does not hide a JCR item.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/sling/trunk@643386 13f79535-47bb-0310-9956-ffa450edef68

--- jcr/resource/src/main/java/org/apache/sling/jcr/resource/internal/helper/bundle/BundleResourceProvider.java
+++ jcr/resource/src/main/java/org/apache/sling/jcr/resource/internal/helper/bundle/BundleResourceProvider.java
@@ -19,12 +19,9 @@
 package org.apache.sling.jcr.resource.internal.helper.bundle;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashSet;
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
-import java.util.NoSuchElementException;
-import java.util.Set;
 import java.util.StringTokenizer;
 
 import javax.servlet.http.HttpServletRequest;
@@ -33,7 +30,6 @@ import org.apache.sling.api.SlingException;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.api.resource.ResourceProvider;
 import org.apache.sling.api.resource.ResourceResolver;
-import org.apache.sling.api.resource.SyntheticResource;
 import org.osgi.framework.Bundle;
 
 public class BundleResourceProvider implements ResourceProvider {
@@ -90,74 +86,19 @@ public class BundleResourceProvider implements ResourceProvider {
             return ((BundleResource) parent).listChildren();
         }
 
-        // otherwise create an iterator which builds the entries based on
-        // the contents of the roots list
-        return new Iterator<Resource>() {
-            private final String parentPath;
-
-            private final ResourceResolver resolver;
-
-            private final Iterator<String> roots;
-
-            private Resource nextResult;
-            
-            private final Set<String> visited = new HashSet<String>();
-            {
-                String pp = parent.getPath();
-                if (!pp.endsWith("/")) {
-                    pp = pp.concat("/");
-                }
-                parentPath = pp;
-                resolver = parent.getResourceResolver();
-                roots = Arrays.asList(getRoots()).iterator();
-                nextResult = seek();
+        // ensure this provider may have children of the parent
+        String parentPath = parent.getPath();
+        for (String rootPath : getRoots()) {
+            if (parentPath.equals(rootPath)
+                || parentPath.startsWith(rootPath.concat("/"))) {
+                return new BundleResourceIterator(parent.getResourceResolver(),
+                    bundle, parent.getPath());
             }
+        }
 
-            public boolean hasNext() {
-                return nextResult != null;
-            }
-
-            public Resource next() {
-                if (nextResult == null) {
-                    throw new NoSuchElementException();
-                }
-                
-                Resource result = nextResult;
-                nextResult = seek();
-                return result;
-            }
-
-            public void remove() {
-                throw new UnsupportedOperationException("remove");
-            }
-
-            private Resource seek() {
-                Resource result = null;
-                while (result == null && roots.hasNext()) {
-                    String path = roots.next();
-                    if (path.startsWith(parentPath)) {
-                        int nextSlash = path.indexOf('/', parentPath.length());
-                        if (nextSlash < 0) {
-                            result = BundleResource.getResource(resolver,
-                                bundle, path);
-                        } else {
-
-                            path = path.substring(0, nextSlash);
-                            if (!visited.contains(path)) {
-                                visited.add(path);
-                                if (resolver.getResource(path) == null) {
-                                    result = new SyntheticResource(resolver,
-                                        path, RESOURCE_TYPE_SYNTHETIC);
-                                }
-                            }
-                        }
-                    }
-
-                }
-                
-                return result;
-            }
-        };
-
+        // the parent resource cannot have children in this provider,
+        // though this is basically not expected, we still have to
+        // be prepared for such a situation
+        return Collections.<Resource>emptyList().iterator();
     }
 }
