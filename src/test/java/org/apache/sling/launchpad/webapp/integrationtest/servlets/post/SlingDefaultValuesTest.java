1c31c0a9a324de991b9545bdac8e5a91172be88c
Carsten Ziegeler <cziegeler@apache.org>
2008-04-04 14:14:55 +0000

    Fix failing test case - starting today, the test failed because of a method clash, both println(string) and println(object) match.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/sling/trunk/launchpad/webapp@644718 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/sling/launchpad/webapp/integrationtest/servlets/post/SlingDefaultValuesTest.java
+++ src/test/java/org/apache/sling/launchpad/webapp/integrationtest/servlets/post/SlingDefaultValuesTest.java
@@ -46,7 +46,7 @@ public class SlingDefaultValuesTest extends HttpTestBase {
         String content = getContent(createdNodeUrl + ".json", CONTENT_TYPE_JSON);
 
         // default behaviour writes empty string
-        assertJavascript("", content, "out.println(data.a)");
+        assertJavascript("undefined", content, "out.println(\"\" + data.a)");
 
         // overwrite with "123"
         props.put("a", "123");
@@ -60,7 +60,7 @@ public class SlingDefaultValuesTest extends HttpTestBase {
         testClient.createNode(createdNodeUrl, props);
         content = getContent(createdNodeUrl + ".json", CONTENT_TYPE_JSON);
 
-        assertJavascript("", content, "out.println(data.a)");
+        assertJavascript("undefined", content, "out.println(\"\" + data.a)");
     }
 
     public void testWithSpecificDefault() throws IOException {
