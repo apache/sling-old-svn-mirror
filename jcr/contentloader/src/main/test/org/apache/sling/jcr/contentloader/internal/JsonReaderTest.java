7ae9e2014d3c0a27bafb6702c8396c14197891e0
Carsten Ziegeler <cziegeler@apache.org>
2008-04-30 07:08:40 +0000

    SLING-400: Move content loading to own bundle.
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/sling/trunk@652310 13f79535-47bb-0310-9956-ffa450edef68

--- jcr/contentloader/src/main/test/org/apache/sling/jcr/contentloader/internal/JsonReaderTest.java
+++ jcr/contentloader/src/main/test/org/apache/sling/jcr/contentloader/internal/JsonReaderTest.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.sling.jcr.resource.internal.loader;
+package org.apache.sling.jcr.contentloader.internal;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
