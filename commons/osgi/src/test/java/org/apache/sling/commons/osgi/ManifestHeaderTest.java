865ed9a3d2357f95faa2bd0491310d9fc1e56706
Carsten Ziegeler <cziegeler@apache.org>
2008-05-15 07:10:03 +0000

    SLING-456: Add manifest header parser and junit test.
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/sling/trunk@656526 13f79535-47bb-0310-9956-ffa450edef68

---
+++ commons/osgi/src/test/java/org/apache/sling/commons/osgi/ManifestHeaderTest.java
@@ -0,0 +1,89 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.sling.commons.osgi;
+
+import junit.framework.TestCase;
+
+/**
+ * Tests for the manifest header parsing.
+ */
+public class ManifestHeaderTest extends TestCase {
+
+    public void testNonExisting() {
+        String header = null;
+        final ManifestHeader entry = ManifestHeader.parse(header);
+        assertNull(entry);
+    }
+
+    public void testSinglePath() {
+        String header = "something";
+        final ManifestHeader entry = ManifestHeader.parse(header);
+        assertEquals(1, entry.getEntries().length);
+        assertEquals(header, entry.getEntries()[0].getValue());
+        assertEquals(0, entry.getEntries()[0].getAttributes().length);
+        assertEquals(0, entry.getEntries()[0].getDirectives().length);
+    }
+
+    public void testSeveralPaths() {
+        String header = "one,two,   three    ,\n   four, \n   five";
+        final ManifestHeader entry = ManifestHeader.parse(header);
+        assertEquals(5, entry.getEntries().length);
+        assertEquals("one", entry.getEntries()[0].getValue());
+        assertEquals(0, entry.getEntries()[0].getAttributes().length);
+        assertEquals(0, entry.getEntries()[0].getDirectives().length);
+        assertEquals("two", entry.getEntries()[1].getValue());
+        assertEquals(0, entry.getEntries()[1].getAttributes().length);
+        assertEquals(0, entry.getEntries()[1].getDirectives().length);
+        assertEquals("three", entry.getEntries()[2].getValue());
+        assertEquals(0, entry.getEntries()[2].getAttributes().length);
+        assertEquals(0, entry.getEntries()[2].getDirectives().length);
+        assertEquals("four", entry.getEntries()[3].getValue());
+        assertEquals(0, entry.getEntries()[3].getAttributes().length);
+        assertEquals(0, entry.getEntries()[3].getDirectives().length);
+        assertEquals("five", entry.getEntries()[4].getValue());
+        assertEquals(0, entry.getEntries()[4].getAttributes().length);
+        assertEquals(0, entry.getEntries()[4].getDirectives().length);
+    }
+
+    public void testAttributes() {
+        String header = "one;a=1;b=2";
+        final ManifestHeader entry = ManifestHeader.parse(header);
+        assertEquals(1, entry.getEntries().length);
+        assertEquals("one", entry.getEntries()[0].getValue());
+        assertEquals(2, entry.getEntries()[0].getAttributes().length);
+        assertEquals(0, entry.getEntries()[0].getDirectives().length);
+        assertEquals("a", entry.getEntries()[0].getAttributes()[0].getName());
+        assertEquals("b", entry.getEntries()[0].getAttributes()[1].getName());
+        assertEquals("1", entry.getEntries()[0].getAttributes()[0].getValue());
+        assertEquals("2", entry.getEntries()[0].getAttributes()[1].getValue());
+    }
+
+    public void testDirectives() {
+        String header = "one;a:=1;b:=2";
+        final ManifestHeader entry = ManifestHeader.parse(header);
+        assertEquals(1, entry.getEntries().length);
+        assertEquals("one", entry.getEntries()[0].getValue());
+        assertEquals(2, entry.getEntries()[0].getDirectives().length);
+        assertEquals(0, entry.getEntries()[0].getAttributes().length);
+        assertEquals("a", entry.getEntries()[0].getDirectives()[0].getName());
+        assertEquals("b", entry.getEntries()[0].getDirectives()[1].getName());
+        assertEquals("1", entry.getEntries()[0].getDirectives()[0].getValue());
+        assertEquals("2", entry.getEntries()[0].getDirectives()[1].getValue());
+    }
+}
