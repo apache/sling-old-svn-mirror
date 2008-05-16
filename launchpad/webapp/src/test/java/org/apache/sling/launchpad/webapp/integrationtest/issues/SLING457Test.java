f3ef76dc03203f9a01b532f5cb58b7925fe07bc6
Felix Meschberger <fmeschbe@apache.org>
2008-05-16 08:50:28 +0000

    SLING-457 Integration test cases
    git-svn-id: https://svn.apache.org/repos/asf/incubator/sling/trunk@656974 13f79535-47bb-0310-9956-ffa450edef68

---
+++ launchpad/webapp/src/test/java/org/apache/sling/launchpad/webapp/integrationtest/issues/SLING457Test.java
@@ -0,0 +1,93 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.sling.launchpad.webapp.integrationtest.issues;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.sling.api.SlingConstants;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ResourceResolver;
+import org.apache.sling.launchpad.webapp.integrationtest.HttpTestBase;
+import org.apache.sling.servlets.post.SlingPostConstants;
+
+public class SLING457Test extends HttpTestBase {
+
+    private String testRootUrl;
+
+    private String contentUrl;
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+
+        final String testRootPathBare = "/" + getClass().getSimpleName() + "/"
+            + System.currentTimeMillis();
+        final String testRootPath = HTTP_BASE_URL + testRootPathBare;
+
+        // the root for our tests
+        testRootUrl = testClient.createNode(testRootPath, null);
+
+        // create the Resource Type "a" location
+        String rtA = testRootPathBare + "/a";
+        testClient.createNode(HTTP_BASE_URL + rtA, null);
+
+        // create the Resource Type "b" location
+        String rtB = testRootPathBare + "/b";
+        Map<String, String> props = new HashMap<String, String>();
+        props.put("sling:resourceSuperType", rtA);
+        testClient.createNode(HTTP_BASE_URL + rtB, props);
+
+        // create content node "x" of type rtB
+        String rtX = testRootPathBare + "/x";
+        props.clear();
+        props.put("sling:resourceType", rtB);
+        contentUrl = testClient.createNode(HTTP_BASE_URL + rtX, props);
+        
+        // create content node "x/image" of type rtB
+        String rtXImage = rtX + "/image";
+        props.clear();
+        props.put("sling:resourceType", "nt:unstructured");
+        testClient.createNode(HTTP_BASE_URL + rtXImage, props);
+
+        uploadTestScript(rtA, "issues/sling457/a-foo.html.jsp", "foo.html.jsp");
+        uploadTestScript(rtB, "issues/sling457/b-b.jsp", "b.jsp");
+
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        testClient.delete(testRootUrl);
+        super.tearDown();
+    }
+    
+    public void testCallFooHtml() throws Exception {
+        String url = contentUrl + ".foo.html";
+        String content = getContent(url, CONTENT_TYPE_HTML);
+        assertTrue("Expected 'foo.html.jsp' in content",
+            content.indexOf("foo.html.jsp") >= 0);
+    }
+
+    public void testCallHtml() throws Exception {
+        String url = contentUrl + ".html";
+        String content = getContent(url, CONTENT_TYPE_HTML);
+        assertTrue("Expected 'foo.html.jsp' in content",
+            content.indexOf("foo.html.jsp") >= 0);
+    }
+}
