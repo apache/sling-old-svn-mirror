2d3b83d51417b3806b51a99c728d2e817f7259b3
Bertrand Delacretaz <bdelacretaz@apache.org>
2008-06-16 15:41:50 +0000

    SLING-466 - render HTML and javascript separately, work in progress
    git-svn-id: https://svn.apache.org/repos/asf/incubator/sling/trunk/scripting/jst@668200 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/sling/scripting/jst/JstScriptEngine.java
+++ src/main/java/org/apache/sling/scripting/jst/JstScriptEngine.java
@@ -19,8 +19,6 @@
 package org.apache.sling.scripting.jst;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.io.Reader;
 import java.util.LinkedList;
@@ -35,15 +33,15 @@ import javax.script.ScriptException;
 
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.resource.Resource;
-import org.apache.sling.api.resource.ResourceMetadata;
 import org.apache.sling.api.scripting.SlingBindings;
 import org.apache.sling.api.scripting.SlingScriptHelper;
 import org.apache.sling.api.wrappers.SlingRequestPaths;
 import org.apache.sling.commons.json.JSONException;
 import org.apache.sling.commons.json.jcr.JsonItemWriter;
 import org.apache.sling.scripting.api.AbstractSlingScriptEngine;
-import org.apache.sling.scripting.javascript.io.EspReader;
 import org.apache.sling.servlets.get.helpers.HtmlRendererServlet;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /** Experimental JST script engine: converts a JST template (using the
  *  same templating syntax as ESP) to client-side javascript code
@@ -55,10 +53,17 @@ public class JstScriptEngine extends AbstractSlingScriptEngine {
 
     private final List<String> libraryScripts = new LinkedList<String>();
     private final HtmlRendererServlet htmlRenderer;
-    private final ScriptFilteredCopy copier = new ScriptFilteredCopy();
+    private final Logger log = LoggerFactory.getLogger(getClass());
     
     // TODO should be configurable or synced with the actual location
     public final static String SLING_JS_PATH = "/system/sling.js";
+    
+    /** Property names to use to build the page title, in order of preference */
+    public final static String [] TITLE_PROPERTY_NAMES = {
+        "title",
+        "name",
+        "description"
+    };
 
     JstScriptEngine(ScriptEngineFactory scriptEngineFactory) {
         super(scriptEngineFactory);
@@ -73,25 +78,27 @@ public class JstScriptEngine extends AbstractSlingScriptEngine {
         // to the client inside a skeleton HTML document, and let the
         // client run the script
         Bindings props = context.getBindings(ScriptContext.ENGINE_SCOPE);
-
         final SlingScriptHelper helper = (SlingScriptHelper) props.get(SlingBindings.SLING);
 
         try {
             final PrintWriter w = helper.getResponse().getWriter();
-            final Reader er = getReader(helper.getScript().getScriptResource());
 
             // access our data (need a Node)
             final Resource r = helper.getRequest().getResource();
-
-            // to render we must have either a Node or a SyntheticResourceData
             final Node n = r.adaptTo(Node.class);
 
             // output HEAD with javascript initializations
             // TODO we should instead parse (at least minimally) the template file, and inject our
             // stuff in the right places
             w.println("<html><head><title id=\"JstPageTitle\">");
-            w.println("JST rendering of " + r.getPath());
+            w.println(getTitle(r, n));
             w.println("</title>");
+            
+            // TODO get additional head stuff from the script?
+            // something like
+            //  <!-- jst:head
+            //      <link rel="stylesheet" href="/apps/foo/foo.css"/>
+            //  -->
 
             // library scripts
             final SlingHttpServletRequest request = helper.getRequest();
@@ -101,15 +108,10 @@ public class JstScriptEngine extends AbstractSlingScriptEngine {
                     + SlingRequestPaths.getServletPath(request)
                     + lib
                 ;
-                w.println("<script src=\"" + fullScriptPath + "\"></script>");
+                w.println("<script type=\"text/javascript\" src=\"" + fullScriptPath + "\"></script>");
             }
 
-            // onLoad method
-            w.println("<script language=\"javascript\">");
-            w.println("function jstOnLoad() { if(typeof onLoad == \"function\") { onLoad(); } }");
-            w.println("</script>");
-
-            // data in JSON format
+            // Node data in JSON format
             final JsonItemWriter j = new JsonItemWriter(null);
             final int maxRecursionLevels = 1;
             w.println("<script language='javascript'>");
@@ -121,15 +123,10 @@ public class JstScriptEngine extends AbstractSlingScriptEngine {
             }
             w.println(";");
             w.println("</script>");
-            w.println("</head><body onLoad=\"jstOnLoad()\">");
-
-            // output our parsed script, first in body
-            w.println("<div id=\"JstRenderingScript\">\n<script language='javascript'>");
-            copier.copy(er,w);
-            w.println("</script>\n</div>");
 
             // default rendering, turned off automatically from the javascript that
             // follows, if javascript is enabled
+            w.println("</head><body>");
             w.println("<div id=\"JstDefaultRendering\">");
             if(n!=null) {
                 htmlRenderer.render(w, r, n);
@@ -138,6 +135,10 @@ public class JstScriptEngine extends AbstractSlingScriptEngine {
             w.println("<script language=\"javascript\">");
             w.println("document.getElementById(\"JstDefaultRendering\").setAttribute(\"style\",\"display:none\");");
             w.println("</script>");
+            
+            // reference to script provided by the JstCodeGeneratorServlet
+            final String scriptUrl = helper.getScript().getScriptResource().getPath() + ".jst.js";
+            w.println("<script type=\"text/javascript\" src=\"" + scriptUrl + "\"></script>");
 
             // all done
             w.println("</body></html>");
@@ -150,27 +151,39 @@ public class JstScriptEngine extends AbstractSlingScriptEngine {
 
         } catch(JSONException je) {
             throw new ScriptException(je);
-
         }
 
         return null;
     }
-
-    private Reader getReader(Resource resource) throws IOException {
-        InputStream ins = resource.adaptTo(InputStream.class);
-        if (ins != null) {
-            String enc = (String) resource.getResourceMetadata().get(ResourceMetadata.CHARACTER_ENCODING);
-            if (enc == null) {
-                enc = "UTF-8";
+    
+    /** Return the title to use for the generated page */ 
+    protected String getTitle(Resource r, Node n) {
+        String title = null;
+        
+        if(n != null) {
+            for(String name : TITLE_PROPERTY_NAMES) {
+                try {
+                    if(n.hasProperty(name)) {
+                        final String s = n.getProperty(name).getString();
+                        if(s.length() > 0) {
+                            title = s;
+                            break;
+                        }
+                    } 
+                } catch(RepositoryException re) {
+                    log.warn("RepositoryException in getTitle()", re);
+                }
             }
-
-            Reader r = new InputStreamReader(ins, enc);
-            EspReader er = new EspReader(r);
-            er.setOutInitStatement("out=document;\n");
-
-            return er;
         }
-
-        return null;
+        
+        if(title == null) {
+            title = r.getPath();
+            if(title.startsWith("/")) {
+                title = title.substring(1);
+            }
+        }
+        
+        return title;
     }
+
 }
