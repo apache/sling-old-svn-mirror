fe27c9e74ba269f6e2a04ae81b3e6f2b69e3a1e4
Felix Meschberger <fmeschbe@apache.org>
2008-06-23 10:38:18 +0000

    SLING-401 add contributed GWT servlet and sample
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/sling/trunk@670511 13f79535-47bb-0310-9956-ffa450edef68

---
+++ extensions/gwt/sample/src/main/java/org/apache/sling/extensions/gwt/sample/client/Notes.java
@@ -0,0 +1,252 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.sling.extensions.gwt.sample.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.rpc.ServiceDefTarget;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.ClickListener;
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Panel;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.TextArea;
+import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.client.ui.Widget;
+
+import java.util.ArrayList;
+
+/**
+ * This class is the starting point for the Sling GWT Sample, a GWT client application that enables the user
+ * to store notes on the server and read them through the GWT widgets.
+ * <p/>
+ * The <code>Notes</code> class represents the entry point and top level widget of the GWT client application.
+ */
+public class Notes implements EntryPoint {
+
+    private static final NotesServiceAsync service = (NotesServiceAsync) GWT.create(NotesService.class);
+    private static final ServiceDefTarget target = (ServiceDefTarget) service;
+
+    {
+        target.setServiceEntryPoint(GWT.getModuleBaseURL() + "notesservice");
+    }
+
+    final TextBox inputNoteTitle = new TextBox();
+    final TextArea inputNoteText = new TextArea();
+    final VerticalPanel notesPanel = new VerticalPanel();
+
+    {
+        inputNoteTitle.setStyleName("formField");
+        inputNoteText.setStyleName("formField");
+        inputNoteText.setVisibleLines(3);
+    }
+
+    /**
+     * This method is called when GWT loads the Notes application (as defined in the Notes.gwt.xml).
+     */
+    public void onModuleLoad() {
+
+        final HorizontalPanel mainpanel = new HorizontalPanel();
+
+        final HTML displayTitle = new HTML("Existing Notes");
+        displayTitle.setStyleName("displayTitle");
+        final HTML entryTitle = new HTML("Create A Note");
+        entryTitle.setStyleName("entryTitle");
+
+        final VerticalPanel displayPanel = new VerticalPanel();
+        displayPanel.setVerticalAlignment(VerticalPanel.ALIGN_TOP);
+        displayPanel.setStyleName("displayPanel");
+        displayPanel.add(displayTitle);
+
+        final VerticalPanel entryPanel = new VerticalPanel();
+        entryPanel.setVerticalAlignment(VerticalPanel.ALIGN_TOP);
+        entryPanel.setStyleName("entryPanel");
+        entryPanel.add(entryTitle);
+
+        displayPanel.add(notesPanel);
+
+        final VerticalPanel form = createForm();
+        entryPanel.add(form);
+
+        mainpanel.add(displayPanel);
+        mainpanel.add(entryPanel);
+
+        RootPanel.get("notes").add(mainpanel);
+
+        getNotes();
+    }
+
+    private VerticalPanel createForm() {
+
+        final VerticalPanel form = new VerticalPanel();
+        form.setStyleName("formPanel");
+
+        final HorizontalPanel titleLine = new HorizontalPanel();
+        final HTML textNoteTitle = new HTML("Title: ");
+        textNoteTitle.setWidth("50px");
+        titleLine.add(textNoteTitle);
+        titleLine.add(inputNoteTitle);
+
+        final HorizontalPanel textLine = new HorizontalPanel();
+        final HTML textNoteText = new HTML("Note: ");
+        textNoteText.setWidth("50px");
+        textLine.add(textNoteText);
+        textLine.add(inputNoteText);
+
+        form.add(titleLine);
+        form.add(textLine);
+        form.add(createButtons());
+
+        return form;
+    }
+
+    private Panel createButtons() {
+
+        final HorizontalPanel panel = new HorizontalPanel();
+
+        Button save = new Button("Save");
+        save.setStyleName("button");
+        save.addClickListener(new ClickListener() {
+
+            public void onClick(Widget widget) {
+                if (validateFormInput()) {
+                    createNote(inputNoteTitle.getText(), inputNoteText.getText());
+                    resetForm();
+                }
+            }
+        });
+
+        Button clear = new Button("Clear");
+        save.setStyleName("button");
+        clear.addClickListener(new ClickListener() {
+
+            public void onClick(Widget widget) {
+                resetForm();
+            }
+        });
+
+        panel.add(save);
+        panel.add(clear);
+
+        return panel;
+    }
+
+    private boolean validateFormInput() {
+
+        if (inputNoteTitle.getText().trim().length() == 0) {
+            Window.alert("Please enter a title for the new note!");
+            inputNoteTitle.setFocus(true);
+            return false;
+        }
+        if (inputNoteText.getText().trim().length() == 0) {
+            Window.alert("Please enter a text for the new note!");
+            inputNoteText.setFocus(true);
+            return false;
+        }
+        return true;
+    }
+
+    private void resetForm() {
+
+        inputNoteTitle.setText("");
+        inputNoteText.setText("");
+    }
+
+    private void createNote(String title, String text) {
+
+        final Note note = new Note();
+        note.setTitle(title);
+        note.setText(text);
+
+        service.createNote(note, new AsyncCallback() {
+
+            public void onFailure(Throwable throwable) {
+                Window.alert("Failed to created note: " + throwable.getMessage());
+            }
+
+            public void onSuccess(Object o) {
+                getNotes();
+            }
+        });
+    }
+
+    private void deleteNote(String path) {
+        service.deleteNote(path, new AsyncCallback() {
+
+            public void onFailure(Throwable throwable) {
+                Window.alert("Failed to delete note: " + throwable.getMessage());
+            }
+
+            public void onSuccess(Object o) {
+                getNotes();
+            }
+        });
+    }
+
+    private void getNotes() {
+
+        notesPanel.clear();
+
+        service.getNotes(new AsyncCallback() {
+            public void onFailure(Throwable throwable) {
+                notesPanel.add(new HTML("No notes stored so far."));
+                Window.alert("Could not retrieve notes: " + throwable.getMessage());
+            }
+
+            public void onSuccess(Object o) {
+                ArrayList notesList = (ArrayList) o;
+                for (int i = 0; i < notesList.size(); i++) {
+                    final Note note = (Note) notesList.get(i);
+
+                    final HorizontalPanel noteEntry = new HorizontalPanel();
+                    noteEntry.setStyleName("noteEntry");
+
+                    final HTML noteTitle = new HTML(note.getTitle());
+                    noteTitle.setStyleName("noteTitle");
+
+                    final HTML noteText = new HTML(note.getText());
+                    noteText.setStyleName("noteText");
+
+                    final Button delButton = new Button("Delete");
+                    delButton.setStyleName("noteControls");
+                    delButton.addClickListener(new ClickListener() {
+
+                        public void onClick(Widget widget) {
+                            deleteNote(note.getPath());
+                        }
+                    });
+
+                    noteEntry.add(noteTitle);
+                    noteEntry.add(noteText);
+                    noteEntry.add(delButton);
+
+                    notesPanel.add(noteEntry);
+                }
+
+                if (notesList.size() == 0) {
+                    notesPanel.add(new HTML("No notes stored so far."));
+                }
+            }
+        });
+    }
+}
---
+++ extensions/gwt/sample/src/main/java/org/apache/sling/extensions/gwt/sample/client/NotesService.java
@@ -0,0 +1,57 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.sling.extensions.gwt.sample.client;
+
+import com.google.gwt.user.client.rpc.RemoteService;
+
+import java.util.ArrayList;
+
+/**
+ * This is the interface to be implemented by the class responsible for handling the GWT RPC calls.
+ * <p/>
+ * It defines the API for creating, deleting and retrieving notes.
+ */
+public interface NotesService extends RemoteService {
+
+    /**
+     * This method is called to create a <code>Note</code>. The note will be persisted into the repository.
+     *
+     * @param note The <code>Note</code> to be created and stored in the repository.
+     * @return The <code>String</code> representing the status message of the successful RPC operation.
+     * @gwt.typeArgs note <org.apache.sling.extensions.gwt.sample.client.Note>
+     */
+    public String createNote(Note note);
+
+    /**
+     * This method is called to retrieve an <code>ArrayList</code> of all notes stored in the repository.
+     *
+     * @return The <code>ArrayList</code> containing all <code>Note</code>s stored on the server.
+     * @gwt.typeArgs <org.apache.sling.extensions.gwt.sample.client.Note>
+     */
+    public ArrayList getNotes();
+
+    /**
+     * This method is called to delete a <code>Note</code> from the repository. To identify the <code>javax.jcr.Node</code>
+     * associated with the <code>Note</code>-POJO, its repository path has to be provided.
+     *
+     * @param path The <code>String</code> representing the path of the <code>javax.jcr.Node</code> this note is associated with.
+     * @return The <code>String</code> representing the status message of the successful RPC operation.
+     */
+    String deleteNote(String path);
+}
