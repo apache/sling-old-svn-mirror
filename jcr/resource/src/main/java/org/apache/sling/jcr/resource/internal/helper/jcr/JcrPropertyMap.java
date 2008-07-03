a3c5b8f0768c449564c1e930a4751849073935b2
Felix Meschberger <fmeschbe@apache.org>
2008-07-03 11:43:34 +0000

    SLING-557 Define ValueMap and implementation for Node based resources
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/sling/trunk@673658 13f79535-47bb-0310-9956-ffa450edef68

--- jcr/resource/src/main/java/org/apache/sling/jcr/resource/internal/helper/jcr/JcrPropertyMap.java
+++ jcr/resource/src/main/java/org/apache/sling/jcr/resource/internal/helper/jcr/JcrPropertyMap.java
@@ -18,8 +18,12 @@
  */
 package org.apache.sling.jcr.resource.internal.helper.jcr;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
@@ -27,11 +31,19 @@ import javax.jcr.Node;
 import javax.jcr.Property;
 import javax.jcr.PropertyIterator;
 import javax.jcr.RepositoryException;
+import javax.jcr.Value;
+import javax.jcr.ValueFormatException;
 
+import org.apache.sling.api.resource.ValueMap;
 import org.apache.sling.jcr.resource.JcrResourceUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-class JcrPropertyMap implements Map<String, Object> {
+class JcrPropertyMap implements ValueMap {
 
+    /** default log */
+    private final Logger log = LoggerFactory.getLogger(getClass());
+    
     private final Node node;
 
     private final Map<String, Object> cache;
@@ -43,6 +55,33 @@ class JcrPropertyMap implements Map<String, Object> {
         this.cache = new HashMap<String, Object>();
         this.fullyRead = false;
     }
+
+    // ---------- ValueMap
+
+    @SuppressWarnings("unchecked")
+    public <T> T get(String name, Class<T> type) {
+        if (type == null) {
+            return (T) get(name);
+        }
+
+        return convertToType(name, type);
+    }
+
+    @SuppressWarnings("unchecked")
+    public <T> T get(String name, T defaultValue) {
+        if (defaultValue == null) {
+            return (T) get(name);
+        }
+
+        T value = get(name, (Class<T>) defaultValue.getClass());
+        if (value == null) {
+            value = defaultValue;
+        }
+
+        return value;
+    }
+
+    // ---------- Map
 
     public Object get(Object key) {
         Object value = cache.get(key);
@@ -63,6 +102,7 @@ class JcrPropertyMap implements Map<String, Object> {
     }
 
     public boolean isEmpty() {
+        // only start reading if there is nothing in the cache yet
         if (cache.isEmpty()) {
             readFully();
         }
@@ -133,7 +173,7 @@ class JcrPropertyMap implements Map<String, Object> {
         }
     }
 
-    // ---------- Unsupported Modification methods -----------------------------
+    // ---------- Unsupported Modification methods
 
     public void clear() {
         throw new UnsupportedOperationException();
@@ -151,4 +191,71 @@ class JcrPropertyMap implements Map<String, Object> {
         throw new UnsupportedOperationException();
     }
 
+    // ---------- Implementation helper
+
+    @SuppressWarnings("unchecked")
+    private <T> T convertToType(String name, Class<T> type) {
+        try {
+            if (node.hasProperty(name)) {
+                Property prop = node.getProperty(name);
+
+                boolean multiValue = prop.getDefinition().isMultiple();
+                boolean array = type.isArray();
+
+                if (array && multiValue) {
+                    return (T) convertToArray(prop.getValues(),
+                        type.getComponentType());
+                } else if (!array && !multiValue) {
+                    return convertToType(prop.getValue(), type);
+                }
+            }
+
+        } catch (ValueFormatException vfe) {
+            log.info("converToType: Cannot convert value of " + name + " to "
+                + type, vfe);
+        } catch (RepositoryException re) {
+            log.info("converToType: Cannot get value of " + name, re);
+        }
+
+        // fall back to nothing
+        return null;
+    }
+
+    private <T> T[] convertToArray(Value[] jcrValues, Class<T> type)
+            throws ValueFormatException, RepositoryException {
+        List<T> values = new ArrayList<T>();
+        for (int i = 0; i < jcrValues.length; i++) {
+            T value = convertToType(jcrValues[i], type);
+            if (value != null) {
+                values.add(value);
+            }
+        }
+
+        @SuppressWarnings("unchecked")
+        T[] result = (T[]) Array.newInstance(type, values.size());
+
+        return values.toArray(result);
+    }
+
+    @SuppressWarnings("unchecked")
+    private <T> T convertToType(Value jcrValue, Class<T> type)
+            throws ValueFormatException, RepositoryException {
+
+        if (String.class == type) {
+            return (T) jcrValue.getString();
+        } else if (Long.class == type) {
+            return (T) new Long(jcrValue.getLong());
+        } else if (Double.class == type) {
+            return (T) new Double(jcrValue.getDouble());
+        } else if (Boolean.class == type) {
+            return (T) Boolean.valueOf(jcrValue.getBoolean());
+        } else if (Calendar.class == type) {
+            return (T) jcrValue.getDate();
+        } else if (Value.class == type) {
+            return (T) jcrValue;
+        }
+
+        // fallback in case of unsupported type
+        return null;
+    }
 }
