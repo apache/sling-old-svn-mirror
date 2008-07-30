d768f89734eef4a239a9cf937faba5075b0280e6
Bertrand Delacretaz <bdelacretaz@apache.org>
2008-07-30 14:17:13 +0000

    SLING-582 - JsCodeGeneratorTest was broken under Cygwin on Windows - contributed by Craig L. Ching, thanks!
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/sling/trunk@681061 13f79535-47bb-0310-9956-ffa450edef68

--- scripting/jst/src/test/java/org/apache/sling/scripting/jst/StringUtil.java
+++ scripting/jst/src/test/java/org/apache/sling/scripting/jst/StringUtil.java
@@ -23,9 +23,23 @@ import java.io.InputStream;
 
 /** Test utilities */
 class StringUtil {
+
+    static private final String NATIVE_LINE_SEP = System.getProperty("line.separator");
+
     /** Replace \n with . in strings to make it easier to compare visually for testing */
     static String flatten(String str) {
-        return str.replace('\n', '.');
+
+        // First replace native line-endings
+        if(str.indexOf(NATIVE_LINE_SEP) >= 0) {
+            str = str.replace(NATIVE_LINE_SEP, ".");
+        }
+
+        // Now find non-native line-endings, e.g. cygwin needs this
+        if(str.indexOf('\n') >= 0) {
+            str = str.replace('\n', '.');
+        }
+
+        return str;
     }
     
     /** Read given Class resource */
