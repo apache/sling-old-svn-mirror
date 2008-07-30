aeb5db334ea77b72226236e66e91168f2a522cb6
Bertrand Delacretaz <bdelacretaz@apache.org>
2008-07-30 14:48:57 +0000

    SLING-582 - move StringUtil.flatten to commons/testing to make it reusable
    git-svn-id: https://svn.apache.org/repos/asf/incubator/sling/trunk@681070 13f79535-47bb-0310-9956-ffa450edef68

--- scripting/jst/src/test/java/org/apache/sling/scripting/jst/ScriptFilteredCopyTest.java
+++ scripting/jst/src/test/java/org/apache/sling/scripting/jst/ScriptFilteredCopyTest.java
@@ -24,6 +24,8 @@ import java.io.StringWriter;
 
 import junit.framework.TestCase;
 
+import org.apache.sling.commons.testing.util.TestStringUtil;
+
 /** Test the ScriptFilteredCopy */
 public class ScriptFilteredCopyTest extends TestCase {
     
@@ -32,7 +34,7 @@ public class ScriptFilteredCopyTest extends TestCase {
     private void runTest(String input, String expected) throws IOException {
         final StringWriter sw = new StringWriter();
         sfc.copy(new StringReader(input), sw);
-        assertEquals(StringUtil.flatten(expected), StringUtil.flatten(sw.toString()));
+        assertEquals(TestStringUtil.flatten(expected), TestStringUtil.flatten(sw.toString()));
     }
     
     public void testNoChanges() throws IOException {
