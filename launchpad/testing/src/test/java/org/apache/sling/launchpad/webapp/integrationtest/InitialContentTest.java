96bb6f959fb796460974d72e3ecd5dccdd79fb43
Bertrand Delacretaz <bdelacretaz@apache.org>
2008-07-31 13:34:51 +0000

    Add some integration tests for initial content loading
    git-svn-id: https://svn.apache.org/repos/asf/incubator/sling/trunk@681366 13f79535-47bb-0310-9956-ffa450edef68

--- launchpad/testing/src/test/java/org/apache/sling/launchpad/webapp/integrationtest/InitialContentTest.java
+++ launchpad/testing/src/test/java/org/apache/sling/launchpad/webapp/integrationtest/InitialContentTest.java
@@ -26,4 +26,24 @@ public class InitialContentTest extends HttpTestBase {
         final String content = getContent(HTTP_BASE_URL + "/index.html", CONTENT_TYPE_HTML);
         assertTrue("Content contains expected marker (" + content + ")",content.contains(expected));
     }
+    
+    public void testRootRedirectProperty() throws IOException {
+    	final String expected = "\"sling:resourceType\":\"sling:redirect\"";
+    	final String content = getContent(HTTP_BASE_URL + "/.json", CONTENT_TYPE_JSON);
+    	assertTrue("Content contains " + expected + " (" + content + ")", content.contains(expected));
+    }
+    
+    public void testInitialContentA() throws IOException {
+    	final String expected = "42";
+    	final String content = getContent(
+    			HTTP_BASE_URL + "/sling-test/sling/initial-content-test/marker.txt", CONTENT_TYPE_PLAIN);
+    	assertTrue("Content contains " + expected + " (" + content + ")", content.contains(expected));
+    }
+    
+    public void testInitialContentB() throws IOException {
+    	final String expected = "46";
+    	final String content = getContent(
+    			HTTP_BASE_URL + "/sling-test/initial-content-folder/folder-content-test/marker.txt", CONTENT_TYPE_PLAIN);
+    	assertTrue("Content contains " + expected + " (" + content + ")", content.contains(expected));
+    }
 }
