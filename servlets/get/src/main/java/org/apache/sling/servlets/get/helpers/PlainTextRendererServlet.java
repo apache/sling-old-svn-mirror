7f53ed1622c39e26de2be316e08d91ab96919753
Carsten Ziegeler <cziegeler@apache.org>
2008-09-02 12:48:49 +0000

    Throw 404 exception if resource is non existing.
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/sling/trunk@691219 13f79535-47bb-0310-9956-ffa450edef68

--- servlets/get/src/main/java/org/apache/sling/servlets/get/helpers/PlainTextRendererServlet.java
+++ servlets/get/src/main/java/org/apache/sling/servlets/get/helpers/PlainTextRendererServlet.java
@@ -25,7 +25,9 @@ import javax.servlet.ServletException;
 
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.SlingHttpServletResponse;
+import org.apache.sling.api.resource.NonExistingResource;
 import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ResourceNotFoundException;
 import org.apache.sling.api.resource.ResourceUtil;
 import org.apache.sling.api.servlets.SlingSafeMethodsServlet;
 
@@ -46,6 +48,10 @@ public class PlainTextRendererServlet extends SlingSafeMethodsServlet {
     protected void doGet(SlingHttpServletRequest req,
             SlingHttpServletResponse resp) throws ServletException, IOException {
         final Resource r = req.getResource();
+        if (r instanceof NonExistingResource) {
+            throw new ResourceNotFoundException("Resource not found at " + r.getPath());
+        }
+
         /*
          * TODO if(srd != null) { renderSyntheticResource(req, resp, srd);
          * return; }
