60852bd30bc82cae2b8277982bd596c929fcee61
Felix Meschberger <fmeschbe@apache.org>
2008-09-25 14:10:25 +0000

    SLING-674 Replace Tomcat juli by own own implementation to have Jasper log to SLF4J
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/sling/trunk@698974 13f79535-47bb-0310-9956-ffa450edef68

---
+++ scripting/jsp/src/main/java/org/apache/juli/logging/LogConfigurationException.java
@@ -0,0 +1,19 @@
+/*
+ * $Url: $
+ * $Id: $
+ *
+ * Copyright 1997-2005 Day Management AG
+ * Barfuesserplatz 6, 4001 Basel, Switzerland
+ * All Rights Reserved.
+ *
+ * This software is the confidential and proprietary information of
+ * Day Management AG, ("Confidential Information"). You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with Day.
+ */
+package org.apache.juli.logging;
+
+public class LogConfigurationException extends RuntimeException {
+
+}
