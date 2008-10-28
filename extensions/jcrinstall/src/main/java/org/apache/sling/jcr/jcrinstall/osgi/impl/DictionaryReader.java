f66f3398a9dc1e3b6b10150b2717e76dfad7f62c
Bertrand Delacretaz <bdelacretaz@apache.org>
2008-10-28 14:29:55 +0000

    SLING-715 - support typed properties in jcrinstall cfg files
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/sling/trunk@708583 13f79535-47bb-0310-9956-ffa450edef68

--- extensions/jcrinstall/src/main/java/org/apache/sling/jcr/jcrinstall/osgi/impl/DictionaryReader.java
+++ extensions/jcrinstall/src/main/java/org/apache/sling/jcr/jcrinstall/osgi/impl/DictionaryReader.java
@@ -1,24 +1,45 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.apache.sling.jcr.jcrinstall.osgi.impl;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.ArrayList;
 import java.util.Dictionary;
 import java.util.Hashtable;
 import java.util.Map;
 import java.util.Properties;
 
+import org.apache.sling.jcr.jcrinstall.osgi.impl.propertyconverter.PropertyConverter;
+import org.apache.sling.jcr.jcrinstall.osgi.impl.propertyconverter.PropertyValue;
+import org.apache.sling.jcr.jcrinstall.osgi.impl.propertyconverter.ValueConverterException;
+
 /** Reads a Dictionary from an InputStream, using the
  *  syntax of the Properties class, enhanced to support
- *  multivalued properties.
+ *  multivalued properties and types supported by the PropertyConverter
  */
 public class DictionaryReader {
-    public static final String ARRAY_MARKER = "[]";
+    private final PropertyConverter converter = new PropertyConverter();
     
     /** Read Dictionary from the given InputStream,
      *  which is *not* closed before returning
      */
-    public static Dictionary<?,?> load(InputStream is) throws IOException {
+    public Dictionary<?,?> load(InputStream is) throws IOException {
         final Properties p = new Properties();
         p.load(is);
         return convert(p);
@@ -29,65 +50,14 @@ public class DictionaryReader {
      *  lists of values, and are converted to an Array. The []
      *  is removed from the property name. 
      */
-    public static Dictionary<?,?> convert(Properties p) {
+    public Dictionary<?,?> convert(Properties p) throws ValueConverterException {
         final Hashtable <String, Object> result = new Hashtable<String, Object>();
         
         for(Map.Entry<Object, Object> e : p.entrySet()) {
-            final String key = (String)e.getKey();
-            if(key.trim().endsWith(ARRAY_MARKER)) {
-                final String newKey = key.substring(0, key.length() - ARRAY_MARKER.length()).trim();
-                result.put(newKey, convertValue((String)e.getValue()));
-            } else {
-                result.put(key, e.getValue());
-            }
+            final PropertyValue v = converter.convert((String)e.getKey(), (String)e.getValue());
+            result.put(v.getKey(), v.getValue());
         }
         
-        return result;
-    }
-    
-    /** Convert value to a String[], trimming all values */
-    public static String [] convertValue(String value) {
-        
-        if(value.trim().length() == 0) {
-            return new String[0];
-        }
-        
-        return splitWithEscapes(value, ',');
-    }
-    
-    /** Split string, ignoring separators that directly follow a backslash.
-     *  All values are trimmed
-     */
-    public static String [] splitWithEscapes(String str, char separator) {
-        final ArrayList<String> a = new ArrayList<String>();
-        StringBuffer current = new StringBuffer();
-        char lastChar = 0;
-        for(int i=0; i < str.length(); i++) {
-            final char c = str.charAt(i);
-            
-            if(c == separator) {
-                if(lastChar == '\\') {
-                    // replace lastchar with c
-                    current.setCharAt(current.length() - 1, c);
-                } else if(current.length() > 0) {
-                    a.add(current.toString());
-                    current = new StringBuffer();
-                }
-            } else {
-                current.append(c);
-            }
-            
-            lastChar = c;
-        }
-        if(current.length() > 0) {
-            a.add(current.toString());
-        }
-        
-        final String [] result = new String[a.size()];
-        int i=0;
-        for(String s : a) {
-            result[i++] = s.trim();
-        }
         return result;
     }
 }
