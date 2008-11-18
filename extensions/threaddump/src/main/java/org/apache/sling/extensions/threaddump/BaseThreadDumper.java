a39c0b8b825495d6ec3daeb7fbd03d46bf626065
Felix Meschberger <fmeschbe@apache.org>
2008-11-18 10:23:37 +0000

    SLING-737 Provide Thread Dumper extension to web console and shell
    git-svn-id: https://svn.apache.org/repos/asf/incubator/sling/trunk@718548 13f79535-47bb-0310-9956-ffa450edef68

---
+++ extensions/threaddump/src/main/java/org/apache/sling/extensions/threaddump/BaseThreadDumper.java
@@ -0,0 +1,164 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.sling.extensions.threaddump;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.URL;
+import java.net.URLClassLoader;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.felix.webconsole.AbstractWebConsolePlugin;
+import org.apache.felix.webconsole.ConfigurationPrinter;
+
+class BaseThreadDumper {
+
+    boolean printThread(PrintWriter pw, long threadId, boolean withStackTrace) {
+        // first get the root thread group
+        ThreadGroup rootGroup = getRootThreadGroup();
+        int numThreads = rootGroup.activeCount();
+        Thread[] threads = new Thread[numThreads * 2];
+        rootGroup.enumerate(threads);
+
+        for (Thread thread : threads) {
+            if (thread != null && thread.getId() == threadId) {
+                printThread(pw, thread, withStackTrace);
+                return true;
+            }
+        }
+
+        return false;
+    }
+    
+    void printThreads(PrintWriter pw, boolean withStackTrace) {
+        // first get the root thread group
+        ThreadGroup rootGroup = getRootThreadGroup();
+
+        printThreadGroup(pw, rootGroup, withStackTrace);
+
+        int numGroups = rootGroup.activeGroupCount();
+        ThreadGroup[] groups = new ThreadGroup[2 * numGroups];
+        rootGroup.enumerate(groups);
+        for (int i = 0; i < groups.length; i++) {
+            printThreadGroup(pw, groups[i], withStackTrace);
+        }
+
+        pw.println();
+    }
+
+    private ThreadGroup getRootThreadGroup() {
+        ThreadGroup rootGroup = Thread.currentThread().getThreadGroup();
+        while (rootGroup.getParent() != null) {
+            rootGroup = rootGroup.getParent();
+        }
+        return rootGroup;
+    }
+    
+    private void printThreadGroup(PrintWriter pw, ThreadGroup group, boolean withStackTrace) {
+        if (group != null) {
+            StringBuffer info = new StringBuffer();
+            info.append("ThreadGroup ").append(group.getName());
+            info.append(" [");
+            info.append("maxprio=").append(group.getMaxPriority());
+
+            info.append(", parent=");
+            if (group.getParent() != null) {
+                info.append(group.getParent().getName());
+            } else {
+                info.append('-');
+            }
+
+            info.append(", isDaemon=").append(group.isDaemon());
+            info.append(", isDestroyed=").append(group.isDestroyed());
+            info.append(']');
+
+            pw.println(info);
+
+            int numThreads = group.activeCount();
+            Thread[] threads = new Thread[numThreads * 2];
+            group.enumerate(threads, false);
+            for (int i = 0; i < threads.length; i++) {
+                printThread(pw, threads[i], withStackTrace);
+            }
+
+            pw.println();
+        }
+    }
+
+    private void printThread(PrintWriter pw, Thread thread, boolean withStackTrace) {
+        if (thread != null) {
+            StringBuffer info = new StringBuffer();
+            info.append("  Thread ").append(thread.getId());
+            info.append('/').append(thread.getName());
+            info.append(" [");
+            info.append("priority=").append(thread.getPriority());
+            info.append(", alive=").append(thread.isAlive());
+            info.append(", daemon=").append(thread.isDaemon());
+            info.append(", interrupted=").append(thread.isInterrupted());
+            info.append(", loader=").append(thread.getContextClassLoader());
+            info.append(']');
+
+            pw.println(info);
+
+            if (withStackTrace) {
+                printClassLoader(pw, thread.getContextClassLoader());
+                printStackTrace(pw, thread.getStackTrace());
+                pw.println();
+            }
+        }
+    }
+
+    private void printClassLoader(PrintWriter pw, ClassLoader classLoader) {
+        if (classLoader != null) {
+            pw.print("    ClassLoader=");
+            pw.println(classLoader);
+            pw.print("      Parent=");
+            pw.println(classLoader.getParent());
+
+            if (classLoader instanceof URLClassLoader) {
+                URLClassLoader loader = (URLClassLoader) classLoader;
+                URL[] urls = loader.getURLs();
+                if (urls != null && urls.length > 0) {
+                    for (int i = 0; i < urls.length; i++) {
+                        pw.print("      ");
+                        pw.print(i);
+                        pw.print(" - ");
+                        pw.println(urls[i]);
+                    }
+                }
+            }
+        }
+    }
+
+    private void printStackTrace(PrintWriter pw, StackTraceElement[] stackTrace) {
+        pw.println("    Stacktrace");
+        if (stackTrace == null || stackTrace.length == 0) {
+            pw.println("      -");
+        } else {
+            for (StackTraceElement stackTraceElement : stackTrace) {
+                pw.print("      ");
+                pw.println(stackTraceElement);
+            }
+        }
+    }
+}
---
+++ extensions/threaddump/src/main/java/org/apache/sling/extensions/threaddump/ThreadDumpCommand.java
@@ -0,0 +1,92 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.sling.extensions.threaddump;
+
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
+
+import org.apache.felix.shell.Command;
+
+public class ThreadDumpCommand extends BaseThreadDumper implements Command {
+
+    private static final String CMD_NAME = "threads";
+
+    private static final String OPT_STACK = "-s";
+
+    public String getName() {
+        return CMD_NAME;
+    }
+
+    public String getShortDescription() {
+        return "dumps the JVM threads";
+    }
+
+    public String getUsage() {
+        return CMD_NAME + " [" + OPT_STACK + "] <id> ...";
+    }
+
+    public void execute(String command, PrintStream out, PrintStream err) {
+
+        // cut off leading command name
+        if (command.startsWith(CMD_NAME)) {
+            command = command.substring(CMD_NAME.length());
+        }
+
+        boolean longListing = false;
+        LinkedList<Long> threadIds = new LinkedList<Long>();
+
+        StringTokenizer tokener = new StringTokenizer(command, ", \t");
+        while (tokener.hasMoreTokens()) {
+            String token = tokener.nextToken().trim();
+            if (OPT_STACK.equals(token)) {
+                longListing = true;
+            } else {
+                try {
+                    long threadId = Long.parseLong(token);
+                    threadIds.add(threadId);
+                } catch (NumberFormatException nfe) {
+                    noSuchThread(err, token);
+                }
+            }
+        }
+
+        PrintWriter pw = new PrintWriter(out);
+
+        if (threadIds.isEmpty()) {
+            printThreads(pw, longListing);
+        } else {
+            while (!threadIds.isEmpty()) {
+                Long threadId = threadIds.removeFirst();
+                if (!printThread(pw, threadId, longListing)) {
+                    noSuchThread(err, threadId);
+                }
+            }
+        }
+
+        pw.flush();
+    }
+
+    private void noSuchThread(PrintStream err, Object threadId) {
+        err.println("No such Thread: " + threadId);
+        err.flush();
+    }
+
+}
