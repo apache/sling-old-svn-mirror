88a7f5708ac8909b1df415b151b94366260bdc37
Bertrand Delacretaz <bdelacretaz@apache.org>
2009-04-15 15:00:12 +0000

    SLING-923 - Servlet resolution examples and integration tests, work in progress
    git-svn-id: https://svn.apache.org/repos/asf/incubator/sling/trunk/launchpad/test-services@765224 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/main/java/org/apache/sling/launchpad/testservices/servlets/PrefixServletMinusOne.java
@@ -0,0 +1,39 @@
+package org.apache.sling.launchpad.testservices.servlets;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/** Example/test Sling Servlet using a prefix to demonstrate how
+ *  PrefixServletZero overrides PrefixServletMinusOne
+ * 
+ * @scr.component immediate="true" metatype="no"
+ * @scr.service interface="javax.servlet.Servlet"
+ * 
+ * @scr.property name="service.description" value="Default Query Servlet"
+ * @scr.property name="service.vendor" value="The Apache Software Foundation"
+ * 
+ * @scr.property name="sling.servlet.prefix"
+ *               value="-1"
+ * @scr.property name="sling.servlet.resourceTypes"
+ *               value="sling/servlet/default"
+ * @scr.property name="sling.servlet.extensions"
+ *               value="TEST_EXT_3"
+ */
+
+@SuppressWarnings("serial")
+public class PrefixServletMinusOne extends TestServlet {  
+}
