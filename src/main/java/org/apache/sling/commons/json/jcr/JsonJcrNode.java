0f554ca4bfa6d27ef8e3bebed5b3383aba1a5f42
Vidar Skauge Ramdal <vramdal@apache.org>
2009-04-17 20:30:29 +0000

    SLING-930 Adding Apache License header, and single-class imports, ref http://markmail.org/message/ovmjsn33uh5sbxnw
    git-svn-id: https://svn.apache.org/repos/asf/incubator/sling/trunk/bundles/commons/json@766143 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/sling/commons/json/jcr/JsonJcrNode.java
+++ src/main/java/org/apache/sling/commons/json/jcr/JsonJcrNode.java
@@ -1,9 +1,31 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.sling.commons.json.jcr;
 
 import org.apache.sling.commons.json.JSONObject;
 import org.apache.sling.commons.json.JSONException;
 
-import javax.jcr.*;
+import javax.jcr.Node;
+import javax.jcr.Property;
+import javax.jcr.PropertyIterator;
+import javax.jcr.PropertyType;
+import javax.jcr.RepositoryException;
+import javax.jcr.Value;
+import javax.jcr.ValueFormatException;
 import java.util.Set;
 import java.util.List;
 import java.util.ArrayList;
