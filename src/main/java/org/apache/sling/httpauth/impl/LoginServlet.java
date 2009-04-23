7e1ad8b0b97ddda2988d29144d40411b3ac0f850
Felix Meschberger <fmeschbe@apache.org>
2009-04-23 08:44:30 +0000

    SLING-939 Adapt HTTP Header Authentication to the new Authentication.login service method
    git-svn-id: https://svn.apache.org/repos/asf/incubator/sling/trunk/bundles/extensions/httpauth@767854 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/sling/httpauth/impl/LoginServlet.java
+++ src/main/java/org/apache/sling/httpauth/impl/LoginServlet.java
@@ -19,15 +19,19 @@
 package org.apache.sling.httpauth.impl;
 
 import java.io.IOException;
-import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServletResponse;
 
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.SlingHttpServletResponse;
 import org.apache.sling.api.servlets.SlingAllMethodsServlet;
+import org.apache.sling.engine.auth.Authenticator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * The <code>LoginServlet</code> TODO
- *
+ * 
  * @scr.component metatype="no"
  * @scr.service interface="javax.servlet.Servlet"
  * @scr.property name="service.description" value="HTTP Header Login Servlet"
@@ -37,172 +41,30 @@ import org.apache.sling.api.servlets.SlingAllMethodsServlet;
  */
 public class LoginServlet extends SlingAllMethodsServlet {
 
+    /** default log */
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    /** @scr.reference cardinality="0..1" policy="dynamic" */
+    private Authenticator authenticator;
+
     @Override
     protected void doGet(SlingHttpServletRequest request,
             SlingHttpServletResponse response) throws IOException {
 
-        response.setContentType("text/html");
-        response.setCharacterEncoding("UTF-8");
-
-        PrintWriter pw = response.getWriter();
-
-        prolog(pw);
-
-        final String contexPath = request.getContextPath();
-        String authType = request.getAuthType();
-        String user = request.getRemoteUser();
-
-        if (authType == null) {
-            login(pw, contexPath);
+        Authenticator authenticator = this.authenticator;
+        if (authenticator != null) {
+            authenticator.login(request, response);
         } else {
-            logout(pw, contexPath, user);
+            log.error("doGet: Authenticator service missing, cannot request authentication");
+            response.sendError(HttpServletResponse.SC_FORBIDDEN,
+                "Cannot request Authentication");
         }
-
-        epilog(pw);
     }
 
     @Override
     protected void doPost(SlingHttpServletRequest request,
             SlingHttpServletResponse response) throws IOException {
         response.sendRedirect(request.getRequestURI());
-    }
-
-    private void login(PrintWriter pw, String contextPath) {
-
-        pw.println("<script>");
-        ajax(pw, contextPath);
-        pw.println("function loginuser() {");
-        pw.println("    var user = document.forms['login'].usr.value;");
-        pw.println("    var pass = document.forms['login'].pwd.value;");
-        pw.println("    sendRequest(user, pass);");
-        pw.println("    document.location = document.location");
-        pw.println("    return false;");
-        pw.println("}");
-        pw.println("</script>");
-
-        pw.println("<form name='login' onsubmit='return loginuser()'>");
-        pw.println("<table align='center'>");
-        pw.println("<tr><td colspan='2' align='center'>You are not currently logged in</td></tr>");
-        pw.println("<tr><td>Name</td><td><input type='text' name='usr' /></td></tr>");
-        pw.println("<tr><td>Password</td><td><input type='text' name='pwd' /></td></tr>");
-        pw.println("<tr><td colspan='2' align='center'><input type='submit' value='Login' onClick='loginuser();'/></td></tr>");
-        pw.println("</table>");
-        pw.println("</form>");
-    }
-
-    private void logout(PrintWriter pw, String contextPath, String user) {
-        pw.println("<script>");
-        ajax(pw, contextPath);
-        pw.println("function logoutuser() {");
-
-        pw.println("    try {");
-        pw.println("        // 'ClearAuthenticationCache' is only available in some browsers");
-        pw.println("        // including the IE; for eg. Firefox, who cannot handle this command,");
-        pw.println("        // we have the try-catch statement");
-
-        pw.println("        // works in IE");
-        pw.println("        document.execCommand('ClearAuthenticationCache');");
-
-        pw.println("    } catch (e) {");
-        pw.println("        sendRequest('"
-            + AuthorizationHeaderAuthenticationHandler.NOT_LOGGED_IN_USER
-            + "', 'null');");
-        pw.println("    }");
-
-        pw.println("    document.location = document.location");
-        pw.println("}");
-        pw.println("</script>");
-
-        pw.println("<table align='center'>");
-        pw.println("<tr><td align='center'>You are logged in as " + user
-            + "</td></tr>");
-        pw.println("<tr><td align='center'><input type='button' value='Logout'  onClick='logoutuser();'/></td></tr>");
-        pw.println("</table>");
-    }
-
-    private void prolog(PrintWriter pw) {
-        pw.println("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01//EN\" \"http://www.w3.org/TR/html4/strict.dtd\">");
-        pw.println("<html>");
-        pw.println("<meta http-equiv=\"Content-Type\" content=\"text/html; utf-8\">");
-        pw.println("<title>Login/Logout</title>");
-        pw.println("<style type=\"text/css\">");
-        pw.println("body {");
-        pw.println("  font-family: Verdana, Arial, Helvetica, sans-serif;");
-        pw.println("  font-size: 10px;");
-        pw.println("  color: black;");
-        pw.println("  background-color: white;");
-        pw.println("}");
-        pw.println("#main {");
-        pw.println("  border: 1px solid black;");
-        pw.println("  margin-top: 25%;");
-        pw.println("  margin-left: 25%;");
-        pw.println("  width: 20em;");
-        pw.println("  padding: 10px;");
-        pw.println("}");
-        pw.println("#main table {");
-        pw.println("  width: 100%;");
-        pw.println("}");
-        pw.println("#main form {");
-        pw.println("  padding: 0px;");
-        pw.println("  margin: 0px;");
-        pw.println("}");
-        pw.println("</style>");
-        pw.println("</head>");
-        pw.println("<body>");
-
-        pw.println("<div id=\"main\">");
-    }
-
-    private void ajax(PrintWriter pw, final String contextPath) {
-        pw.println("//-----------------------------------------------------------------------------");
-        pw.println("// Ajax Support");
-
-        pw.println("// request object, do not access directly, use getXmlHttp instead");
-        pw.println("var xmlhttp = null;");
-        pw.println("function getXmlHttp() {");
-        pw.println("    if (xmlhttp) {");
-        pw.println("        return xmlhttp;");
-        pw.println("   }");
-
-        pw.println("         if (window.XMLHttpRequest) {");
-        pw.println("             xmlhttp = new XMLHttpRequest();");
-        pw.println("         } else if (window.ActiveXObject) {");
-        pw.println("             try {");
-        pw.println("                 xmlhttp = new ActiveXObject('Msxml2.XMLHTTP');");
-        pw.println("             } catch (ex) {");
-        pw.println("                 try {");
-        pw.println("                     xmlhttp = new ActiveXObject('Microsoft.XMLHTTP');");
-        pw.println("                 } catch (ex) {");
-        pw.println("                 }");
-        pw.println("             }");
-        pw.println("         }");
-        pw.println("");
-        pw.println("         return xmlhttp;");
-        pw.println("     }");
-
-        pw.println("     function sendRequest(/* String */ user, /* String */ pass) {");
-        pw.println("         var xmlhttp = getXmlHttp();");
-        pw.println("         if (!xmlhttp) {");
-        pw.println("             return;");
-        pw.println("         }");
-
-        pw.println("         if (xmlhttp.readyState < 4) {");
-        pw.println("             xmlhttp.abort();");
-        pw.println("         }");
-
-        pw.println("         xmlhttp.open('POST', '" + contextPath + "?"
-            + AuthorizationHeaderAuthenticationHandler.REQUEST_LOGIN_PARAMETER
-            + "=1', false, user, pass);");
-
-        pw.println("         xmlhttp.send('');");
-        pw.println("     }");
-
-    }
-
-    private void epilog(PrintWriter pw) {
-        pw.println("</div>");
-        pw.println("</body>");
-        pw.println("</html>");
     }
 
 }
