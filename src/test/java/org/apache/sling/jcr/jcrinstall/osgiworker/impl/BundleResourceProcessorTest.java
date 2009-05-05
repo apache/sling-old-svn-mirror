24cecc9ce3cf72297543538702d0b493e7ba8f38
Bertrand Delacretaz <bdelacretaz@apache.org>
2009-05-05 13:14:14 +0000

    SLING-904 - osgiworker bundle implemented, all tests pass
    git-svn-id: https://svn.apache.org/repos/asf/incubator/sling/trunk/contrib/extensions/jcrinstall/osgiworker@771708 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/test/java/org/apache/sling/jcr/jcrinstall/osgiworker/impl/BundleResourceProcessorTest.java
@@ -0,0 +1,177 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.sling.jcr.jcrinstall.osgiworker.impl;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.jmock.Expectations;
+import org.jmock.Mockery;
+import org.jmock.Sequence;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.FrameworkEvent;
+import org.osgi.framework.FrameworkListener;
+import org.osgi.service.packageadmin.PackageAdmin;
+
+/** Test the BundleResourceProcessor */
+public class BundleResourceProcessorTest {
+
+    private Mockery mockery;
+    private Sequence sequence;
+    private static int counter;
+
+    static class TestStorage extends Storage {
+        int saveCounter;
+
+        TestStorage(File f) throws IOException {
+            super(f);
+        }
+
+        @Override
+        protected void saveToFile() throws IOException {
+            super.saveToFile();
+            saveCounter++;
+        }
+    }
+
+    @org.junit.Before public void setup() {
+        mockery = new Mockery();
+        sequence = mockery.sequence(getClass().getSimpleName());
+    }
+
+    @org.junit.Test public void testInstall() throws Exception {
+
+        final OsgiControllerImpl c = new OsgiControllerImpl();
+        final BundleContext bc = mockery.mock(BundleContext.class);
+        Utilities.setField(c, "bundleContext", bc);
+        final PackageAdmin pa = mockery.mock(PackageAdmin.class);
+        final TestStorage s = new TestStorage(Utilities.getTestFile());
+        Utilities.setStorage(c, s);
+        final Bundle b = mockery.mock(Bundle.class);
+        final Bundle [] bundles = { b };
+        final long bundleId = 1234;
+        final String uri = "/test/bundle.jar";
+        final MockInstallableData data = new MockInstallableData(uri, "some data");
+
+        // We'll try installing a bundle, re-installing to cause
+        // it to be updated, and removing
+        mockery.checking(new Expectations() {{
+            allowing(pa).refreshPackages(null);
+            allowing(pa).resolveBundles(null);
+            allowing(b).start();
+            allowing(b).getSymbolicName();
+            will(returnValue(bundleId + "-name"));
+            allowing(b).getBundleId();
+            will(returnValue(bundleId));
+            allowing(b).getState();
+            allowing(bc).getBundle(bundleId);
+            will(returnValue(b));
+            allowing(bc).getBundles();
+            will(returnValue(bundles));
+            allowing(b).getLocation();
+            will(returnValue(uri));
+            allowing(bc).addFrameworkListener(with(any(FrameworkListener.class)));
+            allowing(bc).getDataFile(with(any(String.class)));
+            will(returnValue(getDataFile()));
+
+            one(bc).installBundle(
+            		with(equal(OsgiControllerImpl.getResourceLocation(uri))), 
+            		with(any(InputStream.class)));
+            inSequence(sequence);
+            will(returnValue(b));
+
+            allowing(bc).getBundle(bundleId);
+            inSequence(sequence);
+            will(returnValue(b));
+
+            one(b).stop();
+            inSequence(sequence);
+            
+            one(b).update(with(any(InputStream.class)));
+            inSequence(sequence);
+
+            one(b).uninstall();
+            inSequence(sequence);
+        }});
+
+        // Do the calls and check some stuff on the way
+        final BundleResourceProcessor p = new BundleResourceProcessor(bc, pa, new MockStartLevel());
+        final OsgiResourceProcessorList proc = new OsgiResourceProcessorList(bc, null, null, null);
+        proc.clear();
+        proc.add(p);
+        Utilities.setField(c, "processors", proc);
+        assertFalse("Before install, uri must not be in list", c.getInstalledUris().contains(uri));
+        
+        // Need to send framework events to p 
+        // TODO: this test is getting too complicated... ;-)
+        class FEThread extends Thread {
+        	boolean active = true;
+        	public FEThread() {
+        		setDaemon(true);
+        		start();
+			}
+        	public void run() {
+        		while(active) {
+        			try {
+        				Thread.sleep(1000L);
+        			} catch(InterruptedException iex) {
+        				active = false;
+        			}
+        			p.frameworkEvent(new FrameworkEvent(FrameworkEvent.PACKAGES_REFRESHED, b, null));
+        		}
+        	}
+        };
+        FEThread t = new FEThread();
+        
+        // do the actual testing
+        c.scheduleInstallOrUpdate(uri, data);
+        c.executeScheduledOperations();
+        assertTrue("After install, uri must be in list", c.getInstalledUris().contains(uri));
+        assertEquals("Digest must have been stored", data.getDigest(), c.getDigest(uri));
+        assertEquals("Storage data has been saved during install", 1, s.saveCounter);
+
+        data.setDigest("digest is now different");
+        c.scheduleInstallOrUpdate(uri, data);
+        c.executeScheduledOperations();
+        assertTrue("After update, uri must be in list", c.getInstalledUris().contains(uri));
+        assertEquals("Digest must have been updated", data.getDigest(), c.getDigest(uri));
+        assertEquals("Storage data has been saved during update", 2, s.saveCounter);
+
+        c.scheduleUninstall(uri);
+        c.executeScheduledOperations();
+        assertFalse("After uninstall, uri must not be in list", c.getInstalledUris().contains(uri));
+        assertEquals("Digest must be gone", null, c.getDigest(uri));
+        assertFalse("After getLastModified, uri must not be in list", c.getInstalledUris().contains(uri));
+        assertEquals("Storage data has been saved during uninstall", 3, s.saveCounter);
+
+        // And verify expectations
+        mockery.assertIsSatisfied();
+        t.active = false;
+    }
+    
+    static File getDataFile() throws IOException {
+        return File.createTempFile(BundleResourceProcessor.class.getSimpleName() + (++counter),".tmp");
+    }
+}
