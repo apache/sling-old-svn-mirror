9f276c1d0307c15d84973789594375098a80ac65
Bertrand Delacretaz <bdelacretaz@apache.org>
2009-05-18 15:38:54 +0000

    SLING-904 - bundle downgrades were not always ignored
    git-svn-id: https://svn.apache.org/repos/asf/incubator/sling/trunk/contrib/extensions/jcrinstall/it@775993 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/sling/jcr/jcrinstall/it/OsgiControllerTest.java
+++ src/test/java/org/apache/sling/jcr/jcrinstall/it/OsgiControllerTest.java
@@ -17,6 +17,7 @@
 package org.apache.sling.jcr.jcrinstall.it;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.ops4j.pax.exam.CoreOptions.felix;
@@ -26,12 +27,10 @@ import static org.ops4j.pax.exam.CoreOptions.waitForFrameworkStartup;
 import static org.ops4j.pax.exam.container.def.PaxRunnerOptions.vmOption;
 
 import java.io.File;
-import java.net.URL;
 import java.util.Dictionary;
 import java.util.Hashtable;
 
 import org.apache.sling.jcr.jcrinstall.osgiworker.DictionaryInstallableData;
-import org.apache.sling.jcr.jcrinstall.osgiworker.InstallableData;
 import org.apache.sling.jcr.jcrinstall.osgiworker.OsgiController;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -47,8 +46,7 @@ import org.osgi.service.cm.ConfigurationAdmin;
 /** Test the OsgiController running in the OSGi framework */
 @RunWith(JUnit4TestRunner.class)
 public class OsgiControllerTest {
-	// TODO System properties don't work as this runs in a separate process
-	public final static String POM_VERSION = System.getProperty("maven.pom.version");
+	public final static String POM_VERSION = System.getProperty("jcrinstall.pom.version");
 	
     @Inject
     protected BundleContext bundleContext;
@@ -84,12 +82,7 @@ public class OsgiControllerTest {
     }
     
     protected File getTestBundle(String bundleName) {
-    	// TODO there must be a better way...but tests don't run in the project folder
-    	// codebase returns a path like "...sling/contrib/extensions/jcrinstall/it/.$tail..."
-    	final URL codebase = getClass().getProtectionDomain().getCodeSource().getLocation();
-    	String path = codebase.getFile();
-    	path = path.substring(0, path.indexOf(".$"));
-    	return new File(path, "/target/testbundles/" + bundleName);
+    	return new File(System.getProperty("jcrinstall.base.dir"), bundleName);
     }
     
     @Test
@@ -117,39 +110,90 @@ public class OsgiControllerTest {
     }
     
     @Test
-    public void testInstallAndRemoveBundle() throws Exception {
-    	final File testBundle = getTestBundle("org.apache.felix.webconsole-1.2.8.jar");
-    	
-    	final String symbolicName = "org.apache.felix.webconsole";
+    public void testInstallUpgradeDowngradeBundle() throws Exception {
+    	final String symbolicName = "jcrinstall-testbundle";
     	final String uri = symbolicName + ".jar";
-    	assertNull("Console bundle must not be present before test", findBundle(symbolicName));
+    	final String BUNDLE_VERSION = "Bundle-Version";
     	
+    	assertNull("Test bundle must not be present before test", findBundle(symbolicName));
+    	
+    	// Install first test bundle and check version
+    	long bundleId = 0;
     	final OsgiController c = getService(OsgiController.class);
-    	final InstallableData d = new SimpleFileInstallableData(testBundle);
-    	c.scheduleInstallOrUpdate(uri, d);
-    	assertNull("Console bundle must still be absent right after scheduleInstallOrUpdate", findBundle(symbolicName));
-    	c.executeScheduledOperations();
-    	assertNotNull("Console bundle must be present after executeScheduledOperations", findBundle(symbolicName));
+    	{
+        	c.scheduleInstallOrUpdate(uri, new SimpleFileInstallableData(getTestBundle("org.apache.sling.jcr.jcrinstall.it-" + POM_VERSION + "-testbundle-1.1.jar")));
+        	assertNull("Test bundle must be absent right after scheduleInstallOrUpdate", findBundle(symbolicName));
+        	c.executeScheduledOperations();
+        	final Bundle b = findBundle(symbolicName);
+        	bundleId = b.getBundleId();
+        	assertNotNull("Test bundle 1.1 must be found after executeScheduledOperations", b);
+        	assertEquals("Version must be 1.1", "1.1", b.getHeaders().get(BUNDLE_VERSION));
+    	}
     	
-    	c.scheduleUninstall(uri);
-    	assertNotNull("Console bundle must still be present after scheduleUninstall", findBundle(symbolicName));
-    	c.executeScheduledOperations();
-    	assertNull("Console bundle must be gone after executeScheduledOperations", findBundle(symbolicName));
+    	// Upgrade to later version, verify
+    	{
+        	c.scheduleInstallOrUpdate(uri, new SimpleFileInstallableData(getTestBundle("org.apache.sling.jcr.jcrinstall.it-" + POM_VERSION + "-testbundle-1.2.jar")));
+        	c.executeScheduledOperations();
+        	final Bundle b = findBundle(symbolicName);
+        	assertNotNull("Test bundle 1.2 must be found after executeScheduledOperations", b);
+        	assertEquals("Version must be 1.2 after upgrade", "1.2", b.getHeaders().get(BUNDLE_VERSION));
+        	assertEquals("Bundle ID must not change after upgrade", bundleId, b.getBundleId());
+    	}
+    	
+    	// Downgrade to lower version, installed bundle must not change
+    	{
+        	c.scheduleInstallOrUpdate(uri, new SimpleFileInstallableData(getTestBundle("org.apache.sling.jcr.jcrinstall.it-" + POM_VERSION + "-testbundle-1.0.jar")));
+        	c.executeScheduledOperations();
+        	final Bundle b = findBundle(symbolicName);
+        	assertNotNull("Test bundle 1.2 must be found after executeScheduledOperations", b);
+        	assertEquals("Version must be 1.2 after ignored downgrade", "1.2", b.getHeaders().get(BUNDLE_VERSION));
+        	assertEquals("Bundle ID must not change after downgrade", bundleId, b.getBundleId());
+    	}
+    	
+    	// Uninstall
+    	{
+        	c.scheduleUninstall(uri);
+        	c.executeScheduledOperations();
+        	final Bundle b = findBundle(symbolicName);
+        	assertNull("Test bundle 1.2 must be gone", b);
+    	}
+    	
+    	// Install lower version, must work
+    	{
+        	c.scheduleInstallOrUpdate(uri, new SimpleFileInstallableData(getTestBundle("org.apache.sling.jcr.jcrinstall.it-" + POM_VERSION + "-testbundle-1.0.jar")));
+        	c.executeScheduledOperations();
+        	final Bundle b = findBundle(symbolicName);
+        	assertNotNull("Test bundle 1.0 must be found after executeScheduledOperations", b);
+        	assertEquals("Version must be 1.0 after uninstall and downgrade", "1.0", b.getHeaders().get(BUNDLE_VERSION));
+        	assertFalse("Bundle ID must have changed after uninstall and reinstall", bundleId == b.getBundleId());
+    	}
     }
-
 
     @org.ops4j.pax.exam.junit.Configuration
     public static Option[] configuration() {
+    	String vmOpt = "-Djrcinstall.testing";
+    	
+    	// make all jcrinstall.* system properties available to OSGi framework VM
+    	for(Object o : System.getProperties().keySet()) {
+    		final String key = (String)o;
+    		if(key.startsWith("jcrinstall.")) {
+    			vmOpt += " -D" + key + "=" + System.getProperty(key);
+    		}
+    	}
+
+    	// optional debugging
     	final String paxDebugPort = System.getProperty("pax.exam.debug.port");
-    	final String debugOptions = "-Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=" + paxDebugPort; 
+    	if(paxDebugPort != null && paxDebugPort.length() > 0) {
+        	vmOpt += " -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=" + paxDebugPort; 
+    	}
     	
         return options(
-            mavenBundle("org.apache.felix", "org.apache.felix.scr", "1.0.6"),
-            mavenBundle("org.apache.felix", "org.apache.felix.configadmin", "1.0.10"),
-            mavenBundle("org.apache.sling", "org.apache.sling.commons.log", "2.0.2-incubator"),
+            mavenBundle("org.apache.felix", "org.apache.felix.scr"),
+            mavenBundle("org.apache.felix", "org.apache.felix.configadmin"),
+            mavenBundle("org.apache.sling", "org.apache.sling.commons.log"),
         	mavenBundle("org.apache.sling", "org.apache.sling.jcr.jcrinstall.osgiworker", POM_VERSION),
             felix(),
-            vmOption(paxDebugPort != null && paxDebugPort.length() > 0 ? debugOptions : "-Dno.pax.debugging"),
+            vmOption(vmOpt),
             waitForFrameworkStartup()
         );
     }
