4aebf203d4df2918d1f39c6367f57042aed04d80
Felix Meschberger <fmeschbe@apache.org>
2009-06-18 09:21:02 +0000

    Move Sling to new TLP location
    git-svn-id: https://svn.eu.apache.org/repos/asf/sling/tags/org.apache.sling.jcr.jackrabbit.server-2.0.2-incubator@785979 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/main/java/org/apache/sling/jcr/jackrabbit/server/Activator.java
@@ -0,0 +1,202 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.sling.jcr.jackrabbit.server;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Hashtable;
+
+import org.apache.sling.jcr.base.util.RepositoryAccessor;
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.Constants;
+import org.osgi.framework.InvalidSyntaxException;
+import org.osgi.framework.ServiceEvent;
+import org.osgi.framework.ServiceListener;
+import org.osgi.framework.ServiceReference;
+import org.osgi.service.cm.Configuration;
+import org.osgi.service.cm.ConfigurationAdmin;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * The <code>Activator</code> TODO
+ */
+public class Activator implements BundleActivator, ServiceListener {
+
+    /** default log */
+    private static final Logger log = LoggerFactory.getLogger(Activator.class);
+
+    public static final String SERVER_REPOSITORY_FACTORY_PID = SlingServerRepository.class.getName();
+
+    /**
+     * The name of the configuration property naming the Sling Context for which
+     * a factory configuration has been created.
+     */
+    public static final String SLING_CONTEXT = "sling.context";
+
+    /**
+     * The name of the framework property containing the default sling context
+     * name.
+     */
+    public static final String SLING_CONTEXT_DEFAULT = "sling.context.default";
+
+    // The name of the Configuration Admin Service
+    private static final String CONFIG_ADMIN_NAME = ConfigurationAdmin.class.getName();
+
+    // this bundle's context, used by verifyConfiguration
+    private BundleContext bundleContext;
+
+    // the name of the default sling context
+    private String slingContext;
+
+    protected String getRepositoryName() {
+        return "jackrabbit";
+    }
+
+    public void start(BundleContext context) {
+
+        this.bundleContext = context;
+
+        // check the name of the default context, nothing to do if none
+        slingContext = context.getProperty(SLING_CONTEXT_DEFAULT);
+        if (slingContext == null) {
+            slingContext = "default";
+        }
+
+        ServiceReference sr = context.getServiceReference(CONFIG_ADMIN_NAME);
+        if (sr != null) {
+
+            // immediately verify the configuration as the service is here
+            verifyConfiguration(sr);
+
+        } else {
+
+            // register as service listener for Configuration Admin to verify
+            // the configuration when the service is registered
+            try {
+                bundleContext.addServiceListener(this, "("
+                    + Constants.OBJECTCLASS + "=" + CONFIG_ADMIN_NAME + ")");
+            } catch (InvalidSyntaxException ise) {
+                log.error(
+                    "start: Failed to register for Configuration Admin Service, will not verify configuration",
+                    ise);
+            }
+        }
+    }
+
+    public void stop(BundleContext arg0) {
+        // nothing to do
+    }
+
+    // ---------- ServiceListener ----------------------------------------------
+
+    public void serviceChanged(ServiceEvent event) {
+        if (event.getType() == ServiceEvent.REGISTERED) {
+
+            // verify the configuration with the newly registered service
+            verifyConfiguration(event.getServiceReference());
+
+            // don't care for any more service state changes
+            bundleContext.removeServiceListener(this);
+        }
+    }
+
+    // ---------- internal -----------------------------------------------------
+
+    private void verifyConfiguration(ServiceReference ref) {
+        ConfigurationAdmin ca = (ConfigurationAdmin) bundleContext.getService(ref);
+        if (ca == null) {
+            log.error("verifyConfiguration: Failed to get Configuration Admin Service from Service Reference");
+            return;
+        }
+
+        try {
+            // find a configuration for theses properties...
+            Configuration[] cfgs = ca.listConfigurations("("
+                + ConfigurationAdmin.SERVICE_FACTORYPID + "="
+                + SERVER_REPOSITORY_FACTORY_PID + ")");
+            if (cfgs != null && cfgs.length > 0) {
+                log.info(
+                    "verifyConfiguration: {} Configurations available for {}, nothing to do",
+                    new Object[] { new Integer(cfgs.length),
+                        SERVER_REPOSITORY_FACTORY_PID });
+                return;
+            }
+
+            // No config, create a default one.
+            Hashtable<String, String> defaultConfig = new Hashtable<String, String>();
+            final String overrideUrl = bundleContext.getProperty(RepositoryAccessor.REPOSITORY_URL_OVERRIDE_PROPERTY);
+            if(overrideUrl != null && overrideUrl.length() > 0) {
+                // Ignore other parameters if override URL (SLING-254) is set 
+                defaultConfig.put(RepositoryAccessor.REPOSITORY_URL_OVERRIDE_PROPERTY, overrideUrl);
+                log.info(RepositoryAccessor.REPOSITORY_URL_OVERRIDE_PROPERTY + "=" + overrideUrl + 
+                    ", using it to create the default configuration");
+                
+            } else {
+               initDefaultConfig(defaultConfig, bundleContext); 
+            }
+            
+            // create the factory and set the properties
+            Configuration config = ca.createFactoryConfiguration(SERVER_REPOSITORY_FACTORY_PID);
+            config.update(defaultConfig);
+
+            log.info("verifyConfiguration: Created configuration {} for {}",
+                config.getPid(), config.getFactoryPid());
+
+        } catch (Throwable t) {
+            log.error(
+                "verifyConfiguration: Cannot check or define configuration", t);
+        } finally {
+            bundleContext.ungetService(ref);
+        }
+    }
+    
+    private void initDefaultConfig(Hashtable<String, String> props, BundleContext bundleContext) throws IOException {
+        File homeDir;
+        String slingHomePath = bundleContext.getProperty("sling.home");
+        if (slingHomePath != null) {
+            homeDir = new File(slingHomePath, getRepositoryName());
+        } else {
+            homeDir = new File(getRepositoryName());
+        }
+
+        // make sure jackrabbit home exists
+        log.info("Creating default config for Jackrabbit in " + homeDir);
+        if (!homeDir.isDirectory()) {
+            if (!homeDir.mkdirs()) {
+                log.info("verifyConfiguration: Cannot create Jackrabbit home "
+                    + homeDir + ", failed creating default configuration");
+                return;
+            }
+        }
+
+        // ensure the configuration file (inside the home Dir !)
+        File configFile = new File(homeDir, "repository.xml");
+        SlingServerRepository.copyFile(bundleContext.getBundle(),
+            "repository.xml", configFile);
+
+        // default config values
+        props.put(SLING_CONTEXT, slingContext);
+        props.put(SlingServerRepository.REPOSITORY_CONFIG_URL,
+            configFile.getPath());
+        props.put(SlingServerRepository.REPOSITORY_HOME_DIR,
+            homeDir.getPath());
+        props.put(SlingServerRepository.REPOSITORY_REGISTRATION_NAME,
+            this.getRepositoryName());
+    }
+}
---
+++ src/main/java/org/apache/sling/jcr/jackrabbit/server/SlingServerRepository.java
@@ -0,0 +1,209 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.sling.jcr.jackrabbit.server;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
+import java.util.Dictionary;
+
+import javax.jcr.Repository;
+import javax.jcr.RepositoryException;
+
+import org.apache.jackrabbit.core.RepositoryImpl;
+import org.apache.jackrabbit.core.config.RepositoryConfig;
+import org.apache.sling.jcr.api.SlingRepository;
+import org.apache.sling.jcr.base.AbstractSlingRepository;
+import org.osgi.framework.Bundle;
+import org.osgi.service.log.LogService;
+
+/**
+ * The <code>SlingServerRepository</code> TODO
+ *
+ * @scr.component label="%repository.name" description="%repository.description"
+ *          factory="org.apache.sling.jcr.jackrabbit.server.SlingServerRepositoryFactory"
+ *
+ * @scr.property name="service.vendor" value="The Apache Software Foundation"
+ * @scr.property name="service.description"
+ *      value="Factory for embedded Jackrabbit Repository Instances"
+ */
+public class SlingServerRepository extends AbstractSlingRepository
+        implements Repository, SlingRepository {
+
+    /**
+     * The name of the configuration property defining the URL to the
+     * repository configuration file (value is
+     * "config").
+     * <p>
+     * If the configuration file is located in the local file system, the
+     * "file:" scheme must still be specified.
+     * <p>
+     * This parameter is mandatory for this activator to start the repository.
+     *
+     * @scr.property value=""
+     */
+    public static final String REPOSITORY_CONFIG_URL = "config";
+
+    /**
+     * The name of the configuration property defining the file system
+     * directory where the repository files are located (value is
+     * "home").
+     * <p>
+     * This parameter is mandatory for this activator to start the repository.
+     *
+     * @scr.property value=""
+     */
+    public static final String REPOSITORY_HOME_DIR = "home";
+
+    /**
+     * @scr.property value=""
+     */
+    public static final String REPOSITORY_REGISTRATION_NAME = "name";
+
+    //---------- Repository Management ----------------------------------------
+    
+    @Override
+    protected Repository acquireRepository() {
+        Repository repository = super.acquireRepository();
+        if (repository != null) {
+            return repository;
+        }
+        
+        @SuppressWarnings("unchecked")
+        Dictionary<String, Object> environment = this.getComponentContext().getProperties();
+        String configURLObj = (String) environment.get(REPOSITORY_CONFIG_URL);
+        String home = (String) environment.get(REPOSITORY_HOME_DIR);
+
+        // somewhat dirty hack to have the derby.log file in a sensible
+        // location, but don't overwrite anything already set
+        if (System.getProperty("derby.stream.error.file") == null) {
+            String derbyLog = home + "/derby.log";
+            System.setProperty("derby.stream.error.file", derbyLog);
+        }
+        
+        InputStream ins = null;
+        try {
+
+            // check whether the URL is a file path
+            File configFile = new File(configURLObj);
+            if (configFile.canRead()) {
+                ins = new FileInputStream(configFile);
+            } else {
+                URL configURL = new URL(configURLObj);
+                ins = configURL.openStream();
+            }
+
+            RepositoryConfig crc = RepositoryConfig.create(ins, home);
+            return RepositoryImpl.create(crc);
+            
+        } catch (IOException ioe) {
+            
+            log(LogService.LOG_ERROR,
+                "acquireRepository: IO problem starting repository from "
+                    + configURLObj + " in " + home, ioe);
+            
+        } catch (RepositoryException re) {
+            
+            log(LogService.LOG_ERROR,
+                "acquireRepository: Repository problem starting repository from "
+                    + configURLObj + " in " + home, re);
+        } finally {
+            if (ins != null) {
+                try {
+                    ins.close();
+                } catch (IOException ioe) {
+                    // ignore
+                }
+            }
+        }
+        
+        // got no repository ....
+        return null;
+    }
+    
+    @Override
+    protected void disposeRepository(Repository repository) {
+        super.disposeRepository(repository);
+
+        if (repository instanceof RepositoryImpl) {
+            
+            try {
+                ((RepositoryImpl) repository).shutdown();
+            } catch (Throwable t) {
+                log(LogService.LOG_ERROR,
+                    "deactivate: Unexpected problem shutting down repository",
+                    t);
+            }
+            
+        } else {
+            log(LogService.LOG_INFO,
+                "Repository is not a RepositoryImpl, nothing to do");
+        }
+    }
+    
+    //---------- Helper -------------------------------------------------------
+
+    public static void copyFile(Bundle bundle, String entryPath, File destFile) throws FileNotFoundException, IOException {
+        if (destFile.canRead()) {
+            // nothing to do, file exists
+            return;
+        }
+
+        // copy from property
+        URL entryURL = bundle.getEntry(entryPath);
+        if (entryURL == null) {
+            throw new FileNotFoundException(entryPath);
+        }
+
+        // check for a file property
+        InputStream source = entryURL.openStream();
+
+        OutputStream dest = null;
+        try {
+
+            // ensure path to parent folder of licFile
+            destFile.getParentFile().mkdirs();
+
+            dest = new FileOutputStream(destFile);
+            byte[] buf = new byte[2048];
+            int rd;
+            while ( (rd = source.read(buf)) >= 0) {
+                dest.write(buf, 0, rd);
+            }
+
+
+        } finally {
+            if (dest != null) {
+                try {
+                    dest.close();
+                } catch (IOException ignore) {
+                }
+            }
+
+            // licSource is not null
+            try {
+                source.close();
+            } catch (IOException ignore) {
+            }
+        }
+    }
+}
