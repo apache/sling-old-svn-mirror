ac96864a9769f2226466f6b3d591ef2f58637d45
Carsten Ziegeler <cziegeler@apache.org>
2009-07-17 08:33:59 +0000

    Clean up javadocs
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk@795008 13f79535-47bb-0310-9956-ffa450edef68

--- bundles/api/src/main/java/org/apache/sling/api/scripting/SlingScriptResolver.java
+++ bundles/api/src/main/java/org/apache/sling/api/scripting/SlingScriptResolver.java
@@ -22,33 +22,16 @@ import org.apache.sling.api.resource.ResourceResolver;
  * The <code>ScriptResolver</code> interface defines the API for a service
  * capable of locating scripts. Where the script is actually located is an
  * implementation detail of the service implementation.
- * <p>
- * Find scripts in the repository, based on the current Resource type. The
- * script filename is built using the current HTTP request method name, followed
- * by the extension of the current request and the desired script extension. For
- * example, a "js" script for a GET request on a Resource of type some/type with
- * request extension "html" should be stored as
- *
- * <pre>
- *      /sling/scripts/some/type/get.html.js
- * </pre>
- *
- * in the repository.
- * <p>
- * Applications of the Sling Framework generally do not need the servlet
- * resolver as resolution of the servlets to process requests and sub-requests
- * through a <code>RequestDispatcher</code> is handled by the Sling Framework.
- * <p>
  */
 public interface SlingScriptResolver {
 
     /**
-     * Finds the given name to a {@link SlingScript}.
+     * Finds the {@link SlingScript} for the given name.
      * <p>
      * The semantic meaning of the name is implementation specific: It may be an
      * absolute path to a <code>Resource</code> providing the script source or
      * it may be a relative path resolved according to some path settings.
-     * Finally, the name may also just be used as an itentifier to find the
+     * Finally, the name may also just be used as an identifier to find the
      * script in some registry.
      *
      * @param resourceResolver The <code>ResourceResolver</code> used to
