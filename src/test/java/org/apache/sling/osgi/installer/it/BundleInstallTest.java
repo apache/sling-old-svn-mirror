ee81bafe2ddc59a6d4eff2da4dafc83c76fd38a8
Bertrand Delacretaz <bdelacretaz@apache.org>
2009-08-13 12:29:10 +0000

    SLING-1078 - separate tests in smaller classes, makes no difference to test execution time as pax web starts framework for each test anyway
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk/installer/osgi/it@803862 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/sling/osgi/installer/it/BundleInstallTest.java
+++ src/test/java/org/apache/sling/osgi/installer/it/BundleInstallTest.java
@@ -22,222 +22,24 @@ import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
-import static org.ops4j.pax.exam.CoreOptions.felix;
-import static org.ops4j.pax.exam.CoreOptions.mavenBundle;
-import static org.ops4j.pax.exam.CoreOptions.options;
-import static org.ops4j.pax.exam.CoreOptions.provision;
-import static org.ops4j.pax.exam.CoreOptions.waitForFrameworkStartup;
-import static org.ops4j.pax.exam.container.def.PaxRunnerOptions.vmOption;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStream;
-import java.util.Dictionary;
-import java.util.Hashtable;
-
-import org.apache.sling.osgi.installer.DictionaryInstallableData;
 import org.apache.sling.osgi.installer.OsgiController;
-import org.apache.sling.osgi.installer.OsgiControllerServices;
 import org.apache.sling.osgi.installer.OsgiControllerStatistics;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.ops4j.pax.exam.Inject;
 import org.ops4j.pax.exam.Option;
 import org.ops4j.pax.exam.junit.JUnit4TestRunner;
 import org.osgi.framework.Bundle;
-import org.osgi.framework.BundleContext;
-import org.osgi.framework.FrameworkEvent;
-import org.osgi.framework.FrameworkListener;
-import org.osgi.framework.ServiceReference;
-import org.osgi.service.cm.Configuration;
-import org.osgi.service.cm.ConfigurationAdmin;
-import org.osgi.service.packageadmin.PackageAdmin;
 
-/** Test the OsgiController running in the OSGi framework
- *  
- * 	This is a rather big test class, as Pax Exam currently starts
- *  the framework for each test class - having all tests here
- *  (as long as it's practical) allows them to run faster.
- *   
- */
 @RunWith(JUnit4TestRunner.class)
-public class OsgiControllerTest implements FrameworkListener {
-	public final static String POM_VERSION = System.getProperty("osgi.installer.pom.version");
-	public final static String JAR_EXT = ".jar";
-	private int packageRefreshEventsCount;
-	
-    @Inject
-    protected BundleContext bundleContext;
-    
-    @SuppressWarnings("unchecked")
-	protected <T> T getService(Class<T> clazz) {
-    	final ServiceReference ref = bundleContext.getServiceReference(clazz.getName());
-    	assertNotNull("getService(" + clazz.getName() + ") must find ServiceReference", ref);
-    	final T result = (T)(bundleContext.getService(ref));
-    	assertNotNull("getService(" + clazz.getName() + ") must find service", result);
-    	return result;
+public class BundleInstallTest extends OsgiControllerTestBase {
+
+    @org.ops4j.pax.exam.junit.Configuration
+    public static Option[] configuration() {
+    	return defaultConfiguration();
     }
     
-    protected void generateBundleEvent() throws Exception {
-        // install a bundle manually to generate a bundle event
-        final File f = getTestBundle("org.apache.sling.osgi.installer.it-" + POM_VERSION + "-testbundle-1.0.jar");
-        final InputStream is = new FileInputStream(f);
-        Bundle b = null;
-        try {
-            b = bundleContext.installBundle(getClass().getName(), is);
-            b.start();
-            final long timeout = System.currentTimeMillis() + 2000L;
-            while(b.getState() != Bundle.ACTIVE && System.currentTimeMillis() < timeout) {
-                Thread.sleep(10L);
-            }
-        } finally {
-            if(is != null) {
-                is.close();
-            }
-            if(b != null) {
-                b.uninstall();
-            }
-        }
-    }
-    
-    public void frameworkEvent(FrameworkEvent event) {
-        if (event.getType() == FrameworkEvent.PACKAGES_REFRESHED) {
-            packageRefreshEventsCount++;
-        }
-    }
-    
-    protected void refreshPackages() {
-        bundleContext.addFrameworkListener(this);
-        final int MAX_REFRESH_PACKAGES_WAIT_SECONDS = 5;
-        final int targetEventCount = packageRefreshEventsCount + 1;
-        final long timeout = System.currentTimeMillis() + MAX_REFRESH_PACKAGES_WAIT_SECONDS * 1000L;
-        
-        final PackageAdmin pa = getService(PackageAdmin.class);
-        pa.refreshPackages(null);
-        
-        try {
-            while(true) {
-                if(System.currentTimeMillis() > timeout) {
-                    break;
-                }
-                if(packageRefreshEventsCount >= targetEventCount) {
-                    break;
-                }
-                try {
-                    Thread.sleep(250L);
-                } catch(InterruptedException ignore) {
-                }
-            }
-        } finally {
-            bundleContext.removeFrameworkListener(this);
-        }
-    }
-    
-    protected Configuration findConfiguration(String pid) throws Exception {
-    	final ConfigurationAdmin ca = getService(ConfigurationAdmin.class);
-    	if(ca != null) {
-	    	final Configuration[] cfgs = ca.listConfigurations(null);
-	    	if(cfgs != null) {
-		    	for(Configuration cfg : cfgs) {
-		    		if(cfg.getPid().equals(pid)) {
-		    			return cfg;
-		    		}
-		    	}
-	    	}
-    	}
-    	return null;
-    }
-    
-    protected Bundle findBundle(String symbolicName) {
-    	for(Bundle b : bundleContext.getBundles()) {
-    		if(symbolicName.equals(b.getSymbolicName())) {
-    			return b;
-    		}
-    	}
-    	return null;
-    }
-    
-    protected File getTestBundle(String bundleName) {
-    	return new File(System.getProperty("osgi.installer.base.dir"), bundleName);
-    }
-    
-    protected void waitForConfigAdmin(boolean shouldBePresent) throws InterruptedException {
-    	final OsgiControllerServices svc = getService(OsgiControllerServices.class);
-    	final int timeout = 5;
-    	final long waitUntil = System.currentTimeMillis() + (timeout * 1000L);
-    	do {
-    		boolean isPresent = svc.getConfigurationAdmin() != null;
-    		if(isPresent == shouldBePresent) {
-    			return;
-    		}
-    		Thread.sleep(100L);
-    	} while(System.currentTimeMillis() < waitUntil);
-    	fail("ConfigurationAdmin service not available after waiting " + timeout + " seconds");
-    }
-    
-    @Test
-    public void testInstallAndRemoveConfig() throws Exception {
-    	final OsgiController c = getService(OsgiController.class);
-    	final Dictionary<String, Object> cfgData = new Hashtable<String, Object>();
-    	cfgData.put("foo", "bar");
-    	final String cfgPid = getClass().getName() + "." + System.currentTimeMillis();
-    	
-    	assertNull("Config " + cfgPid + " must not be found before test", findConfiguration(cfgPid));
-    	
-    	c.scheduleInstallOrUpdate(cfgPid, new DictionaryInstallableData(cfgData));
-    	assertNull("Config " + cfgPid + " must not be found right after scheduleInstall", findConfiguration(cfgPid));
-    	c.executeScheduledOperations();
-    	
-    	final Configuration cfg = findConfiguration(cfgPid);
-    	assertNotNull("Config " + cfgPid + " must be found right after executeScheduledOperations()", cfg);
-    	final String value = (String)cfg.getProperties().get("foo");
-    	assertEquals("Config value must match", "bar", value);
-    	
-    	c.scheduleUninstall(cfgPid);
-    	assertNotNull("Config " + cfgPid + " must still be found right after scheduleUninstall", cfg);
-    	c.executeScheduledOperations();
-    	assertNull("Config " + cfgPid + " must be gone after executeScheduledOperations", findConfiguration(cfgPid));
-    }
-    
-    @Test
-    public void testDeferredConfigInstall() throws Exception {
-    	
-    	final String cfgName = "org.apache.felix.configadmin";
-    	Bundle configAdmin = null;
-    	for(Bundle b : bundleContext.getBundles()) {
-    		if(b.getSymbolicName().equals(cfgName)) {
-    			configAdmin = b;
-    			break;
-    		}
-    	}
-    	assertNotNull(cfgName + " bundle must be found", configAdmin);
-    	waitForConfigAdmin(true);
-    	
-    	final OsgiController c = getService(OsgiController.class);
-    	final Dictionary<String, Object> cfgData = new Hashtable<String, Object>();
-    	cfgData.put("foo", "bar");
-    	final String cfgPid = getClass().getName() + ".deferred." + System.currentTimeMillis();
-    	assertNull("Config " + cfgPid + " must not be found before test", findConfiguration(cfgPid));
-    	
-    	c.scheduleInstallOrUpdate(cfgPid, new DictionaryInstallableData(cfgData));
-    	assertNull("Config " + cfgPid + " must not be found right after scheduleInstall", findConfiguration(cfgPid));
-    	
-    	// Config installs must be deferred if ConfigAdmin service is stopped
-    	configAdmin.stop();
-    	waitForConfigAdmin(false);
-    	c.executeScheduledOperations();
-    	configAdmin.start();
-    	waitForConfigAdmin(true);
-    	assertNull("Config " + cfgPid + " must not be installed if ConfigAdmin was stopped", findConfiguration(cfgPid));
-    	
-    	// with configadmin back, executeScheduledOperations must install deferred configs
-    	c.executeScheduledOperations();
-    	assertNotNull("Config " + cfgPid + " must be installed after restarting ConfigAdmin", findConfiguration(cfgPid));
-    	findConfiguration(cfgPid).delete();
-    	assertNull("Config " + cfgPid + " must be gone after test", findConfiguration(cfgPid));
-    }
-    
-    @Test
+	@Test
     public void testInstallUpgradeDowngradeBundle() throws Exception {
     	final String symbolicName = "osgi-installer-testbundle";
     	final String uri = symbolicName + JAR_EXT;
@@ -427,37 +229,5 @@ public class OsgiControllerTest implements FrameworkListener {
         	assertNotNull(needsB + " must be installed", b);
         	assertTrue(needsB + " must be started now that testB is installed", b.getState() == Bundle.ACTIVE);
     	}
-    }
-
-    @org.ops4j.pax.exam.junit.Configuration
-    public static Option[] configuration() {
-    	String vmOpt = "-Dosgi.installer.testing";
-    	
-    	// This runs in the VM that runs the build, but the tests run in another one.
-    	// Make all osgi.installer.* system properties available to OSGi framework VM
-    	for(Object o : System.getProperties().keySet()) {
-    		final String key = (String)o;
-    		if(key.startsWith("osgi.installer.")) {
-    			vmOpt += " -D" + key + "=" + System.getProperty(key);
-    		}
-    	}
-
-    	// optional debugging
-    	final String paxDebugPort = System.getProperty("pax.exam.debug.port");
-    	if(paxDebugPort != null && paxDebugPort.length() > 0) {
-        	vmOpt += " -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=" + paxDebugPort; 
-    	}
-    	
-        return options(
-                felix(),
-                vmOption(vmOpt),
-                waitForFrameworkStartup(),
-        		provision(
-        	            mavenBundle("org.apache.felix", "org.apache.felix.scr"),
-        	            mavenBundle("org.apache.felix", "org.apache.felix.configadmin"),
-        	            mavenBundle("org.apache.sling", "org.apache.sling.commons.log"),
-        	        	mavenBundle("org.apache.sling", "org.apache.sling.osgi.installer", POM_VERSION)
-        		)
-        );
     }
 }
