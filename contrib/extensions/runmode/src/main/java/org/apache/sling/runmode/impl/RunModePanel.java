846b76635859505f20b8a2c94dacc714db92c47e
Carsten Ziegeler <cziegeler@apache.org>
2009-09-25 10:06:13 +0000

    SLING-1124 : RunMode should only provide a configuration printer for the web console
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk@818804 13f79535-47bb-0310-9956-ffa450edef68

--- contrib/extensions/runmode/src/main/java/org/apache/sling/runmode/impl/RunModePanel.java
+++ contrib/extensions/runmode/src/main/java/org/apache/sling/runmode/impl/RunModePanel.java
@@ -14,79 +14,21 @@
  */
 package org.apache.sling.runmode.impl;
 
-import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Arrays;
 
-import javax.servlet.Servlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.apache.felix.webconsole.AbstractWebConsolePlugin;
 import org.apache.felix.webconsole.ConfigurationPrinter;
 import org.apache.sling.runmode.RunMode;
-import org.osgi.service.component.ComponentContext;
 
 /**
  * @scr.component metatype="no"
  * @scr.service
- * @scr.property name="felix.webconsole.label" valueRef="LABEL"
  */
-public class RunModePanel extends AbstractWebConsolePlugin implements Servlet,
-        ConfigurationPrinter {
-
-    private static final String LABEL = "runmodes";
-
+public class RunModePanel implements ConfigurationPrinter {
     private static final String TITLE = "Run Modes";
 
     /** @scr.reference */
     private RunMode runMode;
-
-    // ---------- AbstractWebConsolePlugin API
-
-    @Override
-    public String getLabel() {
-        return LABEL;
-    }
-
-    @Override
-    public String getTitle() {
-        return TITLE;
-    }
-
-    @Override
-    protected void renderContent(HttpServletRequest request,
-            HttpServletResponse response) throws IOException {
-        PrintWriter pw = response.getWriter();
-
-        pw.println("<pre>");
-        pw.println("</pre>");
-
-        pw.println("<table class='content' cellpadding='0' cellspacing='0' width='100%'>");
-
-        pw.println("<tr class='content'>");
-        pw.println("<th class='content container' colspan='2'>" + getTitle()
-            + "</th>");
-        pw.println("</tr>");
-
-        pw.println("<tr class='content'>");
-        pw.println("<td class='content'>Current Run Modes</td>");
-        pw.println("<td class='content'>");
-
-        String[] modes = runMode.getCurrentRunModes();
-        if (modes == null || modes.length == 0) {
-            pw.println("-");
-        } else {
-            for (String mode : modes) {
-                pw.print(mode);
-                pw.println("<br>");
-            }
-        }
-
-        pw.println("</td>");
-        pw.println("</tr>");
-        pw.println("</table>");
-    }
 
     // ---------- ConfigurationPrinter
 
@@ -101,13 +43,7 @@ public class RunModePanel extends AbstractWebConsolePlugin implements Servlet,
         }
     }
 
-    // ---------- SCR integration
-
-    protected void activate(ComponentContext context) {
-        activate(context.getBundleContext());
-    }
-
-    protected void deactivate(ComponentContext context) {
-        deactivate();
+    public String getTitle() {
+        return TITLE;
     }
 }
