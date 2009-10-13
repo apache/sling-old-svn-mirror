aa3be88db6d5a04532133399075867e57dbe2a4e
Bertrand Delacretaz <bdelacretaz@apache.org>
2009-10-13 16:49:33 +0000

    SLING-1148 - RegisteredResource persisted in the bundle storage area
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk/installer/osgi/it@824840 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/test/java/org/apache/sling/osgi/installer/it/RemovedResourceDetectionTest.java
@@ -0,0 +1,86 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.sling.osgi.installer.it;
+
+import static org.junit.Assert.assertNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.sling.osgi.installer.InstallableResource;
+import org.apache.sling.osgi.installer.OsgiInstaller;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.ops4j.pax.exam.Option;
+import org.ops4j.pax.exam.junit.JUnit4TestRunner;
+import org.osgi.framework.Bundle;
+
+/** Verify that the removal of resources is detected even if
+ *  the OsgiController was stopped when resource was removed.
+ */
+@RunWith(JUnit4TestRunner.class)
+public class RemovedResourceDetectionTest extends OsgiInstallerTestBase {
+    
+    @org.ops4j.pax.exam.junit.Configuration
+    public static Option[] configuration() {
+        return defaultConfiguration();
+    }
+    
+    @Before
+    public void setUp() {
+        setupInstaller();
+    }
+    
+    @After
+    public void tearDown() {
+        super.tearDown();
+    }
+    
+    @Test
+    public void testRemoveResourceAndRestart() throws Exception {
+        
+        final String symbolicNameA = "osgi-installer-testbundle";
+        final String symbolicNameB = "osgi-installer-testB";
+        
+        // Install two bundles and verify
+        assertNull(symbolicNameA + " must be absent before installing", findBundle(symbolicNameA));
+        resetCounters();
+        installer.addResource(getInstallableResource(
+                getTestBundle(BUNDLE_BASE_NAME + "-testbundle-1.1.jar")));
+        waitForInstallerAction(OsgiInstaller.WORKER_THREAD_BECOMES_IDLE_COUNTER, 1);
+        assertBundle("After initial install", symbolicNameA, "1.1", Bundle.ACTIVE);
+        
+        assertNull(symbolicNameB + " must be absent before installing", findBundle(symbolicNameB));
+        resetCounters();
+        installer.addResource(getInstallableResource(
+                getTestBundle(BUNDLE_BASE_NAME + "-testB-1.0.jar")));
+        waitForInstallerAction(OsgiInstaller.WORKER_THREAD_BECOMES_IDLE_COUNTER, 1);
+        assertBundle("After initial install", symbolicNameB, "1.0", Bundle.ACTIVE);
+        
+        // Restart installer, register only second bundle and verify that first one is gone
+        restartInstaller();
+        resetCounters();
+        final List<InstallableResource> data = new ArrayList<InstallableResource>();
+        data.add(getInstallableResource(getTestBundle(BUNDLE_BASE_NAME + "-testB-1.0.jar")));
+        installer.registerResources(data, URL_SCHEME);
+        waitForInstallerAction(OsgiInstaller.WORKER_THREAD_BECOMES_IDLE_COUNTER, 1);
+        assertBundle("After installer restart", symbolicNameB, "1.0", Bundle.ACTIVE);
+        assertNull("Bundle not in second list should be removed", findBundle(symbolicNameA));
+    }
+}
