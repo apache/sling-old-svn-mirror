b78cce12d410db3537e765ae0f0d1d30bb7fbcf9
Carsten Ziegeler <cziegeler@apache.org>
2009-12-30 16:27:28 +0000

    Exclude special folders (like those starting with a dot) and add support for preview images.
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk/samples/slingshot@894663 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/sling/sample/slingshot/Constants.java
+++ src/main/java/org/apache/sling/sample/slingshot/Constants.java
@@ -16,29 +16,58 @@
  */
 package org.apache.sling.sample.slingshot;
 
-public interface Constants {
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ResourceUtil;
+
+public abstract class Constants {
 
     /** This is the resource path for the root of our application. */
-    String APP_ROOT = "/slingshot";
+    public static final String APP_ROOT = "/slingshot";
 
     /** This is the resource path for the root album. */
-    String ALBUMS_ROOT = APP_ROOT + "/albums";
+    public static final String ALBUMS_ROOT = APP_ROOT + "/albums";
 
     /** The resource type of a folder (= album) */
-    String RESOURCETYPE_FOLDER = "nt:folder";
+    public static final String RESOURCETYPE_FOLDER = "nt:folder";
 
     /** The resource type of an extended folder (= album) */
-    String RESOURCETYPE_EXT_FOLDER = "sling:Folder";
+    public static final String RESOURCETYPE_EXT_FOLDER = "sling:Folder";
 
     /** The resource type for an album. */
-    String RESOURCETYPE_ALBUM = "slingshot/Album";
+    public static final String RESOURCETYPE_ALBUM = "slingshot/Album";
 
     /** The resource type of a file (= photo) */
-    String RESOURCETYPE_FILE = "nt:file";
+    public static final String RESOURCETYPE_FILE = "nt:file";
 
     /** The resource type for a photo. */
-    String RESOURCETYPE_PHOTO = "slingshot/Photo";
+    public static final String RESOURCETYPE_PHOTO = "slingshot/Photo";
 
     /** The property containing the tags. */
-    String PROPERTY_SLINGSHOT_TAGS = "slingshot:tags";
+    public static final String PROPERTY_SLINGSHOT_TAGS = "slingshot:tags";
+
+    /** Name of the preview folder */
+    public static final String FOLDER_NAME_PREVIEW = "preview";
+
+    /**
+     * We only include resource which names do not start with a dot
+     * and we also exclude the preview folder.
+     */
+    public static boolean includeAsAlbum(final Resource resource) {
+        final String name = ResourceUtil.getName(resource);
+        if ( name.equals(FOLDER_NAME_PREVIEW) || name.startsWith(".") ) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Exclude all files starting with a dot
+     */
+    public static boolean includeAsMedia(final Resource resource) {
+        final String name = ResourceUtil.getName(resource);
+        if ( name.startsWith(".") ) {
+            return false;
+        }
+        return true;
+    }
 }
