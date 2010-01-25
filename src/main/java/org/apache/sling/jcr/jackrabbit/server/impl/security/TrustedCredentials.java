d4f17cd30b239824dca5618e14b11f87a64f8f6a
Ian Boston <ieb@apache.org>
2010-01-25 15:21:16 +0000

    SLING-1282 Fixed, all the previous commits on this issue that were then reverted have been suashed into a single commit. The first commit listed is the final one fixing problems with impersonation for Admin users.
    Squashed commit of the following:
    
    commit 7b738417cb0276a20039c390571b4c90da69649a
    Author: Ian Boston <ieb@tfd.co.uk>
    Date:   Mon Jan 25 13:05:19 2010 +0000
    
        SLING-1282 Fixed issues where the Principals for the TrustedCredentials were the offical Jackrabbit Principals, and fixed credential callback issues where TrustedCredentials were ignored. This commit passes all build tests.
    
    commit bf0caa466d1f7068d5e2b410bd31ff209610e958
    Author: Ian Boston <ieb@tfd.co.uk>
    Date:   Mon Jan 25 11:26:46 2010 +0000
    
        Revert "Revert "SLING-1282 Need snapshot of JCR Base bundle for the new credentials stuff to work.""
    
        This reverts commit e2511216663b25f1a2250562faae4dc5b4241a99.
    
    commit bcba6e47a72ca0892fc190e32c827edb3ccf99ea
    Author: Ian Boston <ieb@tfd.co.uk>
    Date:   Mon Jan 25 11:26:45 2010 +0000
    
        Revert "Revert "SLING-1282 Set dummy passwords for the default configuration""
    
        This reverts commit 62a512a04f3a7a2c72cac610c422ed49b6dbdc7e.
    
    commit 71c5696829a2edbad8ce5d4d3fcf2f85d0b43a81
    Author: Ian Boston <ieb@tfd.co.uk>
    Date:   Mon Jan 25 11:26:43 2010 +0000
    
        Revert "Revert "SLING-1282 Comment on the password properties not being used by the embedded repository.""
    
        This reverts commit 5874e377d1e6833efd7ff50ca21f04ddb552ec3b.
    
    commit 00f52e69a8ce6400f637647c83ea0cb1cf660077
    Author: Ian Boston <ieb@tfd.co.uk>
    Date:   Mon Jan 25 11:26:42 2010 +0000
    
        Revert "Revert "SLING-1282 added default behaviour back in to allow other SlingRepository implementations to continue to work as before.""
    
        This reverts commit d7428d650e654778dc7213393e310949eb7f1013.
    
    commit bd8a0e97b0f0c7160f7c7d1ba760e349bce971ef
    Author: Ian Boston <ieb@tfd.co.uk>
    Date:   Mon Jan 25 11:26:29 2010 +0000
    
        Revert "Revert "SLING-1282 First pass at using credentials classes, concerned about impersonation.""
    
        This reverts commit 5a2ddc0a885709ef3753b15467b2e7f2329dc22a.
    
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk/bundles/jcr/jackrabbit-server@902837 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/main/java/org/apache/sling/jcr/jackrabbit/server/impl/security/TrustedCredentials.java
@@ -0,0 +1,84 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.sling.jcr.jackrabbit.server.impl.security;
+
+import org.apache.jackrabbit.core.security.authentication.Authentication;
+
+import java.security.Principal;
+
+import javax.jcr.Credentials;
+import javax.jcr.RepositoryException;
+
+/**
+ *
+ */
+public abstract class TrustedCredentials implements Credentials {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 5153578149776402602L;
+
+    private Principal principal;
+    
+    private Authentication authentication;
+
+    /**
+     * @param adminUser
+     */
+    public TrustedCredentials(final String userId) {
+        principal = getPrincipal(userId);
+        authentication = new Authentication() {
+
+            public boolean canHandle(Credentials credentials) {
+                return (credentials instanceof AdministrativeCredentials)
+                        || (credentials instanceof AnonCredentials);
+            }
+
+            public boolean authenticate(Credentials credentials)
+                    throws RepositoryException {
+                return (credentials instanceof AdministrativeCredentials)
+                        || (credentials instanceof AnonCredentials);
+            }
+        };
+    }
+
+    /**
+     * @param userId 
+     * @return
+     */
+    protected abstract Principal getPrincipal(String userId);
+    
+    public Principal getPrincipal() {
+        return principal;
+    }
+    /**
+     * @return
+     */
+    protected Authentication getTrustedAuthentication() {
+        return authentication;
+    }
+
+    /**
+     * @return
+     */
+    public Object getImpersonator() {
+        return null;
+    }
+
+}
