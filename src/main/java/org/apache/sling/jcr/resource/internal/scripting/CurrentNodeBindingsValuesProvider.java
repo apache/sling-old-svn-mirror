a41494824eb2b534ece7822ca0cea108ad54f0dc
Justin Edelson <justin@apache.org>
2010-01-25 20:34:23 +0000

    SLING-1304 - moving currentNode script binding to jcr.resource bundle
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk/bundles/jcr/resource@902967 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/main/java/org/apache/sling/jcr/resource/internal/scripting/CurrentNodeBindingsValuesProvider.java
@@ -0,0 +1,48 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with this
+ * work for additional information regarding copyright ownership. The ASF
+ * licenses this file to You under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.apache.sling.jcr.resource.internal.scripting;
+
+import javax.jcr.Node;
+import javax.script.Bindings;
+
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.scripting.api.BindingsValuesProvider;
+
+/**
+ * BindingsValuesProvider for currentNode object.
+ *
+ * @scr.component immediate="true" metatype="no"
+ * @scr.service
+ *
+ * @scr.property name="service.description" value="CurrentNode BindingsValuesProvider"
+ * @scr.property name="service.vendor" value="The Apache Software Foundation"
+ */
+public class CurrentNodeBindingsValuesProvider implements BindingsValuesProvider {
+
+    /**
+     * {@inheritDoc}
+     */
+    public void addBindings(Bindings bindings) {
+        Resource resource = (Resource) bindings.get("resource");
+        Node node = resource.adaptTo(Node.class);
+        if (node != null) {
+            bindings.put("currentNode", node);
+        }
+
+    }
+
+}
