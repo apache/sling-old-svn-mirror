39a4e2edfa9ffa140e9f4a3437dc596df8f54ef2
Justin Edelson <justin@apache.org>
2010-01-25 19:32:22 +0000

    SLING-1303 SLING-1318 - upgrading SLING-1303 modifications to use generics
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk@902946 13f79535-47bb-0310-9956-ffa450edef68

--- bundles/scripting/core/src/main/java/org/apache/sling/scripting/core/impl/SlingScriptAdapterFactory.java
+++ bundles/scripting/core/src/main/java/org/apache/sling/scripting/core/impl/SlingScriptAdapterFactory.java
@@ -22,7 +22,6 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.Dictionary;
 import java.util.HashMap;
 import java.util.HashSet;
