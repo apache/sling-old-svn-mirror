1daeb230c70fbaefdf4a60461ced37caffc22688
Carsten Ziegeler <cziegeler@apache.org>
2010-02-01 19:37:24 +0000

    SLING-1274 : Make Scala Scripting JSR 223 compliant. Apply patch from Michael Dürig.
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk@905388 13f79535-47bb-0310-9956-ffa450edef68

---
+++ contrib/scripting/scala/script/README.txt
@@ -0,0 +1,29 @@
+Scala Script Engine
+
+JSR 223 compliant Scala scripting engine. The scripting engine supports
+running in an OSGi container or as part of a stand alone application. 
+It has optional support for compiling from/to a JCR repository. 
+
+Getting Started
+===============
+
+This component uses a Maven 2 (http://maven.apache.org/) build
+environment. It requires a Java 5 JDK (or higher) and Maven (http://maven.apache.org/)
+2.2.1 or later. We recommend to use the latest Maven version.
+
+If you have Maven 2 installed, you can compile and
+package the jar using the following command:
+
+    mvn package
+
+See the Maven 2 documentation for other build features.
+
+The latest source code for this component is available in the
+Subversion (http://subversion.tigris.org/) source repository of
+the Apache Software Foundation. If you have Subversion installed,
+you can checkout the latest source using the following command:
+
+    svn checkout http://svn.apache.org/repos/asf/sling/trunk/contrib/scripting/scala/script/
+
+See the Subversion documentation for other source control features.
+
---
+++ contrib/scripting/scala/script/src/test/scala/org/apache/sling/scripting/scala/interpreter/Tests.scala
@@ -0,0 +1,96 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import junit.framework.Assert.{assertEquals, assertFalse, fail}
+
+import java.io.PrintWriter
+
+import javax.script.ScriptException
+
+package org.apache.sling.scripting.scala.interpreter {
+
+/**
+ * Generic test cases. Implementors inject an InterpreterHelper instance.  
+ */  
+trait Tests {
+  var interpreterHelper: InterpreterHelper
+  
+  def testEvalString {
+    val code = "package a { class Testi(vars: TestiVars) { print(1 + 2) }}"
+    assertEquals("3", interpreterHelper.eval("a.Testi", code, Bindings()))
+  }
+
+  def testEvalError {
+    val code = "syntax error"
+    try {
+      interpreterHelper.eval("a.Testi", code, Bindings())
+      fail("Expecting ScriptException")
+    }
+    catch {
+      case _: ScriptException =>  // expected
+    }
+  }
+  
+  def testError {
+    val err = "Some error here";
+    val code = "package a { class Testi(vars: TestiVars) { throw new Error(\"" + err + "\") }}"
+    try {
+      interpreterHelper.eval("a.Testi", code, Bindings())
+      fail("Expecting Exception")
+    }
+    catch {
+      case e: ScriptException if err == e.getCause.getCause.getMessage => // expected
+    }
+  }
+
+  def testScalaInterpreter {
+    val bindings = Bindings()
+    val time = java.util.Calendar.getInstance.getTime
+    bindings.putValue("msg", "Hello world")
+    bindings.putValue("time", time)
+    val code = "package a { class Testi(vars: TestiVars) {import vars._; print(msg + \": \" + time)}}"
+    val result = interpreterHelper.eval("a.Testi", code, bindings)
+    assertEquals("Hello world: " + time, result)
+  }
+  
+  def testCompileExecute {
+    val srcDir = interpreterHelper.srcDir
+    val interpreter = interpreterHelper.interpreter
+
+    val bindings = Bindings()
+    val time = java.util.Calendar.getInstance.getTime
+    bindings.putValue("msg", "Hello world")
+    bindings.putValue("time", time)
+
+    val code = "package a { class Testi(vars: TestiVars) {import vars._; print(msg + \": \" + time)}}"
+    val src = srcDir.fileNamed("Testi.scala")
+    val writer = new PrintWriter(src.output)
+    writer.print(code)
+    writer.close
+
+    val out = new java.io.ByteArrayOutputStream
+    var result = interpreter.compile("a.Testi", src, bindings)
+    assertFalse(result.hasErrors)
+
+    result = interpreter.execute("a.Testi", bindings, None, Some(out))
+    assertFalse(result.hasErrors)
+
+    assertEquals("Hello world: " + time, out.toString)
+  }
+
+}
+
+}
