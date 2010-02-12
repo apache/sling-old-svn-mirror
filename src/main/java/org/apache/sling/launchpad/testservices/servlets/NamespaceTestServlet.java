a2c10db6a7ce0ee4d0d3a3fd4b406043615b6fe5
Justin Edelson <justin@apache.org>
2010-02-12 17:16:42 +0000

    re-adding test for SLING-1366 and interface for SLING-1363
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk/launchpad/test-services@909503 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/main/java/org/apache/sling/launchpad/testservices/servlets/NamespaceTestServlet.java
@@ -0,0 +1,66 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with this
+ * work for additional information regarding copyright ownership. The ASF
+ * licenses this file to You under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.apache.sling.launchpad.testservices.servlets;
+
+import java.io.IOException;
+
+import javax.jcr.RepositoryException;
+import javax.jcr.Session;
+import javax.servlet.ServletException;
+
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.SlingHttpServletResponse;
+import org.apache.sling.api.servlets.SlingSafeMethodsServlet;
+
+/**
+ * Test Servlet which outputs the current namespace mappings.
+ *
+ * @scr.component immediate="true" metatype="no"
+ * @scr.service interface="javax.servlet.Servlet"
+ *
+ * @scr.property name="service.description"
+ *               value="NamespaceMapping Test Servlet"
+ * @scr.property name="service.vendor" value="The Apache Software Foundation"
+ *
+ * @scr.property name="sling.servlet.paths"
+ *               values.0="/testing/NamespaceTestServlet/output"
+ *
+ */
+@SuppressWarnings("serial")
+public class NamespaceTestServlet extends SlingSafeMethodsServlet {
+
+    @Override
+    protected void doGet(SlingHttpServletRequest request, SlingHttpServletResponse response) throws ServletException,
+            IOException {
+        try {
+            response.setContentType("text/plain");
+            Session session = request.getResourceResolver().adaptTo(Session.class);
+
+            response.getWriter().printf("userid=%s", session.getUserID());
+            response.getWriter().println();
+
+            for (String prefix : session.getNamespacePrefixes()) {
+                response.getWriter().printf("%s=%s", prefix, session.getNamespaceURI(prefix));
+                response.getWriter().println();
+            }
+            response.getWriter().flush();
+        } catch (RepositoryException e) {
+            throw new ServletException("Unable to output namespace mappings", e);
+        }
+    }
+
+}
