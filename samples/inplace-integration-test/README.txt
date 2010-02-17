4f88449699ceec5f71af441a9615b6a15dda5739
Justin Edelson <justin@apache.org>
2010-02-17 03:57:20 +0000

    adding sample project for SLING-1364
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk@910815 13f79535-47bb-0310-9956-ffa450edef68

---
+++ samples/inplace-integration-test/README.txt
@@ -0,0 +1,28 @@
+Apache Sling Samples In-Place Integration Test
+
+Provide a sample bundle demonstrating the use of the Maven Launchpad Plugin's ability
+to do in-place integration testing.
+
+Getting Started
+===============
+
+This component uses a Maven 2 (http://maven.apache.org/) build
+environment. It requires a Java 5 JDK (or higher) and Maven (http://maven.apache.org/)
+2.0.7 or later. We recommend to use the latest Maven version.
+
+If you have Maven 2 installed, you can run the sample integration tests
+using the following command:
+
+    mvn verify
+
+See the Maven 2 documentation for other build features.
+
+The latest source code for this component is available in the
+Subversion (http://subversion.tigris.org/) source repository of
+the Apache Software Foundation. If you have Subversion installed,
+you can checkout the latest source using the following command:
+
+    svn checkout http://svn.apache.org/repos/asf/sling/trunk/samples/inplace-integration-test
+
+See the Subversion documentation for other source control features.
+
---
+++ samples/inplace-integration-test/src/test/java/org/apache/sling/samples/itdemo/InitialContentIT.java
@@ -0,0 +1,53 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with this
+ * work for additional information regarding copyright ownership. The ASF
+ * licenses this file to You under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.apache.sling.samples.itdemo;
+
+import java.io.IOException;
+
+import org.apache.sling.commons.testing.integration.HttpTestBase;
+
+/**
+ * A test to ensure that the Initial Content bundle has loaded.
+ */
+public class InitialContentIT extends HttpTestBase {
+
+    public void testIndex() throws IOException {
+        final String expected = "Do not remove this comment, used for Launchpad integration tests";
+        final String content = getContent(HTTP_BASE_URL + "/index.html", CONTENT_TYPE_HTML);
+        assertTrue("Content contains expected marker (" + content + ")", content.contains(expected));
+    }
+
+    public void testRootRedirectProperty() throws IOException {
+        final String expected = "\"sling:resourceType\":\"sling:redirect\"";
+        final String content = getContent(HTTP_BASE_URL + "/.json", CONTENT_TYPE_JSON);
+        assertTrue("Content contains " + expected + " (" + content + ")", content.contains(expected));
+    }
+
+    public void testInitialContentA() throws IOException {
+        final String expected = "42";
+        final String content = getContent(HTTP_BASE_URL + "/sling-test/sling/initial-content-test/marker.txt",
+                CONTENT_TYPE_PLAIN);
+        assertTrue("Content contains " + expected + " (" + content + ")", content.contains(expected));
+    }
+
+    public void testInitialContentB() throws IOException {
+        final String expected = "46";
+        final String content = getContent(HTTP_BASE_URL
+                + "/sling-test/initial-content-folder/folder-content-test/marker.txt", CONTENT_TYPE_PLAIN);
+        assertTrue("Content contains " + expected + " (" + content + ")", content.contains(expected));
+    }
+}
