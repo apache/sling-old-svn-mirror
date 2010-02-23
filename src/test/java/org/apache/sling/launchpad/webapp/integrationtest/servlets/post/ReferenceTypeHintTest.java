7819a5ae96d7732501ecd9769ef1c78f9c46a7cc
Justin Edelson <justin@apache.org>
2010-02-23 19:00:58 +0000

    SLING-1333 - adding multi-valued test post SLING-1407 addition
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk/launchpad/testing@915490 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/sling/launchpad/webapp/integrationtest/servlets/post/ReferenceTypeHintTest.java
+++ src/test/java/org/apache/sling/launchpad/webapp/integrationtest/servlets/post/ReferenceTypeHintTest.java
@@ -19,7 +19,10 @@ package org.apache.sling.launchpad.webapp.integrationtest.servlets.post;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.sling.commons.json.JSONArray;
+import org.apache.sling.commons.json.JSONObject;
 import org.apache.sling.commons.testing.integration.HttpTestBase;
+import org.apache.sling.commons.testing.integration.NameValuePairList;
 import org.apache.sling.servlets.post.SlingPostConstants;
 
 /**
@@ -36,35 +39,44 @@ public class ReferenceTypeHintTest extends HttpTestBase {
     }
 
     public void testReferenceTypes() throws Exception {
-        final Map<String, String> props = new HashMap<String, String>();
-        props.put("a", "");
-        props.put("jcr:mixinTypes", "mix:referenceable");
+        final NameValuePairList props = new NameValuePairList();
+        props.add("a", "");
+        props.add("jcr:mixinTypes", "mix:referenceable");
 
-        final String firstCreatedNodeUrl = testClient.createNode(postUrl + SlingPostConstants.DEFAULT_CREATE_SUFFIX, props);
+        final String firstCreatedNodeUrl = testClient.createNode(postUrl + SlingPostConstants.DEFAULT_CREATE_SUFFIX, props, null, false);
         final String firstUuid = getProperty(firstCreatedNodeUrl, "jcr:uuid");
         final String firstPath = getPath(firstCreatedNodeUrl);
 
-        final String secondCreatedNodeUrl = testClient.createNode(postUrl + SlingPostConstants.DEFAULT_CREATE_SUFFIX, props);
-        final String secondUuid = getProperty(firstCreatedNodeUrl, "jcr:uuid");
-        final String secondPath = getPath(firstCreatedNodeUrl);
+        final String secondCreatedNodeUrl = testClient.createNode(postUrl + SlingPostConstants.DEFAULT_CREATE_SUFFIX, props, null, false);
+        final String secondUuid = getProperty(secondCreatedNodeUrl, "jcr:uuid");
+        final String secondPath = getPath(secondCreatedNodeUrl);
 
         props.clear();
-        props.put("a", firstPath);
-        props.put("a@TypeHint", "Reference");
-        props.put("b", firstPath);
-        props.put("b@TypeHint", "WeakReference");
-        props.put("as", firstPath);
-        props.put("as@TypeHint", "Reference");
-        props.put("bs", firstPath);
-        props.put("bs@TypeHint", "WeakReference");
+        props.add("r", firstPath);
+        props.add("r@TypeHint", "Reference");
+        props.add("w", firstPath);
+        props.add("w@TypeHint", "WeakReference");
+        props.add("rs", firstPath);
+        props.add("rs", secondPath);
+        props.add("rs@TypeHint", "Reference");
+        props.add("ws", firstPath);
+        props.add("ws", secondPath);
+        props.add("ws@TypeHint", "WeakReference");
         final String referencingNodeUrl = testClient.createNode(postUrl + SlingPostConstants.DEFAULT_CREATE_SUFFIX,
-                props);
+                props, null, false);
 
-        String refCreatedValue = getProperty(referencingNodeUrl, "a");
-        String weakrefCreatedValue = getProperty(referencingNodeUrl, "b");
+        String refCreatedValue = getProperty(referencingNodeUrl, "r");
+        String weakrefCreatedValue = getProperty(referencingNodeUrl, "w");
+
+        String[] refCreatedValues = getPropertyArray(referencingNodeUrl, "rs");
+        String[] weakrefCreatedValues = getPropertyArray(referencingNodeUrl, "ws");
 
         assertEquals(firstUuid, refCreatedValue);
         assertEquals(firstUuid, weakrefCreatedValue);
+        assertEquals(firstUuid, refCreatedValues[0]);
+        assertEquals(firstUuid, weakrefCreatedValues[0]);
+        assertEquals(secondUuid, refCreatedValues[1]);
+        assertEquals(secondUuid, weakrefCreatedValues[1]);
     }
 
     private String getPath(String url) {
@@ -73,5 +85,15 @@ public class ReferenceTypeHintTest extends HttpTestBase {
 
     private String getProperty(String url, String name) throws Exception {
         return getContent(url + "/" + name + ".txt", CONTENT_TYPE_PLAIN);
+    }
+
+    private String[] getPropertyArray(String url, String name) throws Exception {
+        JSONObject jo = new JSONObject(getContent(url + "/" + name + ".json", CONTENT_TYPE_JSON));
+        JSONArray arr = jo.getJSONArray(name);
+        String[] result = new String[arr.length()];
+        for (int i = 0; i < arr.length(); i++) {
+            result[i] = arr.getString(i);
+        }
+        return result;
     }
 }
