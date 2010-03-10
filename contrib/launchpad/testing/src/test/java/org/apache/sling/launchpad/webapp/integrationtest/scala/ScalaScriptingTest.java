ca6cb4d43ad0715df6aa2fd64ef4af8651aa44d3
Carsten Ziegeler <cziegeler@apache.org>
2010-03-10 11:39:08 +0000

    SLING-1350 : Many test failures in contrib/launchpad/testing. Applied patch from Michael Dürig
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk@921312 13f79535-47bb-0310-9956-ffa450edef68

--- contrib/launchpad/testing/src/test/java/org/apache/sling/launchpad/webapp/integrationtest/scala/ScalaScriptingTest.java
+++ contrib/launchpad/testing/src/test/java/org/apache/sling/launchpad/webapp/integrationtest/scala/ScalaScriptingTest.java
@@ -16,69 +16,43 @@
  */
 package org.apache.sling.launchpad.webapp.integrationtest.scala;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.io.IOException;
 
 import org.apache.sling.commons.testing.integration.HttpTestBase;
-import org.apache.sling.servlets.post.SlingPostConstants;
 
 public class ScalaScriptingTest extends HttpTestBase {
-    private String testRootUrl;
-    private TestNode testNode;
 
-    @Override
-    protected void setUp() throws Exception {
-        super.setUp();
-
-        final String testRootPath = HTTP_BASE_URL + "/" + getClass().getSimpleName() + "/" + System.currentTimeMillis();
-        testRootUrl = testClient.createNode(testRootPath + SlingPostConstants.DEFAULT_CREATE_SUFFIX, null);
-
-        Map<String, String> nodeProperties = new HashMap<String, String>();
-        nodeProperties.put("jcr:primaryType", "nt:folder");
-
-        // fixme: this is a workaround for the post servlet returning 200 instead of 302
-        // if the path is /var/classes
-        nodeProperties.put(":redirect", "*.json");
-
-        testClient.createNode(HTTP_BASE_URL + "/var/classes", nodeProperties);
-        testNode = new TestNode(testRootPath + "/test", null);
-    }
-
-    @Override
-    protected void tearDown() throws Exception {
-        testClient.delete(testRootUrl);
-        super.tearDown();
-    }
-
-    public void testNoScript() throws Exception {
-        final String content = getContent(testNode.nodeUrl + ".txt", CONTENT_TYPE_PLAIN);
-        assertTrue(content.contains("PlainTextRendererServlet"));
-        assertTrue("Content contains " + testNode.testText + " (" + content + ")", content.contains(testNode.testText));
-    }
-
-    public void testScala() throws Exception {
-        final String toDelete = uploadTestScript(testNode.scriptPath, "scala/rendering-test.scs", "html.scala");
+    public void testIgnore() throws IOException {
         try {
-            checkContent(testNode);
+            String url = HTTP_BASE_URL + "/content/helloworld.html";
+            getContent(url, CONTENT_TYPE_HTML);
         }
-        finally {
-            if(toDelete != null) {
-                testClient.delete(toDelete);
-            }
+        catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        catch (AssertionError e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
         }
     }
 
-    private void checkContent(TestNode node) throws Exception {
-        final String content = getContent(node.nodeUrl + ".html", CONTENT_TYPE_HTML);
-        assertTrue("Scala script executed as expected (" + content + ")", content.contains("<h1>Scala rendering result</h1>"));
+    public void testHelloWorldApp() throws IOException {
+        String url = HTTP_BASE_URL + "/content/helloworld.html";
+        String content = getContent(url, CONTENT_TYPE_HTML);
 
-        final String [] expected = {
-                "using resource.adaptTo:" + node.testText,
-                "using currentNode:" + node.testText,
-        };
-        for(String exp : expected) {
-            assertTrue("Content contains " + exp + "(" + content + ")", content.contains(exp));
-        }
+        assertTrue(content.contains("<h1>Hello World</h1>"));
+        assertTrue(content.contains("My path is /content/helloworld <br></br>"));
+    }
+
+    public void testForumApp() throws IOException {
+        String url = HTTP_BASE_URL + "/content/forum.html";
+        String content = getContent(url, CONTENT_TYPE_HTML);
+
+        assertTrue(content.contains("Welcome to the Scala for Sling forum"));
+        assertTrue(content.contains("(<a href=\"/content/forum/scala4sling.thread.html\">show thread</a>)"));
+        assertTrue(content.contains("(<a href=\"/content/forum/sling.thread.html\">show thread</a>)"));
+        assertTrue(content.contains("<img src=\"/content/forum/sling/logo/jcr:content\"></img>"));
     }
 
 }
