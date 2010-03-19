ba19dbfe09d39b54e4cd8df4df2d6207a6a9df0a
Eric Norman <enorman@apache.org>
2010-03-19 19:31:12 +0000

    SLING-1453 Provide AuthorizablePrivilegesInfo service to help user/group management scripts query what the current user is allowed to do
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk/launchpad/testing@925392 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/sling/launchpad/webapp/integrationtest/userManager/AbstractUserManagerTest.java
+++ src/test/java/org/apache/sling/launchpad/webapp/integrationtest/userManager/AbstractUserManagerTest.java
@@ -16,6 +16,15 @@
  */
 package org.apache.sling.launchpad.webapp.integrationtest.userManager;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.httpclient.Credentials;
+import org.apache.commons.httpclient.NameValuePair;
+import org.apache.commons.httpclient.UsernamePasswordCredentials;
 import org.apache.sling.launchpad.webapp.integrationtest.AbstractAuthenticatedTest;
 
 /**
@@ -23,4 +32,50 @@ import org.apache.sling.launchpad.webapp.integrationtest.AbstractAuthenticatedTe
  */
 public abstract class AbstractUserManagerTest extends AbstractAuthenticatedTest {
 
+	/**
+	 * Helper to assist adding a user to a group
+	 * @param testUserId the user
+	 * @param testGroupId the group
+	 */
+	protected void addUserToGroup(String testUserId, String testGroupId) throws IOException {
+        String postUrl = HTTP_BASE_URL + "/system/userManager/group/" + testGroupId + ".update.html";
+
+		List<NameValuePair> postParams = new ArrayList<NameValuePair>();
+		postParams.add(new NameValuePair(":member", testUserId));
+		
+		Credentials creds = new UsernamePasswordCredentials("admin", "admin");
+		assertAuthenticatedPostStatus(creds, postUrl, HttpServletResponse.SC_OK, postParams, null);
+	}
+
+	/**
+	 * Helper to assist removing a user from a group
+	 * @param testUserId the user
+	 * @param testGroupId the group
+	 */
+	protected void removeUserFromGroup(String testUserId, String testGroupId) throws IOException {
+        String postUrl = HTTP_BASE_URL + "/system/userManager/group/" + testGroupId + ".update.html";
+
+		List<NameValuePair> postParams = new ArrayList<NameValuePair>();
+		postParams.add(new NameValuePair(":member@Delete", testUserId));
+		
+		Credentials creds = new UsernamePasswordCredentials("admin", "admin");
+		assertAuthenticatedPostStatus(creds, postUrl, HttpServletResponse.SC_OK, postParams, null);
+	}
+
+	/**
+	 * Add test user to the 'UserAdmin' group
+	 * @param testUserId the user
+	 */
+	protected void addUserToUserAdminGroup(String testUserId) throws IOException {
+		addUserToGroup(testUserId, "UserAdmin");
+	}
+
+	/**
+	 * Add test user to the 'GroupAdmin' group
+	 * @param testUserId the user
+	 */
+	protected void addUserToGroupAdminGroup(String testUserId) throws IOException {
+		addUserToGroup(testUserId, "GroupAdmin");
+	}
+	
 }
