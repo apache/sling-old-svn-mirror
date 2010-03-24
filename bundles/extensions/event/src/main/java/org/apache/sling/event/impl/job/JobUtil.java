e70084b3570f0ced31bacad9396e51e44e5072f3
Carsten Ziegeler <cziegeler@apache.org>
2010-03-24 14:21:04 +0000

    Put licence header above package statement
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk@927068 13f79535-47bb-0310-9956-ffa450edef68

--- bundles/extensions/event/src/main/java/org/apache/sling/event/impl/job/JobUtil.java
+++ bundles/extensions/event/src/main/java/org/apache/sling/event/impl/job/JobUtil.java
@@ -1,5 +1,3 @@
-package org.apache.sling.event.impl.job;
-
 /*
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
@@ -18,6 +16,8 @@ package org.apache.sling.event.impl.job;
  * specific language governing permissions and limitations
  * under the License.
  */
+package org.apache.sling.event.impl.job;
+
 import java.util.UUID;
 
 import org.apache.sling.event.impl.EventHelper;
