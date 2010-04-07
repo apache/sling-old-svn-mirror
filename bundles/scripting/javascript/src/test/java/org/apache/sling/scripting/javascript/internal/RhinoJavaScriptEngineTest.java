be5d6017989306ae870e48e19851aedd4d767614
Carsten Ziegeler <cziegeler@apache.org>
2010-04-07 12:32:08 +0000

    SLING-1111 : RhinoJavaScriptEngine does not preserve scope between eval()s
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk@931522 13f79535-47bb-0310-9956-ffa450edef68

---
+++ bundles/scripting/javascript/src/test/java/org/apache/sling/scripting/javascript/internal/RhinoJavaScriptEngineTest.java
@@ -0,0 +1,67 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.sling.scripting.javascript.internal;
+
+import javax.script.ScriptEngine;
+import javax.script.ScriptException;
+
+import junit.framework.TestCase;
+
+import org.apache.sling.scripting.javascript.helper.SlingWrapFactory;
+import org.mozilla.javascript.Context;
+import org.mozilla.javascript.ImporterTopLevel;
+import org.mozilla.javascript.Scriptable;
+
+public class RhinoJavaScriptEngineTest extends TestCase {
+
+    public void testPreserveScopeBetweenEvals() throws ScriptException {
+        MockRhinoJavaScriptEngineFactory factory = new MockRhinoJavaScriptEngineFactory();
+        ScriptEngine engine = factory.getScriptEngine();
+        engine.eval("var f = 1");
+        Object result = null;
+        try {
+            result = engine.eval("f += 1");
+        } catch (ScriptException e) {
+            TestCase.fail(e.getMessage());
+        }
+        assertTrue(result instanceof Double);
+        assertEquals(2.0, result);
+    }
+
+    private static class MockRhinoJavaScriptEngineFactory extends RhinoJavaScriptEngineFactory {
+
+        protected SlingWrapFactory wrapFactory;
+
+        @Override
+        public ScriptEngine getScriptEngine() {
+            final Context rhinoContext = Context.enter();
+            Scriptable scope = rhinoContext.initStandardObjects(new ImporterTopLevel(), false);
+            return new RhinoJavaScriptEngine(this, scope);
+        }
+
+        @Override
+        SlingWrapFactory getWrapFactory() {
+            if (wrapFactory == null) {
+                wrapFactory = new SlingWrapFactory();
+            }
+            return wrapFactory;
+        }
+    }
+
+}
