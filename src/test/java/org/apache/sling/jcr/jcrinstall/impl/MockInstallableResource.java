66ad50843c88e9f2cab745b4af9022ea00a4e950
Carsten Ziegeler <cziegeler@apache.org>
2010-06-18 07:37:15 +0000

    SLING-1560 : Improve and clean up code Make DigestUtil private
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk/installer/jcr/jcrinstall@955872 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/sling/jcr/jcrinstall/impl/MockInstallableResource.java
+++ src/test/java/org/apache/sling/jcr/jcrinstall/impl/MockInstallableResource.java
@@ -19,12 +19,16 @@
 package org.apache.sling.jcr.jcrinstall.impl;
 
 import java.io.ByteArrayInputStream;
-import java.io.IOException;
+import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
-import java.security.NoSuchAlgorithmException;
+import java.io.ObjectOutputStream;
+import java.math.BigInteger;
+import java.security.MessageDigest;
 import java.util.Dictionary;
+import java.util.Enumeration;
+import java.util.SortedSet;
+import java.util.TreeSet;
 
-import org.apache.sling.osgi.installer.DigestUtil;
 import org.apache.sling.osgi.installer.InstallableResource;
 import org.apache.sling.osgi.installer.InstallableResourceFactory;
 
@@ -85,13 +89,7 @@ public class MockInstallableResource implements InstallableResource {
         if ( digest != null ) {
             this.digest = digest;
         } else {
-            try {
-                this.digest = DigestUtil.computeDigest(d);
-            } catch (NoSuchAlgorithmException e) {
-                throw new RuntimeException(e);
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
+            this.digest = computeDigest(d);
         }
         this.d = d;
     }
@@ -127,5 +125,41 @@ public class MockInstallableResource implements InstallableResource {
 
     public String getUrl() {
         return this.uri;
+    }
+
+    /** The digest to be used. */
+    private static final String DIGEST_TYPE = "MD5";
+
+    /** convert digest to readable string (http://www.javalobby.org/java/forums/t84420.html) */
+    private static String digestToString(MessageDigest d) {
+        final BigInteger bigInt = new BigInteger(1, d.digest());
+        return new String(bigInt.toString(16));
+    }
+
+    /** Compute digest on all keys of supplied data */
+    private static String computeDigest(Dictionary<String, Object> data) {
+        try {
+            final MessageDigest d = MessageDigest.getInstance(DIGEST_TYPE);
+            final ByteArrayOutputStream bos = new ByteArrayOutputStream();
+            final ObjectOutputStream oos = new ObjectOutputStream(bos);
+
+            final SortedSet<String> sortedKeys = new TreeSet<String>();
+            if(data != null) {
+                for(Enumeration<String> e = data.keys(); e.hasMoreElements(); ) {
+                    final String key = e.nextElement();
+                    sortedKeys.add(key);
+                }
+            }
+            for(String key : sortedKeys) {
+                oos.writeObject(key);
+                oos.writeObject(data.get(key));
+            }
+
+            bos.flush();
+            d.update(bos.toByteArray());
+            return digestToString(d);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
