753eb7258511af23ba29abaaa2971e01e514c313
Justin Edelson <justin@apache.org>
2010-07-08 20:53:59 +0000

    SLING-1054 - adding servlet and resources archetypes
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk/maven/archetypes/servlet@961934 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/main/resources/archetype-resources/src/test/java/ByPathServletTest.java
@@ -0,0 +1,17 @@
+package ${package};
+
+import junit.framework.TestCase;
+
+/**
+ * Unit test for By Path Servlet.
+ */
+public class ByPathServletTest extends TestCase {
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testByPathServlet() {
+        assertTrue(true);
+    }
+    
+}
