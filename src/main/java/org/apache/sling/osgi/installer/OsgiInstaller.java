7f2ae7a45de1ea014bc2e6a39074eadc2924b4f2
Carsten Ziegeler <cziegeler@apache.org>
2010-08-10 18:35:39 +0000

    Fix test cases
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk/installer/osgi/installer@984156 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/sling/osgi/installer/OsgiInstaller.java
+++ src/main/java/org/apache/sling/osgi/installer/OsgiInstaller.java
@@ -20,41 +20,47 @@ package org.apache.sling.osgi.installer;
 
 import java.util.Collection;
 
-/** OSGi Service that installs/updates/removes InstallableData
- * 	in the OSGi framework.
+/**
+ * OSGi Service that installs/updates/removes installable data
+ * {@link InstallableResource} in the OSGi framework.
  *
- *  The client can register a number of such resources, and the
- *  installer decides based on the resource weights, bundle version
- *  numbers, etc. which ones are actually installed.
+ * The client can register a number of such resources, and the
+ * installer decides based on the resource weights, bundle version
+ * numbers, etc. which ones are actually installed.
  *
- *	An InstallableResource can be a bundle, a configuration, and later
- *	we might support deployment packages as well.
+ * An InstallableResource can be a bundle, a configuration, and later
+ * we might support deployment packages as well.
  */
 public interface OsgiInstaller {
 
-	/** Provide the installer with the complete list of installable
-	 * 	resources for a given client.
+	/**
+	 * Provide the installer with the complete list of installable
+	 * resources for a given client.
 	 *
-	 * 	Client must call this at startup and/or when the installer
-	 * 	service becomes available. The installer stores the list of
-	 * 	previously registered/added resources, compares with the new
-	 * 	list and removes resources that have disappeared.
+	 * Client must call this at startup and/or when the installer
+	 * service becomes available. The installer stores the list of
+	 * previously registered/added resources, compares with the new
+	 * list and removes resources that have disappeared.
 	 *
-     *  Invalid resources are ignored.
+     * Invalid resources are ignored.
 	 *
-	 * 	@param data the list of available resources
-	 * 	@param urlScheme identifies the client. All URLs of the supplied data
-	 * 		must use this scheme
+     * @param urlScheme identifies the client.
+	 * @param data the list of available resources
 	 */
 	void registerResources(String urlScheme, Collection<InstallableResource> data);
 
-	/** Inform the installer that a resource is available for installation.
-	 * 	also called if the resource has been modified since it was registered.
-	 *  Invalid resources are ignored.
+	/**
+	 * Inform the installer that a resource is available for installation.
+	 * also called if the resource has been modified since it was registered.
+	 * Invalid resources are ignored.
+     * @param urlScheme identifies the client.
 	 */
 	void addResource(String urlScheme, InstallableResource r);
 
-	/** Inform the installer that a resource is no longer available
-	 * 	@param r an empty InstallableResource, isEmpty() must return true */
+	/**
+	 * Inform the installer that a resource is no longer available
+     * @param urlScheme identifies the client.
+	 * @param id The identifier for the resource
+	 */
 	void removeResource(String urlScheme, String id);
 }
