85f6bc618f6e7a8ef00862d4720e6c0a0e196c64
Felix Meschberger <fmeschbe@apache.org>
2010-08-11 14:04:05 +0000

    SLING-1640 Add ResourceResolver.copy(Map) method to the API and add some JavaDoc to the ResourceResolverFactory
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk/bundles/api@984412 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/sling/api/resource/ResourceResolverFactory.java
+++ src/main/java/org/apache/sling/api/resource/ResourceResolverFactory.java
@@ -21,26 +21,65 @@ package org.apache.sling.api.resource;
 import java.util.Map;
 
 /**
- * The <code>ResourceResolverFactory</code> defines the service API to
- * get and create <code>ResourceResolver</code>s.
+ * The <code>ResourceResolverFactory</code> defines the service API to get and
+ * create <code>ResourceResolver</code>s.
  * <p>
- * As soon as the resource resolver is not used anymore, {@link ResourceResolver#close()}
- * should be called.
+ * As soon as the resource resolver is not used anymore,
+ * {@link ResourceResolver#close()} should be called.
  *
  * @since 2.1
  */
 public interface ResourceResolverFactory {
 
-    /** If this property is set in the authentication information which is passed
-     * into {@link #getResourceResolver(Map)} or {@link #getAdministrativeResourceResolver(Map)}
-     * then after a successful authentication attempt, a sudo to the provided
-     * user id is tried.
+    /**
+     * If this property is set in the authentication information which is passed
+     * into {@link #getResourceResolver(Map)} or
+     * {@link #getAdministrativeResourceResolver(Map)} then after a successful
+     * authentication attempt, a sudo to the provided user id is tried.
      */
     String SUDO_USER_ID = "sudo.user.id";
 
+    /**
+     * Returns a new {@link ResourceResolver} instance with further
+     * configuration taken from the given <code>authenticationInfo</code> map.
+     * Generally this map will contain a user name and password to authenticate.
+     * <p>
+     * If the <code>authenticationInfo</code> map is <code>null</code> the
+     * <code>ResourceResolver</code> returned will generally not be
+     * authenticated and only provide minimal privileges, if any at all.
+     *
+     * @param authenticationInfo A map of further credential information which
+     *            may be used by the implementation to parametrize how the
+     *            resource resolver is created. This may be <code>null</code>.
+     * @return A {@link ResourceResolver} according to the
+     *         <code>authenticationInfo</code>.
+     * @throws LoginException If an error occurrs creating the new
+     *             <code>ResourceResolver</code> with the provided credential
+     *             data.
+     */
     ResourceResolver getResourceResolver(Map<String, Object> authenticationInfo)
-    throws LoginException;
+            throws LoginException;
 
-    ResourceResolver getAdministrativeResourceResolver(Map<String, Object> authenticationInfo)
-    throws LoginException;
+    /**
+     * Returns a new {@link ResourceResolver} instance with administrative
+     * privileges with further configuration taken from the given
+     * <code>authenticationInfo</code> map.
+     * <p>
+     * Note, that if the <code>authenticationInfo</code> map contains the
+     * {@link #SUDO_USER_ID} attribute the <code>ResourceResolver</code>
+     * returned will only have administrative privileges if the user identified
+     * by the property has administrative privileges.
+     *
+     * @param authenticationInfo A map of further credential information which
+     *            may be used by the implementation to parametrize how the
+     *            resource resolver is created. This may be <code>null</code>.
+     * @return A {@link ResourceResolver} with administrative privileges unless
+     *         the {@link #SUDO_USER_ID} was set in the
+     *         <code>authenticationInfo</code>.
+     * @throws LoginException If an error occurrs creating the new
+     *             <code>ResourceResolver</code> with the provided credential
+     *             data.
+     */
+    ResourceResolver getAdministrativeResourceResolver(
+            Map<String, Object> authenticationInfo) throws LoginException;
 }
