a1ae80c41bc38ed6341c8291263e8fc4c4b019c5
Justin Edelson <justin@apache.org>
2010-08-17 17:55:17 +0000

    SLING-1662 - creating gsp scripting engine
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk/bundles/extensions/groovy@986420 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/main/java/org/apache/sling/extensions/groovy/scripting/internal/GSPScriptEngine.java
@@ -0,0 +1,69 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with this
+ * work for additional information regarding copyright ownership. The ASF
+ * licenses this file to You under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.apache.sling.extensions.groovy.scripting.internal;
+
+import groovy.lang.Writable;
+import groovy.text.GStringTemplateEngine;
+import groovy.text.Template;
+import groovy.text.TemplateEngine;
+
+import java.io.IOException;
+import java.io.Reader;
+
+import javax.script.Bindings;
+import javax.script.ScriptContext;
+import javax.script.ScriptEngineFactory;
+import javax.script.ScriptException;
+
+import org.apache.sling.scripting.api.AbstractSlingScriptEngine;
+
+/**
+ * The actual GSP Script Engine, which simply wraps Groovy's 
+ */
+public class GSPScriptEngine extends AbstractSlingScriptEngine {
+
+    private TemplateEngine templateEngine;
+
+    public GSPScriptEngine(ScriptEngineFactory scriptEngineFactory) {
+        super(scriptEngineFactory);
+        this.templateEngine = new GStringTemplateEngine();
+    }
+    
+    public Object eval(Reader reader, ScriptContext ctx) throws ScriptException {
+        Template template = null;
+        try {
+            template = templateEngine.createTemplate(reader);
+        } catch (IOException e) {
+            throw new ScriptException("Unable to compile GSP script: " + e.getMessage());
+        } catch (ClassNotFoundException e) {
+            throw new ScriptException("Unable to compile GSP script: " + e.getMessage());
+        }
+
+        Bindings bindings = ctx.getBindings(ScriptContext.ENGINE_SCOPE);
+
+        Writable result = template.make(bindings);
+
+        try {
+            result.writeTo(ctx.getWriter());
+        } catch (IOException e) {
+            throw new ScriptException("Unable to write result of script execution: " + e.getMessage());
+        }
+
+        return null;
+    }
+
+}
