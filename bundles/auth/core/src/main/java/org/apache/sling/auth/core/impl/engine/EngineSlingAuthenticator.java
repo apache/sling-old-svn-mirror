f35308fed9b30d12b631f59bdd7b6619f262ff5c
Felix Meschberger <fmeschbe@apache.org>
2010-08-23 07:40:36 +0000

    SLING-1679 Use Apache Felix SCR Annotations (instead of @scr JavaDoc tags)
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk@988014 13f79535-47bb-0310-9956-ffa450edef68

--- bundles/auth/core/src/main/java/org/apache/sling/auth/core/impl/engine/EngineSlingAuthenticator.java
+++ bundles/auth/core/src/main/java/org/apache/sling/auth/core/impl/engine/EngineSlingAuthenticator.java
@@ -21,27 +21,30 @@ package org.apache.sling.auth.core.impl.engine;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.felix.scr.annotations.Component;
+import org.apache.felix.scr.annotations.Properties;
+import org.apache.felix.scr.annotations.Property;
+import org.apache.felix.scr.annotations.Reference;
+import org.apache.felix.scr.annotations.Service;
 import org.apache.sling.engine.auth.Authenticator;
 import org.apache.sling.engine.auth.NoAuthenticationHandlerException;
+import org.osgi.framework.Constants;
 
 /**
  * The <code>EngineSlingAuthenticator</code> class is a simple proxy service
  * providing the old Sling Engine {@link Authenticator} service calling into the
  * new standalone Apache Sling
  * {@link org.apache.sling.auth.core.AuthenticationSupport} service.
- *
- * @scr.component metatype="no"
- * @scr.service interface="org.apache.sling.engine.auth.Authenticator"
- * @scr.property name="service.description"
- *               value="Apache Sling Request Authenticator (Legacy Bridge)"
- * @scr.property name="service.vendor" value="The Apache Software Foundation"
  */
+@Component()
+@Service(value = Authenticator.class)
+@Properties( {
+    @Property(name = Constants.SERVICE_DESCRIPTION, value = "Apache Sling Request Authenticator (Legacy Bridge)"),
+    @Property(name = Constants.SERVICE_VENDOR, value = "The Apache Software Foundation") })
 @SuppressWarnings("deprecation")
 public class EngineSlingAuthenticator implements Authenticator {
 
-    /**
-     * @scr.reference
-     */
+    @Reference
     private org.apache.sling.api.auth.Authenticator slingAuthenticator;
 
     public void login(HttpServletRequest request, HttpServletResponse response) {
