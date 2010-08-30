6b0ea19209491a62a6f057b1ec395b35efc81d22
Justin Edelson <justin@apache.org>
2010-08-30 15:59:46 +0000

    SLING-1707 - committing correct version of NodeType printer
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk/bundles/jcr/webconsole@990835 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/sling/jcr/webconsole/internal/NodeTypeConfigurationPrinter.java
+++ src/main/java/org/apache/sling/jcr/webconsole/internal/NodeTypeConfigurationPrinter.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with this
+ * work for additional information regarding copyright ownership. The ASF
+ * licenses this file to You under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package org.apache.sling.jcr.webconsole.internal;
 
 import java.io.PrintWriter;
@@ -22,10 +38,6 @@ import org.apache.felix.webconsole.ConfigurationPrinter;
 import org.apache.felix.webconsole.ModeAwareConfigurationPrinter;
 import org.apache.sling.jcr.api.SlingRepository;
 
-/**
- * @author justin
- *
- */
 /**
  * A Felix WebConsole ConfigurationPrinter which outputs the current JCR
  * nodetypes.
@@ -161,13 +173,13 @@ public class NodeTypeConfigurationPrinter implements ModeAwareConfigurationPrint
 
     private void stopBold(PrintWriter pw, String mode) {
         if (ConfigurationPrinter.MODE_WEB.equals(mode)) {
-            pw.print("</b>");
+         //   pw.print("</b>");
         }
     }
 
     private void startBold(PrintWriter pw, String mode) {
         if (ConfigurationPrinter.MODE_WEB.equals(mode)) {
-            pw.print("<b>");
+          //  pw.print("<b>");
         }
     }
 
