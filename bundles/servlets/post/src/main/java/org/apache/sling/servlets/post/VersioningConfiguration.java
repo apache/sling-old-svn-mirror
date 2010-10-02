899f5ab9156950c473c9e0dd1c7c6ee9f574b19b
Felix Meschberger <fmeschbe@apache.org>
2010-10-02 17:34:56 +0000

    SLING-1796 By default do not checkout checked-in nodes before applying modifications. This is to not enable modifications which were not possible before adding versioning support. This may still be overwritten by configuration to automatically checkout checked-in nodes before modification.
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk@1003843 13f79535-47bb-0310-9956-ffa450edef68

--- bundles/servlets/post/src/main/java/org/apache/sling/servlets/post/VersioningConfiguration.java
+++ bundles/servlets/post/src/main/java/org/apache/sling/servlets/post/VersioningConfiguration.java
@@ -5,9 +5,9 @@
  * licenses this file to You under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -22,11 +22,11 @@ package org.apache.sling.servlets.post;
  * nodes are handled in the post servlet.
  */
 public class VersioningConfiguration implements Cloneable {
-    
-    private boolean autoCheckout = true;
-    
+
+    private boolean autoCheckout = false;
+
     private boolean checkinOnNewVersionableNode = false;
-    
+
     private boolean autoCheckin = true;
 
     @Override
@@ -61,5 +61,5 @@ public class VersioningConfiguration implements Cloneable {
     public void setCheckinOnNewVersionableNode(boolean checkinOnNewVersionableNode) {
         this.checkinOnNewVersionableNode = checkinOnNewVersionableNode;
     }
-    
+
 }
