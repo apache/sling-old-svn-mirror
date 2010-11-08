6d6a57aab086aacc645103c8bb76ce2b9981119f
Justin Edelson <justin@apache.org>
2010-11-08 21:47:57 +0000

    SLING-1863 - upgrading GWT to 2.1.0
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk@1032750 13f79535-47bb-0310-9956-ffa450edef68

--- contrib/extensions/gwt/servlet/src/main/java/org/apache/sling/extensions/gwt/user/server/rpc/SlingRemoteServiceServlet.java
+++ contrib/extensions/gwt/servlet/src/main/java/org/apache/sling/extensions/gwt/user/server/rpc/SlingRemoteServiceServlet.java
@@ -24,6 +24,7 @@ import com.google.gwt.user.server.rpc.SerializationPolicy;
 import com.google.gwt.user.server.rpc.SerializationPolicyLoader;
 import org.osgi.framework.Bundle;
 
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 import java.io.InputStream;
@@ -43,6 +44,101 @@ import java.text.ParseException;
  * </code>
  */
 public class SlingRemoteServiceServlet extends RemoteServiceServlet {
+    
+    // This is a verbatim copy of the method of the same signature from
+    // RemoteServiceServlet, with one exception (noted inline) which loads
+    // the policy file from the bundle, as Sling doesn't support
+    // ServletContext.getResourceAsStream().
+    static SerializationPolicy loadSerializationPolicy(HttpServlet servlet,
+        HttpServletRequest request, String moduleBaseURL, String strongName,
+        Bundle bundle) {
+      // The request can tell you the path of the web app relative to the
+      // container root.
+      String contextPath = request.getContextPath();
+
+      String modulePath = null;
+      if (moduleBaseURL != null) {
+        try {
+          modulePath = new URL(moduleBaseURL).getPath();
+        } catch (MalformedURLException ex) {
+          // log the information, we will default
+          servlet.log("Malformed moduleBaseURL: " + moduleBaseURL, ex);
+        }
+      }
+
+      SerializationPolicy serializationPolicy = null;
+
+      /*
+       * Check that the module path must be in the same web app as the servlet
+       * itself. If you need to implement a scheme different than this, override
+       * this method.
+       */
+      if (modulePath == null || !modulePath.startsWith(contextPath)) {
+        String message = "ERROR: The module path requested, "
+            + modulePath
+            + ", is not in the same web application as this servlet, "
+            + contextPath
+            + ".  Your module may not be properly configured or your client and server code maybe out of date.";
+        servlet.log(message, null);
+      } else {
+        // Strip off the context path from the module base URL. It should be a
+        // strict prefix.
+        String contextRelativePath = modulePath.substring(contextPath.length());
+
+        String serializationPolicyFilePath = SerializationPolicyLoader.getSerializationPolicyFileName(contextRelativePath
+            + strongName);
+
+        // BEGIN - REMOVE CODE
+        // Open the RPC resource file and read its contents.
+        // InputStream is = servlet.getServletContext().getResourceAsStream(
+        //    serializationPolicyFilePath);
+        // END - REMOVE CODE
+        
+        // BEGIN - NEW CODE
+        InputStream is = null;
+        // if the bundle was set by the extending class, load the resource from it instead of the servlet context
+        if (bundle != null) {
+            try {
+                is = bundle.getResource(serializationPolicyFilePath).openStream();
+            } catch (IOException e) {
+                //ignore
+            }
+        } else {
+            is = servlet.getServletContext().getResourceAsStream(serializationPolicyFilePath);
+        }
+        // END - NEW CODE
+        
+        try {
+          if (is != null) {
+            try {
+              serializationPolicy = SerializationPolicyLoader.loadFromStream(is,
+                  null);
+            } catch (ParseException e) {
+              servlet.log("ERROR: Failed to parse the policy file '"
+                  + serializationPolicyFilePath + "'", e);
+            } catch (IOException e) {
+              servlet.log("ERROR: Could not read the policy file '"
+                  + serializationPolicyFilePath + "'", e);
+            }
+          } else {
+            String message = "ERROR: The serialization policy file '"
+                + serializationPolicyFilePath
+                + "' was not found; did you forget to include it in this deployment?";
+            servlet.log(message);
+          }
+        } finally {
+          if (is != null) {
+            try {
+              is.close();
+            } catch (IOException e) {
+              // Ignore this error
+            }
+          }
+        }
+      }
+
+      return serializationPolicy;
+    }
 
     /**
      * The <code>org.osgi.framework.Bundle</code> to load resources from.
@@ -97,107 +193,24 @@ public class SlingRemoteServiceServlet extends RemoteServiceServlet {
     }
 
     /**
-     * Gets the {@link com.google.gwt.user.server.rpc.SerializationPolicy} for given module base URL and strong
+     * Gets the {@link SerializationPolicy} for given module base URL and strong
      * name if there is one.
-     * <p/>
-     * Override this method to provide a {@link com.google.gwt.user.server.rpc.SerializationPolicy} using an
+     * 
+     * Override this method to provide a {@link SerializationPolicy} using an
      * alternative approach.
-     * <p/>
-     * This method has been overriden, so that the serialization policy can be properly loaded as a bundle entry,
-     * as Sling does not support <code>ServletContext.getResourceAsStream()</code>.
-     *
-     * @param request       the HTTP request being serviced
+     * 
+     * @param request the HTTP request being serviced
      * @param moduleBaseURL as specified in the incoming payload
-     * @param strongName    a strong name that uniquely identifies a serialization
-     *                      policy file
-     * @return a {@link com.google.gwt.user.server.rpc.SerializationPolicy} for the given module base URL and
+     * @param strongName a strong name that uniquely identifies a serialization
+     *          policy file
+     * @return a {@link SerializationPolicy} for the given module base URL and
      *         strong name, or <code>null</code> if there is none
      */
-    @Override
-    protected SerializationPolicy doGetSerializationPolicy(HttpServletRequest request, String moduleBaseURL, String strongName) {
-
-        // The request can tell you the path of the web app relative to the
-        // container root.
-        String contextPath = request.getContextPath();
-
-        String modulePath = null;
-        if (moduleBaseURL != null) {
-            try {
-                modulePath = new URL(moduleBaseURL).getPath();
-            } catch (MalformedURLException ex) {
-                // log the information, we will default
-                getServletContext().log("Malformed moduleBaseURL: " + moduleBaseURL, ex);
-            }
-        }
-
-        SerializationPolicy serializationPolicy = null;
-
-        /*
-        * Check that the module path must be in the same web app as the servlet
-        * itself. If you need to implement a scheme different than this, override
-        * this method.
-        */
-        if (modulePath == null || !modulePath.startsWith(contextPath)) {
-            String message = "ERROR: The module path requested, "
-                    + modulePath
-                    + ", is not in the same web application as this servlet, "
-                    + contextPath
-                    + ".  Your module may not be properly configured or your client and server code maybe out of date.";
-            getServletContext().log(message);
-        } else {
-            // Strip off the context path from the module base URL. It should be a
-            // strict prefix.
-            String contextRelativePath = modulePath.substring(contextPath.length());
-
-            String serializationPolicyFilePath = SerializationPolicyLoader.getSerializationPolicyFileName(contextRelativePath
-                    + strongName);
-
-            // Open the RPC resource file read its contents.
-            InputStream is = null;
-            // if the bundle was set by the extending class, load the resource from it instead of the servlet context
-            if (bundle != null) {
-                try {
-                    is = bundle.getResource(serializationPolicyFilePath).openStream();
-                } catch (IOException e) {
-                    //ignore
-                }
-            } else {
-                is = getServletContext().getResourceAsStream(
-                        serializationPolicyFilePath);
-            }
-            try {
-                if (is != null) {
-                    try {
-                        serializationPolicy = SerializationPolicyLoader.loadFromStream(is, null);
-                    } catch (ParseException e) {
-                        getServletContext().log(
-                                "ERROR: Failed to parse the policy file '"
-                                        + serializationPolicyFilePath + "'", e);
-                    } catch (IOException e) {
-                        getServletContext().log(
-                                "ERROR: Could not read the policy file '"
-                                        + serializationPolicyFilePath + "'", e);
-                    }
-                } else {
-                    String message = "ERROR: The serialization policy file '"
-                            + serializationPolicyFilePath
-                            + "' was not found; did you forget to include it in this deployment?";
-                    getServletContext().log(message);
-                }
-            } finally {
-                if (is != null) {
-                    try {
-                        is.close();
-                    } catch (IOException e) {
-                        // Ignore this error
-                    }
-                }
-            }
-        }
-
-        return serializationPolicy;
+    protected SerializationPolicy doGetSerializationPolicy(
+        HttpServletRequest request, String moduleBaseURL, String strongName) {
+      return loadSerializationPolicy(this, request, moduleBaseURL, strongName, bundle);
     }
-
+    
     /**
      * Allows the extending OSGi service to set the bundle it is part of. The bundle is used to provide access
      * to the policy file otherwise loaded by <code>getServletContext().getResourceAsStream()</code> which is not
