cc15219da916d831ee6f81ee92d997cb130578e7
Justin Edelson <justin@apache.org>
2011-01-17 02:41:32 +0000

    SLING-1932 - fixing SLING-1932. if requestContextPath is "/", make it "". also adding a unit test
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk@1059730 13f79535-47bb-0310-9956-ffa450edef68

---
+++ launchpad/integration-tests/src/main/java/org/apache/sling/launchpad/webapp/integrationtest/login/FormGenerationTest.java
@@ -0,0 +1,49 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with this
+ * work for additional information regarding copyright ownership. The ASF
+ * licenses this file to You under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.apache.sling.launchpad.webapp.integrationtest.login;
+
+import java.util.Arrays;
+
+import junit.framework.TestCase;
+
+import org.apache.commons.httpclient.NameValuePair;
+import org.apache.sling.commons.testing.integration.HttpTestBase;
+
+/**
+ * Test of form generation from auth.core.
+ */
+public class FormGenerationTest extends HttpTestBase {
+
+    public void testSelectorForm() throws Exception {
+        String contextPath = getContextPath(HTTP_BASE_URL);
+        String content = getContent(HTTP_BASE_URL + "/system/sling/selector/login", CONTENT_TYPE_HTML,
+                Arrays.asList(new NameValuePair("resource", "/")), 200);
+
+        assertTrue("form action is not correct.", content.contains("action=\"" + contextPath + "/j_security_check\""));
+    }
+
+    private static String getContextPath(String baseURL) {
+        // get the index of the first slash after http:// or https://
+        int idx = baseURL.indexOf('/', 8);
+        if (idx == -1) {
+            return "";
+        } else {
+            return baseURL.substring(idx);
+        }
+    }
+
+}
