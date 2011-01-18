98840cb5b94bd2642cd7d83596c05f69773bf196
Carsten Ziegeler <cziegeler@apache.org>
2011-01-18 18:02:03 +0000

    SLING-1935 :  Remove dependency to web console
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk/bundles/extensions/bundleresource@1060490 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/sling/bundleresource/impl/Activator.java
+++ src/main/java/org/apache/sling/bundleresource/impl/Activator.java
@@ -18,7 +18,6 @@
  */
 package org.apache.sling.bundleresource.impl;
 
-import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -38,26 +37,6 @@ public class Activator implements BundleActivator, BundleListener {
      */
     public static final String BUNDLE_RESOURCE_ROOTS = "Sling-Bundle-Resources";
 
-    /**
-     * Fully qualified name of the Web Console Plugin class. This class will be
-     * loaded dynamically to prevent issues if the Felix Web Console is not
-     * installed in the system (value is
-     * "org.apache.sling.bundleresource.impl.BundleResourceWebConsolePlugin").
-     */
-    private static final String CONSOLE_PLUGIN_CLASS = "org.apache.sling.bundleresource.impl.BundleResourceWebConsolePlugin";
-
-    /**
-     * Name of the initialization method to call on the Web Console Plugin class
-     * (value is "initPlugin").
-     */
-    private static final String METHOD_INIT = "initPlugin";
-
-    /**
-     * Name of the shutdown method to call on the Web Console Plugin class
-     * (value is "destroyPlugin").
-     */
-    private static final String METHOD_DESTROY = "destroyPlugin";
-
     /** default log */
     private final Logger log = LoggerFactory.getLogger(getClass());
 
@@ -65,7 +44,10 @@ public class Activator implements BundleActivator, BundleListener {
 
     private BundleContext bundleContext;
 
-    public void start(BundleContext context) throws Exception {
+    /**
+     * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+     */
+    public void start(final BundleContext context) throws Exception {
 
         this.bundleContext = context;
 
@@ -85,15 +67,14 @@ public class Activator implements BundleActivator, BundleListener {
                 t);
         }
 
-        // hackery thing to prevent problems if the web console is not present
-        callMethod(CONSOLE_PLUGIN_CLASS, METHOD_INIT,
-            new Class<?>[] { BundleContext.class }, new Object[] { context });
-
+        BundleResourceWebConsolePlugin.initPlugin(context);
     }
 
-    public void stop(BundleContext context) throws Exception {
-        // hackery thing to prevent problems if the web console is not present
-        callMethod(CONSOLE_PLUGIN_CLASS, METHOD_DESTROY, null, null);
+    /**
+     * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+     */
+    public void stop(final BundleContext context) throws Exception {
+        BundleResourceWebConsolePlugin.destroyPlugin();
 
         context.removeBundleListener(this);
         this.bundleContext = null;
@@ -103,7 +84,7 @@ public class Activator implements BundleActivator, BundleListener {
      * Loads and unloads any components provided by the bundle whose state
      * changed. If the bundle has been started, the components are loaded. If
      * the bundle is about to stop, the components are unloaded.
-     * 
+     *
      * @param event The <code>BundleEvent</code> representing the bundle state
      *            change.
      */
@@ -148,39 +129,6 @@ public class Activator implements BundleActivator, BundleListener {
                 "removeBundleResourceProvider: Unregistering resources for bundle {}/{}",
                 new Object[] { bundle.getSymbolicName(), bundle.getBundleId() });
             brp.unregisterService();
-        }
-    }
-
-    /**
-     * Helper method to call the static method <code>methodName</code> on the
-     * class <code>clazzName</code> with the given <code>args</code>. This
-     * method operates exclusively using reflection to prevent any issues if the
-     * class cannot be loaded.
-     * <p>
-     * The goal is to enable running the bundle resource provider without a hard
-     * dependency on the Felix Web Console.
-     * 
-     * @param clazzName The fully qualified name of the class whose static
-     *            method is to be called.
-     * @param methodName The name of the method to call. This method must be
-     *            declared in the given class.
-     * @param argTypes The types of arguments of the methods to be able to find
-     *            the method. This may be <code>null</code> if the method has
-     *            no arguments.
-     * @param args The actual arguments to the method. This may be
-     *            <code>null</code> if the method has no arguments.
-     */
-    private void callMethod(String clazzName, String methodName,
-            Class<?>[] argTypes, Object[] args) {
-        try {
-            Class<?> clazz = getClass().getClassLoader().loadClass(clazzName);
-            Method method = clazz.getDeclaredMethod(methodName, argTypes);
-            if (!method.isAccessible()) {
-                method.setAccessible(true);
-            }
-            method.invoke(null, args);
-        } catch (Throwable t) {
-            // ignore anything
         }
     }
 }
