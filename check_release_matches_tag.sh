3e99c6c5bcec7969349a918e7fb5e169a32f907e
Bertrand Delacretaz <bdelacretaz@apache.org>
2011-02-15 12:58:34 +0000

    add my release checking script
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk@1070875 13f79535-47bb-0310-9956-ffa450edef68

---
+++ check_release_matches_tag.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+# check that a Sling staged release matches the corresponding svn tags
+#
+# usage:
+#  sh check_release_matches_tag.sh 004 /tmp/sling-staging
+#
+BASE=$2/$1/org/apache/sling
+TAGBASE=http://svn.apache.org/repos/asf/sling/tags/
+
+function fail() {
+	echo $* >&2
+	exit 1
+}
+
+function check() {
+       TAG=$TAGBASE/$1
+       ZIP=$PWD/$2
+       WORKDIR=workdir/$1/$(date +%s)
+       CUR=$PWD
+       echo
+       echo "Checking $ZIP against $TAG"
+       mkdir -p $WORKDIR
+       cd $WORKDIR > /dev/null
+       unzip $ZIP > /dev/null
+       ZIPDIR=$PWD/$(ls)
+       svn export $TAG svnexport > /dev/null
+       cd svnexport > /dev/null
+       diff -r . $ZIPDIR
+       cd $CUR
+
+}
+
+CURDIR=`pwd`
+cd $BASE || fail "Cannot cd to $BASE"
+
+find . -name *.zip | cut -c 3- | sed 's/\// /g' | while read line
+do
+       set $line
+       TAG=${1}-${2}
+       ZIP=${1}/${2}/${3}
+       check $TAG $ZIP
+done
+md5 $(find . -name *.zip)
+cd $CURDIR
