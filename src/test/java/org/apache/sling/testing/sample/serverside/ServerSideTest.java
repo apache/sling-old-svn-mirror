26729f51321ff0142ec700504799b8c92cf3e6d0
Bertrand Delacretaz <bdelacretaz@apache.org>
2011-02-17 11:25:22 +0000

    SLING-1981 - install additional bundles and prepare for running server-side tests (requires up-to-date stanbol snapshots)
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk/testing/samples/test-tools@1071581 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/test/java/org/apache/sling/testing/sample/serverside/ServerSideTest.java
@@ -0,0 +1,67 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.sling.testing.sample.serverside;
+
+import org.apache.sling.junit.sample.SlingTestBase;
+import org.apache.stanbol.commons.testing.http.RetryLoop;
+import org.junit.Before;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/** Run all server-side tests */ 
+public class ServerSideTest extends SlingTestBase {
+    public static final String JUNIT_SERVLET_PATH = "/system/sling/junit";
+    private static boolean junitServletOk;
+    private final Logger log = LoggerFactory.getLogger(getClass());
+    
+    // TODO compute those timeouts based on a configured factor
+    // to cope with slower testing systems??
+    public static final int JUNIT_SERVLET_TIMEOUT_SECONDS = 60;
+    
+    @Before
+    public void checkJunitServletPresent() throws Exception {
+        if(junitServletOk) {
+            return;
+        }
+    
+        // Retry accessing the junit servlet until it responds or timeout
+        // (as we might just have installed the required bundles)
+        final RetryLoop.Condition c = new RetryLoop.Condition() {
+            public String getDescription() {
+                return "Checking that " + JUNIT_SERVLET_PATH + " returns 200";
+            }
+
+            public boolean isTrue() throws Exception {
+                executor.execute(
+                        builder.buildGetRequest(JUNIT_SERVLET_PATH))
+                .assertStatus(200);
+                return true;
+            }
+                
+        };
+        
+        new RetryLoop(c, JUNIT_SERVLET_TIMEOUT_SECONDS, 500); 
+        junitServletOk = true;
+    }
+    
+    @Test
+    public void testNothing() {
+        // TODO run the actual tests via junit servlet
+        log.info("If we get there that means " + JUNIT_SERVLET_PATH + " works");
+    }
+}
