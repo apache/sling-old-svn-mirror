b8f3029b97377af5e286f739eaaea4e9d22ffdee
Justin Edelson <justin@apache.org>
2011-02-20 23:05:20 +0000

    moving groovy tests to groovy bundle (won't run automatically) and removing groovy bundle from launchpad.builder
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk@1072799 13f79535-47bb-0310-9956-ffa450edef68

--- launchpad/integration-tests/src/main/java/org/apache/sling/launchpad/webapp/integrationtest/SlingResourceTypeRenderingTest.java
+++ launchpad/integration-tests/src/main/java/org/apache/sling/launchpad/webapp/integrationtest/SlingResourceTypeRenderingTest.java
@@ -17,40 +17,12 @@
 package org.apache.sling.launchpad.webapp.integrationtest;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-
-import org.apache.sling.servlets.post.SlingPostConstants;
 
 /** Test creating a Node and rendering it using scripts in
  *  various supported languages, using slingResourceType-based
  *  script resolution
  */
-public class SlingResourceTypeRenderingTest extends RenderingTestBase {
-
-    private String slingResourceType;
-    private String secondFolderOfContentPath;
-
-    @Override
-    protected void setUp() throws Exception {
-        super.setUp();
-
-        // set test values
-        slingResourceType = "integration-test/srt." + System.currentTimeMillis();
-        testText = "This is a test " + System.currentTimeMillis();
-
-        // create the test node, under a path that's specific to this class to allow collisions
-        secondFolderOfContentPath = "" + System.currentTimeMillis();
-        final String url = HTTP_BASE_URL + "/" + getClass().getSimpleName() + "/" + secondFolderOfContentPath + SlingPostConstants.DEFAULT_CREATE_SUFFIX;
-        final Map<String,String> props = new HashMap<String,String>();
-        props.put("sling:resourceType", slingResourceType);
-        props.put("text", testText);
-        displayUrl = testClient.createNode(url, props);
-
-        // the rendering script goes under /apps in the repository
-        scriptPath = "/apps/" + slingResourceType;
-        testClient.mkdirs(WEBDAV_BASE_URL, scriptPath);
-    }
+public class SlingResourceTypeRenderingTest extends AbstractSlingResourceTypeRenderingTest {
 
     public void testWithoutScriptTxt() throws IOException {
         final String content = getContent(displayUrl + ".txt", CONTENT_TYPE_PLAIN);
@@ -74,34 +46,11 @@ public class SlingResourceTypeRenderingTest extends RenderingTestBase {
         }
     }
 
-    public void testGspHtml() throws IOException {
-        final String toDelete = uploadTestScript("rendering-test.gsp","html.gsp");
-        try {
-            final String content = getContent(displayUrl + ".html", CONTENT_TYPE_HTML);
-            assertContains(content, "GSP template");
-            assertContains(content, "<p>" + testText + "</p>");
-            assertContains(content, "<div class=\"SLING-142\" id=\"22\"/>");
-        } finally {
-            testClient.delete(toDelete);
-        }
-    }
-
     public void testEspJavaCode() throws IOException {
         final String toDelete = uploadTestScript("rendering-test.esp","html.esp");
         try {
             final String content = getContent(displayUrl + ".html", CONTENT_TYPE_HTML);
             assertContains(content, "ESP template");
-            assertContains(content, "TestLinkedListTest");
-        } finally {
-            testClient.delete(toDelete);
-        }
-    }
-
-    public void testGspJavaCode() throws IOException {
-        final String toDelete = uploadTestScript("rendering-test.gsp","html.gsp");
-        try {
-            final String content = getContent(displayUrl + ".html", CONTENT_TYPE_HTML);
-            assertContains(content, "GSP template");
             assertContains(content, "TestLinkedListTest");
         } finally {
             testClient.delete(toDelete);
@@ -122,26 +71,6 @@ public class SlingResourceTypeRenderingTest extends RenderingTestBase {
         try {
             final String content = getContent(displayUrl + ".html", CONTENT_TYPE_HTML);
             assertContains(content, "ESP template");
-            assertContains(content, "<p>" + testText + "</p>");
-        } finally {
-            testClient.delete(toDelete);
-        }
-    }
-
-    public void testGspHtmlInAppsFolder() throws IOException {
-        // make sure there's no leftover rendering script
-        {
-            final String content = getContent(displayUrl + ".html", CONTENT_TYPE_HTML);
-            assertFalse("Content must not contain script marker before testing", content.contains("GSP template"));
-        }
-
-        // put our script under /apps/<resource type>
-        final String path = "/apps/" + slingResourceType;
-        testClient.mkdirs(WEBDAV_BASE_URL, path);
-        final String toDelete = uploadTestScript(path,"rendering-test.gsp","html.gsp");
-        try {
-            final String content = getContent(displayUrl + ".html", CONTENT_TYPE_HTML);
-            assertContains(content, "GSP template");
             assertContains(content, "<p>" + testText + "</p>");
         } finally {
             testClient.delete(toDelete);
