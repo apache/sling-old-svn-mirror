fa0cb9e372ad2fb105eb17e5c6bc83f26eb98de4
Eric Norman <enorman@apache.org>
2011-03-29 06:36:23 +0000

    SLING-1847 Redirect after logout does not work with form authentication (+unit test)
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk/launchpad/integration-tests@1086496 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/main/java/org/apache/sling/launchpad/webapp/integrationtest/login/RedirectOnLogoutTest.java
@@ -0,0 +1,55 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.sling.launchpad.webapp.integrationtest.login;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.httpclient.Header;
+import org.apache.commons.httpclient.NameValuePair;
+import org.apache.commons.httpclient.methods.GetMethod;
+import org.apache.sling.commons.testing.integration.HttpTestBase;
+
+/** Verify that redirect to resource after logout works */
+public class RedirectOnLogoutTest extends HttpTestBase {
+    /**
+     * Test SLING-1847
+     * @throws Exception
+     */
+    public void testRedirectToResourceAfterLogout() throws Exception {
+    	//login
+        List<NameValuePair> params = new ArrayList<NameValuePair>();
+        params.add(new NameValuePair("j_username", "admin"));
+        params.add(new NameValuePair("j_password", "admin"));
+        assertPostStatus(HTTP_BASE_URL + "/j_security_check", HttpServletResponse.SC_MOVED_TEMPORARILY, params, null);
+        
+        //...and then...logout with a resource redirect
+        String locationAfterLogout = SERVLET_CONTEXT + "/system/sling/info.sessionInfo.json";
+        final GetMethod get = new GetMethod(HTTP_BASE_URL + "/system/sling/logout");
+        NameValuePair [] logoutParams = new NameValuePair[1];
+        logoutParams[0] = new NameValuePair("resource", locationAfterLogout);
+        get.setQueryString(logoutParams);
+        
+        get.setFollowRedirects(false);
+        final int status = httpClient.executeMethod(get);
+        assertEquals("Expected redirect", HttpServletResponse.SC_MOVED_TEMPORARILY, status);
+        Header location = get.getResponseHeader("Location");
+        assertEquals(HTTP_BASE_URL + locationAfterLogout, location.getValue());
+    }
+}
