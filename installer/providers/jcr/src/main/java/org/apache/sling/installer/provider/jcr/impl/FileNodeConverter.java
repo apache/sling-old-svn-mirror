a6d3f0d60440f84f3a8a361b03374ce844dbf348
Carsten Ziegeler <cziegeler@apache.org>
2011-03-29 09:06:23 +0000

    SLING-1971 : Persist configuration changes not made through the installer
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk@1086523 13f79535-47bb-0310-9956-ffa450edef68

--- installer/providers/jcr/src/main/java/org/apache/sling/installer/provider/jcr/impl/FileNodeConverter.java
+++ installer/providers/jcr/src/main/java/org/apache/sling/installer/provider/jcr/impl/FileNodeConverter.java
@@ -38,7 +38,7 @@ public class FileNodeConverter implements JcrInstaller.NodeConverter {
     private static final String JCR_LAST_MODIFIED = "jcr:lastModified";
     private static final String JCR_CONTENT_LAST_MODIFIED = JCR_CONTENT + "/" + JCR_LAST_MODIFIED;
 
-    private final Logger log = LoggerFactory.getLogger(getClass());
+    private final Logger logger = LoggerFactory.getLogger(getClass());
 
 	/**
 	 * @see org.apache.sling.installer.provider.jcr.impl.JcrInstaller.NodeConverter#convertNode(javax.jcr.Node, int)
@@ -47,17 +47,17 @@ public class FileNodeConverter implements JcrInstaller.NodeConverter {
 	        final Node n,
 	        final int priority)
 	throws RepositoryException {
-		InstallableResource result = null;
+	    final String nodePath = n.getPath();
 		if (n.hasProperty(JCR_CONTENT_DATA) && n.hasProperty(JCR_CONTENT_LAST_MODIFIED)) {
 			try {
-				result = convert(n, n.getPath(), priority);
-			} catch(IOException ioe) {
-				log.info("Conversion failed, node {} ignored ({})", n.getPath(), ioe);
+				return convert(n, nodePath, priority);
+			} catch (final IOException ioe) {
+			    logger.info("Conversion failed, node {} ignored ({})", nodePath, ioe);
 			}
-			return result;
-		}
-		log.debug("Node {} has no {} properties, ignored", n.getPath(),
-				JCR_CONTENT_DATA + " or " + JCR_CONTENT_LAST_MODIFIED);
+		} else {
+		    logger.debug("Node {} has no {} properties, ignored", nodePath,
+    				JCR_CONTENT_DATA + " or " + JCR_CONTENT_LAST_MODIFIED);
+	    }
 		return null;
 	}
 
