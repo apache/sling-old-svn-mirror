4b25db136abc71f8dbbf87539e94b3a2e905ceb2
Bertrand Delacretaz <bdelacretaz@apache.org>
2011-05-23 15:44:36 +0000

    SLING-2082 - escape html characters in HtmlResponse output, with tests
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk/launchpad/integration-tests@1126545 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/sling/launchpad/webapp/integrationtest/issues/SLING2082Test.java
+++ src/main/java/org/apache/sling/launchpad/webapp/integrationtest/issues/SLING2082Test.java
@@ -18,19 +18,33 @@
  */
 package org.apache.sling.launchpad.webapp.integrationtest.issues;
 
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.httpclient.HttpMethodBase;
+import org.apache.commons.httpclient.methods.GetMethod;
 import org.apache.commons.httpclient.methods.PostMethod;
 import org.apache.sling.commons.testing.integration.HttpTestBase;
 
 public class SLING2082Test extends HttpTestBase {
     
-    public void testPOST() throws Exception {
-        final String url = HTTP_BASE_URL + "/" + getClass().getSimpleName() + "/" + Math.random() + ".html/%22%3e%3cscript%3ealert(29679)%3c/script%3e";
-        final PostMethod post = new PostMethod(url);
-        post.setFollowRedirects(false);
-        final int status = httpClient.executeMethod(post);
-        assertEquals(201, status);
-        final String content = getResponseBodyAsStream(post, 0);
+    private void runTest(HttpMethodBase m, int expectedStatus) throws Exception {
+        m.setFollowRedirects(false);
+        final int status = httpClient.executeMethod(m);
+        assertEquals(expectedStatus, status);
+        final String content = getResponseBodyAsStream(m, 0);
         final String scriptTag = "<script>";
         assertFalse("Content should not contain '" + scriptTag + "'", content.contains(scriptTag));
+    }
+    
+    public void testPOST() throws Exception {
+        final String path = "/" + getClass().getSimpleName() + "/" + Math.random() + ".html/%22%3e%3cscript%3ealert(29679)%3c/script%3e";
+        final PostMethod post = new PostMethod(HTTP_BASE_URL + path);
+        runTest(post, HttpServletResponse.SC_CREATED);
+    }
+    
+    public void testOptingServletPost() throws Exception {
+        final String path = "/testing/HtmlResponseServlet";
+        final GetMethod post = new GetMethod(HTTP_BASE_URL + path);
+        runTest(post, HttpServletResponse.SC_GATEWAY_TIMEOUT);
     }
 }
