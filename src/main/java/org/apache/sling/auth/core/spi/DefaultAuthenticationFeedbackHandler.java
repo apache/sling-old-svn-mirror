617d9cc7351dab1c947c4abb4f3d3603b47d9eae
Felix Meschberger <fmeschbe@apache.org>
2011-07-05 11:26:45 +0000

    SLING-2126 Add helper method to validate a target to redirect to after logging in. Also use this method in the redirects in the DefaultAuthenticationFeedbackHandler and AbstractAuthenticationHandler. Finally, since we add a method, increase the micro version number for clients to be able properly require the implementation.
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk/bundles/auth/core@1142992 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/sling/auth/core/spi/DefaultAuthenticationFeedbackHandler.java
+++ src/main/java/org/apache/sling/auth/core/spi/DefaultAuthenticationFeedbackHandler.java
@@ -31,6 +31,29 @@ public class DefaultAuthenticationFeedbackHandler implements
      * Handles an optional request for a redirect after successful
      * authentication and <code>true</code> if the request has been redirected.
      * <p>
+     * This method checks {@link AuthenticationSupport#REDIRECT_PARAMETER}
+     * request parameter for the redirect target. This parameter is handled
+     * as follows:
+     * <ul>
+     * <li>If the parameter does not exist, the method does not redirect and
+     * <code>false</code> is returned.</li>
+     * <li>If the parameter is the string <code>true</code> or is empty, a
+     *  redirect response to the request URI (<code>HttpServletRequest.getRequestURI()</code>)
+     *  is sent and <code>true</code> is returned.</li>
+     * <li>If the parameter is a relative path, the path is made absolute
+     *  by resolving it relative to the request URI
+     *  (<code>HttpServletRequest.getRequestURI()</code>). The resulting
+     *  target is validated with the
+     *  {@link AbstractAuthenticationHandler#isRedirectValid(HttpServletRequest, String)}
+     *  method. If valid a redirect to that target is sent back and <code>true</code>
+     *  is returned. Otherwise a redirect to the servlet context root is
+     *  sent back and <code>true</code> is returned.</li>
+     * <li>If the parameter is an absolute path it is validated with the
+     *  {@link AbstractAuthenticationHandler#isRedirectValid(HttpServletRequest, String)}
+     *  method. If valid a redirect to that path is sent back and <code>true</code>
+     *  is returned. Otherwise a redirect to the servlet context root is
+     *  sent back and <code>true</code> is returned.</li>
+     * <p>
      * If sending the redirect response fails due to some IO problems, the
      * request is still terminated but an error message is logged indicating the
      * problem.
@@ -39,37 +62,24 @@ public class DefaultAuthenticationFeedbackHandler implements
      *         <code>false</code> is returned. Note, that <code>true</code> is
      *         returned regardless of whether sending the redirect response
      *         succeeded or not.
+     *
+     * @since 1.0.4 (bundle version 1.0.8) the target is validated with the
+     *        {@link AbstractAuthenticationHandler#isRedirectValid(HttpServletRequest, String)}
+     *        method.
      */
     public static boolean handleRedirect(final HttpServletRequest request,
             final HttpServletResponse response) {
 
-        final String redirect = request.getParameter(AuthenticationSupport.REDIRECT_PARAMETER);
+        final String redirect = getValidatedRedirectTarget(request);
         if (redirect != null) {
-
-            // find the redirect target
-            final String target;
-            if ("true".equalsIgnoreCase(redirect) || redirect.length() == 0) {
-                // redirect to the same path
-                target = request.getRequestURI();
-
-            } else if (redirect.startsWith("/")) {
-                // absolute target (in the servlet context)
-                target = request.getContextPath() + redirect;
-
-            } else {
-                // redirect relative to the current request
-                target = redirect;
-
-            }
-
             // and redirect ensuring the response is sent to the client
             try {
-                response.sendRedirect(target);
+                response.sendRedirect(redirect);
             } catch (Exception e) {
                 // expected: IOException and IllegalStateException
                 LoggerFactory.getLogger(
                     DefaultAuthenticationFeedbackHandler.class).error(
-                    "handleRedirect: Failed to send redirect to " + target
+                    "handleRedirect: Failed to send redirect to " + redirect
                         + ", aborting request without redirect", e);
             }
 
@@ -79,6 +89,38 @@ public class DefaultAuthenticationFeedbackHandler implements
 
         // no redirect requested
         return false;
+    }
+
+    private static String getValidatedRedirectTarget(
+            final HttpServletRequest request) {
+        String redirect = request.getParameter(AuthenticationSupport.REDIRECT_PARAMETER);
+        if (redirect == null) {
+            return null;
+        }
+
+        // redirect to the same path
+        if ("true".equalsIgnoreCase(redirect) || redirect.length() == 0) {
+            return request.getRequestURI();
+        }
+
+        // redirect relative to the current request (make absolute)
+        if (!redirect.startsWith("/") && !redirect.contains("://")) {
+            String path = request.getRequestURI();
+            path = path.substring(request.getContextPath().length());
+            int lastSlash = path.lastIndexOf('/');
+            path = (lastSlash > 0) ? path.substring(0, lastSlash + 1) : path;
+            redirect = path.concat(redirect);
+        }
+
+        // absolute target (in the servlet context)
+        if (!AbstractAuthenticationHandler.isRedirectValid(request, redirect)) {
+            LoggerFactory.getLogger(DefaultAuthenticationFeedbackHandler.class).error(
+                "handleRedirect: Redirect target '{}' is invalid, redirecting to '/'",
+                redirect);
+            redirect = "/";
+        }
+
+        return request.getContextPath().concat(redirect);
     }
 
     /**
