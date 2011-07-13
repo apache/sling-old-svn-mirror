4f068d7a234728065176801a87b71ded7b064d93
Carsten Ziegeler <cziegeler@apache.org>
2011-07-13 08:30:02 +0000

    SLING-2094 : Adding support to call a sling resource for JSP page exception handler <%@ page errorPage ... %>
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk@1145904 13f79535-47bb-0310-9956-ffa450edef68

---
+++ launchpad/integration-tests/src/main/java/org/apache/sling/launchpad/webapp/integrationtest/issues/SLING2094Test.java
@@ -0,0 +1,64 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.sling.launchpad.webapp.integrationtest.issues;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.sling.launchpad.webapp.integrationtest.JspTestBase;
+
+/** Test the SLING-2094 JSP errorpage statement */
+public class SLING2094Test extends JspTestBase {
+    public final static String TEST_ROOT = "/apps/sling2094";
+    private String testNodePath;
+    
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        
+        scriptPath = TEST_ROOT;
+        
+        testClient.mkdirs(HTTP_BASE_URL, TEST_ROOT);
+        for(String file : new String[] { "custom-error-page.jsp", "sling2094.jsp" }) {
+            uploadTestScript("issues/sling2094/" + file, file);
+        }
+        
+        final Map<String, String> props = new HashMap<String, String>();
+        props.put(SLING_RESOURCE_TYPE, TEST_ROOT);
+        testNodePath = testClient.createNode(HTTP_BASE_URL + TEST_ROOT, props);
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+        testClient.delete(HTTP_BASE_URL + TEST_ROOT);
+    }
+    
+    public void testWithoutError() throws Exception {
+        final String expected = "All good, no exception";
+        final String url = testNodePath + ".html";
+        assertContains(getContent(url, CONTENT_TYPE_HTML), expected);
+    }
+    
+    public void testWithError() throws Exception {
+        final String expected = "witherror selector was specified";
+        final String url = testNodePath + ".witherror.html";
+        assertContains(getContent(url, CONTENT_TYPE_HTML), expected);
+    }
+}
