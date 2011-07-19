b43539f04ac8a888b3ec5aeb13511028bd807173
Justin Edelson <justin@apache.org>
2011-07-19 20:49:53 +0000

    SLING-1476 - adding microbenchmark for adding stats to the statistics object
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk/bundles/engine@1148519 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/test/java/org/apache/sling/engine/benchmarks/AddValueToStatisticsViaExecutorDriver.java
@@ -0,0 +1,51 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with this
+ * work for additional information regarding copyright ownership. The ASF
+ * licenses this file to You under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.apache.sling.engine.benchmarks;
+
+import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import org.apache.commons.math.stat.descriptive.SynchronizedSummaryStatistics;
+
+import com.sun.japex.JapexDriverBase;
+import com.sun.japex.TestCase;
+
+public class AddValueToStatisticsViaExecutorDriver extends JapexDriverBase {
+    
+    private SynchronizedSummaryStatistics statistics;
+    private ExecutorService operationExecutor;
+
+    @Override
+    public void prepare(TestCase tc) {
+        this.statistics = new SynchronizedSummaryStatistics();
+        this.operationExecutor = Executors.newSingleThreadExecutor();
+    }
+
+    @Override
+    public void run(TestCase tc) {
+        Random random = new Random();
+        final long value = random.nextLong();
+        operationExecutor.execute(new Runnable() {
+
+            public void run() {
+                statistics.addValue(value);
+            }
+        });
+    }
+    
+}
