c2b0d5d50287116542f4ecce431a4ff85429c7c9
Carsten Ziegeler <cziegeler@apache.org>
2011-08-03 07:49:44 +0000

    SLING-2141 : Update localhost and server handling
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk@1153377 13f79535-47bb-0310-9956-ffa450edef68

--- contrib/extensions/security/src/test/java/org/apache/sling/security/impl/ReferrerFilterTest.java
+++ contrib/extensions/security/src/test/java/org/apache/sling/security/impl/ReferrerFilterTest.java
@@ -42,20 +42,20 @@ public class ReferrerFilterTest {
     }
 
     @Test public void testHostName() {
-        Assert.assertEquals("somehost", filter.getHost("http://somehost"));
-        Assert.assertEquals("somehost", filter.getHost("http://somehost/somewhere"));
-        Assert.assertEquals("somehost", filter.getHost("http://somehost:4242/somewhere"));
-        Assert.assertEquals("somehost", filter.getHost("http://admin@somehost/somewhere"));
-        Assert.assertEquals("somehost", filter.getHost("http://admin@somehost/somewhere?invald=@gagga"));
-        Assert.assertEquals("somehost", filter.getHost("http://admin@somehost:1/somewhere"));
-        Assert.assertEquals("somehost", filter.getHost("http://admin:admin@somehost/somewhere"));
-        Assert.assertEquals("somehost", filter.getHost("http://admin:admin@somehost:4343/somewhere"));
-        Assert.assertEquals("localhost", filter.getHost("http://localhost"));
-        Assert.assertEquals("127.0.0.1", filter.getHost("http://127.0.0.1"));
-        Assert.assertEquals("localhost", filter.getHost("http://localhost:535"));
-        Assert.assertEquals("127.0.0.1", filter.getHost("http://127.0.0.1:242"));
-        Assert.assertEquals("localhost", filter.getHost("http://localhost:256235/etewteq.ff"));
-        Assert.assertEquals("127.0.0.1", filter.getHost("http://127.0.0.1/wetew.qerq"));
+        Assert.assertEquals("somehost", filter.getHost("http://somehost").host);
+        Assert.assertEquals("somehost", filter.getHost("http://somehost/somewhere").host);
+        Assert.assertEquals("somehost", filter.getHost("http://somehost:4242/somewhere").host);
+        Assert.assertEquals("somehost", filter.getHost("http://admin@somehost/somewhere").host);
+        Assert.assertEquals("somehost", filter.getHost("http://admin@somehost/somewhere?invald=@gagga").host);
+        Assert.assertEquals("somehost", filter.getHost("http://admin@somehost:1/somewhere").host);
+        Assert.assertEquals("somehost", filter.getHost("http://admin:admin@somehost/somewhere").host);
+        Assert.assertEquals("somehost", filter.getHost("http://admin:admin@somehost:4343/somewhere").host);
+        Assert.assertEquals("localhost", filter.getHost("http://localhost").host);
+        Assert.assertEquals("127.0.0.1", filter.getHost("http://127.0.0.1").host);
+        Assert.assertEquals("localhost", filter.getHost("http://localhost:535").host);
+        Assert.assertEquals("127.0.0.1", filter.getHost("http://127.0.0.1:242").host);
+        Assert.assertEquals("localhost", filter.getHost("http://localhost:256235/etewteq.ff").host);
+        Assert.assertEquals("127.0.0.1", filter.getHost("http://127.0.0.1/wetew.qerq").host);
         Assert.assertEquals(null, filter.getHost("http:/admin:admin@somehost:4343/somewhere"));
     }
 
@@ -64,20 +64,17 @@ public class ReferrerFilterTest {
         when(request.getMethod()).thenReturn("POST");
         when(request.getRequestURI()).thenReturn("http://somehost/somewhere");
         when(request.getHeader("referer")).thenReturn(referrer);
-        when(request.getServerName()).thenReturn("me");
         return request;
     }
 
     @Test public void testValidRequest() {
-        Assert.assertEquals(true, filter.isValidRequest(getRequest(null)));
+        Assert.assertEquals(false, filter.isValidRequest(getRequest(null)));
         Assert.assertEquals(true, filter.isValidRequest(getRequest("relative")));
         Assert.assertEquals(true, filter.isValidRequest(getRequest("/relative/too")));
         Assert.assertEquals(true, filter.isValidRequest(getRequest("/relative/but/[illegal]")));
         Assert.assertEquals(false, filter.isValidRequest(getRequest("http://somehost")));
-        Assert.assertEquals(true, filter.isValidRequest(getRequest("http://me")));
         Assert.assertEquals(true, filter.isValidRequest(getRequest("http://localhost")));
         Assert.assertEquals(true, filter.isValidRequest(getRequest("http://127.0.0.1")));
         Assert.assertEquals(false, filter.isValidRequest(getRequest("http://somehost/but/[illegal]")));
-        Assert.assertEquals(true, filter.isValidRequest(getRequest("http://me/but/[illegal]")));
     }
 }
