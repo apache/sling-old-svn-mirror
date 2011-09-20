5a7933d3f7f06ecbbc9796f597c1184dcd39b706
Carsten Ziegeler <cziegeler@apache.org>
2011-09-20 06:10:40 +0000

    SLING-2226 : Move json query servlet to a new compat bundle
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk@1172987 13f79535-47bb-0310-9956-ffa450edef68

--- bundles/servlets/compat/src/main/java/org/apache/sling/servlets/compat/impl/JsonQueryServlet.java
+++ bundles/servlets/compat/src/main/java/org/apache/sling/servlets/compat/impl/JsonQueryServlet.java
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package org.apache.sling.servlets.get.impl;
+package org.apache.sling.servlets.compat.impl;
 
 import java.io.IOException;
 import java.util.ArrayList;
