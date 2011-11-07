b555c564f7e0b7c823a829faca7ef2a6793190aa
Felix Meschberger <fmeschbe@apache.org>
2011-11-07 15:16:54 +0000

    SLING-2080 Apply patch by Angela Schreiber (thank you very much)
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk@1198774 13f79535-47bb-0310-9956-ffa450edef68

--- bundles/jcr/jackrabbit-accessmanager/src/main/java/org/apache/sling/jcr/jackrabbit/accessmanager/PrivilegesInfo.java
+++ bundles/jcr/jackrabbit-accessmanager/src/main/java/org/apache/sling/jcr/jackrabbit/accessmanager/PrivilegesInfo.java
@@ -158,16 +158,16 @@ public class PrivilegesInfo {
 		if (entries != null) {
 			for (AccessControlEntry ace : entries) {
 				Principal principal = ace.getPrincipal();
-				AccessRights accessPrivleges = accessMap.get(principal);
-				if (accessPrivleges == null) {
-					accessPrivleges = new AccessRights();
-					accessMap.put(principal, accessPrivleges);
+				AccessRights accessPrivileges = accessMap.get(principal);
+				if (accessPrivileges == null) {
+					accessPrivileges = new AccessRights();
+					accessMap.put(principal, accessPrivileges);
 				}
 				boolean allow = AccessControlUtil.isAllow(ace);
 				if (allow) {
-					accessPrivleges.getGranted().addAll(Arrays.asList(ace.getPrivileges()));
+					accessPrivileges.getGranted().addAll(Arrays.asList(ace.getPrivileges()));
 				} else {
-					accessPrivleges.getDenied().addAll(Arrays.asList(ace.getPrivileges()));
+					accessPrivileges.getDenied().addAll(Arrays.asList(ace.getPrivileges()));
 				}
 			}
 		}
