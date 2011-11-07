3040572f12093c1a1df5c0259d2e3e93e8cfe1fe
Felix Meschberger <fmeschbe@apache.org>
2011-11-07 14:25:43 +0000

    SLING-2267 Upgrade to Maven Bundle plugin 2.3.4 and make use of BND package level annotations for package exports
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk@1198746 13f79535-47bb-0310-9956-ffa450edef68

---
+++ bundles/auth/core/src/main/java/org/apache/sling/auth/core/spi/package-info.java
@@ -0,0 +1,35 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+/**
+ * Provides API to be implemented by provides of authentication
+ * mechanisms. The most important interface (and service definition) is
+ * the {@link org.apache.sling.auth.core.spi.AuthenticationHandler}
+ * interface with the
+ * {@link org.apache.sling.auth.core.spi.AbstractAuthenticationHandler}
+ * being an abstract base implementation from which concrete
+ * implementations may inherit.
+ *
+ * @version 1.0.4
+ */
+@Version("1.0.4")
+package org.apache.sling.auth.core.spi;
+
+import aQute.bnd.annotation.Version;
+
