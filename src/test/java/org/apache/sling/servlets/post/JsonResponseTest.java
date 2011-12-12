2d5b2b59caf4331ef019268c6bdd0a24d1038efb
Justin Edelson <justin@apache.org>
2011-12-12 16:46:31 +0000

    SLING-2320 - adding unit test of JsonRenderer and making dependent changes in testing
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk/bundles/servlets/post@1213305 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/sling/servlets/post/JsonResponseTest.java
+++ src/test/java/org/apache/sling/servlets/post/JsonResponseTest.java
@@ -21,6 +21,7 @@ import junit.framework.TestCase;
 import org.apache.sling.commons.json.JSONArray;
 import org.apache.sling.commons.json.JSONException;
 import org.apache.sling.commons.json.JSONObject;
+import org.apache.sling.commons.testing.sling.MockSlingHttpServletResponse;
 import org.apache.sling.servlets.post.JSONResponse;
 
 import javax.servlet.ServletOutputStream;
@@ -60,7 +61,7 @@ public class JsonResponseTest extends TestCase {
     public void testSetError() throws IOException, JSONException {
         String errMsg = "Dummy error";
         res.setError(new Error(errMsg));
-        MockHttpServletResponse resp = new MockHttpServletResponse();
+        MockSlingHttpServletResponse resp = new MockSlingHttpServletResponse();
         res.send(resp, true);
         JSONObject json = res.getJson();
         JSONObject error = assertProperty(json, "error", JSONObject.class);
@@ -70,7 +71,7 @@ public class JsonResponseTest extends TestCase {
 
     public void testSend() throws Exception {
         res.onChange("modified", "argument1");
-        MockHttpServletResponse response = new MockHttpServletResponse();
+        MockSlingHttpServletResponse response = new MockSlingHttpServletResponse();
         res.send(response, true);
         JSONObject result = new JSONObject(response.getOutput().toString());
         assertProperty(result, HtmlResponse.PN_STATUS_CODE, HttpServletResponse.SC_OK);
@@ -97,170 +98,6 @@ public class JsonResponseTest extends TestCase {
         } catch (ClassCastException e) {
             TestCase.fail("Object is of unexpected type. Expected: " + clazz.getName() + ", actual: " + obj.getClass().getName());
             return null;
-        }
-    }
-
-    private static class MockHttpServletResponse implements HttpServletResponse {
-
-        private StringBuffer output = new StringBuffer();
-        private String contentType;
-        private String encoding;
-        private int status = SC_OK;
-
-        public StringBuffer getOutput() {
-            return output;
-        }
-
-        public void addCookie(Cookie cookie) {
-            throw new UnsupportedOperationException("Not implemented: " + getClass().getName() + ".addCookie");
-        }
-
-        public boolean containsHeader(String s) {
-            throw new UnsupportedOperationException("Not implemented: " + getClass().getName() + ".containsHeader");
-        }
-
-        public String encodeURL(String s) {
-            throw new UnsupportedOperationException("Not implemented: " + getClass().getName() + ".encodeURL");
-        }
-
-        public String encodeRedirectURL(String s) {
-            throw new UnsupportedOperationException("Not implemented: " + getClass().getName() + ".encodeRedirectURL");
-        }
-
-        public String encodeUrl(String s) {
-            throw new UnsupportedOperationException("Not implemented: " + getClass().getName() + ".encodeUrl");
-        }
-
-        public String encodeRedirectUrl(String s) {
-            throw new UnsupportedOperationException("Not implemented: " + getClass().getName() + ".encodeRedirectUrl");
-        }
-
-        public void sendError(int i, String s) throws IOException {
-            throw new UnsupportedOperationException("Not implemented: " + getClass().getName() + ".sendError");
-        }
-
-        public void sendError(int i) throws IOException {
-            throw new UnsupportedOperationException("Not implemented: " + getClass().getName() + ".sendError");
-        }
-
-        public void sendRedirect(String s) throws IOException {
-            throw new UnsupportedOperationException("Not implemented: " + getClass().getName() + ".sendRedirect");
-        }
-
-        public void setDateHeader(String s, long l) {
-            throw new UnsupportedOperationException("Not implemented: " + getClass().getName() + ".setDateHeader");
-        }
-
-        public void addDateHeader(String s, long l) {
-            throw new UnsupportedOperationException("Not implemented: " + getClass().getName() + ".addDateHeader");
-        }
-
-        public void setHeader(String s, String s1) {
-            throw new UnsupportedOperationException("Not implemented: " + getClass().getName() + ".setHeader");
-        }
-
-        public void addHeader(String s, String s1) {
-            throw new UnsupportedOperationException("Not implemented: " + getClass().getName() + ".addHeader");
-        }
-
-        public void setIntHeader(String s, int i) {
-            throw new UnsupportedOperationException("Not implemented: " + getClass().getName() + ".setIntHeader");
-        }
-
-        public void addIntHeader(String s, int i) {
-            throw new UnsupportedOperationException("Not implemented: " + getClass().getName() + ".addIntHeader");
-        }
-
-        public void setStatus(int i) {
-            this.status = i;
-        }
-
-        public void setStatus(int i, String s) {
-            this.status = i;
-        }
-
-        public String getCharacterEncoding() {
-            return encoding;
-        }
-
-        public String getContentType() {
-            return contentType;
-        }
-
-        public ServletOutputStream getOutputStream() throws IOException {
-            throw new UnsupportedOperationException("Not implemented: " + getClass().getName() + ".getOutputStream");
-        }
-
-        public PrintWriter getWriter() throws IOException {
-            MockHttpServletResponse.MockWriter writer = new MockWriter(output);
-            return new PrintWriter(writer);
-        }
-
-        public void setCharacterEncoding(String encoding) {
-            this.encoding = encoding;
-        }
-
-        public void setContentLength(int i) {
-            throw new UnsupportedOperationException("Not implemented: " + getClass().getName() + ".setContentLength");
-        }
-
-        public void setContentType(String contentType) {
-            this.contentType = contentType;
-        }
-
-        public void setBufferSize(int i) {
-            throw new UnsupportedOperationException("Not implemented: " + getClass().getName() + ".setBufferSize");
-        }
-
-        public int getBufferSize() {
-            throw new UnsupportedOperationException("Not implemented: " + getClass().getName() + ".getBufferSize");
-        }
-
-        public void flushBuffer() throws IOException {
-            throw new UnsupportedOperationException("Not implemented: " + getClass().getName() + ".flushBuffer");
-        }
-
-        public void resetBuffer() {
-            throw new UnsupportedOperationException("Not implemented: " + getClass().getName() + ".resetBuffer");
-        }
-
-        public boolean isCommitted() {
-            throw new UnsupportedOperationException("Not implemented: " + getClass().getName() + ".isCommitted");
-        }
-
-        public void reset() {
-            throw new UnsupportedOperationException("Not implemented: " + getClass().getName() + ".reset");
-        }
-
-        public void setLocale(Locale locale) {
-            throw new UnsupportedOperationException("Not implemented: " + getClass().getName() + ".setLocale");
-        }
-
-        public Locale getLocale() {
-            throw new UnsupportedOperationException("Not implemented: " + getClass().getName() + ".getLocale");
-        }
-
-        private class MockWriter extends Writer {
-            private StringBuffer buf;
-
-            public MockWriter(StringBuffer output) {
-                buf = output;
-            }
-
-            @Override
-            public void write(char[] cbuf, int off, int len) throws IOException {
-                buf.append(cbuf, off, len);
-            }
-
-            @Override
-            public void flush() throws IOException {
-                buf.setLength(0);
-            }
-
-            @Override
-            public void close() throws IOException {
-                buf = null;
-            }
         }
     }
 
