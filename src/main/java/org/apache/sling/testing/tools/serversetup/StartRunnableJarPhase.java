78b9a9097958752863639f0a202fd9c4ef105501
Bertrand Delacretaz <bdelacretaz@apache.org>
2012-01-13 23:54:42 +0000

    SLING-2368 - ServerSetup utility, work in progress
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk/testing/tools@1231404 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/main/java/org/apache/sling/testing/tools/serversetup/StartRunnableJarPhase.java
@@ -0,0 +1,77 @@
+package org.apache.sling.testing.tools.serversetup;
+
+import java.util.Properties;
+
+import org.apache.commons.exec.ProcessDestroyer;
+import org.apache.commons.exec.ShutdownHookProcessDestroyer;
+import org.apache.sling.testing.tools.jarexec.JarExecutor;
+import org.apache.sling.testing.tools.jarexec.JarExecutor.ExecutorException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/** SetupPhase that uses a JarExecutor to start
+ *  a runnable jar, and stop it at system shutdown
+ *  if our SetupServer wants that.
+ */
+public class StartRunnableJarPhase implements SetupPhase {
+
+    private final Logger log = LoggerFactory.getLogger(getClass());
+    private final String id;
+    private final String description;
+    private final JarExecutor executor;
+    
+    public StartRunnableJarPhase(final ServerSetup owner, String id, String description, Properties config) throws ExecutorException {
+        this.id = id;
+        this.description = description;
+        
+        /** Our JarExecutor uses a ProcessDestroyer which does noting
+         *  if our ServerSetup owner would not run
+         *  a shutdown task with our name + SHUTDOWN_ID_SUFFIX
+         */
+        final String shutdownId = id + ServerSetup.SHUTDOWN_ID_SUFFIX;
+        final ProcessDestroyer destroyer = new ShutdownHookProcessDestroyer() {
+            @Override
+            public void run() {
+                if(owner.getPhasesToRun().contains(shutdownId)) {
+                    log.info(
+                            "{}: {} allows {} phase to run, shutting down runnable jar", 
+                            new Object[] { this, owner, shutdownId } );
+                    super.run();
+                } else {
+                    log.info(
+                            "{}: {} does not {} phase to run, doing nothing", 
+                            new Object[] { this, owner, shutdownId } );
+                }
+            }
+            
+        };
+        
+        executor = new JarExecutor(config) {
+            @Override
+            protected ProcessDestroyer getProcessDestroyer() {
+                return destroyer;
+            }
+            
+        };
+    }
+    
+    /** @inheritDoc */
+    public void run(ServerSetup owner) throws Exception {
+        executor.start();
+    }
+
+    /** @inheritDoc */
+    public boolean isStartupPhase() {
+        return true;
+    }
+
+    /** @inheritDoc */
+    public String getDescription() {
+        return description;
+    }
+
+    /** @inheritDoc */
+    public String getId() {
+        return id;
+    }
+}
---
+++ src/test/java/org/apache/sling/testing/tools/test/ServerSetupSingletonTest.java
@@ -0,0 +1,144 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.sling.testing.tools.test;
+
+import static org.junit.Assert.fail;
+import static org.junit.Assert.assertEquals;
+
+import java.util.Properties;
+
+import org.apache.sling.testing.tools.serversetup.ServerSetup;
+import org.apache.sling.testing.tools.serversetup.ServerSetupSingleton;
+import org.junit.Before;
+import org.junit.Test;
+
+/** Test the ServerSetupSingleton */
+public class ServerSetupSingletonTest {
+    private ServerSetup serverSetup;
+    private Properties props;
+    
+    @Before
+    public void setup() throws Exception {
+        props = new Properties();
+        props.setProperty(ServerSetupSingleton.CLASS_NAME_PROP, TestServerSetup.class.getName());
+        props.setProperty(ServerSetup.PHASES_TO_RUN_PROP, "one, \t\n two, three, four, five  \t");
+        serverSetup = ServerSetupSingleton.instance(props);
+        serverSetup.setConfig(props);
+        TestSetupPhase.clearExecutionLog();
+        TestSetupPhase.failingPhases = "";
+    }
+    
+    @Test
+    public void testStartup() throws Exception {
+        serverSetup.setupTestServer();
+        assertEquals("Expecting all startup phases to have run",
+                "one,two,three", TestSetupPhase.executionLog.toString());
+        
+        serverSetup.setupTestServer();
+        assertEquals("Expecting second setup call to have no effect",
+                "one,two,three", TestSetupPhase.executionLog.toString());
+    }
+    
+    @Test
+    public void testShutdown() throws Exception {
+        serverSetup.shutdown();
+        assertEquals("Expecting all shutdown phases to have run",
+                "four,five", TestSetupPhase.executionLog.toString());
+        
+        serverSetup.shutdown();
+        assertEquals("Expecting second shutdown call to be ignored",
+                "four,five", TestSetupPhase.executionLog.toString());
+    }
+    
+    @Test
+    public void testStartupAndShutdown() throws Exception {
+        serverSetup.setupTestServer();
+        assertEquals("Expecting all startup phases to have run",
+                "one,two,three", TestSetupPhase.executionLog.toString());
+        
+        serverSetup.shutdown();
+        assertEquals("Expecting all phases to have run",
+                "one,two,three,four,five", TestSetupPhase.executionLog.toString());
+    }
+    
+    @Test
+    public void testStartupSomeOnly() throws Exception {
+        props.setProperty(ServerSetup.PHASES_TO_RUN_PROP, "one, three, five");
+        serverSetup.setConfig(props);
+        
+        serverSetup.setupTestServer();
+        assertEquals("Expecting only two startup phases to have run",
+                "one,three", TestSetupPhase.executionLog.toString());
+        
+        serverSetup.setupTestServer();
+        assertEquals("Expecting second setup call to have no effect",
+                "one,three", TestSetupPhase.executionLog.toString());
+    }
+    
+    @Test
+    public void testShutdownSomeOnly() throws Exception {
+        props.setProperty(ServerSetup.PHASES_TO_RUN_PROP, "four");
+        serverSetup.setConfig(props);
+        
+        serverSetup.shutdown();
+        assertEquals("Expecting only one shutdown phase to have run",
+                "four", TestSetupPhase.executionLog.toString());
+        
+        serverSetup.shutdown();
+        assertEquals("Expecting second setup call to have no effect",
+                "four", TestSetupPhase.executionLog.toString());
+    }
+    
+    @Test
+    public void testFailingStartup() {
+        TestSetupPhase.failingPhases = "two, five";
+        
+        // setupTestServer will fail every time it's called
+        // after a failure, as that means the server is unusable
+        for(int i=0; i < 3; i++) {
+            try {
+                serverSetup.setupTestServer();
+                fail("startup should have failed");
+            } catch(Exception ignored) {
+            }
+            
+            assertEquals("Expecting only one startup phase to have run",
+                    "one", TestSetupPhase.executionLog.toString());
+        }
+    }
+    
+    @Test
+    public void testFailingShutdown() throws Exception {
+        TestSetupPhase.failingPhases = "two, five";
+        
+        try {
+            serverSetup.shutdown();
+            fail("shutdown should have failed");
+        } catch(Exception ignored) {
+        }
+        
+        assertEquals("Expecting only one startup phase to have run",
+                "four", TestSetupPhase.executionLog.toString());
+        
+        // Calling shutdown again does not throw an Exception again,
+        // it's not really useful at shutdown.
+        serverSetup.shutdown();
+        
+        assertEquals("Still expecting only one startup phase to have run",
+                "four", TestSetupPhase.executionLog.toString());
+    }
+}
