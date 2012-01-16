d0a55101020cc336dfe6397837c8fbebf37a789c
Felix Meschberger <fmeschbe@apache.org>
2012-01-16 15:31:40 +0000

    SLING-2361 Replace the use of Commons Math SummaryStatistics with a simple formula calculated on-demand.  - Add a test case to compare the operation of the formula with the SummaryStatistics values  - Keep Commons Math as a test dependency
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk@1232029 13f79535-47bb-0310-9956-ffa450edef68

--- bundles/engine/src/main/java/org/apache/sling/engine/jmx/RequestProcessorMBean.java
+++ bundles/engine/src/main/java/org/apache/sling/engine/jmx/RequestProcessorMBean.java
@@ -5,9 +5,9 @@
  * licenses this file to You under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -18,20 +18,53 @@ package org.apache.sling.engine.jmx;
 
 /**
  * This is the management interface for the SlingRequestProcessor.
- *
  */
 public interface RequestProcessorMBean {
 
+    /**
+     * Returns the number of requests collected since last resetting the
+     * statistics.
+     *
+     * @see #resetStatistics()
+     */
     long getRequestsCount();
-    
-    double getMaxRequestDurationMsec();
 
+    /**
+     * Returns the time in milliseconds used by the longest request since last
+     * resetting the statistics.
+     *
+     * @see #resetStatistics()
+     */
+    long getMaxRequestDurationMsec();
+
+    /**
+     * Returns the time in milliseconds used by the shortest request since last
+     * resetting the statistics.
+     *
+     * @see #resetStatistics()
+     */
+    long getMinRequestDurationMsec();
+
+    /**
+     * Returns the mean request processing time in milliseconds since resetting
+     * the statistics.
+     *
+     * @see #resetStatistics()
+     */
     double getMeanRequestDurationMsec();
-    
-    double getMinRequestDurationMsec();
 
+    /**
+     * Returns the standard deviation of requests since resetting the
+     * statistics. If zero or one requests have been collected only, this method
+     * returns zero.
+     *
+     * @see #resetStatistics()
+     */
     double getStandardDeviationDurationMsec();
 
+    /**
+     * Resets all statistics values and restarts from zero.
+     */
     void resetStatistics();
 
 }
---
+++ bundles/engine/src/test/java/org/apache/sling/engine/impl/RequestProcessorMBeanImplTest.java
@@ -0,0 +1,66 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.sling.engine.impl;
+
+import java.util.Random;
+
+import javax.management.NotCompliantMBeanException;
+
+import junit.framework.TestCase;
+
+import org.apache.commons.math.stat.descriptive.SummaryStatistics;
+
+public class RequestProcessorMBeanImplTest extends TestCase {
+
+    /**
+     * Asserts that the simple standard deviation algorithm used by the
+     * RequestProcessorMBeanImpl is equivalent to the Commons Math
+     * SummaryStatistics implementation
+     *
+     * @throws NotCompliantMBeanException not expected
+     */
+    public void test_statistics() throws NotCompliantMBeanException {
+        final SummaryStatistics mathStats = new SummaryStatistics();
+        final RequestProcessorMBeanImpl bean = new RequestProcessorMBeanImpl();
+
+        final Random random = new Random(System.currentTimeMillis() / 17);
+        final int num = 10000;
+        final int min = 85;
+        final int max = 250;
+        for (int i = 0; i < num; i++) {
+            final long value = min + random.nextInt(max - min);
+            mathStats.addValue(value);
+            bean.addRequestDuration(value);
+        }
+
+        TestCase.assertEquals("Number of points must be the same", mathStats.getN(), bean.getRequestsCount());
+        TestCase.assertEquals("Min must be equal", (long) mathStats.getMin(), bean.getMinRequestDurationMsec());
+        TestCase.assertEquals("Max must be equal", (long) mathStats.getMax(), bean.getMaxRequestDurationMsec());
+        assertAlmostEqual("Mean", mathStats.getMean(), bean.getMeanRequestDurationMsec(), num);
+        assertAlmostEqual("Standard Deviation", mathStats.getStandardDeviation(),
+            bean.getStandardDeviationDurationMsec(), num);
+    }
+
+    private void assertAlmostEqual(final String message, final double v1, final double v2, int samples) {
+        final double centi = v1 / samples;
+        if (v2 < (v1 - centi) || v2 > (v1 + centi)) {
+            TestCase.fail(message + " (expected: " + v2 + " in (" + (v1 - centi) + "," + (v1 + centi) + "))");
+        }
+    }
+}
