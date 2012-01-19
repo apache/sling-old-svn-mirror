8698567469bd81c005785b06121438fcd24da474
Carsten Ziegeler <cziegeler@apache.org>
2012-01-19 16:16:55 +0000

    SLING-2376 : New Startup Features
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk@1233447 13f79535-47bb-0310-9956-ffa450edef68

--- launchpad/base/src/main/java/org/apache/sling/launchpad/base/impl/DirectoryUtil.java
+++ launchpad/base/src/main/java/org/apache/sling/launchpad/base/impl/DirectoryUtil.java
@@ -18,6 +18,9 @@ package org.apache.sling.launchpad.base.impl;
 
 import java.io.File;
 import java.io.FileFilter;
+import java.util.Map;
+
+import org.apache.sling.launchpad.base.shared.SharedConstants;
 
 public class DirectoryUtil {
 
@@ -26,13 +29,23 @@ public class DirectoryUtil {
      */
     private static final String[] BUNDLE_EXTENSIONS = { ".jar", ".war" };
 
+    /**
+     * The path of startup bundles in the sling home
+     */
+    public static final String PATH_STARTUP = "startup";
+
+    /**
+     * The path of startup bundles in the sling home
+     */
+    public static final String PATH_CONF = "conf";
+
     //---------- FileFilter implementations to scan startup folders
 
     /**
      * Simple directory filter
      */
     public static final FileFilter DIRECTORY_FILTER = new FileFilter() {
-        public boolean accept(File f) {
+        public boolean accept(final File f) {
             return f.isDirectory();
         }
     };
@@ -41,7 +54,7 @@ public class DirectoryUtil {
      * Simple bundle file filter
      */
     public static final FileFilter BUNDLE_FILE_FILTER = new FileFilter() {
-        public boolean accept(File f) {
+        public boolean accept(final File f) {
             return f.isFile() && isBundle(f.getName());
         }
     };
@@ -52,12 +65,34 @@ public class DirectoryUtil {
      * @param path the path to the file
      * @return true if the path could be a bundle
      */
-    public static boolean isBundle(String path) {
+    public static boolean isBundle(final String path) {
         for (String extension : BUNDLE_EXTENSIONS) {
             if (path.endsWith(extension)) {
                 return true;
             }
         }
         return false;
+    }
+
+    public static File getHomeDir(final Map<String, String> properties) {
+        String home = properties.get(SharedConstants.SLING_LAUNCHPAD);
+        if (home == null) {
+            home = properties.get(SharedConstants.SLING_HOME);
+        }
+        return new File(home);
+    }
+
+    /**
+     * Return the config dir.
+     */
+    public static File getConfigDir(final Map<String, String> properties) {
+        return new File(getHomeDir(properties), PATH_CONF);
+    }
+
+    /**
+     * Return the startup dir.
+     */
+    public static File getStartupDir(final Map<String, String> properties) {
+        return new File(getHomeDir(properties), PATH_STARTUP);
     }
 }
