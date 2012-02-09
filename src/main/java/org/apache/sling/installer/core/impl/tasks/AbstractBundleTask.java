c4c5c5b1a889f66284a7726139d1c864327e0a9a
Carsten Ziegeler <cziegeler@apache.org>
2012-02-09 20:39:15 +0000

    SLING-2414 : Install bundles in the order of their start level
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk/installer/core@1242512 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/sling/installer/core/impl/tasks/AbstractBundleTask.java
+++ src/main/java/org/apache/sling/installer/core/impl/tasks/AbstractBundleTask.java
@@ -18,6 +18,7 @@
  */
 package org.apache.sling.installer.core.impl.tasks;
 
+import org.apache.sling.installer.api.InstallableResource;
 import org.apache.sling.installer.api.tasks.TaskResourceGroup;
 import org.apache.sling.installer.core.impl.AbstractInstallTask;
 import org.osgi.framework.Bundle;
@@ -52,6 +53,51 @@ public abstract class AbstractBundleTask extends AbstractInstallTask {
 
     protected BundleTaskCreator getCreator() {
         return this.creator;
+    }
+
+    /**
+     * Detect the start level for the resource.
+     */
+    protected int getBundleStartLevel() {
+        int startLevel = 0;
+        final Object providedLevel;
+
+        if (this.getResource().getDictionary() != null) {
+            if ( this.getResource().getDictionary().get(InstallableResource.BUNDLE_START_LEVEL) != null ) {
+                providedLevel = this.getResource().getDictionary().get(InstallableResource.BUNDLE_START_LEVEL);
+            } else {
+                providedLevel = this.getResource().getDictionary().get(InstallableResource.INSTALLATION_HINT);
+            }
+        } else {
+            providedLevel = null;
+        }
+        if ( providedLevel != null ) {
+            if ( providedLevel instanceof Number ) {
+                startLevel = ((Number)providedLevel).intValue();
+            } else {
+                try {
+                    startLevel = Integer.valueOf(providedLevel.toString());
+                } catch (final NumberFormatException nfe) {
+                    // ignore this
+                }
+            }
+        }
+        return startLevel;
+    }
+
+    /**
+     * Get sortable start level - low levels before high levels
+     */
+    protected String getSortableStartLevel() {
+        final int startLevel = this.getBundleStartLevel();
+        if ( startLevel == 0 ) {
+            return "999";
+        } else if ( startLevel < 10 ) {
+            return "00" + String.valueOf(startLevel);
+        } else if ( startLevel < 100 ) {
+            return "0" + String.valueOf(startLevel);
+        }
+        return String.valueOf(startLevel);
     }
 
     /**
