3884c55dd74bc931d69db996d16f75cf4bc2eedf
Bertrand Delacretaz <bdelacretaz@apache.org>
2012-08-20 16:19:59 +0000

    SLING-2567 - recursive test passes + logging added to RefreshDependenciesUtil
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk@1375092 13f79535-47bb-0310-9956-ffa450edef68

--- installer/core/src/main/java/org/apache/sling/installer/core/impl/util/RefreshDependenciesUtil.java
+++ installer/core/src/main/java/org/apache/sling/installer/core/impl/util/RefreshDependenciesUtil.java
@@ -18,7 +18,6 @@
  */
 package org.apache.sling.installer.core.impl.util;
 
-import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -34,6 +33,7 @@ import org.slf4j.LoggerFactory;
 /** Find out if a packages refresh will affect a given bundle */
 class RefreshDependenciesUtil {
 
+    private final Logger log = LoggerFactory.getLogger(getClass());
     private final PackageAdmin pckAdmin;
 
     public RefreshDependenciesUtil(final PackageAdmin pa) {
@@ -56,41 +56,51 @@ class RefreshDependenciesUtil {
      * Check whether a packages refresh of the supplied bundles would affect targetBundle
      */
     boolean isBundleAffected(Bundle targetBundle, final List<Bundle> bundles) {
-        // we put all bundle ids into a set
+        final Set<Long> checkedId = new HashSet<Long>();
+        final boolean result = isBundleAffected(checkedId, targetBundle, bundles);
+        log.debug("isBundleAffected({}, {}) -> {}", new Object[] { targetBundle, bundles, result});
+        return result;
+    }
+    
+    private boolean isBundleAffected(Set<Long> checkedId, Bundle targetBundle, final List<Bundle> bundles) {
+        
+        // Avoid cycles
+        if(checkedId.contains(targetBundle.getBundleId())) {
+            return false;
+        }
+        checkedId.add(targetBundle.getBundleId());
+        log.debug("Checking if {} is affected by {}", targetBundle, bundles);
+        
+        // ID of bundles that we check against
         final Set<Long> ids = new HashSet<Long>();
         for(final Bundle b : bundles) {
             ids.add(b.getBundleId());
         }
-
-        final Set<Long> processed = new HashSet<Long>();
-        final List<Bundle> toProcess = new ArrayList<Bundle>();
-        toProcess.add(targetBundle);
-        processed.add(targetBundle.getBundleId());
-
-        while ( !toProcess.isEmpty() ) {
-            final Bundle bundle = toProcess.remove(0);
-
-            if ( ids.contains(bundle.getBundleId()) ) {
-                return true;
-            }
-
-            for(final String name : this.getImportPackages(bundle) ) {
-
-                final ExportedPackage[] pcks = this.pckAdmin.getExportedPackages(name);
-                if ( pcks != null ) {
-                    for(final ExportedPackage pck : pcks) {
-                        final Bundle exportingBundle = pck.getExportingBundle();
-                        if ( exportingBundle.getBundleId() == 0 || exportingBundle.getBundleId() == targetBundle.getBundleId() ) {
-                            continue;
-                        }
-                        if ( ids.contains(exportingBundle.getBundleId()) ) {
-                            return true;
-                        }
-                        if ( !processed.contains(exportingBundle.getBundleId())) {
-                            processed.add(exportingBundle.getBundleId());
-                            toProcess.add(exportingBundle);
-                        }
+        
+        // Find all bundles from which we import packages, return true if one of them is in our bundles list,
+        // and call this method recursively on them as well.
+        // We don't care about possible duplicates in there, will be removed by the above cycle avoidance code.
+        for(final String name : getImportPackages(targetBundle)) {
+            final ExportedPackage[] pcks = this.pckAdmin.getExportedPackages(name);
+            if ( pcks == null ) {
+                log.debug("No bundles present that export {}", name);
+            } else {
+                log.debug("{} imports {}", targetBundle, name);
+                for(final ExportedPackage pck : pcks) {
+                    final Bundle exportingBundle = pck.getExportingBundle();
+                    log.debug("Checking {} which exports {}", exportingBundle, pck.getName());
+                    if ( exportingBundle.getBundleId() == 0 || exportingBundle.getBundleId() == targetBundle.getBundleId() ) {
+                        log.debug("Exporting bundle {} is framework bundle or self, ignored", exportingBundle);
+                        continue;
+                    } else if ( ids.contains(exportingBundle.getBundleId()) ) {
+                        log.debug("Bundle {} affects {} due to package {}, returning true", 
+                                new Object[] { exportingBundle, targetBundle, pck.getName() });
+                        return true;
+                    } else if(isBundleAffected(checkedId, exportingBundle, bundles)) {
+                        log.debug("{} recursively affects {}, returning true", exportingBundle, targetBundle); 
+                        return true;
                     }
+                    log.debug("{} does not affect {}", exportingBundle, targetBundle); 
                 }
             }
         }
