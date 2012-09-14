77bff3a28d7d20ec43c411e674767084fcdcf58b
Jean Christophe Kautzmann <jckautzmann@apache.org>
2012-09-14 13:01:12 +0000

    SLING-2594 - server-side test script without TEST_PASSED string should fail
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk/testing/junit/scriptable@1384750 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/test/java/org/apache/sling/junit/tests/TestAllPathsTest.java
@@ -0,0 +1,44 @@
+/*************************************************************************
+ *
+ * ADOBE CONFIDENTIAL
+ * __________________
+ *
+ *  Copyright 2012 Adobe Systems Incorporated
+ *  All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains
+ * the property of Adobe Systems Incorporated and its suppliers,
+ * if any.  The intellectual and technical concepts contained
+ * herein are proprietary to Adobe Systems Incorporated and its
+ * suppliers and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from Adobe Systems Incorporated.
+ **************************************************************************/
+package java.org.apache.sling.junit.tests;
+
+import org.junit.Test;
+import org.apache.sling.junit.scriptable.TestAllPaths;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class TestAllPathsTest {
+
+    @Test
+    public void checkTestTest() throws Exception {
+        final String comment =      "# this is a comment" + "\n";
+        final String empty =        "    " + "\n";
+        final String testPassed =   "TEST_PASSED" + "\n";
+        final String any =          "this is any line" + " \n";
+        final String script1 = comment +  empty + testPassed;
+        final String script2 = comment +  empty + testPassed + testPassed;
+        final String script3 = comment +  empty + testPassed + any;
+        final String script4 = comment +  empty;
+        assertTrue(TestAllPaths.checkTest(script1));
+        assertFalse(TestAllPaths.checkTest(script2));
+        assertFalse(TestAllPaths.checkTest(script3));
+        assertFalse(TestAllPaths.checkTest(script4));
+    }
+
+}
