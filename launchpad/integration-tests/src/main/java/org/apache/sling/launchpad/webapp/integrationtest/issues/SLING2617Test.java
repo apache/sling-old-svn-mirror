1e3e42ee6188ca6a2c52b00914a077e46449207f
Bertrand Delacretaz <bdelacretaz@apache.org>
2012-10-10 16:10:08 +0000

    SLING-2617 - integration tests
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk@1396663 13f79535-47bb-0310-9956-ffa450edef68

---
+++ launchpad/integration-tests/src/main/java/org/apache/sling/launchpad/webapp/integrationtest/issues/SLING2617Test.java
@@ -0,0 +1,55 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.sling.launchpad.webapp.integrationtest.issues;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.sling.commons.testing.integration.HttpTestBase;
+
+public class SLING2617Test extends HttpTestBase {
+    
+    private final String TEST_PATH = "/" + getClass().getSimpleName();
+
+    public void testDateBeanProperties() throws Exception {
+        final String [] mustContain = { 
+                "New time (123456)",
+                "JSP should have access to org.apache.sling.scripting.jsp.jasper.JasperException: YES",
+                "All good!" 
+        };
+
+        final TestNode tn = new TestNode(HTTP_BASE_URL + TEST_PATH, null);
+
+        String toDelete = null;
+        try {
+            toDelete = uploadTestScript(tn.scriptPath, "issues/sling2617/bean-set-get.jsp", "html.jsp");
+            final String content = getContent(tn.nodeUrl + ".html", CONTENT_TYPE_HTML,
+                    null, HttpServletResponse.SC_OK);
+
+            for(String str : mustContain) {
+                assertTrue("Content must contain " + str + " (" + content + ")", content.contains(str));
+            }
+
+        } finally {
+            if (toDelete != null) {
+                testClient.delete(toDelete);
+            }
+            tn.delete();
+        }
+    }
+}
