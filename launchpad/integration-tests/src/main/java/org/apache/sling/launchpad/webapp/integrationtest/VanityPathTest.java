bb7008845bb2f776b91ac169c98824e8e13637e2
Justin Edelson <justin@apache.org>
2012-10-30 16:36:04 +0000

    SLING-2639 - adding check for extension in resource names with vanity paths
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk@1403772 13f79535-47bb-0310-9956-ffa450edef68

--- launchpad/integration-tests/src/main/java/org/apache/sling/launchpad/webapp/integrationtest/VanityPathTest.java
+++ launchpad/integration-tests/src/main/java/org/apache/sling/launchpad/webapp/integrationtest/VanityPathTest.java
@@ -17,9 +17,13 @@
 package org.apache.sling.launchpad.webapp.integrationtest;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import org.apache.commons.httpclient.NameValuePair;
 import org.apache.commons.httpclient.methods.GetMethod;
 import org.apache.sling.commons.testing.integration.HttpTestBase;
 import org.apache.sling.servlets.post.SlingPostConstants;
@@ -145,7 +149,39 @@ public class VanityPathTest extends HttpTestBase {
         assertNotNull(location);
         assertEquals(removeHttpBase(createdNodeUrl) + ".test.json", location);
     }
-    
+
+    /** test vanity path on a path with an extension with a redirect */
+    public void testRedirectOnPathWithExtension() throws IOException {
+        // create a node with a vanity path
+        Map<String, String> props = new HashMap<String, String>();
+        props.put("jcr:mixinTypes", "sling:VanityPath");
+        props.put("sling:vanityPath", vanityPath);
+        props.put("sling:redirect", "true");
+        String createdNodeUrl = testClient.createNode(postUrl, props);
+        
+        String pathWithExtension = removeHttpBase(createdNodeUrl) + ".ext";
+        List<NameValuePair> moveParams = Arrays.asList(
+                new NameValuePair(":dest", pathWithExtension),
+                new NameValuePair(":operation", "move"));
+        assertPostStatus(createdNodeUrl, 201, moveParams, "Could not move created node from " + createdNodeUrl);
+        createdNodeUrl = createdNodeUrl + ".ext";
+        
+        waitForMapReload();
+
+        // get the created node's vanity path without following redirects
+        GetMethod get = new GetMethod(vanityUrl);
+        get.setFollowRedirects(false);
+        int status = httpClient.executeMethod(get);
+
+        // expect temporary redirect ...
+        assertEquals(302, status);
+
+        // ... to the created node
+        String location = get.getResponseHeader("Location").getValue();
+        assertNotNull(location);
+        assertEquals(pathWithExtension, location);
+    }
+
     private String removeHttpBase(String url) {
         return url.startsWith(HTTP_BASE_URL) ? url.substring(HTTP_BASE_URL.length()) : url;
     }
