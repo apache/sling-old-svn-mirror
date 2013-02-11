d414bb3115a276c4ac37330b957a9da4d3e3542e
Antonio Sanso <asanso@apache.org>
2013-02-11 12:38:55 +0000

    SLING-2718 - Add integration test for the error handling mechanism. - adding more integration tests
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk/launchpad/integration-tests@1444751 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/sling/launchpad/webapp/integrationtest/servlets/resolver/errorhandler/ErrorHandlingTest.java
+++ src/main/java/org/apache/sling/launchpad/webapp/integrationtest/servlets/resolver/errorhandler/ErrorHandlingTest.java
@@ -17,9 +17,14 @@
 package org.apache.sling.launchpad.webapp.integrationtest.servlets.resolver.errorhandler;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+
+import org.apache.commons.httpclient.NameValuePair;
 import org.apache.sling.launchpad.webapp.integrationtest.JspTestBase;
+import org.apache.sling.servlets.post.SlingPostConstants;
 
 
 /** Test the sling error handling mechanism http://sling.apache.org/site/errorhandling.html*/
@@ -54,8 +59,7 @@ public class ErrorHandlingTest extends JspTestBase {
 		uploadTestScript("servlets/errorhandler/500.jsp", "sling/servlet/errorhandler/500.jsp");
 		uploadTestScript("servlets/errorhandler/401.jsp", "sling/servlet/errorhandler/401.jsp");
 		uploadTestScript(THROW_ERROR_PATH+"/"+THROW_ERROR_PAGE, THROW_ERROR_PATH+"/"+THROW_ERROR_PAGE);
-		uploadTestScript(THROW_ERROR_PATH+"/"+"POST.jsp", THROW_ERROR_PATH+"/"+"POST.jsp");
-
+ 
 		final Map<String, String> props = new HashMap<String, String>();
 		props.put(SLING_RESOURCE_TYPE, TEST_ROOT+"/"+THROW_ERROR_PATH);
 		testNodePath = testClient.createNode(HTTP_BASE_URL + TEST_ROOT, props);
@@ -96,9 +100,19 @@ public class ErrorHandlingTest extends JspTestBase {
 	
 	public void test_500_errorhandling_POST_operation() throws IOException{	
 		final String expected = "Internal Server Error (500) - custom error page";
-		final String url =  testNodePath +SELECTOR_500+".html"; 
+		final String url =  testNodePath +".html"; 
+		uploadTestScript(THROW_ERROR_PATH+"/"+"POST.jsp", THROW_ERROR_PATH+"/"+"POST.jsp");
 		assertContains(getContent(url, CONTENT_TYPE_HTML,null,500,HTTP_METHOD_POST), expected);
 		//assertNotContains(getContent(url, CONTENT_TYPE_HTML,null,200), "All good");
  	}
+	
+	public void test_errorhandling_POST_operation_SlingPostServlet() throws IOException{
+		final String expected = "Exception thrown - custom error page";
+		final String url =  testNodePath +".html";
+		List <NameValuePair> params=new ArrayList<NameValuePair>();
+        params.add(new NameValuePair(SlingPostConstants.RP_OPERATION,"notExistingOperation"));
+        params.add(new NameValuePair(SlingPostConstants.RP_SEND_ERROR,"true"));        
+        assertContains(getContent(url, CONTENT_TYPE_HTML,params,500,HTTP_METHOD_POST), expected);
+	}
 
 }
