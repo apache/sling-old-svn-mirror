7d98561ec3029e18f29c631321194c0d3a2bb6fb
Felix Meschberger <fmeschbe@apache.org>
2013-03-21 14:48:49 +0000

    Switch back to using a regular ServiceUserName service tacking the service bundle as an argument.
    Reason: It makes no sense to impose on the use of the ServiceUserName serive on the using bundle. Rather the use of this service is a property of the ResourceResolverFactory and the SlingRepository service which want to derive the actual service name from its using bundle.
    
    git-svn-id: https://svn.apache.org/repos/asf/sling/whiteboard/fmeschbe/deprecate_login_administrative/serviceusermapper@1459341 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/sling/serviceusermapping/ServiceUserMapper.java
+++ src/main/java/org/apache/sling/serviceusermapping/ServiceUserMapper.java
@@ -18,27 +18,61 @@
  */
 package org.apache.sling.serviceusermapping;
 
+import org.osgi.framework.Bundle;
+
 import aQute.bnd.annotation.ProviderType;
 
+/**
+ * The <code>ServiceUserMapper</code> service can be used to map a service
+ * provided by a bundle to the name of a user account used to access the
+ * ResourceResolver used by the service to access its data.
+ * <p>
+ * The goal of this service is to allow services to be implemented accessing the
+ * storage with service-specific accounts which are tailored to allow the
+ * service appropriate access without requiring administrative level access to
+ * the storage.
+ * <p>
+ * In addition to allowing to phase out the use of
+ * {@code ResourceResolver.getAdministrativeResourceResolver} and
+ * {@code SlingRepository.loginAdministrative} it also allows to better account
+ * for changes to the storage by the different services.
+ * <p>
+ * This service is not intended to be used by the general user but by
+ * implementations of the {@code ResourceResolverFactory} and
+ * {@code SlingRepository} services.
+ * <p>
+ * This service is not intended to be implemented by clients.
+ */
 @ProviderType
 public interface ServiceUserMapper {
+
+    /**
+     * The name of the Bundle manifest header providing the name of the service
+     * provided by the bundle. If this header is missing or empty, the bundle's
+     * symbolic name is used instead to name the service.
+     */
+    String BUNDLE_HEADER_SERVICE_NAME = "Sling-Service";
 
     /**
      * Returns the name of the service represented by the {@code bundle} and the
      * {@code serviceInfo}.
      *
+     * @param bundle The bundle implementing the service request access to
+     *            resources.
      * @param serviceInfo Additional information about the concrete service
      *            requesting access. This parameter is optional and may be
      *            {@code null}.
      * @return The name of the service represented by the bundle along with the
      *         additional service information.
      */
-    String getServiceName(String serviceInfo);
+    String getServiceName(Bundle bundle, String serviceInfo);
 
     /**
      * Returns the name of a user to the be used to access the Sling Resource
      * tree or the JCR Repository.
      *
+     * @param bundle The bundle implementing the service request access to
+     *            resources.
      * @param serviceInfo Additional information about the concrete service
      *            requesting access. This parameter is optional and may be
      *            {@code null}.
@@ -46,6 +80,5 @@ public interface ServiceUserMapper {
      *         for the service. This may be {@code null} to only grant guest
      *         level (or anonymous level) access to the resources.
      */
-    String getUserForService(String serviceInfo);
-
+    String getUserForService(Bundle bundle, String serviceInfo);
 }
