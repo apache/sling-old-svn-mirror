35635b63ade0e27a37a6acc47dd02aa9e117b9a8
Mike Müller <mykee@apache.org>
2013-03-30 17:17:36 +0000

    SLING-2698 - resource access security service for resource providers without backing ACLs, Part 4
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk/bundles/api@1462804 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/sling/api/security/ResourceAccessSecurity.java
+++ src/main/java/org/apache/sling/api/security/ResourceAccessSecurity.java
@@ -19,35 +19,55 @@
 package org.apache.sling.api.security;
 
 import org.apache.sling.api.resource.Resource;
-
+import org.apache.sling.api.resource.ResourceResolver;
 
 /**
- * The <code>ResourceAccessSecurity</code> defines a service API which might 
- * be used in implementations of resource providers where the underlaying 
- * persistence layer does not have any ACLs. The service should it make
- * easy to implement a lightweight access control in such sort of providers.
- *
- * - Expected to only be implemented once in the framework/application
- *   (much like the OSGi LogService or Configuration Admin Service)
- * - ResourceProvider implementations are encouraged to use 
- *   this service for access control unless the underlying
- *   storage already has it.
- *
+ * The <code>ResourceAccessSecurity</code> defines a service API which might be
+ * used in implementations of resource providers where the underlaying
+ * persistence layer does not have any ACLs. The service should it make easy to
+ * implement a lightweight access control in such sort of providers.
+ * 
+ * - Expected to only be implemented once in the framework/application (much
+ * like the OSGi LogService or Configuration Admin Service) - ResourceProvider
+ * implementations are encouraged to use this service for access control unless
+ * the underlying storage already has it.
+ * 
  */
 
 public interface ResourceAccessSecurity {
-    
-    public Resource checkReadPermission( Resource resource );
-    public boolean canCreate( String absPathName, String user );
-    public boolean canUpdate( Resource resource );
-    public boolean canDelete( Resource resource );
-    public boolean canExecute( Resource resource );
 
-    public boolean canReadValue( Resource resource, String valueName );
-    public boolean canCreateValue( Resource resource, String valueName );
-    public boolean canUpdateValue( Resource resource, String valueName );
-    public boolean canDeleteValue( Resource resource, String valueName );
+    public Resource getReadableResource(Resource resource);
 
-    public String sanitizeQuery( String query, String language, String user ) throws AccessSecurityException;
+    public boolean canCreate(String absPathName, ResourceResolver resourceResolver);
+
+    public boolean canUpdate(Resource resource);
+
+    public boolean canDelete(Resource resource);
+
+    public boolean canExecute(Resource resource);
+
+    public boolean canReadValue(Resource resource, String valueName);
+
+    public boolean canSetValue(Resource resource, String valueName);
+
+    public boolean canDeleteValue(Resource resource, String valueName);
+
+    /**
+     * Allows to transform the query based on the current
+     * user's credentials. Can be used to narrow down queries to omit results
+     * that the current user is not allowed to see anyway, speeding up
+     * downstream access control.
+     * 
+     * Query transformations are not critical with respect to access control as results
+     * are checked using the canRead.. methods anyway. 
+     * 
+     * @param query the query
+     * @param language the language in which the query is expressed
+     * @param resourceResolver the resource resolver which resolves the query
+     * @return the transformed query
+     * @throws AccessSecurityException 
+     */
+    public String transformQuery(String query, String language, ResourceResolver resourceResolver)
+            throws AccessSecurityException;
 
 }
