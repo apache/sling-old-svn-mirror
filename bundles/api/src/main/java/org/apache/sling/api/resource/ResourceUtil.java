647cd894cfbd7c7e0c54a13d0d4cafcaf321eabd
Carsten Ziegeler <cziegeler@apache.org>
2013-04-30 07:45:17 +0000

    SLING-2845 :  ResourceUtil.getOrCreateResource should catch PersistenceException when calling create
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk@1477494 13f79535-47bb-0310-9956-ffa450edef68

--- bundles/api/src/main/java/org/apache/sling/api/resource/ResourceUtil.java
+++ bundles/api/src/main/java/org/apache/sling/api/resource/ResourceUtil.java
@@ -574,7 +574,17 @@ public class ResourceUtil {
             if ( autoCommit ) {
                 resolver.refresh();
             }
-            rsrc = resolver.create(parentResource, name, resourceProperties);
+            try {
+                rsrc = resolver.create(parentResource, name, resourceProperties);
+            } catch ( final PersistenceException pe ) {
+                // this could be thrown because someone else tried to create this
+                // node concurrently
+                resolver.refresh();
+                rsrc = resolver.getResource(parentResource, name);
+                if ( rsrc == null ) {
+                    throw pe;
+                }
+            }
             if ( autoCommit ) {
                 try {
                     resolver.commit();
