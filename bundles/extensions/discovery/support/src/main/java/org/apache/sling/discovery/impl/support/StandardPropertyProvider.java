c5715dd34e5079f67cf0277e908d5600c5f51e8e
Carsten Ziegeler <cziegeler@apache.org>
2013-05-14 08:11:21 +0000

    Create new support module
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk@1482246 13f79535-47bb-0310-9956-ffa450edef68

--- bundles/extensions/discovery/support/src/main/java/org/apache/sling/discovery/impl/support/StandardPropertyProvider.java
+++ bundles/extensions/discovery/support/src/main/java/org/apache/sling/discovery/impl/support/StandardPropertyProvider.java
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package org.apache.sling.discovery.impl.standalone;
+package org.apache.sling.discovery.impl.support;
 
 import java.util.ArrayList;
 import java.util.Collection;
