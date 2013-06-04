3a9f18447a30f3220c2279675687ca8f3b8d7d0e
Robert Munteanu <rombert@apache.org>
2013-06-04 09:10:51 +0000

    SLING-2893 - Add documentation and integration tests to verify XML parsing functionality
    Added a SaxTest
    
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk@1489350 13f79535-47bb-0310-9956-ffa450edef68

---
+++ launchpad/integration-tests/src/main/java/org/apache/sling/launchpad/webapp/integrationtest/xml/SaxTest.java
@@ -0,0 +1,38 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.sling.launchpad.webapp.integrationtest.xml;
+
+import java.io.IOException;
+
+import org.apache.sling.commons.testing.integration.HttpTestBase;
+
+/**
+ * The <tt>SaxTest</tt> verifies that a simple SAX operation is successful
+ * 
+ */
+public class SaxTest extends HttpTestBase {
+
+    public void testSaxExecutionIsSuccessful() throws IOException {
+
+        String content = getContent(HTTP_BASE_URL + "/bin/sax.xml", CONTENT_TYPE_PLAIN);
+
+        assertEquals(content, "SAX");
+    }
+
+}
