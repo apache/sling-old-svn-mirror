f6e64956af240e731497e7e042dc488c2fd0edc2
Felix Meschberger <fmeschbe@apache.org>
2013-08-05 10:08:50 +0000

    SLING-2944 Implement Service User Mapper
    * Add ServiceUserMapper service and implementation bundle
    * Add service login methods to ResourceResolverFactory
      and SlingRepository
    * Add implementations of new methods
    
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk/bundles/api@1510413 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/sling/api/resource/ResourceProviderFactory.java
+++ src/main/java/org/apache/sling/api/resource/ResourceProviderFactory.java
@@ -23,71 +23,111 @@ import java.util.Map;
 /**
  * The <code>ResourceProviderFactory</code> defines the service API to get and
  * create <code>ResourceProviders</code>s dynamically on a per usage base.
- *
  * Instead of sharing a resource provider between resource resolvers, the
  * factory allows to create an own instance of a resource provider per resource
- * resolver.
- * The factory also supports authentication.
+ * resolver. The factory also supports authentication.
  * <p>
- * If the resource provider is not used anymore and implements the {@link DynamicResourceProvider}
- * interface, the close method should be called.
+ * If the resource provider is not used anymore and implements the
+ * {@link DynamicResourceProvider} interface, the close method should be called.
  *
  * @since 2.2.0
  */
 public interface ResourceProviderFactory {
 
     /**
-     * A required resource provider factory is accessed directly when a new resource resolver
-     * is created. Only if authentication against all required resource provider factories
-     * is successful, a resource resolver is created by the resource resolver factory.
-     * Boolean service property, default value is <code>false</true>
+     * A required resource provider factory is accessed directly when a new
+     * resource resolver is created. Only if authentication against all required
+     * resource provider factories is successful, a resource resolver is created
+     * by the resource resolver factory. Boolean service property, default value
+     * is <code>false</true>
      */
     String PROPERTY_REQUIRED = "required";
+
+    /**
+     * The authentication information property referrring to the bundle
+     * providing a service for which a resource provider is to be retrieved. If
+     * this property is provided, the
+     * {@link ResourceResolverFactory#SUBSERVICE} property may also be
+     * present.
+     * <p>
+     * {@link ResourceResolverFactory} implementations must provide this
+     * property if their implementation of the
+     * {@link ResourceResolverFactory#getServiceResourceResolver(Map)} method
+     * use a resource provider factory.
+     * <p>
+     * The type of this property, if present, is
+     * <code>org.osgi.framework.Bundle</code>.
+     *
+     * @since 2.4 (bundle version 2.5.0)
+     */
+    String SERVICE_BUNDLE = "sling.service.bundle";
 
     /**
      * Returns a new {@link ResourceProvider} instance with further
      * configuration taken from the given <code>authenticationInfo</code> map.
      * Generally this map will contain a user name and password to authenticate.
      * <p>
+     * The <code>authenticationInfo</code> map will in general contain the same
+     * information as provided to the respective {@link ResourceResolver}
+     * method. For
+     * <p>
      * If the <code>authenticationInfo</code> map is <code>null</code> the
-     * <code>ResourceProvider</code> returned will generally not be authenticated
-     * and only provide minimal privileges, if any at all.
+     * <code>ResourceProvider</code> returned will generally not be
+     * authenticated and only provide minimal privileges, if any at all.
+     * <p>
+     * Implementations must ignore the {@value ResourceResolverFactory#USER}
+     * property the {@link #SERVICE_BUNDLE} property is provided to implement
+     * service authentication.
+     * <p>
+     * The {@value ResourceResolverFactory#USER_IMPERSONATION} property is
+     * obeyed but requires that the actual user has permission to impersonate as
+     * the requested user. If such permission is missing, a
+     * {@code LoginException} is thrown.
      *
-     * @param authenticationInfo
-     *            A map of further credential information which may be used by
-     *            the implementation to parameterize how the resource provider is
-     *            created. This may be <code>null</code>.
-     * @return A {@link ResourceProvider} according to the <code>authenticationInfo</code>.
-     * @throws LoginException
-     *             If an error occurs creating the new <code>ResourceProvider</code> with the
-     *             provided credential data.
+     * @param authenticationInfo A map of further credential information which
+     *            may be used by the implementation to parameterize how the
+     *            resource provider is created. This may be <code>null</code>.
+     * @return A {@link ResourceProvider} according to the
+     *         <code>authenticationInfo</code>.
+     * @throws LoginException If an error occurs creating the new
+     *             <code>ResourceProvider</code> with the provided credential
+     *             data.
+     * @see <a
+     *      href="http://sling.apache.org/documentation/the-sling-engine/service-authentication.html">Service
+     *      Authentication</a>
      */
     ResourceProvider getResourceProvider(Map<String, Object> authenticationInfo) throws LoginException;
 
     /**
      * Returns a new {@link ResourceProvider} instance with administrative
-     * privileges with further configuration taken from the given <code>authenticationInfo</code>
-     * map.
+     * privileges with further configuration taken from the given
+     * <code>authenticationInfo</code> map.
      * <p>
      * Note, that if the <code>authenticationInfo</code> map contains the
-     * {@link ResourceResolverFactory#USER_IMPERSONATION} attribute the <code>ResourceProvider</code> returned will only
-     * have administrative privileges if the user identified by the property has administrative
+     * {@link ResourceResolverFactory#USER_IMPERSONATION} attribute the
+     * <code>ResourceProvider</code> returned will only have administrative
+     * privileges if the user identified by the property has administrative
      * privileges.
      * <p>
-     * <b><i>NOTE: This method is intended for use by infrastructure bundles to access the
-     * resource tree and provide general services. This method MUST not be used to handle client
-     * requests of whatever kinds. To handle client requests a regular authenticated resource
-     * provider retrieved through {@link #getResourceProvider(Map)} must be used.</i></b>
+     * Implementations of this method should throw {@code LoginException} if
+     * they don't support it.
      *
-     * @param authenticationInfo
-     *            A map of further credential information which may be used by
-     *            the implementation to parameterize how the resource provider is
-     *            created. This may be <code>null</code>.
+     * @param authenticationInfo A map of further credential information which
+     *            may be used by the implementation to parameterize how the
+     *            resource provider is created. This may be <code>null</code>.
      * @return A {@link ResourceProvider} with administrative privileges unless
-     *         the {@link ResourceResolverFactory#USER_IMPERSONATION} was set in the <code>authenticationInfo</code>.
-     * @throws LoginException
-     *             If an error occurs creating the new <code>ResourceResolverFactory</code> with the
-     *             provided credential data.
+     *         the {@link ResourceResolverFactory#USER_IMPERSONATION} was set in
+     *         the <code>authenticationInfo</code>.
+     * @throws LoginException If an error occurs creating the new
+     *             <code>ResourceResolverFactory</code> with the provided
+     *             credential data.
+     * @deprecated as of 2.4 (bundle version 2.5.0) because of inherent security
+     *             issues. Implementations may implement this method at their
+     *             discretion but must support the new service based resource
+     *             provider generation in the {@link #getResourceProvider(Map)}
+     *             method honouring the {@link #SERVICE_BUNDLE} and
+     *             {@link ResourceResolverFactory#SUBSERVICE} properties.
      */
+    @Deprecated
     ResourceProvider getAdministrativeResourceProvider(Map<String, Object> authenticationInfo) throws LoginException;
 }
--- src/main/java/org/apache/sling/api/resource/package-info.java
+++ src/main/java/org/apache/sling/api/resource/package-info.java
@@ -17,7 +17,7 @@
  * under the License.
  */
 
-@Version("2.3.2")
+@Version("2.4")
 package org.apache.sling.api.resource;
 
 import aQute.bnd.annotation.Version;
