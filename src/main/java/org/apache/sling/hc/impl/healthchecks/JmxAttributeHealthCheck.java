22ccdfc3c02846d8e92d53dcca92bff30044f9ee
Bertrand Delacretaz <bdelacretaz@apache.org>
2013-08-08 13:11:07 +0000

    SLING-2987 - add metatype + reorganize packages
    git-svn-id: https://svn.apache.org/repos/asf/sling/branches/SLING-2987-healthcheck-redesign/hc-core@1511770 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/sling/hc/impl/healthchecks/JmxAttributeHealthCheck.java
+++ src/main/java/org/apache/sling/hc/impl/healthchecks/JmxAttributeHealthCheck.java
@@ -15,50 +15,54 @@
  * KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  */
-package org.apache.sling.hc.impl;
+package org.apache.sling.hc.impl.healthchecks;
 
+import java.lang.management.ManagementFactory;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
-import javax.script.Bindings;
-import javax.script.ScriptEngine;
-import javax.script.ScriptEngineManager;
+import javax.management.MBeanServer;
+import javax.management.ObjectName;
 
 import org.apache.felix.scr.annotations.Activate;
 import org.apache.felix.scr.annotations.Component;
 import org.apache.felix.scr.annotations.ConfigurationPolicy;
 import org.apache.felix.scr.annotations.Property;
-import org.apache.felix.scr.annotations.Reference;
 import org.apache.felix.scr.annotations.Service;
 import org.apache.sling.commons.osgi.PropertiesUtil;
 import org.apache.sling.hc.api.Constants;
 import org.apache.sling.hc.api.HealthCheck;
 import org.apache.sling.hc.api.Result;
 import org.apache.sling.hc.api.ResultLog;
-import org.osgi.framework.BundleContext;
+import org.apache.sling.hc.util.SimpleConstraintChecker;
 import org.osgi.service.component.ComponentContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-/** {@link HealthCheck} that checks a scriptable expression */
-@Component(configurationFactory=true, policy=ConfigurationPolicy.REQUIRE, metatype=true)
+/** {@link HealthCheck} that checks a single JMX attribute */
+@Component(
+        name="org.apache.sling.hc.JmxAttributeHealthCheck",
+        configurationFactory=true, 
+        policy=ConfigurationPolicy.REQUIRE, 
+        metatype=true)
 @Service
-public class ScriptableHealthCheck implements HealthCheck {
+public class JmxAttributeHealthCheck implements HealthCheck {
 
     private final Logger log = LoggerFactory.getLogger(getClass());
     private final Map<String, String> info = new HashMap<String, String>();
-    private String expression;
-    private String languageExtension;
-    private BundleContext bundleContext;
-    
-    public static final String DEFAULT_LANGUAGE_EXTENSION = "ecma";
+    private String mbeanName;
+    private String attributeName;
+    private String constraint;
 
     @Property
-    public static final String PROP_EXPRESSION = "expression";
+    public static final String PROP_OBJECT_NAME = "mbean.name";
     
     @Property
-    public static final String PROP_LANGUAGE_EXTENSION = "language.extension";
+    public static final String PROP_ATTRIBUTE_NAME = "attribute.name";
+    
+    @Property
+    public static final String PROP_CONSTRAINT = "attribute.value.constraint";
     
     @Property(cardinality=50)
     public static final String PROP_TAGS = Constants.HC_TAGS;
@@ -66,44 +70,36 @@ public class ScriptableHealthCheck implements HealthCheck {
     @Property
     public static final String PROP_NAME = Constants.HC_NAME;
     
-    @Reference
-    private ScriptEngineManager scriptEngineManager;
-    
     @Activate
     public void activate(ComponentContext ctx) {
-        bundleContext = ctx.getBundleContext();
-        expression = PropertiesUtil.toString(ctx.getProperties().get(PROP_EXPRESSION), "");
-        languageExtension = PropertiesUtil.toString(ctx.getProperties().get(PROP_LANGUAGE_EXTENSION), DEFAULT_LANGUAGE_EXTENSION);
+        mbeanName = PropertiesUtil.toString(ctx.getProperties().get(PROP_OBJECT_NAME), "");
+        attributeName = PropertiesUtil.toString(ctx.getProperties().get(PROP_ATTRIBUTE_NAME), "");
+        constraint = PropertiesUtil.toString(ctx.getProperties().get(PROP_CONSTRAINT), "");
         
-        info.put(PROP_EXPRESSION, expression);
-        info.put(PROP_LANGUAGE_EXTENSION, languageExtension);
+        info.put(PROP_OBJECT_NAME, mbeanName);
+        info.put(PROP_ATTRIBUTE_NAME, attributeName);
+        info.put(PROP_CONSTRAINT, constraint);
         info.put(Constants.HC_NAME, PropertiesUtil.toString(ctx.getProperties().get(Constants.HC_NAME), ""));
         info.put(Constants.HC_TAGS, 
                 Arrays.asList(PropertiesUtil.toStringArray(ctx.getProperties().get(Constants.HC_TAGS), new String[] {})).toString());
         
-        log.info("Activated, name={}, languageExtension={}, expression={}", languageExtension, expression);
+        log.info("Activated with HealthCheck name={}, objectName={}, attribute={}, constraint={}", 
+                new Object[] { info.get(Constants.HC_NAME), mbeanName, attributeName, constraint });
     }
     
     @Override
     public Result execute(ResultLog log) {
         final Result result = new Result(this, log);
-        log.debug("Checking expression [{}], language extension=[{}]", expression, languageExtension);
+        log.debug("Checking {} / {} with constraint {}", new Object[] { mbeanName, attributeName, constraint });
         try {
-            final ScriptEngine engine = scriptEngineManager.getEngineByExtension(languageExtension);
-            if(engine == null) {
-                log.warn("No ScriptEngine available for extension {}", languageExtension);
-            } else {
-                // TODO pluggable Bindings? Reuse the Sling bindings providers?
-                final Bindings b = engine.createBindings();
-                b.put("jmx", new JmxScriptBinding(log));
-                b.put("osgi", new OsgiScriptBinding(bundleContext, log));
-                final Object value = engine.eval(expression, b);
-                if(value!=null && "true".equals(value.toString())) {
-                    log.debug("Expression [{}] evaluates to true as expected", expression);
-                } else {
-                    log.warn("Expression [{}] does not evaluate to true, value={}", expression, value);
-                }
+            final MBeanServer jmxServer = ManagementFactory.getPlatformMBeanServer();
+            final ObjectName objectName = new ObjectName(mbeanName);
+            if(jmxServer.queryNames(objectName, null).size() == 0) {
+                log.error("MBean not found: {}", objectName);
             }
+            final Object value = jmxServer.getAttribute(objectName, attributeName);
+            log.debug("{} {} returns {}", new Object[] { mbeanName, attributeName, value });
+            new SimpleConstraintChecker().check(value, constraint, log);
         } catch(Exception e) {
             log.warn(e.toString(), e);
         }
