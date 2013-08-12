94656d1586c1c6905547c7bc7bb50cfb9498d153
Bertrand Delacretaz <bdelacretaz@apache.org>
2013-08-12 16:22:26 +0000

    SLING-2987 - split into several bundles
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk/contrib/extensions/healthcheck/api@1513189 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/sling/hc/util/SimpleConstraintCheckerTest.java
+++ src/test/java/org/apache/sling/hc/util/SimpleConstraintCheckerTest.java
@@ -15,7 +15,7 @@
  * KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  */
-package org.apache.sling.hc.impl;
+package org.apache.sling.hc.util;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
