e6866b4f8f3126fe578288e1d2a678c8d3379e4e
Bertrand Delacretaz <bdelacretaz@apache.org>
2013-09-09 12:28:01 +0000

    SLING-3048 - Filter that populates an slf4j MDC with request info - contributed by Chetan Mehrotra, thanks!
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk/contrib/extensions/slf4j-mdc@1521071 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/main/java/org/apache/sling/extensions/mdc/internal/SlingMDCFilter.java
@@ -0,0 +1,80 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.sling.extensions.mdc.internal;
+
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.resource.ResourceResolver;
+import org.slf4j.MDC;
+
+import javax.jcr.Session;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import java.io.IOException;
+
+class SlingMDCFilter implements Filter {
+    public static final String SLING_USER = "sling.userId";
+    public static final String JCR_SESSION_ID = "jcr.sessionId";
+
+    private static final String[] DEFAULT_KEY_NAMES = {
+            SLING_USER,
+            JCR_SESSION_ID
+    };
+
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse,
+                         FilterChain filterChain) throws IOException, ServletException {
+        final SlingHttpServletRequest request = (SlingHttpServletRequest) servletRequest;
+
+        try {
+            insertIntoMDC(request);
+            filterChain.doFilter(request, servletResponse);
+        } finally {
+            clearMDC();
+        }
+    }
+
+    private void clearMDC() {
+        for (String key : DEFAULT_KEY_NAMES) {
+            MDC.remove(key);
+        }
+    }
+
+    private void insertIntoMDC(SlingHttpServletRequest request) {
+        ResourceResolver rr = request.getResourceResolver();
+        if(rr.getUserID() != null){
+            MDC.put(SLING_USER,rr.getUserID());
+        }
+
+        Session session = rr.adaptTo(Session.class);
+        if(session != null){
+            MDC.put(JCR_SESSION_ID,session.toString());
+        }
+    }
+
+    public void destroy() {
+
+    }
+}
