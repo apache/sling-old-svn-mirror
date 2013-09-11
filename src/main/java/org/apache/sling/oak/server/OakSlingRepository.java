880e0ed408fa0115afaef5affe63d57ed5e1559e
Bertrand Delacretaz <bdelacretaz@apache.org>
2013-09-11 15:48:17 +0000

    SLING-2788 - loginAdministrative and Sling node types work now
    git-svn-id: https://svn.apache.org/repos/asf/sling/whiteboard/bdelacretaz/bdelacretaz-oak-server@1521913 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/sling/oak/server/OakSlingRepository.java
+++ src/main/java/org/apache/sling/oak/server/OakSlingRepository.java
@@ -17,7 +17,11 @@
  */
 package org.apache.sling.oak.server;
 
+import static com.google.common.collect.ImmutableSet.of;
+import static com.google.common.collect.Lists.newArrayList;
 import static java.util.Collections.singleton;
+import static org.apache.jackrabbit.oak.plugins.index.IndexConstants.INDEX_DEFINITIONS_NAME;
+import static org.apache.jackrabbit.oak.plugins.index.IndexUtils.createIndexDefinition;
 
 import java.security.Principal;
 import java.security.PrivilegedActionException;
@@ -44,10 +48,27 @@ import org.apache.felix.scr.annotations.Service;
 import org.apache.jackrabbit.oak.Oak;
 import org.apache.jackrabbit.oak.api.AuthInfo;
 import org.apache.jackrabbit.oak.api.ContentRepository;
-import org.apache.jackrabbit.oak.jcr.RepositoryImpl;
+import org.apache.jackrabbit.oak.jcr.repository.RepositoryImpl;
+import org.apache.jackrabbit.oak.plugins.commit.ConflictValidatorProvider;
+import org.apache.jackrabbit.oak.plugins.commit.JcrConflictHandler;
+import org.apache.jackrabbit.oak.plugins.index.aggregate.AggregateIndexProvider;
+import org.apache.jackrabbit.oak.plugins.index.aggregate.NodeAggregator;
+import org.apache.jackrabbit.oak.plugins.index.aggregate.SimpleNodeAggregator;
 import org.apache.jackrabbit.oak.plugins.index.lucene.LuceneIndexEditorProvider;
 import org.apache.jackrabbit.oak.plugins.index.lucene.LuceneIndexProvider;
+import org.apache.jackrabbit.oak.plugins.index.lucene.util.LuceneIndexHelper;
+import org.apache.jackrabbit.oak.plugins.index.nodetype.NodeTypeIndexProvider;
+import org.apache.jackrabbit.oak.plugins.index.property.PropertyIndexEditorProvider;
+import org.apache.jackrabbit.oak.plugins.index.property.PropertyIndexProvider;
+import org.apache.jackrabbit.oak.plugins.name.NameValidatorProvider;
+import org.apache.jackrabbit.oak.plugins.name.NamespaceValidatorProvider;
+import org.apache.jackrabbit.oak.plugins.nodetype.RegistrationEditorProvider;
+import org.apache.jackrabbit.oak.plugins.nodetype.TypeEditorProvider;
+import org.apache.jackrabbit.oak.plugins.nodetype.write.InitialContent;
+import org.apache.jackrabbit.oak.plugins.version.VersionEditorProvider;
 import org.apache.jackrabbit.oak.security.SecurityProviderImpl;
+import org.apache.jackrabbit.oak.spi.commit.EditorHook;
+import org.apache.jackrabbit.oak.spi.lifecycle.RepositoryInitializer;
 import org.apache.jackrabbit.oak.spi.security.ConfigurationParameters;
 import org.apache.jackrabbit.oak.spi.security.SecurityProvider;
 import org.apache.jackrabbit.oak.spi.security.authentication.AuthInfoImpl;
@@ -57,6 +78,8 @@ import org.apache.jackrabbit.oak.spi.security.privilege.PrivilegeConstants;
 import org.apache.jackrabbit.oak.spi.security.user.UserConfiguration;
 import org.apache.jackrabbit.oak.spi.security.user.UserConstants;
 import org.apache.jackrabbit.oak.spi.security.user.action.AccessControlAction;
+import org.apache.jackrabbit.oak.spi.state.NodeBuilder;
+import org.apache.jackrabbit.oak.spi.state.NodeState;
 import org.apache.jackrabbit.oak.spi.state.NodeStore;
 import org.apache.jackrabbit.oak.spi.whiteboard.OsgiWhiteboard;
 import org.apache.jackrabbit.oak.spi.whiteboard.Whiteboard;
@@ -89,11 +112,34 @@ public class OakSlingRepository extends AbstractNamespaceMappingRepository
         final Whiteboard whiteboard = new OsgiWhiteboard(ctx.getBundleContext());
         securityProvider = new SecurityProviderImpl(buildSecurityConfig());
         final Oak oak = new Oak(nodeStore)
+        .with(new InitialContent())
+        .with(new ExtraSlingContent())
+
+        .with(JcrConflictHandler.JCR_CONFLICT_HANDLER)
+        .with(new EditorHook(new VersionEditorProvider()))
+
         .with(securityProvider)
+
+        .with(new NameValidatorProvider())
+        .with(new NamespaceValidatorProvider())
+        .with(new TypeEditorProvider())
+        .with(new RegistrationEditorProvider())
+        .with(new ConflictValidatorProvider())
+
+        // index stuff
+        .with(new PropertyIndexEditorProvider())
+
+        .with(new PropertyIndexProvider())
+        .with(new NodeTypeIndexProvider())
+
         .with(new LuceneIndexEditorProvider())
-        .with(new LuceneIndexProvider())
+        .with(AggregateIndexProvider.wrap(new LuceneIndexProvider()
+                .with(getNodeAggregator())))
+
+        .with(getDefaultWorkspace())
         .withAsyncIndexing()
-        .with(whiteboard);
+        .with(whiteboard)
+        ;
         
         final ContentRepository contentRepository = oak.createContentRepository();  
         jcrRepository = new JcrRepositoryHacks(contentRepository, whiteboard, securityProvider);
@@ -104,6 +150,12 @@ public class OakSlingRepository extends AbstractNamespaceMappingRepository
     @Deactivate
     protected void deactivate(ComponentContext ctx) {
         tearDown();
+    }
+    
+    private static NodeAggregator getNodeAggregator() {
+        return new SimpleNodeAggregator()
+            .newRuleWithName("nt:file", newArrayList("jcr:content"))
+            ;
     }
     
     @Override
@@ -175,6 +227,59 @@ public class OakSlingRepository extends AbstractNamespaceMappingRepository
         }
 
         return adminSession;
+    }
+    
+    private static final class ExtraSlingContent implements RepositoryInitializer {
+
+        @Override
+        public NodeState initialize(NodeState state) {
+            NodeBuilder root = state.builder();
+            if (root.hasChildNode(INDEX_DEFINITIONS_NAME)) {
+                NodeBuilder index = root.child(INDEX_DEFINITIONS_NAME);
+                
+                // jcr: 
+                property(index, "jcrLanguage", "jcr:language");
+                property(index, "jcrLockOwner", "jcr:lockOwner");
+                
+                // sling:
+                property(index, "slingAlias", "sling:alias");
+                property(index, "slingResource", "sling:resource");
+                property(index, "slingResourceType", "sling:resourceType");
+                property(index, "slingVanityPath", "sling:vanityPath");
+                
+                // various
+                property(index, "event.job.topic", "event.job.topic");
+                property(index, "extensionType", "extensionType");
+                property(index, "lockCreated", "lock.created");
+                property(index, "status", "status");
+                property(index, "type", "type");
+
+                // lucene full-text index
+                if (!index.hasChildNode("lucene")) {
+                    LuceneIndexHelper.newLuceneIndexDefinition(
+                            index, "lucene", LuceneIndexHelper.JR_PROPERTY_INCLUDES,
+                            of(
+                               "jcr:createdBy",
+                               "jcr:lastModifiedBy", 
+                               "sling:alias", 
+                               "sling:resourceType",
+                               "sling:vanityPath"),
+                            "async");
+                }
+                
+            }
+            return root.getNodeState();
+        }
+        
+        /**
+         * A convenience method to create a non-unique property index.
+         */
+        private static void property(NodeBuilder index, String indexName, String propertyName) {
+            if (!index.hasChildNode(indexName)) {
+                createIndexDefinition(index, indexName, true, false, singleton(propertyName), null);
+            }
+        }
+        
     }
     
     // TODO: use proper osgi configuration (once that works in oak)
