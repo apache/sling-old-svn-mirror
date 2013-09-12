266d9f42e3d0b9f2e4122cfc2e6b375c0610b52d
Stefan Egli <stefanegli@apache.org>
2013-09-12 15:16:05 +0000

    SLING-3056 : eclipse-m2e-ui plugin and m2e-feature added, resulting from a separation of m2eclipse dependent code from the slingclipse plugins and features. This adds a new feature to the p2update config too.
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk/tooling/ide@1522611 13f79535-47bb-0310-9956-ffa450edef68

---
+++ eclipse-m2e-ui/src/org/apache/sling/ide/eclipse/m2e/internal/Activator.java
@@ -0,0 +1,28 @@
+package org.apache.sling.ide.eclipse.m2e.internal;
+
+import org.eclipse.core.runtime.Plugin;
+import org.osgi.framework.BundleContext;
+
+public class Activator extends Plugin {
+
+    public static final String PLUGIN_ID = "org.apache.sling.ide.eclipse-m2e-ui";
+    public static Activator INSTANCE;
+
+    public static Activator getDefault() {
+        return INSTANCE;
+    }
+
+    @Override
+    public void start(BundleContext context) throws Exception {
+        super.start(context);
+
+        INSTANCE = this;
+    }
+
+    @Override
+    public void stop(BundleContext context) throws Exception {
+        INSTANCE = null;
+
+        super.stop(context);
+    }
+}
