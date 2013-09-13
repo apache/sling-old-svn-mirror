94c62f93f7c2ed0cfb006b000fad427f71e5f34f
Stefan Egli <stefanegli@apache.org>
2013-09-13 07:08:43 +0000

    SLING-3056 : removed another usage of m2e in eclipse-ui, replaced using a dom, plus moved EmbeddedArchetypeInstaller to eclipse-m2e-ui
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk@1522796 13f79535-47bb-0310-9956-ffa450edef68

--- tooling/ide/eclipse-m2e-ui/src/org/apache/sling/ide/eclipse/m2e/EmbeddedArchetypeInstaller.java
+++ tooling/ide/eclipse-m2e-ui/src/org/apache/sling/ide/eclipse/m2e/EmbeddedArchetypeInstaller.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.sling.ide.eclipse.core;
+package org.apache.sling.ide.eclipse.m2e;
 
 import java.io.File;
 import java.io.FileInputStream;
