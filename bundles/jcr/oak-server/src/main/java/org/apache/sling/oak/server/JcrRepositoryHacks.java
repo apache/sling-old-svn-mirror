492633421a8b38d6e6381475d4389120b61afd2c
Bertrand Delacretaz <bdelacretaz@apache.org>
2013-10-18 12:57:06 +0000

    SLING-3190 - moving Oak repository and tests under bundles/jcr
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk@1533431 13f79535-47bb-0310-9956-ffa450edef68

---
+++ bundles/jcr/oak-server/src/main/java/org/apache/sling/oak/server/JcrRepositoryHacks.java
@@ -0,0 +1,102 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The SF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+package org.apache.sling.oak.server;
+
+import java.util.Map;
+
+import javax.jcr.Credentials;
+import javax.jcr.RepositoryException;
+import javax.jcr.Session;
+
+import org.apache.jackrabbit.oak.Oak;
+import org.apache.jackrabbit.oak.api.ContentRepository;
+import org.apache.jackrabbit.oak.jcr.repository.RepositoryImpl;
+import org.apache.jackrabbit.oak.spi.security.SecurityProvider;
+import org.apache.jackrabbit.oak.spi.whiteboard.Whiteboard;
+
+class JcrRepositoryHacks extends RepositoryImpl {
+    
+    // TODO TCCL switching shouldn't be needed?
+    // LoginModules are not found without this
+    static abstract class LoginHelper {
+        Session TCCLLogin() throws RepositoryException {
+            final Thread thread = Thread.currentThread();
+            final ClassLoader loader = thread.getContextClassLoader();
+            try {
+                thread.setContextClassLoader(Oak.class.getClassLoader());
+                return doLogin();
+            } finally {
+                thread.setContextClassLoader(loader);
+            }
+            
+        }
+        
+        protected abstract Session doLogin() throws RepositoryException;
+    };
+    
+	JcrRepositoryHacks(ContentRepository contentRepository, Whiteboard whiteboard, SecurityProvider securityProvider) {
+		super(contentRepository, whiteboard, securityProvider);
+	}
+	
+    @Override
+    public Session login() throws RepositoryException {
+        return new LoginHelper() {
+            protected Session doLogin() throws RepositoryException {
+                return JcrRepositoryHacks.super.login();
+            }
+        }.TCCLLogin();
+    }
+
+    @Override
+    public Session login(final Credentials creds, final String workspace, final Map<String, Object> opt)
+            throws RepositoryException {
+        return new LoginHelper() {
+            protected Session doLogin() throws RepositoryException {
+                return JcrRepositoryHacks.super.login(creds, workspace, opt);
+            }
+        }.TCCLLogin();
+    }
+
+    @Override
+    public Session login(final Credentials credentials) throws RepositoryException {
+        return new LoginHelper() {
+            protected Session doLogin() throws RepositoryException {
+                return JcrRepositoryHacks.super.login(credentials);
+            }
+        }.TCCLLogin();
+    }
+
+    @Override
+    public Session login(final String workspace) throws RepositoryException {
+        return new LoginHelper() {
+            protected Session doLogin() throws RepositoryException {
+                return JcrRepositoryHacks.super.login(workspace);
+            }
+        }.TCCLLogin();
+    }
+
+    @Override
+    public Session login(final Credentials credentials, final String workspace)
+            throws RepositoryException {
+        return new LoginHelper() {
+            protected Session doLogin() throws RepositoryException {
+                return JcrRepositoryHacks.super.login(credentials, workspace);
+            }
+        }.TCCLLogin();
+    }
+}
---
+++ bundles/jcr/oak-server/src/main/java/org/apache/sling/oak/server/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Contains in implementation of a SlingRepository based on Apache Oak.
+ * The package is not exported.
+ */
+package org.apache.sling.oak.server;
