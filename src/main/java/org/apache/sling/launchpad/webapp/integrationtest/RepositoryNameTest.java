221940a71fd87228f8a2b4300d38424ebe97f7c9
Bertrand Delacretaz <bdelacretaz@apache.org>
2013-10-23 14:01:07 +0000

    SLING-3155 - more precise repository name check
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk/launchpad/integration-tests@1535033 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/sling/launchpad/webapp/integrationtest/RepositoryNameTest.java
+++ src/main/java/org/apache/sling/launchpad/webapp/integrationtest/RepositoryNameTest.java
@@ -16,27 +16,34 @@
  */
 package org.apache.sling.launchpad.webapp.integrationtest;
 
-import java.io.IOException;
-import java.util.regex.Pattern;
+import java.util.HashMap;
+import java.util.Map;
 
+import org.apache.sling.commons.json.JSONObject;
 import org.apache.sling.commons.testing.integration.HttpTestBase;
 
 /** Verify the repository name, to make sure we're testing the right one */
 public class RepositoryNameTest extends HttpTestBase {
     
-    /** Use the same property that Sling uses to switch between repositories.
-     *  Current run modes are "jackrabbit" and "oak".
-     */
+    /** We use the same property that Sling uses to switch between repositories */
     public static final String RUN_MODE_PROP = "sling.run.modes";
     public static final String DEFAULT_RUN_MODE = "jackrabbit";
     
-    public void testName() throws IOException {
-        final String path = "/testing/RepositoryDescriptors.txt";
+    public static final Map<String, String> RUNMODE_TO_NAME = new HashMap<String, String>();
+    
+    static {
+        RUNMODE_TO_NAME.put("jackrabbit", "Jackrabbit");
+        RUNMODE_TO_NAME.put("oak", "Apache Jackrabbit Oak");
+    }
+    
+    public void testName() throws Exception {
         final String runMode = System.getProperty(RUN_MODE_PROP, DEFAULT_RUN_MODE);
-        final String content = getContent(HTTP_BASE_URL + path, CONTENT_TYPE_PLAIN).toLowerCase();
-        final Pattern expectedPattern = Pattern.compile("(?s).*jcr.repository.name=[^$]*" + runMode + ".*");
-        assertTrue(
-                "Expecting lowercased content at " + path + " to match " + expectedPattern + ", content=" + content, 
-                expectedPattern.matcher(content).matches());
+        final String name = RUNMODE_TO_NAME.get(runMode);
+        assertNotNull("Expecting to have a repository name for run mode " + runMode, name);
+        
+        final String path = "/testing/RepositoryDescriptors.json";
+        final JSONObject json = new JSONObject(getContent(HTTP_BASE_URL + path, CONTENT_TYPE_JSON));
+        final String key = "jcr.repository.name";
+        assertEquals("Expecting " + key + "=" + name, name, json.getJSONObject("descriptors").getString(key));
     }
 }
