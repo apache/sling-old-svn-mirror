3bbf32020b016c0426acbef43c9051161179c9a9
Carsten Ziegeler <cziegeler@apache.org>
2014-01-14 14:55:20 +0000

    SLING-3278 : Provide a HealthCheckExecutor service. Add hasTimedOut method to execution result
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk/bundles/extensions/healthcheck/core@1558063 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/sling/hc/api/execution/HealthCheckExecutionResult.java
+++ src/main/java/org/apache/sling/hc/api/execution/HealthCheckExecutionResult.java
@@ -9,6 +9,9 @@ import aQute.bnd.annotation.ProviderType;
 
 /**
  * Interface for health check executions via the {@link HealthCheckExecutor}.
+ *
+ * If the execution of the health check timed out, the method
+ *
  */
 @ProviderType
 public interface HealthCheckExecutionResult {
@@ -25,10 +28,18 @@ public interface HealthCheckExecutionResult {
 
     /**
      * Get the date, the health check finished or if the
-     * execution timed out, <code>null</code> is returned.
-     * @return The finished date of the execution or <code>null</code>
+     * execution timed out, the execution was aborted.
+     * @return The finished date of the execution.
      */
     Date getFinishedAt();
+
+    /**
+     * Returns true if the execution has timed out. In this
+     * case the result does not reflect the real result of the
+     * underlying check, but a result indicating the timeout.
+     * @return <code>true</code> if execution timed out.
+     */
+    boolean hasTimedOut();
 
     /**
      * Get the meta data about the health check service
