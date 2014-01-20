a2a5c87c0420312f7d9bb09dc0057fa7ad5c733c
Tommaso Teofili <tommaso@apache.org>
2014-01-20 17:29:59 +0000

    SLING-3327 - applied Marius Petria's patch, added missing files
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk@1559786 13f79535-47bb-0310-9956-ffa450edef68

--- contrib/extensions/replication/src/main/java/org/apache/sling/replication/queue/impl/jobhandling/ReplicationAgentJobConsumer.java
+++ contrib/extensions/replication/src/main/java/org/apache/sling/replication/queue/impl/jobhandling/ReplicationAgentJobConsumer.java
@@ -16,18 +16,17 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package org.apache.sling.replication.agent.impl;
+package org.apache.sling.replication.queue.impl.jobhandling;
 
 import org.apache.sling.event.jobs.Job;
 import org.apache.sling.event.jobs.consumer.JobConsumer;
+import org.apache.sling.replication.queue.ReplicationQueueItem;
+import org.apache.sling.replication.queue.ReplicationQueueProcessor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import org.apache.sling.replication.agent.AgentReplicationException;
 import org.apache.sling.replication.agent.ReplicationAgent;
 import org.apache.sling.replication.queue.impl.jobhandling.JobHandlingUtils;
-import org.apache.sling.replication.serialization.ReplicationPackage;
-import org.apache.sling.replication.serialization.ReplicationPackageBuilder;
 
 /**
  * {@link JobConsumer} for {@link ReplicationAgent}s using {@link org.apache.sling.replication.queue.impl.jobhandling.JobHandlingReplicationQueue}
@@ -37,43 +36,26 @@ public class ReplicationAgentJobConsumer implements JobConsumer {
     private final Logger log = LoggerFactory.getLogger(getClass());
 
     private final ReplicationAgent agent;
-
-    private final ReplicationPackageBuilder packageBuilder;
+    private final ReplicationQueueProcessor queueProcessor;
 
     public ReplicationAgentJobConsumer(ReplicationAgent agent,
-                    ReplicationPackageBuilder packageBuilder) {
+                    ReplicationQueueProcessor queueProcessor) {
         this.agent = agent;
-        this.packageBuilder = packageBuilder;
+        this.queueProcessor = queueProcessor;
     }
 
     public JobResult process(Job job) {
-        try {
-            if (log.isInfoEnabled()) {
-                log.info("processing job {}", job.getId());
-            }
-            ReplicationPackage item = JobHandlingUtils.getPackage(packageBuilder, job);
-            if (item != null) {
-                if (log.isInfoEnabled()) {
-                    log.info("processing item {} ", item);
-                }
-                boolean processingResult = agent.process(item);
-                JobResult jobResult = processingResult ? JobResult.OK : JobResult.FAILED;
-                if (log.isInfoEnabled()) {
-                    log.info("item {} processed {} ", item, jobResult);
-                }
-                return jobResult;
-            } else {
-                if (log.isInfoEnabled()) {
-                    log.info("cannot get a replication package from job with id {}", job.getId());
-                }
-                return JobResult.FAILED;
-            }
-        } catch (AgentReplicationException e) {
-            if (log.isErrorEnabled()) {
-                log.error("agent failed processing job", e);
-            }
-            return JobResult.FAILED;
+        if (log.isInfoEnabled()) {
+            log.info("processing job {}", job.getId());
         }
+
+        ReplicationQueueItem info = JobHandlingUtils.getPackage(job);
+        boolean processingResult = queueProcessor.process(info);
+        JobResult jobResult = processingResult ? JobResult.OK : JobResult.FAILED;
+
+        log.info("item {} processed {} ", info, jobResult);
+
+        return jobResult;
     }
 
 }
