1ca6262fcc8e475f44f8ff8f378dd4884b1bcf51
Justin Edelson <justin@apache.org>
2014-03-03 17:58:20 +0000

    SLING-3430 - only embedding necessary parts of beanutils
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk@1573638 13f79535-47bb-0310-9956-ffa450edef68

---
+++ bundles/extensions/models/integration-tests/src/main/java/org/apache/sling/models/it/ViaTest.java
@@ -0,0 +1,75 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.sling.models.it;
+
+import static org.junit.Assert.*;
+
+import javax.jcr.Node;
+import javax.jcr.Session;
+
+import org.apache.commons.lang.RandomStringUtils;
+import org.apache.sling.api.adapter.AdapterManager;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ResourceResolver;
+import org.apache.sling.api.resource.ResourceResolverFactory;
+import org.apache.sling.junit.annotations.SlingAnnotationsTestRunner;
+import org.apache.sling.junit.annotations.TestReference;
+import org.apache.sling.models.it.models.SourceObject;
+import org.apache.sling.models.it.models.ViaModel;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(SlingAnnotationsTestRunner.class)
+public class ViaTest {
+
+    @TestReference
+    private ResourceResolverFactory rrFactory;
+
+    @TestReference
+    private AdapterManager adapterManager;
+
+    @Test
+    public void test() throws Exception {
+        String value = RandomStringUtils.randomAlphanumeric(10);
+
+        ResourceResolver resolver = null;
+        Node createdNode = null;
+        try {
+            resolver = rrFactory.getAdministrativeResourceResolver(null);
+            Session session = resolver.adaptTo(Session.class);
+            Node rootNode = session.getRootNode();
+            createdNode = rootNode.addNode("test_" + RandomStringUtils.randomAlphanumeric(10));
+            createdNode.setProperty("testProperty", value);
+            session.save();
+
+            Resource resource = resolver.getResource(createdNode.getPath());
+            SourceObject obj = new SourceObject(resource);
+
+            ViaModel model = adapterManager.getAdapter(obj, ViaModel.class);
+
+            assertNotNull("Model is null", model);
+            assertEquals("Test Property is not set correctly", value, model.getTestProperty());
+        } finally {
+            if (createdNode != null) {
+                createdNode.remove();
+            }
+            if (resolver != null) {
+                resolver.close();
+            }
+        }
+    }
+}
