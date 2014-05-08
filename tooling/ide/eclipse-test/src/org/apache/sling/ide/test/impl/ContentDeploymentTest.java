b3900f7b8956b5271dd79292322923c1c8b3483e
Robert Munteanu <rombert@apache.org>
2014-05-08 13:31:39 +0000

    SLING-3544 - Integration tests running against a live Sling Launchpad instance
    Centralized repository access login in a RepositoryAccessor helper.
    
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk@1593276 13f79535-47bb-0310-9956-ffa450edef68

--- tooling/ide/eclipse-test/src/org/apache/sling/ide/test/impl/ContentDeploymentTest.java
+++ tooling/ide/eclipse-test/src/org/apache/sling/ide/test/impl/ContentDeploymentTest.java
@@ -16,27 +16,17 @@
  */
 package org.apache.sling.ide.test.impl;
 
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.junit.Assert.assertThat;
-
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 
-import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.HttpException;
 import org.apache.commons.httpclient.URIException;
-import org.apache.commons.httpclient.UsernamePasswordCredentials;
-import org.apache.commons.httpclient.auth.AuthScope;
-import org.apache.commons.httpclient.methods.GetMethod;
-import org.apache.commons.httpclient.methods.PostMethod;
-import org.apache.commons.httpclient.methods.multipart.MultipartRequestEntity;
-import org.apache.commons.httpclient.methods.multipart.Part;
-import org.apache.commons.httpclient.methods.multipart.StringPart;
 import org.apache.sling.ide.test.impl.helpers.DisableDebugStatusHandlers;
 import org.apache.sling.ide.test.impl.helpers.ExternalSlingLaunchpad;
 import org.apache.sling.ide.test.impl.helpers.LaunchpadConfig;
 import org.apache.sling.ide.test.impl.helpers.ProjectAdapter;
 import org.apache.sling.ide.test.impl.helpers.ServerAdapter;
+import org.apache.sling.ide.test.impl.helpers.RepositoryAccessor;
 import org.apache.sling.ide.test.impl.helpers.SlingWstServer;
 import org.apache.sling.ide.test.impl.helpers.TemporaryProject;
 import org.eclipse.core.resources.IProject;
@@ -92,7 +82,8 @@ public class ContentDeploymentTest {
         Thread.sleep(1000); // for good measure, make sure the output is there - TODO replace with polling
 
         // verify that file is created
-        assertSimpleCallsSucceeds("hello, world");
+        RepositoryAccessor repo = new RepositoryAccessor(config);
+        repo.assertGetIsSuccessful("hello.txt", "hello, world");
 
         project.createOrUpdateFile(Path.fromPortableString("jcr_root/hello.txt"), new ByteArrayInputStream(
                 "goodbye, world".getBytes()));
@@ -100,56 +91,19 @@ public class ContentDeploymentTest {
         Thread.sleep(2000); // for good measure, make sure the output is there - TODO replace with polling
 
         // verify that file is updated
-        assertSimpleCallsSucceeds("goodbye, world");
+        repo.assertGetIsSuccessful("hello.txt", "goodbye, world");
 
         project.deleteMember(Path.fromPortableString("jcr_root/hello.txt"));
 
         Thread.sleep(2000); // for good measure, make sure the output is there - TODO replace with polling
+
         // verify that the file is deleted
-        assertResourceNotFound();
-    }
-
-    private void assertSimpleCallsSucceeds(String expectedOutput) throws IOException, HttpException, URIException {
-        HttpClient c = new HttpClient();
-        GetMethod gm = new GetMethod(config.getUrl() + "hello.txt");
-        try {
-            int status = c.executeMethod(gm);
-
-            assertThat("Unexpected status code for " + gm.getURI(), status, equalTo(200));
-            assertThat("Unexpected response for " + gm.getURI(), gm.getResponseBodyAsString(), equalTo(expectedOutput));
-
-        } finally {
-            gm.releaseConnection();
-        }
-    }
-
-    private void assertResourceNotFound() throws IOException, HttpException, URIException {
-        HttpClient c = new HttpClient();
-        GetMethod gm = new GetMethod(config.getUrl() + "hello.txt");
-        try {
-            int status = c.executeMethod(gm);
-
-            assertThat("Unexpected status code for " + gm.getURI(), status, equalTo(404));
-        } finally {
-            gm.releaseConnection();
-        }
+        repo.assertGetReturns404("hello.txt");
     }
 
     @After
     public void cleanUp() throws HttpException, IOException {
-        HttpClient c = new HttpClient();
-        c.getParams().setAuthenticationPreemptive(true);
-        c.getState().setCredentials(AuthScope.ANY,
-                new UsernamePasswordCredentials(config.getUsername(), config.getPassword()));
-        PostMethod pm = new PostMethod(config.getUrl() + "hello.txt");
-        Part[] parts = { new StringPart(":operation", "delete") };
-        pm.setRequestEntity(new MultipartRequestEntity(parts, pm.getParams()));
-        try {
-            int status = c.executeMethod(pm);
 
-            System.out.println("Delete operation got status " + status);
-        } finally {
-            pm.releaseConnection();
-        }
+        new RepositoryAccessor(config).tryDeleteResource("hello.txt");
     }
 }
