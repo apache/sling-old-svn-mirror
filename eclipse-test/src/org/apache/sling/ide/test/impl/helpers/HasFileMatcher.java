85d3eac50ac5856190beb1c8fc27ee05dadd9f23
Robert Munteanu <rombert@apache.org>
2014-05-16 14:21:07 +0000

    SLING-3161 - SlingIDE Import Content wizard should respect .vltignore files
    Added EclipseResourceMatchers
    
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk/tooling/ide@1595203 13f79535-47bb-0310-9956-ffa450edef68

---
+++ eclipse-test/src/org/apache/sling/ide/test/impl/helpers/HasFileMatcher.java
@@ -0,0 +1,88 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.sling.ide.test.impl.helpers;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
+
+import org.apache.commons.io.IOUtils;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.CoreException;
+import org.hamcrest.Description;
+import org.hamcrest.TypeSafeMatcher;
+
+public class HasFileMatcher extends TypeSafeMatcher<IProject> {
+
+    public static final HasFileMatcher hasFile(String filePath, byte[] contents) {
+        return new HasFileMatcher(filePath, contents);
+    }
+
+    private final String fileName;
+    private final byte[] contents;
+
+
+    public HasFileMatcher(String fileName, byte[] contents) {
+        this.fileName = fileName;
+        this.contents = contents;
+    }
+
+    @Override
+    public void describeTo(Description description) {
+        description.appendText("project with a filed named " + fileName + " and contents (elided)");
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.hamcrest.TypeSafeMatcher#matchesSafely(java.lang.Object)
+     */
+    @Override
+    protected boolean matchesSafely(IProject item) {
+        if (item == null) {
+            return false;
+        }
+
+        IResource maybeFile = item.findMember(fileName);
+
+        if (maybeFile == null || maybeFile.getType() != IResource.FILE) {
+            return false;
+        }
+
+        IFile file = (IFile) maybeFile;
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+        InputStream in = null;
+        try {
+            in = file.getContents();
+
+            IOUtils.copy(in, out);
+        } catch (CoreException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            IOUtils.closeQuietly(in);
+        }
+
+        return Arrays.equals(out.toByteArray(), contents);
+    }
+
+}
