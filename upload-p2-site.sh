1b3f10e16594d432cc913c85c5b755ef53956dce
Robert Munteanu <rombert@apache.org>
2014-05-28 20:33:12 +0000

    upload-p2-site.sh - do not rebuild the project
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk/tooling/ide@1598131 13f79535-47bb-0310-9956-ffa450edef68

--- upload-p2-site.sh
+++ upload-p2-site.sh
@@ -2,14 +2,11 @@
 
 SITE_LOCATION='public_html/sling/ide/preview'
 
-# 1. Build fresh package
-mvn -q clean verify
-
-# 2. Clean the current update site
+# 1. Clean the current update site
 ssh people.apache.org "rm -rf $SITE_LOCATION/*"
 
-# 3. Upload the new update site
+# 2. Upload the new update site
 scp p2update/target/*.zip people.apache.org:$SITE_LOCATION
 
-# 4. Unzip the new update site
+# 3. Unzip the new update site
 ssh people.apache.org "cd $SITE_LOCATION && unzip -o *.zip && rm -f *.zip"
