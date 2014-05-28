d2846fcd1c1297149213479488d5d148cd8acb7e
Stefan Egli <stefanegli@apache.org>
2014-05-28 15:57:48 +0000

    SLING-3615 : added TychoJREHelper which allows to explicitly install a particular JRE (configured in the pom via system property 'tycho.jre.helper.jre.home'). This is meant as a workaround until someone finds a better/more appropriate solution
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk/tooling/ide@1598056 13f79535-47bb-0310-9956-ffa450edef68

---
+++ eclipse-test/src/org/apache/sling/ide/test/impl/TychoJREHelper.java
@@ -0,0 +1,51 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.sling.ide.test.impl;
+
+import java.io.File;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.jdt.internal.launching.StandardVMType;
+import org.eclipse.jdt.launching.IVMInstall;
+import org.eclipse.jdt.launching.IVMInstallType;
+import org.eclipse.jdt.launching.JavaRuntime;
+import org.eclipse.jdt.launching.VMStandin;
+
+public class TychoJREHelper {
+
+    public static boolean hasDefaultVM() {
+        IVMInstall vm = JavaRuntime.getDefaultVMInstall();
+        return vm!=null;
+    }
+    
+    public static void installDefaultVM(String jrePath) throws CoreException {
+        final IVMInstallType vmInstallType = JavaRuntime.getVMInstallType(StandardVMType.ID_STANDARD_VM_TYPE);
+        String id = null;
+        do {
+            id = String.valueOf(System.currentTimeMillis());
+        } while (vmInstallType.findVMInstall(id) != null);
+
+        VMStandin newVm = new VMStandin(vmInstallType, id);
+        newVm.setName("Default-VM");
+        File installLocation = new File(jrePath);
+        newVm.setInstallLocation(installLocation);
+        newVm.convertToRealVM();
+        JavaRuntime.setDefaultVMInstall(newVm, new NullProgressMonitor());
+    }
+
+}
