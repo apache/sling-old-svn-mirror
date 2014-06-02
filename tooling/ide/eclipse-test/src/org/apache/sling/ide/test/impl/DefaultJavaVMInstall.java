a2e339499d6292d24e7690be18c22ea43f9ff6d8
Stefan Egli <stefanegli@apache.org>
2014-06-02 07:59:29 +0000

    SLING-3615 : applying SLING-3615-2.patch from Robert Munteanu, thx for the refactoring!
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk@1599108 13f79535-47bb-0310-9956-ffa450edef68

--- tooling/ide/eclipse-test/src/org/apache/sling/ide/test/impl/DefaultJavaVMInstall.java
+++ tooling/ide/eclipse-test/src/org/apache/sling/ide/test/impl/DefaultJavaVMInstall.java
@@ -16,25 +16,31 @@
  */
 package org.apache.sling.ide.test.impl;
 
+
 import java.io.File;
 
-import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.jdt.internal.launching.StandardVMType;
 import org.eclipse.jdt.launching.IVMInstall;
 import org.eclipse.jdt.launching.IVMInstallType;
 import org.eclipse.jdt.launching.JavaRuntime;
 import org.eclipse.jdt.launching.VMStandin;
+import org.junit.rules.ExternalResource;
 
-public class TychoJREHelper {
-
-    public static boolean hasDefaultVM() {
-        IVMInstall vm = JavaRuntime.getDefaultVMInstall();
-        return vm!=null;
-    }
+public class DefaultJavaVMInstall extends ExternalResource {
     
-    public static void installDefaultVM(String jrePath) throws CoreException {
+    @Override
+    protected void before() throws Throwable {
+        
+        if (JavaRuntime.getDefaultVMInstall() != null) {
+            return;
+        }
+
+        String jreHome = System.getProperty("java.home");
+        File installLocation = new File(jreHome);
+
         final IVMInstallType vmInstallType = JavaRuntime.getVMInstallType(StandardVMType.ID_STANDARD_VM_TYPE);
+        // find an unused VM id
         String id = null;
         do {
             id = String.valueOf(System.currentTimeMillis());
@@ -42,10 +48,12 @@ public class TychoJREHelper {
 
         VMStandin newVm = new VMStandin(vmInstallType, id);
         newVm.setName("Default-VM");
-        File installLocation = new File(jrePath);
         newVm.setInstallLocation(installLocation);
         IVMInstall realVm = newVm.convertToRealVM();
         JavaRuntime.setDefaultVMInstall(realVm, new NullProgressMonitor());
-    }
 
+        // wait for the default vm reconfiguration to settle
+        // TODO - find something to poll agains rather than sleeping blindly
+        Thread.sleep(5000);
+    }
 }
