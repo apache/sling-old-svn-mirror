7e4b6067535f99b4e386164cf6d3b383ba07f9e5
Robert Munteanu <rombert@apache.org>
2014-06-24 14:00:08 +0000

    SLING-3693 - Repository import wizard blocks UI thread and is not cancellable
    Added ProgressUtils to ease work with cancellable progress monitors.
    
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk/tooling/ide@1605086 13f79535-47bb-0310-9956-ffa450edef68

---
+++ eclipse-core/src/org/apache/sling/ide/eclipse/core/progress/ProgressUtils.java
@@ -0,0 +1,38 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.sling.ide.eclipse.core.progress;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.OperationCanceledException;
+
+/**
+ * The <tt>ProgressUtils</tt> provides convenience methods for working with {@link IProgressMonitor} instances.
+ *
+ */
+public abstract class ProgressUtils {
+
+    public static void advance(IProgressMonitor monitor, int ticks) {
+        monitor.worked(ticks);
+        if (monitor.isCanceled()) {
+            throw new OperationCanceledException();
+        }
+    }
+
+    private ProgressUtils() {
+
+    }
+}
