c120ee70dace8eb096f288103104c84753873b41
Bertrand Delacretaz <bdelacretaz@apache.org>
2014-07-22 15:00:22 +0000

    SLING-3786 - add tests for the conversion of JSON data to strings
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk/bundles/commons/json@1612584 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/test/java/org/apache/sling/commons/json/JSONWriterTest.java
@@ -0,0 +1,97 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.sling.commons.json;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.io.StringWriter;
+
+import org.apache.sling.commons.json.io.JSONWriter;
+import org.junit.Before;
+import org.junit.Test;
+
+/** Test the String formatting functionality of JSONObject */
+public class JSONWriterTest {
+    private JSONWriter w;
+    private StringWriter output;
+    
+    @Before
+    public void setup() {
+        output = new StringWriter();
+        w = new JSONWriter(output);
+    }
+    
+    private DespacedResult write() throws JSONException {
+        w.object();
+        w.key("foo").value("bar");
+        w.key("array");
+        w.array().value(1).value("two").value(3.0).value(false).endArray();
+        w.key("last").value("one");
+        w.endObject();
+        return new DespacedResult(output.toString());
+    }
+    
+    @Test
+    public void testSetTidy() {
+        assertFalse(w.isTidy());
+        w.setTidy(true);
+        assertTrue(w.isTidy());
+    }
+    
+    @Test
+    public void testStandardWrite() throws JSONException {
+        final DespacedResult r = write();
+        r.expect(
+                "_foo_:_bar_", 
+                "_array_:[1,_two_,3,false]");
+    }
+    
+    @Test
+    public void testTidyWrite() throws JSONException {
+        w.setTidy(true);
+        final DespacedResult r = write();
+        r.expect(
+                "-nl-_foo_:_bar_", 
+                "-nl-_array_:[-nl-1,-nl-_two_,-nl-3,-nl-false-nl-]");
+    }
+    
+    @Test
+    public void testEmpty() throws JSONException {
+        assertTrue(output.toString().length() == 0);
+    }
+    
+    @Test(expected=JSONException.class)
+    public void testMisplacedArray() throws JSONException {
+        w.object().array();
+    }
+    
+    @Test(expected=JSONException.class)
+    public void testMisplacedKey() throws JSONException {
+        w.array().key("foo");
+    }
+    
+    @Test(expected=JSONException.class)
+    public void testMisplacedEndObjectA() throws JSONException {
+        w.object().endObject().endObject();
+    }
+    
+    @Test(expected=JSONException.class)
+    public void testMisplacedEndObjectB() throws JSONException {
+        w.endObject();
+    }
+}
