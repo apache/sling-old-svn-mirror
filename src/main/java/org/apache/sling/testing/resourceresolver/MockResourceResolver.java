6eecd9afb844234e09c59ab59d12dfadf06ddb67
Carsten Ziegeler <cziegeler@apache.org>
2014-08-15 16:18:33 +0000

    SLING-3853 : MockResourceResolver does not extend SlingAdaptable. Apply patch from Stefan Seifert
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk/testing/resourceresolver-mock@1618221 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/sling/testing/resourceresolver/MockResourceResolver.java
+++ src/main/java/org/apache/sling/testing/resourceresolver/MockResourceResolver.java
@@ -31,13 +31,14 @@ import java.util.Set;
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.sling.api.SlingConstants;
+import org.apache.sling.api.adapter.SlingAdaptable;
 import org.apache.sling.api.resource.LoginException;
 import org.apache.sling.api.resource.PersistenceException;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.api.resource.ResourceResolver;
 import org.osgi.service.event.Event;
 
-public class MockResourceResolver implements ResourceResolver {
+public class MockResourceResolver extends SlingAdaptable implements ResourceResolver {
 
     private final Map<String, Map<String, Object>> resources;
 
@@ -51,11 +52,6 @@ public class MockResourceResolver implements ResourceResolver {
             final Map<String, Map<String, Object>> resources) {
         this.options = options;
         this.resources = resources;
-    }
-
-    @Override
-    public <AdapterType> AdapterType adaptTo(Class<AdapterType> type) {
-        return null;
     }
 
     @Override
