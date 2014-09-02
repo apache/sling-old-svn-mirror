a8450f1acea6032cd1c409f79ba67612e2f50593
Robert Munteanu <rombert@apache.org>
2014-09-02 13:43:08 +0000

    SLING-3888 - Move groovy extension bundle to contrib/scripting
    Make the move for the groovy bundle from extensions to scripting.
    
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk@1621993 13f79535-47bb-0310-9956-ffa450edef68

--- bundles/extensions/groovy/src/main/java/org/apache/sling/scripting/groovy/scripting/internal/GSPScriptEngineFactory.java
+++ bundles/extensions/groovy/src/main/java/org/apache/sling/scripting/groovy/scripting/internal/GSPScriptEngineFactory.java
@@ -14,7 +14,7 @@
  * License for the specific language governing permissions and limitations under
  * the License.
  */
-package org.apache.sling.extensions.groovy.scripting.internal;
+package org.apache.sling.scripting.groovy.scripting.internal;
 
 import java.util.Collections;
 import java.util.List;
