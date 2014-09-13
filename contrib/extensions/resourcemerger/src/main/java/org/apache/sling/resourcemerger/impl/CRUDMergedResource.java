c5240f9546097a99a637c78df01b37f0f855e108
Carsten Ziegeler <cziegeler@apache.org>
2014-09-13 13:35:37 +0000

    SLING-3909 : Merged ResourceProviders should be optionally modifiable
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk@1624735 13f79535-47bb-0310-9956-ffa450edef68

--- contrib/extensions/resourcemerger/src/main/java/org/apache/sling/resourcemerger/impl/CRUDMergedResource.java
+++ contrib/extensions/resourcemerger/src/main/java/org/apache/sling/resourcemerger/impl/CRUDMergedResource.java
@@ -18,8 +18,12 @@
  */
 package org.apache.sling.resourcemerger.impl;
 
+import java.util.Collection;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import org.apache.sling.api.resource.ModifiableValueMap;
 import org.apache.sling.api.resource.PersistenceException;
@@ -27,6 +31,8 @@ import org.apache.sling.api.resource.Resource;
 import org.apache.sling.api.resource.ResourceResolver;
 import org.apache.sling.api.resource.ResourceUtil;
 import org.apache.sling.api.resource.ValueMap;
+import org.apache.sling.api.wrappers.DeepReadValueMapDecorator;
+import org.apache.sling.api.wrappers.ValueMapDecorator;
 import org.apache.sling.resourcemerger.spi.MergedResourcePicker;
 
 /**
@@ -75,14 +81,107 @@ public class CRUDMergedResource extends MergedResource {
                 final Resource copyResource = this.getResourceResolver().getResource(paths[paths.length - 1]);
                 try {
                     final Resource newResource = ResourceUtil.getOrCreateResource(this.getResourceResolver(), highestRsrc.getPath(), copyResource.getResourceType(), null, false);
-                    return (AdapterType)newResource.adaptTo(ModifiableValueMap.class);
+                    final ModifiableValueMap target = newResource.adaptTo(ModifiableValueMap.class);
+                    if ( target != null ) {
+                        return (AdapterType)new ModifiableProperties(this, target);
+                    }
                 } catch ( final PersistenceException pe) {
                     // we ignore this for now
-                    return null;
                 }
+                return super.adaptTo(type);
             }
-            return (AdapterType)highestRsrc.adaptTo(ModifiableValueMap.class);
+            final ModifiableValueMap target = highestRsrc.adaptTo(ModifiableValueMap.class);
+            if ( target != null ) {
+                return (AdapterType)new ModifiableProperties(this, target);
+            }
         }
         return super.adaptTo(type);
+    }
+
+    private static final class ModifiableProperties implements ModifiableValueMap {
+
+        private final ModifiableValueMap targetMap;
+
+        private final ValueMap properties;
+
+        public ModifiableProperties(final Resource rsrc, final ModifiableValueMap targetMap) {
+            this.properties = new DeepReadValueMapDecorator(rsrc, new ValueMapDecorator(new HashMap<String, Object>(rsrc.getValueMap())));
+            this.targetMap = targetMap;
+        }
+
+        public <T> T get(final String name, final Class<T> type) {
+            return properties.get(name, type);
+        }
+
+        public <T> T get(final String name, final T defaultValue) {
+            return properties.get(name, defaultValue);
+        }
+
+        public int size() {
+            return properties.size();
+        }
+
+        public boolean isEmpty() {
+            return properties.isEmpty();
+        }
+
+        public boolean containsKey(final Object key) {
+            return properties.containsKey(key);
+        }
+
+        public boolean containsValue(final Object value) {
+            return properties.containsValue(value);
+        }
+
+        public Object get(final Object key) {
+            return properties.get(key);
+        }
+
+        public Object put(final String key, final Object value) {
+            final Object result = this.properties.get(key);
+            this.targetMap.put(key, value);
+            return result;
+        }
+
+        public Object remove(final Object key) {
+            final Object result = this.properties.get(key);
+            if ( this.targetMap.remove(key) == null ) {
+                final String[] hiddenProps = this.targetMap.get(MergedResourceConstants.PN_HIDE_PROPERTIES, String[].class);
+                final String[] newHiddenProps;
+                if ( hiddenProps == null || hiddenProps.length == 0 ) {
+                    newHiddenProps = new String[] {key.toString()};
+                } else {
+                    newHiddenProps = new String[hiddenProps.length + 1];
+                    System.arraycopy(hiddenProps, 0, newHiddenProps, 0, hiddenProps.length);
+                    newHiddenProps[hiddenProps.length] = key.toString();
+                }
+                this.targetMap.put(MergedResourceConstants.PN_HIDE_PROPERTIES, newHiddenProps);
+            }
+            return result;
+        }
+
+        public void putAll(final Map<? extends String, ? extends Object> m) {
+            if ( m != null ) {
+                for(final Map.Entry<? extends String, ? extends Object> entry : m.entrySet()) {
+                    this.put(entry.getKey(), entry.getValue());
+                }
+            }
+        }
+
+        public void clear() {
+            // not supported
+        }
+
+        public Set<String> keySet() {
+            return this.properties.keySet();
+        }
+
+        public Collection<Object> values() {
+            return this.properties.values();
+        }
+
+        public Set<Entry<String, Object>> entrySet() {
+            return this.properties.entrySet();
+        }
     }
 }
