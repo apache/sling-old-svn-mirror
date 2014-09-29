ec99530f930ae9e507c733d1a842e236249b7ce3
Tommaso Teofili <tommaso@apache.org>
2014-09-29 13:31:47 +0000

    SLING-3978 - added 'sample' replication bundle
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk@1628179 13f79535-47bb-0310-9956-ffa450edef68

--- contrib/extensions/replication/core/src/main/java/org/apache/sling/replication/communication/ReplicationEndpoint.java
+++ contrib/extensions/replication/core/src/main/java/org/apache/sling/replication/communication/ReplicationEndpoint.java
@@ -39,4 +39,24 @@ public class ReplicationEndpoint {
         return uri;
     }
 
+    @Override
+    public String toString() {
+        return uri.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        ReplicationEndpoint that = (ReplicationEndpoint) o;
+
+        return uri.equals(that.uri);
+
+    }
+
+    @Override
+    public int hashCode() {
+        return uri.hashCode();
+    }
 }
