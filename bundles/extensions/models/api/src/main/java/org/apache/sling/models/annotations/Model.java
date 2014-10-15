4ceffe3a031413ccdc9f65d2320e96bfb465b789
sseifert <sseifert@unknown>
2014-10-15 13:00:00 +0000

    SLING-4055 "Model" annotation should be @Documented
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk@1632005 13f79535-47bb-0310-9956-ffa450edef68

--- bundles/extensions/models/api/src/main/java/org/apache/sling/models/annotations/Model.java
+++ bundles/extensions/models/api/src/main/java/org/apache/sling/models/annotations/Model.java
@@ -16,6 +16,7 @@
  */
 package org.apache.sling.models.annotations;
 
+import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -26,6 +27,7 @@ import java.lang.annotation.Target;
  */
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
+@Documented
 public @interface Model {
 
     /**
