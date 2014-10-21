eb260a129d6830ea253f089c0b15fec6d7471ab2
Bertrand Delacretaz <bdelacretaz@apache.org>
2014-10-21 09:35:46 +0000

    SLING-4079 - ignore non-installable resources, based on a contribution by Radu Cotescu, thanks!
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk/launchpad/installer@1633329 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/sling/launchpad/installer/impl/LaunchpadConfigInstaller.java
+++ src/main/java/org/apache/sling/launchpad/installer/impl/LaunchpadConfigInstaller.java
@@ -90,7 +90,8 @@ public class LaunchpadConfigInstaller {
 
                     final URL url = resourceProvider.getResource(path);
                     if(url == null){
-                    	throw new RuntimeException("Retrieved null resource for path: "+path);
+                        logger.debug("Launchpad ignoring path '{}' due to null URL", path);
+                        continue;
                     }
                     Dictionary<String, Object> dict = null;
                     if ( InstallableResource.TYPE_FILE.equals(resourceType) ) {
