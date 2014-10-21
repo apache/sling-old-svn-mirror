09a9287abaae16925a127413bfa67708f9b2ce53
sseifert <sseifert@unknown>
2014-10-21 15:37:46 +0000

    remove auto-generated TODO tag
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk@1633400 13f79535-47bb-0310-9956-ffa450edef68

--- bundles/extensions/models/impl/src/test/java/org/apache/sling/models/testmodels/classes/SubClass.java
+++ bundles/extensions/models/impl/src/test/java/org/apache/sling/models/testmodels/classes/SubClass.java
@@ -35,7 +35,6 @@ public class SubClass extends SuperClass {
         try {
             Thread.sleep(100);
         } catch (InterruptedException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
         }
         postConstructCalledTimestampInSub = System.currentTimeMillis();
