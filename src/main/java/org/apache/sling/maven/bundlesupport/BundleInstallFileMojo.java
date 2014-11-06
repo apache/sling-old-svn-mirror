1a26457daf2900fa5f378cfcbaa0bfff778fda13
Stefan Seifert <sseifert@apache.org>
2014-11-06 22:47:53 +0000

    cosmetic code cleanup - remove some unused imports and a unused variable
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk/tooling/maven/maven-sling-plugin@1637253 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/sling/maven/bundlesupport/BundleInstallFileMojo.java
+++ src/main/java/org/apache/sling/maven/bundlesupport/BundleInstallFileMojo.java
@@ -22,7 +22,6 @@ import java.util.List;
 
 import org.apache.maven.artifact.Artifact;
 import org.apache.maven.artifact.factory.ArtifactFactory;
-import org.apache.maven.artifact.metadata.ArtifactMetadataSource;
 import org.apache.maven.artifact.repository.ArtifactRepository;
 import org.apache.maven.artifact.repository.ArtifactRepositoryFactory;
 import org.apache.maven.artifact.repository.ArtifactRepositoryPolicy;
@@ -30,7 +29,6 @@ import org.apache.maven.artifact.repository.layout.ArtifactRepositoryLayout;
 import org.apache.maven.artifact.resolver.AbstractArtifactResolutionException;
 import org.apache.maven.artifact.resolver.ArtifactResolver;
 import org.apache.maven.plugin.MojoExecutionException;
-import org.apache.maven.plugin.MojoFailureException;
 import org.codehaus.plexus.util.StringUtils;
 
 /**
