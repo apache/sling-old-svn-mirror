5b48c807c29241324a11b0056d0d10f4609780a1
Konrad Windszus <kwin@apache.org>
2014-11-19 16:28:08 +0000

    SLILNG-4027 removal of ValidatorLookupService
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk@1640589 13f79535-47bb-0310-9956-ffa450edef68

--- contrib/extensions/validation/core/src/main/java/org/apache/sling/validation/impl/ChildResourceImpl.java
+++ contrib/extensions/validation/core/src/main/java/org/apache/sling/validation/impl/ChildResourceImpl.java
@@ -1,11 +1,12 @@
 package org.apache.sling.validation.impl;
 
+import java.util.Map;
 import java.util.Set;
 
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.validation.api.ChildResource;
 import org.apache.sling.validation.api.ResourceProperty;
-import org.apache.sling.validation.api.ValidatorLookupService;
+import org.apache.sling.validation.api.Validator;
 import org.apache.sling.validation.impl.util.JCRBuilder;
 
 /**
@@ -16,13 +17,13 @@ public class ChildResourceImpl implements ChildResource {
     private String name;
     private Set<ResourceProperty> properties;
 
-    public ChildResourceImpl(Resource modelResource, Resource childResource, ValidatorLookupService validatorLookupService) {
+    public ChildResourceImpl(Resource modelResource, Resource childResource, Map<String, Validator<?>> validatorsMap) {
         String root = modelResource.getPath();
         if (!childResource.getPath().startsWith(root)) {
             throw new IllegalArgumentException("Expected resource " + childResource.getPath() + " to be under root path " + root);
         }
         name = childResource.getPath().replaceFirst(root + "/", "").replaceAll(Constants.CHILDREN + "/", "");
-        properties = JCRBuilder.buildProperties(validatorLookupService, childResource.getChild(Constants.PROPERTIES));
+        properties = JCRBuilder.buildProperties(validatorsMap, childResource.getChild(Constants.PROPERTIES));
     }
 
     @Override
