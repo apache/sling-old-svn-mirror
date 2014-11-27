5a35c7ef77498f4b71f617913a945552a55f0e52
Konrad Windszus <kwin@apache.org>
2014-11-27 14:52:29 +0000

    SLING-4011, allow pattern matching on properties and child resources
    also some refactoring of the ValidationServiceImplTest
    
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk/contrib/extensions/validation/core@1642166 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/sling/validation/impl/ChildResourceImpl.java
+++ src/main/java/org/apache/sling/validation/impl/ChildResourceImpl.java
@@ -1,9 +1,13 @@
 package org.apache.sling.validation.impl;
 
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
 
 import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ValueMap;
 import org.apache.sling.validation.api.ChildResource;
 import org.apache.sling.validation.api.ResourceProperty;
 import org.apache.sling.validation.api.Validator;
@@ -14,16 +18,37 @@ import org.apache.sling.validation.impl.util.JCRBuilder;
  */
 public class ChildResourceImpl implements ChildResource {
 
-    private String name;
-    private Set<ResourceProperty> properties;
+    private final String name;
+    private final Pattern namePattern;
+    private final Set<ResourceProperty> properties;
+    private final List<ChildResource> children;
 
-    public ChildResourceImpl(Resource modelResource, Resource childResource, Map<String, Validator<?>> validatorsMap) {
+    public ChildResourceImpl(Resource modelResource, Resource childResource, Map<String, Validator<?>> validatorsMap, List<ChildResource> children) {
         String root = modelResource.getPath();
         if (!childResource.getPath().startsWith(root)) {
             throw new IllegalArgumentException("Expected resource " + childResource.getPath() + " to be under root path " + root);
         }
-        name = childResource.getPath().replaceFirst(root + "/", "").replaceAll(Constants.CHILDREN + "/", "");
+        // if pattern is set, always use that
+        ValueMap childrenProperties = childResource.adaptTo(ValueMap.class);
+        if (childrenProperties == null) {
+            throw new IllegalStateException("Could not adapt resource " + childResource.getPath() + " to ValueMap");
+        }
+        if (childrenProperties.containsKey(Constants.NAME_REGEX)) {
+            name = null;
+            String regex = childrenProperties.get(Constants.NAME_REGEX, String.class);
+            try {
+                namePattern = Pattern.compile(regex);
+            } catch (PatternSyntaxException e) {
+                throw new IllegalArgumentException("Given regular expression is invalid: " + regex);
+            }
+        } else {
+            // otherwise fall back to the name
+            name = childResource.getName();
+            namePattern = null;
+        }
+        
         properties = JCRBuilder.buildProperties(validatorsMap, childResource.getChild(Constants.PROPERTIES));
+        this.children = children;
     }
 
     @Override
@@ -34,5 +59,14 @@ public class ChildResourceImpl implements ChildResource {
     @Override
     public Set<ResourceProperty> getProperties() {
         return properties;
+    }
+
+    @Override
+    public Pattern getNamePattern() {
+        return namePattern;
+    }
+    
+    public List<ChildResource> getChildren() {
+        return children;
     }
 }
