628d7412727756b1ec1cba198dfa5f8c91f9caf6
Felix Meschberger <fmeschbe@apache.org>
2014-12-11 09:15:04 +0000

    SLING-4236 Clarify XSSAPI with respect to null and empty input strings
    * Include patch by Radu Cotescu (thanks a lot)
    * Extend patch to getValid* and filterHtml methods
    * Leverage TestCase.assertEquals for result validation
    * Add more tests for empty and null input
    * Add LongValidationRule 
    
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk@1644580 13f79535-47bb-0310-9956-ffa450edef68

--- contrib/extensions/xss/src/main/java/org/apache/sling/xss/impl/XSSAPIImpl.java
+++ contrib/extensions/xss/src/main/java/org/apache/sling/xss/impl/XSSAPIImpl.java
@@ -44,50 +44,60 @@ public class XSSAPIImpl implements XSSAPI {
 
     private Validator validator = ESAPI.validator();
 
+    private static final Pattern PATTERN_AUTO_DIMENSION = Pattern.compile("['\"]?auto['\"]?");
+
     /**
      * @see org.apache.sling.xss.XSSAPI#getValidInteger(String, int)
      */
     public Integer getValidInteger(String integer, int defaultValue) {
-        try {
-            if (integer == null || integer.length() == 0) {
-                return defaultValue;
-            } else {
+        if (integer != null && integer.length() > 0) {
+            try {
                 return validator.getValidInteger("XSS", integer, -2000000000, 2000000000, false);
+            } catch (Exception e) {
+                // ignore
             }
-        } catch (Exception e) {
-            return defaultValue;
         }
+
+        // fall through to default if empty, null, or validation failure
+        return defaultValue;
     }
 
     /**
      * @see org.apache.sling.xss.XSSAPI#getValidLong(String, long)
      */
     public Long getValidLong(String source, long defaultValue) {
-        try {
-            if (source == null || source.length() == 0) {
-                return defaultValue;
-            } else {
-                return validator.getValidNumber("XSS", source, -9000000000000000000L, 9000000000000000000L, false).longValue();
+        if (source != null && source.length() > 0) {
+            try {
+                LongValidationRule ivr = new LongValidationRule( "number", ESAPI.encoder(), -9000000000000000000L, 9000000000000000000L );
+                ivr.setAllowNull(false);
+                return ivr.getValid("XSS", source);
+            } catch (Exception e) {
+                // ignore
             }
-        } catch (Exception e) {
-            return defaultValue;
         }
+
+        // fall through to default if empty, null, or validation failure
+        return defaultValue;
     }
 
     /**
      * @see org.apache.sling.xss.XSSAPI#getValidDimension(String, String)
      */
     public String getValidDimension(String dimension, String defaultValue) {
-        try {
-            if (dimension == null || dimension.length() == 0) {
-                return defaultValue;
-            } else if (dimension.matches("['\"]?auto['\"]?")) {
+        if (dimension != null && dimension.length() > 0) {
+            if (PATTERN_AUTO_DIMENSION.matcher(dimension).matches()) {
                 return "\"auto\"";
             }
-            return validator.getValidInteger("XSS", dimension, -10000, 10000, false).toString();
-        } catch (Exception e) {
-            return defaultValue;
+
+            try {
+                return validator.getValidInteger("XSS", dimension, -10000, 10000, false).toString();
+            } catch (Exception e) {
+                // ignore
+            }
         }
+
+        // fall through to default if empty, null, or validation failure
+        return defaultValue;
     }
 
     private static final String LINK_PREFIX = "<a href=\"";
@@ -147,7 +157,7 @@ public class XSSAPIImpl implements XSSAPI {
      * @see org.apache.sling.xss.XSSAPI#getValidHref(String)
      */
     public String getValidHref(final String url) {
-        try {
+        if (url != null && url.length() > 0) {
             // Percent-encode characters that are not allowed in unquoted
             // HTML attributes: ", ', >, <, ` and space. We don't encode =
             // since this would break links with query parameters.
@@ -163,31 +173,32 @@ public class XSSAPIImpl implements XSSAPI {
             final String safeHtml = xssFilter.filter(ProtectionContext.HTML_HTML_CONTENT, testHtml);
             // if the xssFilter didn't like the input string we just return ""
             // otherwise we return the mangled url without encoding
-            if (!safeHtml.equals(testHtml)) {
-                return "";
-            } else {
+            if (safeHtml.equals(testHtml)) {
                 return mangleNamespaces(encodedUrl);
             }
-        } catch (final NullPointerException e) {
-            // ProtectionContext was null - simply return an empty string
-            return "";
         }
+
+        // fall through to empty string
+        return "";
     }
 
     /**
      * @see org.apache.sling.xss.XSSAPI#getValidJSToken(String, String)
      */
     public String getValidJSToken(String token, String defaultValue) {
-        token = token.trim();
-        String q = token.substring(0, 1);
-        if (q.matches("['\"]") && token.endsWith(q)) {
-            String literal = token.substring(1, token.length() - 1);
-            return q + encodeForJSString(literal) + q;
-        } else if (token.matches("[0-9a-zA-Z_$][0-9a-zA-Z_$.]*")) {
-            return token;
-        } else {
-            return defaultValue;
+        if (token != null && token.length() > 0) {
+            token = token.trim();
+            String q = token.substring(0, 1);
+            if (q.matches("['\"]") && token.endsWith(q)) {
+                String literal = token.substring(1, token.length() - 1);
+                return q + encodeForJSString(literal) + q;
+            } else if (token.matches("[0-9a-zA-Z_$][0-9a-zA-Z_$.]*")) {
+                return token;
+            }
         }
+
+        // fall through to default value
+        return defaultValue;
     }
 
     private static final String NON_ASCII = "\\x00\\x08\\x0B\\x0C\\x0E-\\x1F";
@@ -224,29 +235,33 @@ public class XSSAPIImpl implements XSSAPI {
      * @see org.apache.sling.xss.XSSAPI#getValidStyleToken(String, String)
      */
     public String getValidStyleToken(String token, String defaultValue) {
-        if (token.matches(CSS_TOKEN)) {
+        if (token != null && token.length() > 0 && token.matches(CSS_TOKEN)) {
             return token;
         }
+
         return defaultValue;
-    }
+   }
 
     /**
      * @see org.apache.sling.xss.XSSAPI#getValidCSSColor(String, String)
      */
     public String getValidCSSColor(String color, String defaultColor) {
-        color = color.trim();
-        /*
-         * Avoid security implications by including only the characters required to specify colors in hex
-         * or functional notation. Critical characters disallowed: x (as in expression(...)),
-         * u (as in url(...)) and semi colon (as in escaping the context of the color value). 
-         */
-        if (color.matches("(?i)[#a-fghlrs(+0-9-.%,) \\t\\n\\x0B\\f\\r]+")) {
-            return color;
+        if (color != null && color.length() > 0) {
+            color = color.trim();
+            /*
+             * Avoid security implications by including only the characters required to specify colors in hex
+             * or functional notation. Critical characters disallowed: x (as in expression(...)),
+             * u (as in url(...)) and semi colon (as in escaping the context of the color value).
+             */
+            if (color.matches("(?i)[#a-fghlrs(+0-9-.%,) \\t\\n\\x0B\\f\\r]+")) {
+                return color;
+            }
+            // named color values
+            if (color.matches("(?i)[a-zA-Z \\t\\n\\x0B\\f\\r]+")) {
+                return color;
+            }
         }
-        // named color values
-        if (color.matches("(?i)[a-zA-Z \\t\\n\\x0B\\f\\r]+")) {
-            return color;
-        }
+
         return defaultColor;
     }
 
@@ -258,42 +273,42 @@ public class XSSAPIImpl implements XSSAPI {
      * @see org.apache.sling.xss.XSSAPI#encodeForHTML(String)
      */
     public String encodeForHTML(String source) {
-        return Encode.forHtml(source);
+        return source == null ? null : Encode.forHtml(source);
     }
 
     /**
      * @see org.apache.sling.xss.XSSAPI#encodeForHTMLAttr(String)
      */
     public String encodeForHTMLAttr(String source) {
-        return Encode.forHtmlAttribute(source);
+        return source == null ? null : Encode.forHtmlAttribute(source);
     }
 
     /**
      * @see org.apache.sling.xss.XSSAPI#encodeForXML(String)
      */
     public String encodeForXML(String source) {
-        return Encode.forXml(source);
+        return source == null ? null : Encode.forXml(source);
     }
 
     /**
      * @see org.apache.sling.xss.XSSAPI#encodeForXMLAttr(String)
      */
     public String encodeForXMLAttr(String source) {
-        return Encode.forXmlAttribute(source);
+        return source == null ? null : Encode.forXmlAttribute(source);
     }
 
     /**
      * @see org.apache.sling.xss.XSSAPI#encodeForJSString(String)
      */
     public String encodeForJSString(String source) {
-        return Encode.forJavaScript(source);
+        return source == null ? null : Encode.forJavaScript(source);
     }
 
     /**
      * @see org.apache.sling.xss.XSSAPI#encodeForCSSString(String)
      */
     public String encodeForCSSString(String source) {
-        return Encode.forCssString(source);
+        return source == null ? null : Encode.forCssString(source);
     }
 
     // =============================================================================================
