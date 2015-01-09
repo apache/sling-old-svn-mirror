138271c673130893906b59a7c3f150b98ad5909c
Carsten Ziegeler <cziegeler@apache.org>
2015-01-09 11:56:46 +0000

    SLING-4291 : NPE if optional prefix is not configured
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk/contrib/jcr/resourcesecurity@1650508 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/sling/jcr/resourcesecurity/impl/ResourceAccessGateFactory.java
+++ src/main/java/org/apache/sling/jcr/resourcesecurity/impl/ResourceAccessGateFactory.java
@@ -73,8 +73,11 @@ public class ResourceAccessGateFactory
     protected void activate(final Map<String, Object> props) {
         this.jcrPath = PropertiesUtil.toString(props.get(PROP_JCR_PATH), null);
         this.prefix = PropertiesUtil.toString(props.get(PROP_PREFIX), null);
-        if ( !this.prefix.endsWith("/") ) {
-            this.prefix = this.prefix + "/";
+        if ( this.prefix != null ) {
+            if ( !this.prefix.endsWith("/") ) {
+                this.prefix = this.prefix + "/";
+            }
+            this.jcrPath = this.jcrPath + "/";
         }
     }
 
@@ -89,7 +92,7 @@ public class ResourceAccessGateFactory
         if ( session != null ) {
             String checkPath = this.jcrPath;
             if ( this.prefix != null && path.startsWith(this.prefix) ) {
-                checkPath = this.jcrPath + path.substring(this.prefix.length() - 1);
+                checkPath = this.jcrPath + path.substring(this.prefix.length());
             }
             try {
                 granted = session.hasPermission(checkPath, permission);
