c97784608641514eaaec00c012312437f898ab4a
Oliver Lietz <olli@apache.org>
2015-01-23 09:32:06 +0000

    SLING-3518 make filter internal
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk@1654155 13f79535-47bb-0310-9956-ffa450edef68

--- contrib/extensions/urlrewriter/src/main/java/org/apache/sling/urlrewriter/internal/SlingUrlRewriteFilter.java
+++ contrib/extensions/urlrewriter/src/main/java/org/apache/sling/urlrewriter/internal/SlingUrlRewriteFilter.java
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package org.apache.sling.urlrewriter;
+package org.apache.sling.urlrewriter.internal;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
