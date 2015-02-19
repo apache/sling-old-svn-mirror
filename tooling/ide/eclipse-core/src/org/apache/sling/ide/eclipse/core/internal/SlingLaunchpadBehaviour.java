f79369f58a1220db85eb1b34880d32784ebe01b9
Robert Munteanu <rombert@apache.org>
2015-02-19 22:34:04 +0000

    SLING-3781 - Fix unstable Eclipse-based tests
    Remove attempt to ensure parents exist when a resource delete triggers
    an AddOrUpdateCommand execution.
    
    This was first of all broken, and second of all unneeded. The parents
    must exist since we get a delete event, therefore they must've existed
    in the project and must've been published.
    
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk@1661020 13f79535-47bb-0310-9956-ffa450edef68

--- tooling/ide/eclipse-core/src/org/apache/sling/ide/eclipse/core/internal/SlingLaunchpadBehaviour.java
+++ tooling/ide/eclipse-core/src/org/apache/sling/ide/eclipse/core/internal/SlingLaunchpadBehaviour.java
@@ -424,13 +424,7 @@ public class SlingLaunchpadBehaviour extends ServerBehaviourDelegateWithModulePu
                             execute(command);
                             break;
                         case IModuleResourceDelta.REMOVED:
-                            Command<?> removeCmd = removeFileCommand(repository, resourceDelta.getModuleResource());
-                            if (removeCmd != null && isAddOrUpdateCommand(removeCmd)) {
-                                ensureParentIsPublished(resourceDelta.getModuleResource(), repository, allResources,
-                                        handledPaths);
-                                addedOrUpdatedResources.add(resourceDelta.getModuleResource());
-                            }
-                            execute(removeCmd);
+                            execute(removeFileCommand(repository, resourceDelta.getModuleResource()));
                             break;
                     }
                 }
@@ -464,11 +458,6 @@ public class SlingLaunchpadBehaviour extends ServerBehaviourDelegateWithModulePu
         setModulePublishState(module, IServer.PUBLISH_STATE_NONE);
 //        setServerPublishState(IServer.PUBLISH_STATE_NONE);
 	}
-
-    private boolean isAddOrUpdateCommand(Command<?> removeCmd) {
-        // TODO - this is fragile and lacks extensibility
-        return removeCmd.getClass().getName().endsWith("AddOrUpdateNodeCommand");
-    }
 
     /**
      * Ensures that the parent of this resource has been published to the repository
