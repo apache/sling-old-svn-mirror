ff10f6ae74059730c67a22d8444b1411fbc32e21
Carsten Ziegeler <cziegeler@apache.org>
2015-03-13 10:10:10 +0000

    SLING-4312 : update javadocs
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk/bundles/extensions/serviceusermapper@1666388 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/sling/serviceusermapping/ServiceUserMapped.java
+++ src/main/java/org/apache/sling/serviceusermapping/ServiceUserMapped.java
@@ -9,6 +9,9 @@ import aQute.bnd.annotation.ProviderType;
  * For example setting the reference target to "(subServiceName=mySubService)" ensures that your component only starts when the subService is available.
  * The subServiceName will not be set for mappings that do not have one, and those can be referenced with a negating target "(!(subServiceName=*))".
  * Trying to reference a sub service from a bundle for which it was not registered for will not work.
+ *
+ * As the service user mapper implementation is using a fallback, it is usually best to use a reference target that includes both
+ * options, the sub service name and the fallback, therefore a target like "(|((subServiceName=mySubService)(!(subServiceName=*))))" should be used.
  */
 @ProviderType
 public interface ServiceUserMapped {
