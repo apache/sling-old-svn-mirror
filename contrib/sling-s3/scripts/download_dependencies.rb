f4929897cf4a20448052ed394c51417ff25cc9fc
tomekr <tomekr@unknown>
2015-03-18 21:35:42 +0000

    sling-s3: display Maven progress; fixed testing local deps
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk@1667624 13f79535-47bb-0310-9956-ffa450edef68

--- contrib/sling-s3/scripts/download_dependencies.rb
+++ contrib/sling-s3/scripts/download_dependencies.rb
@@ -4,16 +4,23 @@ DEP_PLUGIN = 'org.apache.maven.plugins:maven-dependency-plugin:2.10:get'
 SNAPSHOT_REPO = 'https://repository.apache.org/content/repositories/snapshots'
 LOCAL_REPO = '~/.m2/repository'
 
+def run cmd
+  output = ""
+  IO.popen(cmd).each do |line|
+    puts line.chomp
+    output += line
+  end
+  output
+end
+
 def download groupId, artifactId, version
   puts "#{groupId}:#{artifactId}:#{version}"
-  local = "#{LOCAL_REPO}/#{groupId.gsub('.', '/')}/#{artifactId}/#{version}"
+  local = "#{LOCAL_REPO}/#{groupId.gsub('.', '/')}/#{artifactId}/#{version}/#{artifactId}-#{version}.jar"
   if File.exists?(File.expand_path(local))
     puts "(/) Already installed"
     return
   end
-  result = `mvn #{DEP_PLUGIN}\
-            -DremoteRepositories=#{SNAPSHOT_REPO}\
-            -Dartifact=#{groupId}:#{artifactId}:#{version}`
+  result = run "mvn #{DEP_PLUGIN} -DremoteRepositories=#{SNAPSHOT_REPO} -Dartifact=#{groupId}:#{artifactId}:#{version} -Dtransitive=false"
   if result.include? 'BUILD SUCCESS'
     puts "(/) Downloaded"
   else
