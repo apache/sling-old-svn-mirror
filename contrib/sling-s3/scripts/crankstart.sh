ae5b770b75c5971721942454c268b0be0920e95a
tomekr <tomekr@unknown>
2015-03-21 08:44:40 +0000

    SLING-4502 Allow to use alternative runner in the crankstart.sh
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk@1668209 13f79535-47bb-0310-9956-ffa450edef68

--- contrib/sling-s3/scripts/crankstart.sh
+++ contrib/sling-s3/scripts/crankstart.sh
@@ -10,4 +10,9 @@ REPO="https://repository.apache.org/content/repositories/snapshots@snapshots@id=
 REPO="${REPO},http://repo1.maven.org/maven2/@id=central"
 MVN_OPTS="${MVN_OPTS} -Dorg.ops4j.pax.url.mvn.repositories=${REPO}"
 
-java ${MVN_OPTS} -jar contrib/crankstart.jar "$@"
+# If CRANKSTART_ENV is set, don't start anything.
+# This lets alternate start mechanisms source all settings in this file then use an alternate exec method,
+# like start-stop-daemon, which doesn't work well with shell script when additional args are needed.
+if [ ! "${CRANKSTART_ENV}" ] ; then
+       java ${MVN_OPTS} -jar contrib/crankstart.jar "$@"
+fi
