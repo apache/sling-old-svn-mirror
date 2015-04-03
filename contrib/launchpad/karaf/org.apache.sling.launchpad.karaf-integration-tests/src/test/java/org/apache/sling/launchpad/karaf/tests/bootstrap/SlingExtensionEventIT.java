a1294f714a77c9d6a8ee90b9fad73d61a7e9eaca
Oliver Lietz <olli@apache.org>
2015-04-03 06:56:05 +0000

    SLING-3027 SLING-3017 add Sling Distributed Event Admin to event feature
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk@1671004 13f79535-47bb-0310-9956-ffa450edef68

--- contrib/launchpad/karaf/org.apache.sling.launchpad.karaf-integration-tests/src/test/java/org/apache/sling/launchpad/karaf/tests/bootstrap/SlingExtensionEventIT.java
+++ contrib/launchpad/karaf/org.apache.sling.launchpad.karaf-integration-tests/src/test/java/org/apache/sling/launchpad/karaf/tests/bootstrap/SlingExtensionEventIT.java
@@ -50,4 +50,11 @@ public class SlingExtensionEventIT extends KarafTestSupport {
         assertEquals(Bundle.ACTIVE, bundle.getState());
     }
 
+    @Test
+    public void testOrgApacheSlingEventDea() {
+        final Bundle bundle = findBundle("org.apache.sling.event.dea");
+        assertNotNull(bundle);
+        assertEquals(Bundle.ACTIVE, bundle.getState());
+    }
+
 }
