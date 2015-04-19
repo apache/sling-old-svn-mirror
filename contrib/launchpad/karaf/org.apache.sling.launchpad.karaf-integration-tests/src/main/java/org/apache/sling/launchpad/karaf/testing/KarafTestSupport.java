4ef018c42ff66b45dedb9e7d9b6ef002a431ca94
Oliver Lietz <olli@apache.org>
2015-04-19 17:47:31 +0000

    SLING-3017 upgrade Pax Exam to 4.5.0
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk@1674647 13f79535-47bb-0310-9956-ffa450edef68

--- contrib/launchpad/karaf/org.apache.sling.launchpad.karaf-integration-tests/src/main/java/org/apache/sling/launchpad/karaf/testing/KarafTestSupport.java
+++ contrib/launchpad/karaf/org.apache.sling.launchpad.karaf-integration-tests/src/main/java/org/apache/sling/launchpad/karaf/testing/KarafTestSupport.java
@@ -151,7 +151,7 @@ public abstract class KarafTestSupport {
             editConfigurationFilePut("etc/org.apache.karaf.management.cfg", "rmiServerPort", Integer.toString(rmiServerPort)),
             editConfigurationFilePut("etc/org.apache.karaf.shell.cfg", "sshPort", Integer.toString(sshPort)),
             editConfigurationFilePut("etc/org.ops4j.pax.web.cfg", "org.osgi.service.http.port", Integer.toString(httpPort)),
-            mavenBundle().groupId("org.ops4j.pax.tinybundles").artifactId("tinybundles").version("2.1.0"),
+            mavenBundle().groupId("org.ops4j.pax.tinybundles").artifactId("tinybundles").version("2.1.1"),
             mavenBundle().groupId("biz.aQute.bnd").artifactId("biz.aQute.bndlib").version("2.4.1"),
             karafTestSupportBundle()
         );
