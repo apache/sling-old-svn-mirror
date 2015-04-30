9b305ddbdc9439fac1089893e41a98a03ec0a876
Konrad Windszus <kwin@apache.org>
2015-04-30 14:16:36 +0000

    SLING-4447 move Sightly Use Provider for Models into dedicated bundle
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk@1677000 13f79535-47bb-0310-9956-ffa450edef68

--- contrib/scripting/sightly/models-use-provider/src/main/java/org/apache/sling/scripting/sightly/models/impl/ModelFactoryUseProvider.java
+++ contrib/scripting/sightly/models-use-provider/src/main/java/org/apache/sling/scripting/sightly/models/impl/ModelFactoryUseProvider.java
@@ -14,10 +14,12 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.sling.models.impl.sightly;
+package org.apache.sling.scripting.sightly.models.impl;
+
 
 import java.util.HashMap;
 import java.util.Map;
+
 import javax.script.Bindings;
 import javax.script.SimpleBindings;
 import javax.servlet.ServletRequest;
