3cd94fd5b3ac036b95470c9822a8d02851f9e749
Carsten Ziegeler <cziegeler@apache.org>
2015-05-31 06:00:43 +0000

    Improve javadocs for Adaptable
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk@1682678 13f79535-47bb-0310-9956-ffa450edef68

--- bundles/api/src/main/java/org/apache/sling/api/adapter/Adaptable.java
+++ bundles/api/src/main/java/org/apache/sling/api/adapter/Adaptable.java
@@ -36,20 +36,20 @@ public interface Adaptable {
     /**
      * Adapts the adaptable to another type.
      * <p>
-     * Please not that it is explicitly left as an implementation detail whether
+     * Please note that it is explicitly left as an implementation detail whether
      * each call to this method with the same <code>type</code> yields the same
      * object or a new object on each call.
      * <p>
      * Implementations of this method should document their adapted types as
-     * well as their behaviour with respect to returning newly created or not
-     * instance on each call.
+     * well as their behaviour with respect to returning newly created or same
+     * instances on each call.
      *
-     * @param <AdapterType> The generic type to which this resource is adapted
+     * @param <AdapterType> The generic type to which this object is adapted
      *            to
      * @param type The Class object of the target type, such as
      *            <code>javax.jcr.Node.class</code> or
      *            <code>java.io.File.class</code>
-     * @return The adapter target or <code>null</code> if the resource cannot
+     * @return The adapter target or <code>null</code> if the object cannot
      *         adapt to the requested type
      */
     @CheckForNull <AdapterType> AdapterType adaptTo(@Nonnull Class<AdapterType> type);
