a020685e5097aeea608c71f8d4219a6ce9c35884
Stefan Seifert <sseifert@apache.org>
2015-09-03 06:17:31 +0000

    SLING-4990 Support HttpSession invalidate, new, lastAccessedTime, maxInteractiveInterval
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk@1700936 13f79535-47bb-0310-9956-ffa450edef68

--- testing/mocks/sling-mock/src/main/java/org/apache/sling/testing/mock/sling/servlet/MockHttpSession.java
+++ testing/mocks/sling-mock/src/main/java/org/apache/sling/testing/mock/sling/servlet/MockHttpSession.java
@@ -37,6 +37,9 @@ public final class MockHttpSession implements HttpSession {
     private final Map<String, Object> attributeMap = new HashMap<String, Object>();
     private final String sessionID = UUID.randomUUID().toString();
     private final long creationTime = System.currentTimeMillis();
+    private boolean invalidated = false;
+    private boolean isNew = true;
+    private int maxActiveInterval = 1800;
 
     @Override
     public ServletContext getServletContext() {
@@ -45,12 +48,14 @@ public final class MockHttpSession implements HttpSession {
 
     @Override
     public Object getAttribute(final String name) {
+        checkInvalidatedState();
         return this.attributeMap.get(name);
     }
 
     @SuppressWarnings("unchecked")
     @Override
     public Enumeration<String> getAttributeNames() {
+        checkInvalidatedState();
         return IteratorUtils.asEnumeration(this.attributeMap.keySet().iterator());
     }
 
@@ -61,68 +66,92 @@ public final class MockHttpSession implements HttpSession {
 
     @Override
     public long getCreationTime() {
+        checkInvalidatedState();
         return this.creationTime;
     }
 
     @Override
     public Object getValue(final String name) {
+        checkInvalidatedState();
         return getAttribute(name);
     }
 
     @Override
     public String[] getValueNames() {
+        checkInvalidatedState();
         return this.attributeMap.keySet().toArray(new String[this.attributeMap.keySet().size()]);
     }
 
     @Override
     public void putValue(final String name, final Object value) {
+        checkInvalidatedState();
         setAttribute(name, value);
     }
 
     @Override
     public void removeAttribute(final String name) {
+        checkInvalidatedState();
         this.attributeMap.remove(name);
     }
 
     @Override
     public void removeValue(final String name) {
+        checkInvalidatedState();
         this.attributeMap.remove(name);
     }
 
     @Override
     public void setAttribute(final String name, final Object value) {
+        checkInvalidatedState();
         this.attributeMap.put(name, value);
-    }
-
-    // --- unsupported operations ---
-    @Override
-    public long getLastAccessedTime() {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public int getMaxInactiveInterval() {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    @SuppressWarnings("deprecation")
-    public javax.servlet.http.HttpSessionContext getSessionContext() {
-        throw new UnsupportedOperationException();
     }
 
     @Override
     public void invalidate() {
-        throw new UnsupportedOperationException();
+        checkInvalidatedState();
+        this.invalidated = true;
+    }
+    
+    private void checkInvalidatedState() {
+        if (invalidated) {
+            throw new IllegalStateException("Session is already invalidated.");
+        }
+    }
+    
+    public boolean isInvalidated() {
+        return invalidated;
     }
 
     @Override
     public boolean isNew() {
-        throw new UnsupportedOperationException();
+        checkInvalidatedState();
+        return isNew;
+    }
+    
+    public void setNew(boolean isNew) {
+        this.isNew = isNew;
+    }
+
+    @Override
+    public long getLastAccessedTime() {
+        checkInvalidatedState();
+        return creationTime;
+    }
+
+    @Override
+    public int getMaxInactiveInterval() {
+        return maxActiveInterval;
     }
 
     @Override
     public void setMaxInactiveInterval(final int interval) {
+        this.maxActiveInterval = interval;
+    }
+
+    // --- unsupported operations ---
+    @Override
+    @SuppressWarnings("deprecation")
+    public javax.servlet.http.HttpSessionContext getSessionContext() {
         throw new UnsupportedOperationException();
     }
 
