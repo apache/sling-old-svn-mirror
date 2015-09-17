d0b18fc06245f2491b9dc289c990487de174820a
Bertrand Delacretaz <bdelacretaz@apache.org>
2015-09-17 14:41:29 +0000

    SLING-5039 - add OSGiService, a variant of Service that uses generics
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk/testing/junit/core@1703635 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/sling/junit/rules/Service.java
+++ src/main/java/org/apache/sling/junit/rules/Service.java
@@ -27,6 +27,8 @@ import org.osgi.framework.ServiceReference;
 /**
  * Allows a test class to obtain a reference to an OSGi service. This rule embodies the logic to get a bundle context,
  * obtain a service reference, fetch the reference to the object and perform the proper cleanup after the test has run.
+ * 
+ * The newer OSGiService class uses generics and is more convenient to use. 
  */
 public class Service implements TestRule {
 
