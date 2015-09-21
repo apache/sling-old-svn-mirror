6404b4f32975f61b0fbf95f6ccc25dc324fa254b
Bertrand Delacretaz <bdelacretaz@apache.org>
2015-09-21 15:59:51 +0000

    SLING-5040 - client-side customizer options
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk/launchpad/integration-tests@1704336 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/sling/junit/teleporter/customizers/LaunchpadCustomizer.java
+++ src/main/java/org/apache/sling/junit/teleporter/customizers/LaunchpadCustomizer.java
@@ -17,8 +17,10 @@
 package org.apache.sling.junit.teleporter.customizers;
 
 import static org.junit.Assert.fail;
+
 import org.apache.sling.commons.testing.integration.HttpTest;
 import org.apache.sling.junit.rules.TeleporterRule;
+import org.apache.sling.launchpad.webapp.integrationtest.teleporter.TeleporterOptionsTest;
 import org.apache.sling.testing.teleporter.client.ClientSideTeleporter;
 
 /** TeleporterRule Customizer used for Sling launchpad integration tests.
@@ -29,13 +31,20 @@ public class LaunchpadCustomizer implements TeleporterRule.Customizer {
 
     private final static HttpTest H = new HttpTest();
     private final static int testReadyTimeout = Integer.getInteger("ClientSideTeleporter.testReadyTimeoutSeconds",12);
+    public static String PROP_OPTIONS = ("Sling.LaunchpadCustomizer.Test.Options"); 
     
     @Override
     /** Customize the client-side TeleporterRule by first waiting
      *  for Sling to be ready and then setting it up with the test server
      *  URL, timeout etc.
      */
-    public void customize(TeleporterRule t) {
+    public void customize(TeleporterRule t, String options) {
+        // Used to test the options mechanism
+        if(TeleporterOptionsTest.OPTIONS.equals(options)) {
+            throw new TeleporterOptionsTest.OptionsException(options);
+        }
+
+        // Setup Sling and the ClientSideTeleporter
         try {
             H.setUp();
         } catch(Exception e) {
