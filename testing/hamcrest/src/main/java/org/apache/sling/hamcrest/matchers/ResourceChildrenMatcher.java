19cf7b6392cb81fe5f46f26d26ba11ea66332470
Robert Munteanu <rombert@apache.org>
2015-09-25 09:32:45 +0000

    SLING-5063 - Create a set of Hamcrest matchers for JUnit tests
    Initial version
    
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk@1705261 13f79535-47bb-0310-9956-ffa450edef68

---
+++ testing/hamcrest/src/main/java/org/apache/sling/hamcrest/matchers/ResourceChildrenMatcher.java
@@ -0,0 +1,52 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.sling.hamcrest.matchers;
+
+import java.util.List;
+
+import org.apache.sling.api.resource.Resource;
+import org.hamcrest.Description;
+import org.hamcrest.TypeSafeMatcher;
+
+public class ResourceChildrenMatcher extends TypeSafeMatcher<Resource> {
+    
+    private final List<String> childNames;
+    
+    public ResourceChildrenMatcher(List<String> childNames) {
+        if ( childNames == null || childNames.isEmpty() ) {
+            throw new IllegalArgumentException("childNames is null or empty");
+        }
+        
+        this.childNames = childNames;
+    }
+
+    @Override
+    public void describeTo(Description description) {
+        description.appendText("Resource with children ").appendValueList("[", ",", "]", childNames);
+    }
+
+    @Override
+    protected boolean matchesSafely(Resource item) {
+        for ( String childName : childNames ) {
+            if ( item.getChild(childName) == null ) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+}
