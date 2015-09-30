615e5eb6260f95d48f4e75084a7c11cc56a1ab79
Radu Cotescu <radu@apache.org>
2015-09-30 10:14:07 +0000

    SLING-4971 - "static" node name not allowed as a script ancestor
    * defined a set of reserved Java keywords that will get escaped when detected in Resource paths
    
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk@1705994 13f79535-47bb-0310-9956-ffa450edef68

---
+++ bundles/scripting/sightly/engine/src/test/java/org/apache/sling/scripting/sightly/impl/engine/compiled/SourceIdentifierTest.java
@@ -0,0 +1,52 @@
+/*******************************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+package org.apache.sling.scripting.sightly.impl.engine.compiled;
+
+import org.apache.sling.api.resource.Resource;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class SourceIdentifierTest {
+
+    private static SourceIdentifier sourceIdentifier;
+
+    @BeforeClass
+    public static void setUp() {
+        final Resource resource = mock(Resource.class);
+        when(resource.getPath()).thenReturn("/apps/blah/static/foo/foo.html");
+        sourceIdentifier = new SourceIdentifier(null, null, null, resource, "SightlyJava_");
+    }
+
+    @Test
+    public void testGetClassName() throws Exception {
+        assertEquals("SightlyJava_foo", sourceIdentifier.getClassName());
+    }
+
+    @Test
+    public void testGetPackageName() throws Exception {
+        assertEquals("apps.blah.static_.foo", sourceIdentifier.getPackageName());
+    }
+
+    @Test
+    public void testGetFullyQualifiedName() throws Exception {
+        assertEquals("apps.blah.static_.foo.SightlyJava_foo", sourceIdentifier.getFullyQualifiedName());
+    }
+}
