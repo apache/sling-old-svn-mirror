26b30a04359433c994c768065cf2e984cb49f431
Robert Munteanu <rombert@apache.org>
2015-10-05 11:14:45 +0000

    SLING-3766 - Store the project's content sync root property in the filesystem
    Fix + tests
    
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk@1706803 13f79535-47bb-0310-9956-ffa450edef68

--- tooling/ide/eclipse-test/src/org/apache/sling/ide/test/impl/ProjectUtilTest.java
+++ tooling/ide/eclipse-test/src/org/apache/sling/ide/test/impl/ProjectUtilTest.java
@@ -17,6 +17,7 @@
 package org.apache.sling.ide.test.impl;
 
 import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.nullValue;
 import static org.junit.Assert.assertThat;
 
 import java.io.ByteArrayInputStream;
@@ -24,10 +25,13 @@ import java.io.ByteArrayInputStream;
 import org.apache.sling.ide.eclipse.core.ProjectUtil;
 import org.apache.sling.ide.test.impl.helpers.ProjectAdapter;
 import org.apache.sling.ide.test.impl.helpers.TemporaryProject;
+import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.Path;
+import org.eclipse.core.runtime.QualifiedName;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
@@ -35,23 +39,34 @@ public class ProjectUtilTest {
 
     @Rule
     public TemporaryProject projectRule = new TemporaryProject();
+    
+    private IProject contentProject;
+    private ProjectAdapter project;
 
-    @Test
-    public void slingContentProjectSyncedResourcesAreExportable() throws CoreException, InterruptedException {
-
+    @Before
+    public void createEmptyContentProject() throws Exception {
         // create faceted project
-        IProject contentProject = projectRule.getProject();
+        contentProject = projectRule.getProject();
 
-        ProjectAdapter project = new ProjectAdapter(contentProject);
+        project = new ProjectAdapter(contentProject);
         project.addNatures("org.eclipse.wst.common.project.facet.core.nature");
 
         // install bundle facet
         project.installFacet("sling.content", "1.0");
 
+        // create filter file
         project.createVltFilterWithRoots();
+        // create content sync dir
+        project.ensureDirectoryExists(Path.fromPortableString("jcr_root"));
+
+    }
+    
+    @Test
+    public void slingContentProjectSyncedResourcesAreExportable() throws CoreException, InterruptedException {
+
+
         project.createOrUpdateFile(Path.fromPortableString("jcr_root/test/hello.txt"), new ByteArrayInputStream(
                 "goodbye, world".getBytes()));
-
 
         IPath filterPath = ProjectUtil.findFilterPath(contentProject);
         assertThat("filterPath.absolute", filterPath.isAbsolute(), equalTo(true));
@@ -59,5 +74,45 @@ public class ProjectUtilTest {
         assertThat("filterPath", filterPath.makeRelativeTo(contentProject.getLocation()).toPortableString(),
                 equalTo("META-INF/vault/filter.xml"));
 
+    }
+    
+    @Test
+    public void defaultContentSyncRootIsReturned() throws Exception {
+
+        IFolder syncDirectory = ProjectUtil.getSyncDirectory(contentProject);
+        
+        assertThat(syncDirectory.getProjectRelativePath(), equalTo(Path.fromPortableString("jcr_root")));
+    }
+    
+    @Test
+    public void customContentSyncRootIsObeyed() throws Exception {
+        
+        IPath contentSyncPath = Path.fromPortableString("src/main/content/jcr_root");
+        
+        project.ensureDirectoryExists(contentSyncPath);
+        ProjectUtil.setSyncDirectoryPath(contentProject, contentSyncPath);
+        
+        assertThat(ProjectUtil.getSyncDirectory(contentProject).getProjectRelativePath(), equalTo(contentSyncPath));
+    }
+    
+    @Test
+    public void oldContentSyncRootIsMigrated() throws Exception {
+
+        QualifiedName oldPropertyQName = new QualifiedName("org.apache.sling.ide.eclipse-core", "sync_root");
+        
+        IPath contentSyncPath = Path.fromPortableString("src/main/content/jcr_root");
+        
+        project.ensureDirectoryExists(contentSyncPath);
+        
+        // simulate the old property being set
+        contentProject.setPersistentProperty(oldPropertyQName, contentSyncPath.toString());
+        
+        // query the data through the API, old value should be obeyed
+        assertThat("Old sync_root not obeyed", ProjectUtil.getSyncDirectory(contentProject).getProjectRelativePath(), equalTo(contentSyncPath));
+        // the old store should no longer have the config value
+        assertThat("Old property not removed", contentProject.getPersistentProperty(oldPropertyQName), nullValue());
+        // a second query should work just the same, to make sure that after deleting the data from the old store we get the right value back
+        assertThat("sync_root not obeyed after old store for property was deleted", 
+                ProjectUtil.getSyncDirectory(contentProject).getProjectRelativePath(), equalTo(contentSyncPath));
     }
 }
