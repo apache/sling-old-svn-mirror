4f18d4094dd209460184cd8760edc4effefd82ba
Robert Munteanu <rombert@apache.org>
2015-10-09 15:09:51 +0000

    SLING-4189 - Add basic code completion for Sightly
    Disable the HTML validator when installing the Sightly nature in order
    to prevent it from marking Sightly-specific constructs as invalid.
    
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk/tooling/ide@1707756 13f79535-47bb-0310-9956-ffa450edef68

--- eclipse-sightly-core/src/org/apache/sling/ide/eclipse/sightly/internal/SightlyFacetInstallDelegate.java
+++ eclipse-sightly-core/src/org/apache/sling/ide/eclipse/sightly/internal/SightlyFacetInstallDelegate.java
@@ -21,13 +21,55 @@ import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.wst.common.project.facet.core.IDelegate;
 import org.eclipse.wst.common.project.facet.core.IProjectFacetVersion;
+import org.eclipse.wst.validation.Validator;
+import org.eclipse.wst.validation.internal.ValManager;
+import org.eclipse.wst.validation.internal.ValPrefManagerProject;
+import org.eclipse.wst.validation.internal.ValidatorMutable;
+import org.eclipse.wst.validation.internal.model.ProjectPreferences;
 
+/**
+ * The <tt>SightlyFacetInstallDelegate</tt> ensures that the HTML validator is not active
+ * 
+ * <p>Since the Sightly validators delegate to the HTML counterparts and also ensure that 
+ * Sightly-specific constructs do not trigger validation problems it is completely safe
+ * to disable the HTML validator</p> 
+ */
+@SuppressWarnings("restriction")
 public class SightlyFacetInstallDelegate implements IDelegate {
+
+    private static final String HTML_VALIDATOR_ID = "org.eclipse.wst.html.core.HTMLValidator";
 
     @Override
     public void execute(IProject project, IProjectFacetVersion version, Object config, IProgressMonitor monitor)
             throws CoreException {
 
+        Validator[] validators = ValManager.getDefault().getValidators(project);
+        ValidatorMutable[] mutis = new ValidatorMutable[validators.length];
+        for ( int i = 0 ; i < validators.length; i++) {
+            mutis[i] = new ValidatorMutable(validators[i]);
+        }
+
+        boolean changed = false;
         
+        for ( ValidatorMutable validator : mutis ) {
+            if ( HTML_VALIDATOR_ID.equals(validator.getId()) ) {
+                if ( validator.isManualValidation() || validator.isBuildValidation() ) {
+                    validator.setBuildValidation(false);
+                    validator.setManualValidation(false);
+                    changed = true;
+                }
+            }
+        }
+
+        ProjectPreferences projectPreferences = ValManager.getDefault().getProjectPreferences(project);
+        if ( !projectPreferences.getOverride() ) {
+            projectPreferences = new ProjectPreferences(project, true, projectPreferences.getSuspend(), null);
+        }
+
+        if ( changed || !projectPreferences.getOverride() ) {
+            ValPrefManagerProject prefManager = new ValPrefManagerProject(project);
+            prefManager.savePreferences(projectPreferences, mutis);
+        }
+               
     }
 }
