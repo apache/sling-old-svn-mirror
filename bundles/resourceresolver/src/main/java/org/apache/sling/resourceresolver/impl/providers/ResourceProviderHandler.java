5bd86ec9d98a452965467ec9f9fcdd68e1616b1f
Carsten Ziegeler <cziegeler@apache.org>
2015-10-28 08:41:12 +0000

    SLING-5204 : Implement provider lifecycle methods
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk@1710962 13f79535-47bb-0310-9956-ffa450edef68

--- bundles/resourceresolver/src/main/java/org/apache/sling/resourceresolver/impl/providers/ResourceProviderHandler.java
+++ bundles/resourceresolver/src/main/java/org/apache/sling/resourceresolver/impl/providers/ResourceProviderHandler.java
@@ -18,70 +18,43 @@
  */
 package org.apache.sling.resourceresolver.impl.providers;
 
-import java.util.Dictionary;
-import java.util.Hashtable;
-
-import org.apache.sling.api.SlingConstants;
-import org.apache.sling.resourceresolver.impl.legacy.LegacyResourceProviderWhiteboard;
 import org.apache.sling.resourceresolver.impl.providers.tree.Pathable;
 import org.apache.sling.spi.resource.provider.ResourceProvider;
 import org.osgi.framework.BundleContext;
-import org.osgi.framework.Constants;
-import org.osgi.service.event.Event;
-import org.osgi.service.event.EventAdmin;
 
+/**
+ * Holder for a resource provider service.
+ */
 public class ResourceProviderHandler implements Comparable<ResourceProviderHandler>, Pathable {
 
     private final ResourceProviderInfo info;
 
     private final BundleContext bundleContext;
 
-    private final EventAdmin eventAdmin;
-
     private volatile ResourceProvider<?> provider;
 
-    public ResourceProviderHandler(final BundleContext bc, final ResourceProviderInfo info, final EventAdmin eventAdmin) {
+    public ResourceProviderHandler(final BundleContext bc, final ResourceProviderInfo info) {
         this.info = info;
         this.bundleContext = bc;
-        this.eventAdmin = eventAdmin;
     }
 
     public ResourceProviderInfo getInfo() {
         return this.info;
     }
 
-    public ResourceProvider<?> getResourceProvider() {
-        ResourceProvider<?> rp = this.provider;
-        if ( rp == null ) {
-            synchronized ( this ) {
-                if ( this.provider == null ) {
-                    this.provider = (ResourceProvider<?>) this.bundleContext.getService(this.info.getServiceReference());
-                }
-                rp = this.provider;
-                postEvent(SlingConstants.TOPIC_RESOURCE_PROVIDER_ADDED);
-            }
-        }
-        return rp;
+    public boolean activate() {
+        this.provider = (ResourceProvider<?>) this.bundleContext.getService(this.info.getServiceReference());
+        return this.provider != null;
     }
 
-    private void postEvent(String topic) {
-        final Dictionary<String, Object> eventProps = new Hashtable<String, Object>();
-        eventProps.put(SlingConstants.PROPERTY_PATH, info.getPath());
-        String pid = (String) info.getServiceReference().getProperty(Constants.SERVICE_PID);
-        if (pid == null) {
-            pid = (String) info.getServiceReference().getProperty(LegacyResourceProviderWhiteboard.ORIGINAL_SERVICE_PID);
-        }
-        if (pid != null) {
-            eventProps.put(Constants.SERVICE_PID, pid);
-        }
-        eventAdmin.postEvent(new Event(topic, eventProps));
+    public ResourceProvider<?> getResourceProvider() {
+        return this.provider;
     }
 
     public void deactivate() {
         if ( this.provider != null ) {
             this.provider = null;
             this.bundleContext.ungetService(this.info.getServiceReference());
-            postEvent(SlingConstants.TOPIC_RESOURCE_PROVIDER_REMOVED);
         }
     }
 
