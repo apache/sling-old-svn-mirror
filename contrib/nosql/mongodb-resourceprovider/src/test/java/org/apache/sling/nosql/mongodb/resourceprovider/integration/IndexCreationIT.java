8ac79c08541b4929e614db7edf18bf1de8a11e37
Stefan Seifert <sseifert@apache.org>
2015-11-24 22:39:37 +0000

    SLING-5078 Missing indexes on MongoDBResourceProvider (patch supplied by Norberto Leite) update path to ignore error when index already exists - log error if it could not be created do some refactoring on provided integration test to make sure the mongodb connection parameters passed via command line are used update to latest table mongodb java driver
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk@1716273 13f79535-47bb-0310-9956-ffa450edef68

---
+++ contrib/nosql/mongodb-resourceprovider/src/test/java/org/apache/sling/nosql/mongodb/resourceprovider/integration/IndexCreationIT.java
@@ -0,0 +1,73 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.sling.nosql.mongodb.resourceprovider.integration;
+
+import static org.junit.Assert.assertNotNull;
+
+import java.util.Arrays;
+
+import org.apache.sling.nosql.mongodb.resourceprovider.impl.MongoDBNoSqlAdapter;
+import org.bson.Document;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.mongodb.MongoClient;
+
+public class IndexCreationIT {
+
+	private MongoDBNoSqlAdapter obj;
+	
+	private MongoClient mongoClient;
+	private String database;
+	private String collection;
+		
+	@Before
+	public void setUp() throws Exception {
+	    String connectionString = System.getProperty("connectionString", "localhost:27017");
+        database =  System.getProperty("database", "sling") + "_indextest";
+        collection = System.getProperty("collection", "resources");
+		mongoClient = new MongoClient(connectionString);
+		obj = new MongoDBNoSqlAdapter(mongoClient, database, collection);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		mongoClient.dropDatabase(database);
+		mongoClient.close();
+	}
+
+	@Test
+	public void testIndexesPresent() {
+		assertNotNull(obj);
+		
+		//expecting at least 3 indexes (_id, _path, _parentPath)
+		int expected = 3;
+		int actual = 0;
+		
+		final String[] expectedIndexesNames=  {"_id_", "_path_1", "_parentPath_1"};
+		
+		for( Document d : mongoClient.getDatabase(database).getCollection(collection).listIndexes()){
+			assert Arrays.asList(expectedIndexesNames).contains( d.get("name") );
+			actual++;
+		}
+		assert expected == actual;
+	}
+
+}
