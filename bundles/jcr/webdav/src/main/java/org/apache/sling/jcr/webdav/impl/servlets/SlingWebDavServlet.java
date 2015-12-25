6133e1aea936dea811b10ec7efa246d0ccbdbfe8
Oliver Lietz <olli@apache.org>
2015-12-25 17:26:44 +0000

    SLING-5402 Component context not unset on SlingDeleteManager when deactivating SlingWebDavServlet
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk@1721687 13f79535-47bb-0310-9956-ffa450edef68

--- bundles/jcr/webdav/src/main/java/org/apache/sling/jcr/webdav/impl/servlets/SlingWebDavServlet.java
+++ bundles/jcr/webdav/src/main/java/org/apache/sling/jcr/webdav/impl/servlets/SlingWebDavServlet.java
@@ -258,6 +258,7 @@ public class SlingWebDavServlet extends SimpleWebdavServlet {
         this.ioManager.setComponentContext(null);
         this.propertyManager.setComponentContext(null);
         this.copyMoveManager.setComponentContext(null);
+        this.deleteManager.setComponentContext(null);
     }
 
     public void bindIOHandler(final ServiceReference ioHandlerReference) {
