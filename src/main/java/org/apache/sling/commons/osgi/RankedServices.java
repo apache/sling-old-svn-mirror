f0f66df2ccd9bdc6fa0cfd0df70cefee0d3563e2
Konrad Windszus <kwin@apache.org>
2015-12-26 16:11:38 +0000

    SLING-5403 remove unnecessary import
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk/bundles/commons/osgi@1721740 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/sling/commons/osgi/RankedServices.java
+++ src/main/java/org/apache/sling/commons/osgi/RankedServices.java
@@ -19,7 +19,6 @@
 package org.apache.sling.commons.osgi;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
