35b2626bbfff601c906c786a079fc26c5f689d46
Carsten Ziegeler <cziegeler@apache.org>
2015-12-30 07:19:04 +0000

    Remove new query api and move it to the next API version Rename JCRQueryProvider to QueryLanguageProvider Move new utility classes to separate package
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk@1722270 13f79535-47bb-0310-9956-ffa450edef68

--- bundles/api/src/test/java/org/apache/sling/api/resource/util/PathSetTest.java
+++ bundles/api/src/test/java/org/apache/sling/api/resource/util/PathSetTest.java
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package org.apache.sling.api.resource;
+package org.apache.sling.api.resource.util;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
--- bundles/api/src/test/java/org/apache/sling/api/resource/util/PathTest.java
+++ bundles/api/src/test/java/org/apache/sling/api/resource/util/PathTest.java
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package org.apache.sling.api.resource;
+package org.apache.sling.api.resource.util;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
