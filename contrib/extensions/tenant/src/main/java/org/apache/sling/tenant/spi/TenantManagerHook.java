0e5e7e93ddf768590ff0a7d3889203b96f75930a
Carsten Ziegeler <cziegeler@apache.org>
2016-01-04 13:34:17 +0000

    SLING-5240 : Remove getAdministrativeResourceResolver() usage from org.apache.sling.tenant
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk@1722866 13f79535-47bb-0310-9956-ffa450edef68

--- contrib/extensions/tenant/src/main/java/org/apache/sling/tenant/spi/TenantManagerHook.java
+++ contrib/extensions/tenant/src/main/java/org/apache/sling/tenant/spi/TenantManagerHook.java
@@ -20,7 +20,6 @@ package org.apache.sling.tenant.spi;
 
 import java.util.Map;
 
-import org.apache.sling.api.resource.ResourceResolver;
 import org.apache.sling.tenant.Tenant;
 
 import aQute.bnd.annotation.ConsumerType;
@@ -31,50 +30,49 @@ import aQute.bnd.annotation.ConsumerType;
  * Tools can hook into the tenant creation, changing a tenant and removing
  * thereof by implementing this interface.
  *
- * @deprecated This interface will not be supported in future versions. Use
- *             {@link TenantManagerHook} instead.
+ * @since 1.1
  */
-@Deprecated
 @ConsumerType
-public interface TenantCustomizer {
+public interface TenantManagerHook {
 
     /**
-     * Method called to create or update the given tenant. The method may return
-     * additional properties to be added to the Tenant's property list. The
-     * ResourceResolver allows for access to the persistence.
-     * <p>
-     * The {@code ResourceResolver.commit} method must not be called by this
-     * method.
+     * Method called to create the given tenant. The method may return
+     * additional properties to be added to the Tenant's property list.
      * <p>
      * This method is not expected to throw an exception. Any exception thrown
      * is logged but otherwise ignored.
      *
      * @param tenant The {@link Tenant} to be configured by this call
-     * @param resolver The {@code ResourceResolver} providing access to the
-     *            persistence for further setup. Note, that this
-     *            {@code resolver} will have administrative privileges.
      * @return Additional properties to be added to the tenant. These properties
      *         may later be accessed through the {@linkplain Tenant tenant's}
      *         property accessor methods. {@code null} or an empty map may be
      *         returned to not add properties.
      */
-    Map<String, Object> setup(Tenant tenant, ResourceResolver resolver);
+    Map<String, Object> setup(Tenant tenant);
+
+    /**
+     * Method called to update the given tenant. The method may return
+     * additional properties to be added to the Tenant's property list.
+     * <p>
+     * This method is not expected to throw an exception. Any exception thrown
+     * is logged but otherwise ignored.
+     *
+     * @param tenant The {@link Tenant} to be configured by this call
+     * @return Additional properties to be added to the tenant. These properties
+     *         may later be accessed through the {@linkplain Tenant tenant's}
+     *         property accessor methods. {@code null} or an empty map may be
+     *         returned to not add properties.
+     */
+    Map<String, Object> change(Tenant tenant);
 
     /**
      * Called to remove the setup for the given Tenant. This reverts all changes
-     * done by the #setup method. The ResourceResolver allows for access to the
-     * persistence.
-     * <p>
-     * The {@code ResourceResolver.commit} method must not be called by this
-     * method.
+     * done by the #setup method.
      * <p>
      * This method is not expected to throw an exception. Any exception thrown
      * is logged but otherwise ignored.
      *
      * @param tenant The {@link Tenant} about to be removed
-     * @param resolver The {@code ResourceResolver} providing access to the
-     *            persistence for further cleanup. Note, that this
-     *            {@code resolver} will have administrative privileges.
      */
-    void remove(Tenant tenant, ResourceResolver resolver);
+    void remove(Tenant tenant);
 }
