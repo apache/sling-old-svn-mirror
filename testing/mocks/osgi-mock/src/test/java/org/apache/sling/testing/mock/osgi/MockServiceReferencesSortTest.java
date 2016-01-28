898f5850c82c6a621ab8fc265fdda2ad8d18349d
Bertrand Delacretaz <bdelacretaz@apache.org>
2016-01-28 11:14:14 +0000

    SLING-5462 - demonstrate the wrong sorting of mock service references
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk@1727309 13f79535-47bb-0310-9956-ffa450edef68

---
+++ testing/mocks/osgi-mock/src/test/java/org/apache/sling/testing/mock/osgi/MockServiceReferencesSortTest.java
@@ -0,0 +1,48 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.sling.testing.mock.osgi;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.osgi.framework.BundleContext;
+
+/** Test the service-ranking based sorting of mock service references */
+public class MockServiceReferencesSortTest {
+    
+    private BundleContext bundleContext;
+
+    @Before
+    public void setUp() {
+        bundleContext = MockOsgi.newBundleContext();
+    }
+
+    @After
+    public void tearDown() {
+        MockOsgi.shutdown(bundleContext);
+    }
+    
+    @Test
+    public void testServicesOrder() {
+        // TODO this should return the same result as in RealServiceReferencesSortTest
+        assertEquals("54321", ServiceReferencesUtil.getSortedServicesString(bundleContext));
+    }
+}
