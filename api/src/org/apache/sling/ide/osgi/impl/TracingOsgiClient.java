e9e3a132a18c020e0a9829e01dac7ce636866e26
Robert Munteanu <rombert@apache.org>
2016-02-17 20:50:34 +0000

    SLING-3605 - Debug based on bundles deployed on the server
    Add OsgiClient.findSourceReferences
    
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk/tooling/ide@1730936 13f79535-47bb-0310-9956-ffa450edef68

--- api/src/org/apache/sling/ide/osgi/impl/TracingOsgiClient.java
+++ api/src/org/apache/sling/ide/osgi/impl/TracingOsgiClient.java
@@ -18,10 +18,12 @@ package org.apache.sling.ide.osgi.impl;
 
 import java.io.InputStream;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.apache.sling.ide.osgi.OsgiClient;
 import org.apache.sling.ide.osgi.OsgiClientException;
+import org.apache.sling.ide.osgi.SourceReference;
 import org.apache.sling.ide.transport.CommandExecutionProperties;
 import org.osgi.framework.Version;
 import org.osgi.service.event.Event;
@@ -101,6 +103,11 @@ public class TracingOsgiClient implements OsgiClient {
     public void installLocalBundle(InputStream jarredBundle, String sourceLocation) throws OsgiClientException {
 
         logInstallLocalBundle(jarredBundle, sourceLocation);
+    }
+    
+    @Override
+    public List<SourceReference> findSourceReferences() throws OsgiClientException {
+        return osgiClient.findSourceReferences();
     }
 
 }
