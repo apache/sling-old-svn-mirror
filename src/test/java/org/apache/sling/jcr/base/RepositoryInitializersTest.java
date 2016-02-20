d258464b932317e760e5561c4374e354f9f6ceaa
Carsten Ziegeler <cziegeler@apache.org>
2016-02-20 15:15:25 +0000

    Reverting SLING-5456 in order to release 2.3.2 (SLING-5531)
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk/bundles/jcr/base@1731411 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/sling/jcr/base/RepositoryInitializersTest.java
+++ src/test/java/org/apache/sling/jcr/base/RepositoryInitializersTest.java
@@ -21,23 +21,19 @@ package org.apache.sling.jcr.base;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-import java.util.Hashtable;
-
 import javax.jcr.Repository;
 import javax.jcr.RepositoryException;
 import javax.jcr.Session;
 
 import org.apache.sling.commons.osgi.SortingServiceTracker;
 import org.apache.sling.jcr.api.SlingRepository;
-import org.apache.sling.jcr.api.SlingRepositoryInitializer;
+//import org.apache.sling.jcr.api.SlingRepositoryInitializer;
 import org.apache.sling.testing.mock.jcr.MockJcr;
 import org.apache.sling.testing.mock.sling.ResourceResolverType;
 import org.apache.sling.testing.mock.sling.junit.SlingContext;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
-import org.junit.Test;
-import org.osgi.framework.Constants;
 import org.osgi.framework.ServiceReference;
 
 /** Test the SlingRepositoryInitializer mechanism */
@@ -45,19 +41,19 @@ public class RepositoryInitializersTest {
 
     @Rule
     public final SlingContext context = new SlingContext(ResourceResolverType.JCR_MOCK);
-    
+
     private static final String DEFAULT_WORKSPACE = "defws";
     private AbstractSlingRepositoryManager asrm;
     private Repository repository;
     private Session session;
     private SortingServiceTracker<SlingRepository> repoTracker;
-    
+
     /** We initially get a SlingRepository service from the Sling Mocks, and we're
      *  interested to find out whether the repository manager also registers one
      *  in our tests.
      */
     private int initialRepoServicesCount;
-    
+
     @Before
     public void setup() throws RepositoryException {
         repoTracker = new SortingServiceTracker<SlingRepository>(context.bundleContext(), SlingRepository.class.getName());
@@ -65,75 +61,75 @@ public class RepositoryInitializersTest {
         repository = MockJcr.newRepository();
         session = repository.login();
         asrm = new MockSlingRepositoryManager(repository);
-        
+
         initialRepoServicesCount = countRepositoryServices();
         assertAdditionalRepositoryServices(0);
     }
-    
+
     @After
     public void cleanup() {
         repoTracker.close();
         asrm.stop();
         session.logout();
     }
-    
+/*
     private void registerInitializer(String id, int serviceRanking) {
         final SlingRepositoryInitializer init = new TestInitializer(id);
         final Hashtable<String, Object> props = new Hashtable<String, Object>();
         props.put(Constants.SERVICE_RANKING, new Integer(serviceRanking));
         context.bundleContext().registerService(SlingRepositoryInitializer.class.getName(), init, props);
     }
-    
+*/
     private void assertTestInitializerProperty(String expected) throws RepositoryException {
         final String path = TestInitializer.getPropertyPath();
         assertTrue("Expecting property at " + path, session.propertyExists(path));
         assertEquals("Expecting correct value at " + path, expected, session.getProperty(path).getString());
     }
-    
+
     private int countRepositoryServices() {
         final ServiceReference [] refs = repoTracker.getServiceReferences();
         return (refs == null ? 0 : refs.length);
     }
-    
+
     private void assertAdditionalRepositoryServices(int expected) {
-        assertEquals("Expecting " + expected + " SlingRepository services", 
+        assertEquals("Expecting " + expected + " SlingRepository services",
                 expected + initialRepoServicesCount, countRepositoryServices());
     }
-    
+
     private void assertStart(boolean expected) {
         final boolean actual = asrm.start(context.bundleContext(), DEFAULT_WORKSPACE, false);
         assertEquals("Expecting start to return " + expected, expected, actual);
     }
-    
+    /*
     @Test
     public void inOrderInitializers() throws RepositoryException {
         for(int i=1; i < 4; i++) {
             registerInitializer(String.valueOf(i), i * 100);
         }
-        
+
         assertStart(true);
-        
-        // TODO this should really be 1,2,3 but the Sling OSGi mocks sort 
+
+        // TODO this should really be 1,2,3 but the Sling OSGi mocks sort
         // in the wrong order w.r.t service ranking, see SLING-5462,
         // will be fixed in osgi-mock 2.0.2
         assertTestInitializerProperty("3,2,1,");
         assertAdditionalRepositoryServices(1);
     }
-    
+
     @Test
     public void reverseOrderInitializers() throws RepositoryException {
         for(int i=1; i < 4; i++) {
             registerInitializer(String.valueOf(i), i * -100);
         }
-        
+
         assertStart(true);
-        
+
         // TODO see comment in inOrderInitializers, this should really
         // be 3,2,1, caused by SLING-5462
         assertTestInitializerProperty("1,2,3,");
         assertAdditionalRepositoryServices(1);
     }
-    
+
     @Test
     public void noRepositoryOnException() throws RepositoryException {
         registerInitializer("a", 1);
@@ -141,20 +137,20 @@ public class RepositoryInitializersTest {
         registerInitializer("c", 3);
 
         assertStart(false);
-        
-        // The repository manager does not register a service in this case 
+
+        // The repository manager does not register a service in this case
         assertAdditionalRepositoryServices(0);
     }
-    
+
     @Test
     public void noRepositoryOnError() throws RepositoryException {
         registerInitializer("a", 1);
         registerInitializer("ERROR", 2);
         registerInitializer("c", 3);
-        
+
         assertStart(false);
-        
-        // The repository manager does not register a service in this case 
+
+        // The repository manager does not register a service in this case
         assertAdditionalRepositoryServices(0);
-    }
+    }*/
 }
