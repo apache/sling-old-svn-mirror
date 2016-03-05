cabb4d401d9852a8cc520f3493fb25c52c177428
Oliver Lietz <olli@apache.org>
2016-03-05 14:06:40 +0000

    remove unused imports
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk/bundles/extensions/validation@1733720 13f79535-47bb-0310-9956-ffa450edef68

--- core/src/test/java/org/apache/sling/validation/impl/ValidationModelRetrieverImplTest.java
+++ core/src/test/java/org/apache/sling/validation/impl/ValidationModelRetrieverImplTest.java
@@ -23,8 +23,6 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Map.Entry;
-import java.util.regex.Pattern;
 
 import javax.annotation.Nonnull;
 
