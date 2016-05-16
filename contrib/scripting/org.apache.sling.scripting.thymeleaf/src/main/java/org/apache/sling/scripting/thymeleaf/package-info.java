57c95a1222b7ce03e61909af3d7b6a220950e802
Oliver Lietz <olli@apache.org>
2016-05-16 13:42:36 +0000

    SLING-5075 Upgrade Thymeleaf to 3.0
    set version of package org.apache.sling.scripting.thymeleaf to 1.0.0
    
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk@1744048 13f79535-47bb-0310-9956-ffa450edef68

--- contrib/scripting/org.apache.sling.scripting.thymeleaf/src/main/java/org/apache/sling/scripting/thymeleaf/package-info.java
+++ contrib/scripting/org.apache.sling.scripting.thymeleaf/src/main/java/org/apache/sling/scripting/thymeleaf/package-info.java
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-@Version("0.0.2")
+@Version("1.0.0")
 package org.apache.sling.scripting.thymeleaf;
 
 import org.osgi.annotation.versioning.Version;
