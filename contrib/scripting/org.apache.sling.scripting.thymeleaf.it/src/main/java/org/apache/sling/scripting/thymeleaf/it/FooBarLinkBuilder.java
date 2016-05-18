2a53a815abb36b88193bc9a995c33da888ed2595
Oliver Lietz <olli@apache.org>
2016-05-18 10:57:00 +0000

    SLING-5680 Add Integration Tests for Scripting Thymeleaf
    add tests for link builder and decoupled template logic
    
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk@1744389 13f79535-47bb-0310-9956-ffa450edef68

---
+++ contrib/scripting/org.apache.sling.scripting.thymeleaf.it/src/main/java/org/apache/sling/scripting/thymeleaf/it/FooBarLinkBuilder.java
@@ -0,0 +1,49 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.sling.scripting.thymeleaf.it;
+
+import java.util.Map;
+
+import org.thymeleaf.context.IExpressionContext;
+import org.thymeleaf.linkbuilder.ILinkBuilder;
+
+public class FooBarLinkBuilder implements ILinkBuilder {
+
+    public FooBarLinkBuilder() {
+    }
+
+    @Override
+    public String getName() {
+        return getClass().getSimpleName();
+    }
+
+    @Override
+    public Integer getOrder() {
+        return 0;
+    }
+
+    @Override
+    public String buildLink(final IExpressionContext expressionContext, final String base, final Map<String, Object> parameters) {
+        if ("foo".equals(base)) {
+            return "foobar";
+        }
+        return null;
+    }
+
+}
