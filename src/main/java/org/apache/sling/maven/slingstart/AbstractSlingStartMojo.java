af5befd943e0717675f976b3333246d232d35ff8
Carsten Ziegeler <cziegeler@apache.org>
2016-05-19 12:45:11 +0000

    SLING-5738 : Add support for configuring a pattern for the model files to be read
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk/tooling/maven/slingstart-maven-plugin@1744562 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/sling/maven/slingstart/AbstractSlingStartMojo.java
+++ src/main/java/org/apache/sling/maven/slingstart/AbstractSlingStartMojo.java
@@ -39,6 +39,13 @@ public abstract class AbstractSlingStartMojo extends AbstractMojo {
     private File modelDirectory;
 
     /**
+     * The model file name pattern
+     * This parameter is evaluated in the DependencyLifecycleParticipant
+     */
+    @Parameter
+    private String modelPattern;
+
+    /**
      * Inlined model, supported since version 1.3.
      * This parameter is evaluated in the DependencyLifecycleParticipant
      */
