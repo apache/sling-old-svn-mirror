5fbef4d16910a702999174e432ea3dc523f41ea7
Konrad Windszus <kwin@apache.org>
2016-05-20 11:00:36 +0000

    SLING-5740 fix generating a locale from a string
    provide additional test cases for that
    This closes #142
    
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk@1744704 13f79535-47bb-0310-9956-ffa450edef68

---
+++ bundles/extensions/i18n/src/test/java/org/apache/sling/i18n/impl/JcrResourceBundleProviderTest.java
@@ -0,0 +1,54 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.sling.i18n.impl;
+
+import java.util.Locale;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class JcrResourceBundleProviderTest {
+
+    @Test
+    public void testToLocale() {
+        // empty string must return default locale
+        Assert.assertEquals(Locale.getDefault(), JcrResourceBundleProvider.toLocale(""));
+        
+        // only language part being set
+        Assert.assertEquals(Locale.ENGLISH, JcrResourceBundleProvider.toLocale("en"));
+        Assert.assertEquals(Locale.GERMAN, JcrResourceBundleProvider.toLocale("de"));
+        // for invalid languages assume default language
+        Assert.assertEquals(new Locale(Locale.getDefault().getLanguage()), JcrResourceBundleProvider.toLocale("-"));
+        
+        // both language and country being set
+        Assert.assertEquals(Locale.GERMANY, JcrResourceBundleProvider.toLocale("de_DE"));
+        Assert.assertEquals(Locale.UK, JcrResourceBundleProvider.toLocale("en_GB"));
+        // for invalid languages assume default language
+        Assert.assertEquals(new Locale(Locale.getDefault().getLanguage(), "GB"), JcrResourceBundleProvider.toLocale("-_GB"));
+        // for invalid countries assume default country
+        Assert.assertEquals(new Locale("en", Locale.getDefault().getCountry()), JcrResourceBundleProvider.toLocale("en_-"));
+    
+        // language, country and variant being set
+        Assert.assertEquals(new Locale(Locale.UK.getLanguage(), Locale.UK.getCountry(), "variant1"), JcrResourceBundleProvider.toLocale("en_GB_variant1"));
+        
+        // parts after the variant are just ignored
+        Assert.assertEquals(new Locale(Locale.UK.getLanguage(), Locale.UK.getCountry(), "variant1"), JcrResourceBundleProvider.toLocale("en_GB_variant1_something"));
+    }
+}
