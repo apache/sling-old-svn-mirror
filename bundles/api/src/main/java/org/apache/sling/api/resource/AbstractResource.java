e9b8861b33464f6e58c521cb9cf2976ee92abcd6
Konrad Windszus <kwin@apache.org>
2016-06-01 11:19:48 +0000

    adjust javadoc of getParent() to actual implementation
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk@1746429 13f79535-47bb-0310-9956-ffa450edef68

--- bundles/api/src/main/java/org/apache/sling/api/resource/AbstractResource.java
+++ bundles/api/src/main/java/org/apache/sling/api/resource/AbstractResource.java
@@ -56,9 +56,7 @@ public abstract class AbstractResource
     /**
      * Returns the parent resource of this resource.
      * <p>
-     * This method is implemented by getting the parent resource path first
-     * calling the {@link ResourceUtil#getParent(String)} method and then to
-     * retrieve that resource from the resource resolver.
+     * This method is implemented by retrieving that resource from the resource resolver.
      */
     public Resource getParent() {
         return getResourceResolver().getParent(this);
