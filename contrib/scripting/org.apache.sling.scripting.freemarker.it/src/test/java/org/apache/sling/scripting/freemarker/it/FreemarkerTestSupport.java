ade4cdc5bea597be90a096337402d34dbfba2a40
Oliver Lietz <olli@apache.org>
2016-06-23 21:32:19 +0000

    SLING-5692 Add Integration Tests for Scripting FreeMarker
    use Pax Exam Options instead of Karaf Features
    
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk@1750023 13f79535-47bb-0310-9956-ffa450edef68

--- contrib/scripting/org.apache.sling.scripting.freemarker.it/src/test/java/org/apache/sling/scripting/freemarker/it/FreemarkerTestSupport.java
+++ contrib/scripting/org.apache.sling.scripting.freemarker.it/src/test/java/org/apache/sling/scripting/freemarker/it/FreemarkerTestSupport.java
@@ -19,23 +19,40 @@
 package org.apache.sling.scripting.freemarker.it;
 
 import java.io.File;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.util.Dictionary;
 
 import javax.inject.Inject;
 import javax.script.ScriptEngineFactory;
 
+import org.apache.sling.api.servlets.ServletResolver;
 import org.apache.sling.auth.core.AuthenticationSupport;
 import org.apache.sling.engine.SlingRequestProcessor;
-import org.apache.sling.launchpad.karaf.testing.KarafTestSupport;
+import org.apache.sling.testing.paxexam.SlingOptions;
 import org.ops4j.pax.exam.Configuration;
 import org.ops4j.pax.exam.Option;
-import org.ops4j.pax.exam.OptionUtils;
 import org.ops4j.pax.exam.util.Filter;
+import org.osgi.service.cm.ConfigurationAdmin;
+import org.osgi.service.http.HttpService;
 
+import static org.apache.sling.testing.paxexam.SlingOptions.slingExtensionModels;
+import static org.apache.sling.testing.paxexam.SlingOptions.slingJcrOak;
+import static org.apache.sling.testing.paxexam.SlingOptions.slingLaunchpadOak;
+import static org.apache.sling.testing.paxexam.SlingOptions.slingScripting;
+import static org.ops4j.pax.exam.CoreOptions.bootDelegationPackage;
 import static org.ops4j.pax.exam.CoreOptions.bundle;
+import static org.ops4j.pax.exam.CoreOptions.composite;
+import static org.ops4j.pax.exam.CoreOptions.junitBundles;
+import static org.ops4j.pax.exam.CoreOptions.keepCaches;
 import static org.ops4j.pax.exam.CoreOptions.mavenBundle;
-import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.editConfigurationFilePut;
+import static org.ops4j.pax.exam.CoreOptions.workingDirectory;
+import static org.ops4j.pax.exam.cm.ConfigurationAdminOptions.newConfiguration;
 
-public abstract class FreemarkerTestSupport extends KarafTestSupport {
+public abstract class FreemarkerTestSupport {
+
+    @Inject
+    protected ServletResolver servletResolver;
 
     @Inject
     protected SlingRequestProcessor slingRequestProcessor;
@@ -44,25 +61,74 @@ public abstract class FreemarkerTestSupport extends KarafTestSupport {
     protected AuthenticationSupport authenticationSupport;
 
     @Inject
+    protected HttpService httpService;
+
+    @Inject
     @Filter(value = "(names=freemarker)")
     protected ScriptEngineFactory scriptEngineFactory;
 
+    @Inject
+    protected ConfigurationAdmin configurationAdmin;
+
+    protected static synchronized int findFreePort() {
+        try {
+            final ServerSocket serverSocket = new ServerSocket(0);
+            final int port = serverSocket.getLocalPort();
+            serverSocket.close();
+            return port;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    protected int httpPort() throws IOException {
+        final Dictionary<String, Object> properties = configurationAdmin.getConfiguration("org.apache.felix.http").getProperties();
+        return Integer.parseInt(properties.get("org.osgi.service.http.port").toString());
+    }
+
     @Configuration
     public Option[] configuration() {
+        final String workingDirectory = String.format("target/paxexam/%s", getClass().getSimpleName());
         final String filename = System.getProperty("bundle.filename");
         final File file = new File(filename);
-        return OptionUtils.combine(baseConfiguration(),
-            editConfigurationFilePut("etc/org.ops4j.pax.logging.cfg", "log4j.rootLogger", "ERROR, out, sift, osgi:*"),
+        return new Option[]{
+            keepCaches(),
+            workingDirectory(workingDirectory),
+            launchpad(workingDirectory),
+            // test app bundle
             bundle(file.toURI().toString()),
-            addSlingFeatures(
-                "sling-launchpad-oak-tar",
-                "sling-scripting",
-                "sling-extension-models"
-            ),
+            // FreeMarker
             mavenBundle().groupId("org.apache.sling").artifactId("org.apache.sling.scripting.freemarker").versionAsInProject(),
-            mavenBundle().groupId("org.javassist").artifactId("javassist").versionAsInProject(),
+            mavenBundle().groupId("org.apache.servicemix.specs").artifactId("org.apache.servicemix.specs.jaxp-api-1.4").versionAsInProject(),
+            bootDelegationPackage("javax.swing.*"),
+            // testing
             mavenBundle().groupId("org.jsoup").artifactId("jsoup").versionAsInProject(),
-            mavenBundle().groupId("org.apache.servicemix.bundles").artifactId("org.apache.servicemix.bundles.hamcrest").versionAsInProject()
+            mavenBundle().groupId("org.apache.servicemix.bundles").artifactId("org.apache.servicemix.bundles.hamcrest").versionAsInProject(),
+            junitBundles()
+        };
+    }
+
+    protected static Option launchpad(final String workingDirectory) {
+        final int httpPort = findFreePort();
+        final String slingHome = String.format("%s/sling", workingDirectory);
+        final String repositoryHome = String.format("%s/repository", slingHome);
+        final String localIndexDir = String.format("%s/index", repositoryHome);
+        return composite(
+            slingJcrOak(), // TODO if slingJcrOak() is called elsewhere, ResourceResolverFactory will not be created
+            slingLaunchpadOak(),
+            slingExtensionModels(),
+            slingScripting(),
+            newConfiguration("org.apache.felix.http")
+                .put("org.osgi.service.http.port", httpPort)
+                .asOption(),
+            newConfiguration("org.apache.jackrabbit.oak.plugins.segment.SegmentNodeStoreService")
+                .put("repository.home", repositoryHome)
+                .put("name", "Default NodeStore")
+                .asOption(),
+            newConfiguration("org.apache.jackrabbit.oak.plugins.index.lucene.LuceneIndexProviderService")
+                .put("localIndexDir", localIndexDir)
+                .asOption(),
+            mavenBundle().groupId("org.apache.jackrabbit").artifactId("oak-segment").version(SlingOptions.versionResolver)
         );
     }
 
