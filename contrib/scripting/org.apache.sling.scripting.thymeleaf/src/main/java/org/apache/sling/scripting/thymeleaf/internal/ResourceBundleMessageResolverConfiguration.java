c2147280aa1d438bd8da3c000b4ed649eeb437d4
Oliver Lietz <olli@apache.org>
2016-07-01 15:04:37 +0000

    SLING-5075 Upgrade Thymeleaf to 3.0
    tweak names and descriptions
    
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk@1750956 13f79535-47bb-0310-9956-ffa450edef68

--- contrib/scripting/org.apache.sling.scripting.thymeleaf/src/main/java/org/apache/sling/scripting/thymeleaf/internal/ResourceBundleMessageResolverConfiguration.java
+++ contrib/scripting/org.apache.sling.scripting.thymeleaf/src/main/java/org/apache/sling/scripting/thymeleaf/internal/ResourceBundleMessageResolverConfiguration.java
@@ -22,8 +22,8 @@ import org.osgi.service.metatype.annotations.AttributeDefinition;
 import org.osgi.service.metatype.annotations.ObjectClassDefinition;
 
 @ObjectClassDefinition(
-    name = "Apache Sling Scripting Thymeleaf “ResourceBundle Message Resolver”",
-    description = "resource bundle message resolver for Sling Scripting Thymeleaf"
+    name = "Apache Sling Scripting Thymeleaf “ResourceBundle MessageResolver”",
+    description = "Resolves messages by using Sling's resource bundle provider from Sling i18n."
 )
 @interface ResourceBundleMessageResolverConfiguration {
 
