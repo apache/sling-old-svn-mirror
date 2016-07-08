0fe51b6af9935ca59132963ec2f32f2489bcb23d
Stefan Seifert <sseifert@apache.org>
2016-07-08 15:45:02 +0000

    SLING-5770 add support for custom resource resolver factory activator configuration via SlingContextBuilder add support for additional callbacks before setup and after teardown
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk@1751927 13f79535-47bb-0310-9956-ffa450edef68

--- testing/mocks/sling-mock/src/main/java/org/apache/sling/testing/mock/sling/junit/package-info.java
+++ testing/mocks/sling-mock/src/main/java/org/apache/sling/testing/mock/sling/junit/package-info.java
@@ -19,5 +19,5 @@
 /**
  * Rule for providing easy access to Sling context in JUnit tests.
  */
-@aQute.bnd.annotation.Version("3.1")
+@aQute.bnd.annotation.Version("3.2")
 package org.apache.sling.testing.mock.sling.junit;
