ced01c057d65adb6fc7e62ab2a1aca2c57c18bad
Carsten Ziegeler <cziegeler@apache.org>
2016-07-24 11:02:02 +0000

    Update to parent pom 27
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk@1753905 13f79535-47bb-0310-9956-ffa450edef68

--- bundles/commons/osgi/src/main/java/org/apache/sling/commons/osgi/bundleversion/package-info.java
+++ bundles/commons/osgi/src/main/java/org/apache/sling/commons/osgi/bundleversion/package-info.java
@@ -16,5 +16,5 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-@aQute.bnd.annotation.Version("2.2")
+@org.osgi.annotation.versioning.Version("2.2")
 package org.apache.sling.commons.osgi.bundleversion;
