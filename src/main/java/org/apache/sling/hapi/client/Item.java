b1f1fc445f08f7c887328386b6c2186fd6a58f4b
Andrei Dulvac <dulvac@apache.org>
2016-08-10 11:38:38 +0000

    SLING-5880 Fixed service interface
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk/contrib/extensions/hapi/client@1755727 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/sling/hapi/client/Item.java
+++ src/main/java/org/apache/sling/hapi/client/Item.java
@@ -20,21 +20,22 @@ package org.apache.sling.hapi.client;
  ******************************************************************************/
 
 import org.apache.http.NameValuePair;
-
-import java.util.List;
 import java.util.Set;
 
 /**
- *
+ * An HTML item representation. This maps to an html element and contains all the child elements.
+ * The child <i>semantic</i> elements (properties, links, forms) and the Item's metadata (src, href, value)
+ * are accessible through the dedicated methods
  */
 public interface Item {
     /**
-     * Returns the property of the item having the given name.
+     * Returns the property of the item having the given name. The property is a child Item.
      */
     Items prop(String name) throws ClientException;
 
     /**
-     * Return a List of all the properties of this item
+     * Return a List of all the properties of this item.
+     * The returned Strings are names that can be used for {@link #prop(String)}
      */
     Set<String> props() throws ClientException;
 
@@ -81,12 +82,12 @@ public interface Item {
     String src();
 
     /**
-     * Follow a hyperlink and get a new {@see Document} representation
+     * Follow a hyperlink and get a new {@link Document} representation
      */
     Document follow() throws ClientException;
 
     /**
-     * Submits this form item and returns a new {@see Document} representation
+     * Submits this form item and returns a new {@link Document} representation
      */
     Document submit(Iterable<NameValuePair> data) throws ClientException;
 }
