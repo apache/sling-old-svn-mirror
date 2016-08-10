2a0fbf9fc4a457a87a5737aae09ae3650ac0ff24
Carsten Ziegeler <cziegeler@apache.org>
2016-08-10 12:48:46 +0000

    Update to latest parent pom, clean up pom, use OSGi annotations
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk/contrib/extensions/hapi/client@1755735 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/sling/hapi/client/package-info.java
+++ src/main/java/org/apache/sling/hapi/client/package-info.java
@@ -17,7 +17,5 @@
  * under the License.
  ******************************************************************************/
 
-@Version("1.0.0")
+@org.osgi.annotation.versioning.Version("1.0.0")
 package org.apache.sling.hapi.client;
-
-import aQute.bnd.annotation.Version;
