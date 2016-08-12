4a0677a5cc136ee30ba1f6a53087d23b7362735a
Oliver Lietz <olli@apache.org>
2016-08-12 11:40:57 +0000

    move implementation to package internal
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk/bundles/jcr/oak-server@1756161 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/sling/jcr/oak/server/internal/TCCLWrappingJackrabbitRepository.java
+++ src/main/java/org/apache/sling/jcr/oak/server/internal/TCCLWrappingJackrabbitRepository.java
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package org.apache.sling.jcr.oak.server;
+package org.apache.sling.jcr.oak.server.internal;
 
 import java.util.Map;
 
