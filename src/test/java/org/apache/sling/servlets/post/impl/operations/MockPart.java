f425f4eaf096c8d0db37b8905ec934bc90aa37b5
Ian Boston <ieb@apache.org>
2016-08-12 11:57:57 +0000

    SLING-5948 Support Streaming - Added unit test coverage and tested in launchpad
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk/bundles/servlets/post@1756168 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/test/java/org/apache/sling/servlets/post/impl/operations/MockPart.java
@@ -0,0 +1,96 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.sling.servlets.post.impl.operations;
+
+import javax.servlet.http.Part;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Collection;
+import java.util.Map;
+
+public class MockPart implements Part {
+
+    private Map<String, Object> headers;
+    private long size;
+    private String submittedFileName;
+    private String name;
+    private String contentType;
+    private InputStream inputStream;
+
+    public MockPart(String name, String contentType, String submittedFileName, long size, InputStream inputStream) {
+        this.name = name;
+        this.contentType = contentType;
+        this.submittedFileName = submittedFileName;
+        this.size = size;
+        this.inputStream = inputStream;
+    }
+
+    @Override
+    public InputStream getInputStream() throws IOException {
+
+        return inputStream;
+    }
+
+    @Override
+    public String getContentType() {
+        return contentType;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String getSubmittedFileName() {
+        return submittedFileName;
+    }
+
+    @Override
+    public long getSize() {
+        return size;
+    }
+
+    @Override
+    public void write(String s) throws IOException {
+        throw new UnsupportedOperationException("Writing a part to disk is not supported.");
+
+    }
+
+    @Override
+    public void delete() throws IOException {
+
+    }
+
+    @Override
+    public String getHeader(String s) {
+        return (String) headers.get(s);
+    }
+
+    @Override
+    public Collection<String> getHeaders(String s) {
+        return (Collection<String>) headers.get(s);
+    }
+
+    @Override
+    public Collection<String> getHeaderNames() {
+        return headers.keySet();
+    }
+}
