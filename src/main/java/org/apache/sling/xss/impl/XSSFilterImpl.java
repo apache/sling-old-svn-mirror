630d0227bd5c754e29fda9fe04aecb29041127c8
Radu Cotescu <radu@apache.org>
2016-08-22 12:53:04 +0000

    SLING-4560 - XSSAPI#getValidHref is empty for valid Bengali or Hindi characters
    * applied patch submitted by Lars Krapf to only extend the character classes accepted
    by the OWASP URL regexes, instead of completely changing them like in r1756802
    
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk/bundles/extensions/xss@1757160 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/sling/xss/impl/XSSFilterImpl.java
+++ src/main/java/org/apache/sling/xss/impl/XSSFilterImpl.java
@@ -58,8 +58,8 @@ public class XSSFilterImpl implements XSSFilter, EventHandler {
     static final Attribute DEFAULT_HREF_ATTRIBUTE = new Attribute(
             "href",
             Arrays.asList(
-                    Pattern.compile("(?!.*javascript:)(([^?#]*))?([^?#]*)(\\?([^#]*))?(#(.*))?(\\s)*"),
-                    Pattern.compile("(\\s)*((ht|f)tp(s?)://|mailto:)(([^?#]*))?([^?#]*)(\\?([^#]*))?(#(.*))?(\\s)*")
+                    Pattern.compile("([\\p{L}\\p{M}*+\\p{N}\\\\\\.\\#@\\$%\\+&;\\-_~,\\?=/!\\*\\(\\)]*|\\#(\\w)+)"),
+                    Pattern.compile("(\\s)*((ht|f)tp(s?)://|mailto:)[\\p{L}\\p{M}*+\\p{N}]+[\\p{L}\\p{M}*+\\p{N}\\p{Zs}\\.\\#@\\$%\\+&;:\\-_~,\\?=/!\\*\\(\\)]*(\\s)*")
             ),
             Collections.<String>emptyList(),
             "removeAttribute", ""
