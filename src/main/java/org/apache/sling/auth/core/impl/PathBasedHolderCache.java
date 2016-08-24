e3402ac5f33da5645557f8a699f727385905d4ed
Carsten Ziegeler <cziegeler@apache.org>
2016-08-24 09:18:00 +0000

    SLING-5993 : Improve auth requirement whiteboard implementation
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk/bundles/auth/core@1757481 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/sling/auth/core/impl/PathBasedHolderCache.java
+++ src/main/java/org/apache/sling/auth/core/impl/PathBasedHolderCache.java
@@ -21,7 +21,6 @@ package org.apache.sling.auth.core.impl;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.SortedSet;
@@ -92,34 +91,6 @@ public class PathBasedHolderCache<Type extends PathBasedHolder> {
                         byHostMap.remove(holder.host);
                     } else {
                         byHostMap.put(holder.host, set);
-                    }
-                }
-            }
-        } finally {
-            this.rwLock.writeLock().unlock();
-        }
-    }
-
-    /**
-     * Remove all holders which "equal" the provided holder
-     * @param holder Template holder
-     */
-    public void removeAllMatchingHolders(final Type holder) {
-        this.rwLock.writeLock().lock();
-        try {
-            for(final Map.Entry<String,  Map<String, SortedSet<Type>>> entry : this.cache.entrySet()) {
-                final Iterator<Map.Entry<String, SortedSet<Type>>> innerIter = entry.getValue().entrySet().iterator();
-                while ( innerIter.hasNext() ) {
-                    final Map.Entry<String, SortedSet<Type>> innerEntry = innerIter.next();
-                    final Iterator<Type> iter = innerEntry.getValue().iterator();
-                    while ( iter.hasNext() ) {
-                        final Type current = iter.next();
-                        if ( holder.equals(current) ) {
-                            iter.remove();
-                        }
-                    }
-                    if ( innerEntry.getValue().isEmpty() ) {
-                        innerIter.remove();
                     }
                 }
             }
