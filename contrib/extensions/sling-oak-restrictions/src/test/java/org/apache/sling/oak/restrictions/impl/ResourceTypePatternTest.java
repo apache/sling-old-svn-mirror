cb47bfedeb949fb4bdf663ed73d6b4039feda3e4
georg.henzler <georg.henzler@netcentric.biz>
2016-09-14 19:44:40 +0200

    SLING-6045 Improving maven GAV/java package name

--- contrib/extensions/sling-oak-restrictions/src/test/java/org/apache/sling/oak/restrictions/impl/ResourceTypePatternTest.java
+++ contrib/extensions/sling-oak-restrictions/src/test/java/org/apache/sling/oak/restrictions/impl/ResourceTypePatternTest.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.sling.slingoakrestrictions.impl;
+package org.apache.sling.oak.restrictions.impl;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
