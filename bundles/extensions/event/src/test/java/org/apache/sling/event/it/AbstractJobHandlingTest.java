e077616db54decd5d5153b41370b08d08b50ee45
Carsten Ziegeler <cziegeler@apache.org>
2016-09-20 10:06:21 +0000

    SLING-5998 : NewJobSender should move to new ResourceChangeListener API
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk@1761546 13f79535-47bb-0310-9956-ffa450edef68

--- bundles/extensions/event/src/test/java/org/apache/sling/event/it/AbstractJobHandlingTest.java
+++ bundles/extensions/event/src/test/java/org/apache/sling/event/it/AbstractJobHandlingTest.java
@@ -49,6 +49,7 @@ import org.apache.sling.event.jobs.consumer.JobExecutor;
 import org.ops4j.pax.exam.Configuration;
 import org.ops4j.pax.exam.CoreOptions;
 import org.ops4j.pax.exam.Option;
+import org.ops4j.pax.exam.cm.ConfigurationAdminOptions;
 import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.BundleException;
@@ -97,8 +98,14 @@ public abstract class AbstractJobHandlingTest {
 
         String localRepo = System.getProperty("maven.repo.local", "");
 
+        final String jackrabbitVersion = "2.13.1";
+        final String oakVersion = "1.5.7";
+
+        final String slingHome = new File(buildDir + File.separatorChar + "sling_" + System.currentTimeMillis()).getAbsolutePath();
+
         return options(
-                frameworkProperty("sling.home").value(new File(buildDir + File.separatorChar + "sling_" + System.currentTimeMillis()).getAbsolutePath()),
+                frameworkProperty("sling.home").value(slingHome),
+                frameworkProperty("repository.home").value(slingHome + File.separatorChar + "repository"),
                 when( localRepo.length() > 0 ).useOptions(
                         systemProperty("org.ops4j.pax.url.mvn.localRepository").value(localRepo)
                 ),
@@ -106,6 +113,57 @@ public abstract class AbstractJobHandlingTest {
                         systemProperty(PORT_CONFIG).value(System.getProperty(PORT_CONFIG))),
                 systemProperty("pax.exam.osgi.unresolved.fail").value("true"),
 
+                ConfigurationAdminOptions.newConfiguration("org.apache.felix.jaas.ConfigurationSpi")
+                    .create(true)
+                    .put("jaas.defaultRealmName", "jackrabbit.oak")
+                    .put("jaas.configProviderName", "FelixJaasProvider")
+                    .asOption(),
+                ConfigurationAdminOptions.factoryConfiguration("org.apache.felix.jaas.Configuration.factory")
+                    .create(true)
+                    .put("jaas.controlFlag", "optional")
+                    .put("jaas.classname", "org.apache.jackrabbit.oak.spi.security.authentication.GuestLoginModule")
+                    .put("jaas.ranking", 300)
+                    .asOption(),
+                ConfigurationAdminOptions.factoryConfiguration("org.apache.felix.jaas.Configuration.factory")
+                    .create(true)
+                    .put("jaas.controlFlag", "required")
+                    .put("jaas.classname", "org.apache.jackrabbit.oak.security.authentication.user.LoginModuleImpl")
+                    .asOption(),
+                ConfigurationAdminOptions.factoryConfiguration("org.apache.felix.jaas.Configuration.factory")
+                    .create(true)
+                    .put("jaas.controlFlag", "sufficient")
+                    .put("jaas.classname", "org.apache.jackrabbit.oak.security.authentication.token.TokenLoginModule")
+                    .put("jaas.ranking", 200)
+                    .asOption(),
+                ConfigurationAdminOptions.newConfiguration("org.apache.jackrabbit.oak.security.authentication.AuthenticationConfigurationImpl")
+                    .create(true)
+                    .put("org.apache.jackrabbit.oak.authentication.configSpiName", "FelixJaasProvider")
+                    .asOption(),
+                ConfigurationAdminOptions.newConfiguration("org.apache.jackrabbit.oak.security.user.UserConfigurationImpl")
+                    .create(true)
+                    .put("groupsPath", "/home/groups")
+                    .put("usersPath", "/home/users")
+                    .put("defaultPath", "1")
+                    .put("importBehavior", "besteffort")
+                    .asOption(),
+                ConfigurationAdminOptions.newConfiguration("org.apache.jackrabbit.oak.security.user.RandomAuthorizableNodeName")
+                    .create(true)
+                    .put("enabledActions", new String[] {"org.apache.jackrabbit.oak.spi.security.user.action.AccessControlAction"})
+                    .put("userPrivilegeNames", new String[] {"jcr:all"})
+                    .put("groupPrivilegeNames", new String[] {"jcr:read"})
+                    .asOption(),
+                ConfigurationAdminOptions.newConfiguration("org.apache.jackrabbit.oak.spi.security.user.action.DefaultAuthorizableActionProvider")
+                    .create(true)
+                    .put("length", 21)
+                    .asOption(),
+                ConfigurationAdminOptions.newConfiguration("org.apache.jackrabbit.oak.plugins.segment.SegmentNodeStoreService")
+                    .create(true)
+                    .put("name", "Default NodeStore")
+                    .asOption(),
+                ConfigurationAdminOptions.newConfiguration("org.apache.sling.resourceresolver.impl.observation.OsgiObservationBridge")
+                    .create(true)
+                    .put("enabled", true)
+                    .asOption(),
                 // logging
                 systemProperty("pax.exam.logging").value("none"),
                 mavenBundle("org.apache.sling", "org.apache.sling.commons.log", "4.0.6"),
@@ -129,9 +187,10 @@ public abstract class AbstractJobHandlingTest {
 
                 // infrastructure
                 mavenBundle("org.apache.felix", "org.apache.felix.http.servlet-api", "1.1.2"),
+                mavenBundle("org.apache.felix", "org.apache.felix.http.jetty", "3.1.6"),
                 mavenBundle("org.apache.felix", "org.apache.felix.eventadmin", "1.4.4"),
-                mavenBundle("org.apache.felix", "org.apache.felix.scr", "2.0.2"),
-                mavenBundle("org.apache.felix", "org.apache.felix.configadmin", "1.8.8"),
+                mavenBundle("org.apache.felix", "org.apache.felix.scr", "2.0.4"),
+                mavenBundle("org.apache.felix", "org.apache.felix.configadmin", "1.8.10"),
                 mavenBundle("org.apache.felix", "org.apache.felix.inventory", "1.0.4"),
                 mavenBundle("org.apache.felix", "org.apache.felix.metatype", "1.1.2"),
 
@@ -146,33 +205,45 @@ public abstract class AbstractJobHandlingTest {
 
                 mavenBundle("org.apache.sling", "org.apache.sling.auth.core", "1.3.12"),
                 mavenBundle("org.apache.sling", "org.apache.sling.discovery.api", "1.0.2"),
+                mavenBundle("org.apache.sling", "org.apache.sling.discovery.commons", "1.0.12"),
                 mavenBundle("org.apache.sling", "org.apache.sling.discovery.standalone", "1.0.2"),
 
-                mavenBundle("org.apache.sling", "org.apache.sling.api", "2.9.0"),
-                mavenBundle("org.apache.sling", "org.apache.sling.resourceresolver", "1.2.6"),
+                mavenBundle("org.apache.sling", "org.apache.sling.api", "2.14.2"),
+                mavenBundle("org.apache.sling", "org.apache.sling.resourceresolver", "1.4.18"),
                 mavenBundle("org.apache.sling", "org.apache.sling.adapter", "2.1.2"),
-                mavenBundle("org.apache.sling", "org.apache.sling.jcr.resource", "2.5.6"),
+                mavenBundle("org.apache.sling", "org.apache.sling.jcr.resource", "2.8.0"),
                 mavenBundle("org.apache.sling", "org.apache.sling.jcr.classloader", "3.2.2"),
                 mavenBundle("org.apache.sling", "org.apache.sling.jcr.contentloader", "2.1.8"),
-                mavenBundle("org.apache.sling", "org.apache.sling.engine", "2.3.6"),
+                mavenBundle("org.apache.sling", "org.apache.sling.engine", "2.6.2"),
                 mavenBundle("org.apache.sling", "org.apache.sling.serviceusermapper", "1.2.2"),
 
                 mavenBundle("org.apache.sling", "org.apache.sling.jcr.jcr-wrapper", "2.0.0"),
-                mavenBundle("org.apache.sling", "org.apache.sling.jcr.api", "2.3.0"),
-                mavenBundle("org.apache.sling", "org.apache.sling.jcr.base", "2.3.0"),
-                mavenBundle("org.apache.jackrabbit", "jackrabbit-api", "2.10.1"),
-                mavenBundle("org.apache.jackrabbit", "jackrabbit-jcr-commons", "2.10.1"),
-                mavenBundle("org.apache.jackrabbit", "jackrabbit-spi", "2.10.1"),
-                mavenBundle("org.apache.jackrabbit", "jackrabbit-spi-commons", "2.10.1"),
-                mavenBundle("org.apache.jackrabbit", "jackrabbit-jcr-rmi", "2.10.1"),
-                mavenBundle("org.apache.derby", "derby", "10.5.3.0_1"),
-                mavenBundle("org.apache.sling", "org.apache.sling.jcr.jackrabbit.server", "2.3.0"),
+                mavenBundle("org.apache.sling", "org.apache.sling.jcr.api", "2.4.0"),
+                mavenBundle("org.apache.sling", "org.apache.sling.jcr.base", "2.4.0"),
+
+                mavenBundle("com.google.guava", "guava", "15.0"),
+                mavenBundle("org.apache.jackrabbit", "jackrabbit-api", jackrabbitVersion),
+                mavenBundle("org.apache.jackrabbit", "jackrabbit-jcr-commons", jackrabbitVersion),
+                mavenBundle("org.apache.jackrabbit", "jackrabbit-spi", jackrabbitVersion),
+                mavenBundle("org.apache.jackrabbit", "jackrabbit-spi-commons", jackrabbitVersion),
+                mavenBundle("org.apache.jackrabbit", "jackrabbit-jcr-rmi", jackrabbitVersion),
+
+                mavenBundle("org.apache.felix", "org.apache.felix.jaas", "0.0.4"),
+
+                mavenBundle("org.apache.jackrabbit", "oak-core", oakVersion),
+                mavenBundle("org.apache.jackrabbit", "oak-commons", oakVersion),
+                mavenBundle("org.apache.jackrabbit", "oak-lucene", oakVersion),
+                mavenBundle("org.apache.jackrabbit", "oak-blob", oakVersion),
+                mavenBundle("org.apache.jackrabbit", "oak-jcr", oakVersion),
+
+                mavenBundle("org.apache.jackrabbit", "oak-segment", oakVersion),
+
+                mavenBundle("org.apache.sling", "org.apache.sling.jcr.oak.server", "1.1.0"),
 
                 mavenBundle("org.apache.sling", "org.apache.sling.testing.tools", "1.0.6"),
                 mavenBundle("org.apache.httpcomponents", "httpcore-osgi", "4.1.2"),
                 mavenBundle("org.apache.httpcomponents", "httpclient-osgi", "4.1.2"),
 
-                mavenBundle("org.apache.sling", "org.apache.sling.discovery.commons", "1.0.12"),
 
                 // SLING-5560: delaying start of the sling.event bundle to
                 // ensure the parameter 'startup.delay' is properly set to 1sec
@@ -212,7 +283,7 @@ public abstract class AbstractJobHandlingTest {
             // ignore
         }
     }
-    
+
     public void setup() throws IOException {
         // set load delay to 3 sec
         final org.osgi.service.cm.Configuration c2 = this.configAdmin.getConfiguration("org.apache.sling.event.impl.jobs.jcr.PersistenceHandler", null);
@@ -221,7 +292,7 @@ public abstract class AbstractJobHandlingTest {
         // and startup.delay to 1sec - otherwise default of 30sec breaks tests!
         p2.put(JobManagerConfiguration.PROPERTY_STARTUP_DELAY, 1L);
         c2.update(p2);
-        
+
         // SLING-5560 : since the above (re)config is now applied, we're safe
         // to go ahead and start the sling.event bundle.
         // this time, the JobManagerConfiguration will be activated
