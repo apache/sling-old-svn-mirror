edbd586c92a3177c232fac4c6483ad7916c41c44
Tommaso Teofili <tommaso@apache.org>
2016-10-26 11:59:09 +0000

    SLING-6189 - applied patch from Simone Tripodi for UCDTSP mbean
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk@1766673 13f79535-47bb-0310-9956-ffa450edef68

---
+++ contrib/extensions/distribution/core/src/main/java/org/apache/sling/distribution/monitor/impl/UserCredentialsDistributionTransportSecretMBeanImpl.java
@@ -0,0 +1,40 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.sling.distribution.monitor.impl;
+
+/**
+ * The {@link UserCredentialsDistributionTransportSecretMBean} implementation.
+ */
+public class UserCredentialsDistributionTransportSecretMBeanImpl
+    implements UserCredentialsDistributionTransportSecretMBean {
+
+    private static final String ADMIN = "admin";
+
+    private final String username;
+
+    public UserCredentialsDistributionTransportSecretMBeanImpl(String username) {
+        this.username = username;
+    }
+
+    @Override
+    public boolean useAdministrativeTransport() {
+        return ADMIN.equals(username);
+    }
+
+}
