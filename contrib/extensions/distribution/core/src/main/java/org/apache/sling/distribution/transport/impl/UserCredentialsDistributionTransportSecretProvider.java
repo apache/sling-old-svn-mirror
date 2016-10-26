ac6847b250dc3c2576cc73c146b86168478bfafa
Tommaso Teofili <tommaso@apache.org>
2016-10-26 11:58:43 +0000

    SLING-6189 - applied patch from Simone Tripodi for UCDTSP mbean
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk@1766672 13f79535-47bb-0310-9956-ffa450edef68

--- contrib/extensions/distribution/core/src/main/java/org/apache/sling/distribution/transport/impl/UserCredentialsDistributionTransportSecretProvider.java
+++ contrib/extensions/distribution/core/src/main/java/org/apache/sling/distribution/transport/impl/UserCredentialsDistributionTransportSecretProvider.java
@@ -20,18 +20,27 @@ package org.apache.sling.distribution.transport.impl;
 
 import java.net.URI;
 import java.util.Collections;
+import java.util.Dictionary;
 import java.util.HashMap;
+import java.util.Hashtable;
 import java.util.Map;
+
+import javax.management.ObjectName;
 
 import org.apache.felix.scr.annotations.Activate;
 import org.apache.felix.scr.annotations.Component;
 import org.apache.felix.scr.annotations.ConfigurationPolicy;
+import org.apache.felix.scr.annotations.Deactivate;
 import org.apache.felix.scr.annotations.Property;
 import org.apache.felix.scr.annotations.Service;
 import org.apache.sling.commons.osgi.PropertiesUtil;
 import org.apache.sling.distribution.component.impl.DistributionComponentConstants;
+import org.apache.sling.distribution.monitor.impl.UserCredentialsDistributionTransportSecretMBean;
+import org.apache.sling.distribution.monitor.impl.UserCredentialsDistributionTransportSecretMBeanImpl;
 import org.apache.sling.distribution.transport.DistributionTransportSecret;
 import org.apache.sling.distribution.transport.DistributionTransportSecretProvider;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
 
 @Component(metatype = true,
         label = "Apache Sling Distribution Transport Credentials - User Credentials based DistributionTransportSecretProvider",
@@ -58,10 +67,30 @@ public class UserCredentialsDistributionTransportSecretProvider implements
     private String username;
     private String password;
 
+    private ServiceRegistration mbeanServiceRegistration;
+
     @Activate
-    protected void activate(Map<String, Object> config) {
+    protected void activate(BundleContext context, Map<String, Object> config) {
         username = PropertiesUtil.toString(config.get(USERNAME), "").trim();
         password = PropertiesUtil.toString(config.get(PASSWORD), "").trim();
+
+        String id = String.valueOf(username.hashCode());
+
+        Dictionary<String, String> mbeanProps = new Hashtable<String, String>();
+        mbeanProps.put("jmx.objectname", "org.apache.sling.distribution:type=transport,id=" + ObjectName.quote(id));
+
+        UserCredentialsDistributionTransportSecretMBean mbean =
+                        new UserCredentialsDistributionTransportSecretMBeanImpl(username);
+        mbeanServiceRegistration =
+                        context.registerService(UserCredentialsDistributionTransportSecretMBean.class.getName(), mbean, mbeanProps);
+    }
+
+    @Deactivate
+    protected void deactivate() {
+        if (mbeanServiceRegistration != null) {
+            mbeanServiceRegistration.unregister();
+        }
+        mbeanServiceRegistration = null;
     }
 
     public DistributionTransportSecret getSecret(URI uri) {
