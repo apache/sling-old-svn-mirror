14cda428f22b24b215f7b1e12190e86005c35011
Carsten Ziegeler <cziegeler@apache.org>
2016-10-31 17:16:30 +0000

    Move to latest parent pom and use R6 OSGi annotations
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk@1767335 13f79535-47bb-0310-9956-ffa450edef68

--- bundles/jcr/resource/src/main/java/org/apache/sling/jcr/resource/internal/helper/jcr/PathMapper.java
+++ bundles/jcr/resource/src/main/java/org/apache/sling/jcr/resource/internal/helper/jcr/PathMapper.java
@@ -21,14 +21,13 @@ package org.apache.sling.jcr.resource.internal.helper.jcr;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
-import org.apache.felix.scr.annotations.Activate;
-import org.apache.felix.scr.annotations.Component;
-import org.apache.felix.scr.annotations.Property;
-import org.apache.felix.scr.annotations.PropertyUnbounded;
-import org.apache.felix.scr.annotations.Service;
-import org.apache.sling.commons.osgi.PropertiesUtil;
+import org.osgi.framework.Constants;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.metatype.annotations.AttributeDefinition;
+import org.osgi.service.metatype.annotations.Designate;
+import org.osgi.service.metatype.annotations.ObjectClassDefinition;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -39,24 +38,29 @@ import org.slf4j.LoggerFactory;
  * @deprecated
  */
 @Deprecated
-@Service(value = PathMapper.class)
-@Component(metatype = true,
-        label = "Apache Sling JCR Resource Provider Path Mapper",
-        description = "This service provides path mappings for JCR nodes.")
-
+@Designate(ocd = PathMapper.Config.class)
+@Component(service = PathMapper.class,
+           property = {
+                   Constants.SERVICE_VENDOR + "=The Apache Software Foundation"
+           })
 public class PathMapper {
+
+    @ObjectClassDefinition(name = "Apache Sling JCR Resource Provider Path Mapper",
+        description = "This service provides path mappings for JCR nodes.")
+    public @interface Config {
+
+        @AttributeDefinition(name = "Path Mapping",
+                description = "Defines an obtional path mapping for a path." +
+                        "Each mapping entry is expressed as follow: <JCRPath>:<resourcePath>. As an example: /foo:/libs, " +
+                        "this maps the JCR node /foo to the resource /libs. If the resource path is specified as '.', " +
+                        " the JCR tree is not visible in the resource tree. This should not be considered a security feature " +
+                        " as the nodes are still accessible through the JCR api. Mapping a JCR path to the root is not allowed. " +
+                        "The mappings are evaluated as ordered in the configuration.")
+        String[] path_mapping();
+    }
 
     /** Logger */
     private final Logger log = LoggerFactory.getLogger(getClass());
-
-    @Property(unbounded = PropertyUnbounded.ARRAY,
-            label = "Path mapping", description = "Defines an obtional path mapping for a path." +
-            "Each mapping entry is expressed as follow: <JCRPath>:<resourcePath>. As an example: /foo:/libs, " +
-            "this maps the JCR node /foo to the resource /libs. If the resource path is specified as '.', " +
-            " the JCR tree is not visible in the resource tree. This should not be considered a security feature " +
-            " as the nodes are still accessible through the JCR api. Mapping a JCR path to the root is not allowed. " +
-            "The mappings are evaluated as ordered in the configuration.")
-    private static final String PATH_MAPPING = "path.mapping";
 
     /** The mappings. */
     private final List<Mapping> mappings = new ArrayList<Mapping>();
@@ -81,9 +85,9 @@ public class PathMapper {
     }
 
     @Activate
-    private void activate(final Map<String, Object> props) {
+    private void activate(final Config configObj) {
         mappings.clear();
-        final String[] config = PropertiesUtil.toStringArray(props.get(PATH_MAPPING), null);
+        final String[] config = configObj.path_mapping();
         if ( config != null ) {
             log.warn("The Apache Sling JCR Path Mapper is deprecated.");
             for (final String mapping : config) {
