78083ef44e274b327defaf847c7ae5a294fe5560
Carsten Ziegeler <cziegeler@apache.org>
2016-10-31 09:26:50 +0000

    SLING-5779 : Packaged OSGi config intermittently do not get installed before bundle start
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk@1767230 13f79535-47bb-0310-9956-ffa450edef68

--- installer/factories/configuration/src/main/java/org/apache/sling/installer/factories/configuration/impl/ConfigInstallTask.java
+++ installer/factories/configuration/src/main/java/org/apache/sling/installer/factories/configuration/impl/ConfigInstallTask.java
@@ -77,10 +77,10 @@ public class ConfigInstallTask extends AbstractConfigTask {
                     if ( this.factoryPid != null ) {
                         this.aliasPid = config.getPid();
                     }
-                    this.setFinishedState(ResourceState.INSTALLED, this.getCompositeAliasPid());
                     this.getLogger().debug("Configuration " + config.getPid()
                                 + " " + (created ? "created" : "updated")
                                 + " from " + getResource());
+                    this.setFinishedState(ResourceState.INSTALLED, this.getCompositeAliasPid());
                     final Operation op = new Coordinator.Operation(config.getPid(), config.getFactoryPid(), false);
                     Coordinator.SHARED.add(op);
                 } else {
