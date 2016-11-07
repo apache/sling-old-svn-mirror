c435b3d156398636c4162f75da647eb15142b8c2
Bertrand Delacretaz <bdelacretaz@apache.org>
2016-11-07 13:41:00 +0000

    SLING-6219 - reponit grammar support for create/delete user
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk@1768509 13f79535-47bb-0310-9956-ffa450edef68

--- bundles/extensions/repoinit/parser/src/test/java/org/apache/sling/repoinit/parser/test/OperationToStringVisitor.java
+++ bundles/extensions/repoinit/parser/src/test/java/org/apache/sling/repoinit/parser/test/OperationToStringVisitor.java
@@ -23,7 +23,9 @@ import java.util.Collection;
 import org.apache.sling.repoinit.parser.operations.AclLine;
 import org.apache.sling.repoinit.parser.operations.CreatePath;
 import org.apache.sling.repoinit.parser.operations.CreateServiceUser;
+import org.apache.sling.repoinit.parser.operations.CreateUser;
 import org.apache.sling.repoinit.parser.operations.DeleteServiceUser;
+import org.apache.sling.repoinit.parser.operations.DeleteUser;
 import org.apache.sling.repoinit.parser.operations.RegisterNodetypes;
 import org.apache.sling.repoinit.parser.operations.OperationVisitor;
 import org.apache.sling.repoinit.parser.operations.RegisterNamespace;
@@ -49,6 +51,16 @@ class OperationToStringVisitor implements OperationVisitor {
     @Override
     public void visitDeleteServiceUser(DeleteServiceUser s) {
         out.println(s.toString());
+    }
+
+    @Override
+    public void visitCreateUser(CreateUser u) {
+        out.println(u.toString());
+    }
+
+    @Override
+    public void visitDeleteUser(DeleteUser u) {
+        out.println(u.toString());
     }
 
     @Override
