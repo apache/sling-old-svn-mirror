aaf0373ac0186bdd897fbdbc3910ce1587aea975
Carsten Ziegeler <cziegeler@apache.org>
2016-11-08 16:06:00 +0000

    Update to parent pom 29
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk@1768718 13f79535-47bb-0310-9956-ffa450edef68

--- bundles/extensions/repoinit/parser/src/main/java/org/apache/sling/repoinit/parser/operations/package-info.java
+++ bundles/extensions/repoinit/parser/src/main/java/org/apache/sling/repoinit/parser/operations/package-info.java
@@ -15,7 +15,6 @@
  * limitations under the License.
  ******************************************************************************/
 
-@Version("3.0.0")
+@org.osgi.annotation.versioning.Version("3.0.0")
 package org.apache.sling.repoinit.parser.operations;
 
-import aQute.bnd.annotation.Version;
