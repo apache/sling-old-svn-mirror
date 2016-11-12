5f8e80b2ce762d371cc83e0d2e9549387234ec1f
Carsten Ziegeler <cziegeler@apache.org>
2016-11-12 15:11:04 +0000

    SLING-6278 : Provide tooling to create an archive with the provisioning model and all artifacts
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk/tooling/maven/slingstart-maven-plugin@1769385 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/sling/maven/slingstart/BuildConstants.java
+++ src/main/java/org/apache/sling/maven/slingstart/BuildConstants.java
@@ -20,6 +20,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.sling.provisioning.model.ModelConstants;
+import org.apache.sling.provisioning.model.io.ModelArchiveWriter;
 
 public abstract class BuildConstants {
 
@@ -30,6 +31,9 @@ public abstract class BuildConstants {
 
     // Model artifact name
     public static final String MODEL_ARTIFACT_NAME = "slingstart.txt";
+
+    // Model archive name
+    public static final String MODEL_ARCHIVE_NAME = "slingstart." + ModelArchiveWriter.DEFAULT_EXTENSION;
 
     // Types
 
@@ -54,6 +58,8 @@ public abstract class BuildConstants {
     public static final String CLASSIFIER_APP = "app";
 
     public static final String CLASSIFIER_WEBAPP = "webapp";
+
+    public static final String CLASSIFIER_MAR = ModelArchiveWriter.DEFAULT_EXTENSION;
 
     // Manifest attributes
 
