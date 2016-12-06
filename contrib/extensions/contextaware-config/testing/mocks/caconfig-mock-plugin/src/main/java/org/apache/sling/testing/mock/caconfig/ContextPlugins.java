791419891aeb86e1f2cdef4a81a508ce0e2d41d5
Stefan Seifert <sseifert@apache.org>
2016-12-06 11:33:47 +0000

    SLING-6360 support both impl 1.1 und 1.2
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk@1772869 13f79535-47bb-0310-9956-ffa450edef68

--- contrib/extensions/contextaware-config/testing/mocks/caconfig-mock-plugin/src/main/java/org/apache/sling/testing/mock/caconfig/ContextPlugins.java
+++ contrib/extensions/contextaware-config/testing/mocks/caconfig-mock-plugin/src/main/java/org/apache/sling/testing/mock/caconfig/ContextPlugins.java
@@ -19,16 +19,12 @@
 package org.apache.sling.testing.mock.caconfig;
 
 import org.apache.sling.caconfig.impl.ConfigurationBuilderAdapterFactory;
-import org.apache.sling.caconfig.impl.ConfigurationInheritanceStrategyMultiplexer;
 import org.apache.sling.caconfig.impl.ConfigurationResolverImpl;
-import org.apache.sling.caconfig.impl.def.DefaultConfigurationInheritanceStrategy;
 import org.apache.sling.caconfig.impl.def.DefaultConfigurationPersistenceStrategy;
 import org.apache.sling.caconfig.impl.metadata.AnnotationClassConfigurationMetadataProvider;
 import org.apache.sling.caconfig.impl.metadata.ConfigurationMetadataProviderMultiplexer;
-import org.apache.sling.caconfig.impl.override.ConfigurationOverrideManager;
 import org.apache.sling.caconfig.management.impl.ConfigurationManagerImpl;
 import org.apache.sling.caconfig.management.impl.ConfigurationPersistenceStrategyMultiplexer;
-import org.apache.sling.caconfig.management.impl.ContextPathStrategyMultiplexerImpl;
 import org.apache.sling.caconfig.resource.impl.ConfigurationResourceResolverImpl;
 import org.apache.sling.caconfig.resource.impl.ConfigurationResourceResolvingStrategyMultiplexer;
 import org.apache.sling.caconfig.resource.impl.def.DefaultConfigurationResourceResolvingStrategy;
@@ -40,88 +36,118 @@ import org.osgi.annotation.versioning.ProviderType;
 
 /**
  * Mock context plugins.
+ * The plugin supports both 1.0/1.1 and 1.2+ version of the Impl/SPI.
  */
 @ProviderType
 public final class ContextPlugins {
 
-  private ContextPlugins() {
-    // constants only
-  }
+    private ContextPlugins() {
+        // constants only
+    }
 
-  /**
-   * Context plugin for Sling Context-Aware Configuration.
-   */
-  public static final ContextPlugin<? extends SlingContextImpl> CACONFIG = new AbstractContextPlugin<SlingContextImpl>() {
-    @Override
-    public void afterSetUp(SlingContextImpl context) throws Exception {
-        registerConfigurationResourceResolver(context);
-        registerConfigurationResolver(context);
-        registerConfigurationManagement(context);
-        registerConfigurationResourceResolverDefaultImpl(context);
-        registerConfigurationResolverDefaultImpl(context);
+    /**
+     * Context plugin for Sling Context-Aware Configuration.
+     */
+    public static final ContextPlugin<? extends SlingContextImpl> CACONFIG = new AbstractContextPlugin<SlingContextImpl>() {
+        @Override
+        public void afterSetUp(SlingContextImpl context) throws Exception {
+            registerConfigurationResourceResolver(context);
+            registerConfigurationResolver(context);
+            registerConfigurationManagement(context);
+            registerConfigurationResourceResolverDefaultImpl(context);
+            registerConfigurationResolverDefaultImpl(context);
+
+            // Scan MANIFEST.MF in the classpath and automatically register all Configuration annotation classes found.
+            ConfigurationMetadataUtil.addAnnotationClassesForManifestEntries(context.bundleContext());
+        }
+    };
+
+    /**
+     * Context plugin for Sling Context-Aware Configuration (without the default implementations).
+     */
+    public static final ContextPlugin<? extends SlingContextImpl> CACONFIG_NODEF = new AbstractContextPlugin<SlingContextImpl>() {
+        @Override
+        public void afterSetUp(SlingContextImpl context) throws Exception {
+            registerConfigurationResourceResolver(context);
+            registerConfigurationResolver(context);
+            registerConfigurationManagement(context);
+        }
+    };
+
+    /**
+     * Register all services for ConfigurationResourceResolver (without the default implementations).
+     * @param context Sling context
+     */
+    private static void registerConfigurationResourceResolver(SlingContextImpl context) {
         
-        // Scan MANIFEST.MF in the classpath and automatically register all Configuration annotation classes found.
-        ConfigurationMetadataUtil.addAnnotationClassesForManifestEntries(context.bundleContext());
+        if (!registerByClassName(context, "org.apache.sling.caconfig.management.impl.ContextPathStrategyMultiplexerImpl")) {
+            // fallback to impl 1.1
+            registerByClassName(context, "org.apache.sling.caconfig.resource.impl.ContextPathStrategyMultiplexer");
+        }
+        
+        context.registerInjectActivateService(new ConfigurationResourceResolvingStrategyMultiplexer());
+        context.registerInjectActivateService(new ConfigurationResourceResolverImpl());
     }
-  };
 
-  /**
-   * Context plugin for Sling Context-Aware Configuration (without the default implentations).
-   */
-  public static final ContextPlugin<? extends SlingContextImpl> CACONFIG_NODEF = new AbstractContextPlugin<SlingContextImpl>() {
-    @Override
-    public void afterSetUp(SlingContextImpl context) throws Exception {
-        registerConfigurationResourceResolver(context);
-        registerConfigurationResolver(context);
-        registerConfigurationManagement(context);
+    /**
+     * Register default implementations for for ConfigurationResourceResolver.
+     * @param context Sling context
+     */
+    private static void registerConfigurationResourceResolverDefaultImpl(SlingContextImpl context) {
+        context.registerInjectActivateService(new DefaultContextPathStrategy());
+        context.registerInjectActivateService(new DefaultConfigurationResourceResolvingStrategy());
     }
-  };
 
-  /**
-   * Register all services for ConfigurationResourceResolver (without the default implementations).
-   * @param context Sling context
-   */
-  private static void registerConfigurationResourceResolver(SlingContextImpl context) {
-      context.registerInjectActivateService(new ContextPathStrategyMultiplexerImpl());
-      context.registerInjectActivateService(new ConfigurationResourceResolvingStrategyMultiplexer());
-      context.registerInjectActivateService(new ConfigurationResourceResolverImpl());
-  }
+    /**
+     * Register all services for ConfigurationResolver (without the default implementations).
+     * @param context Sling context
+     */
+    private static void registerConfigurationResolver(SlingContextImpl context) {
+        context.registerInjectActivateService(new ConfigurationPersistenceStrategyMultiplexer());
+        
+        // only required for impl 1.2+
+        registerByClassName(context, "org.apache.sling.caconfig.impl.ConfigurationInheritanceStrategyMultiplexer");
+        
+        // only required for impl 1.2+
+        registerByClassName(context, "org.apache.sling.caconfig.impl.override.ConfigurationOverrideManager");
 
-  /**
-   * Register default implementations for for ConfigurationResourceResolver.
-   * @param context Sling context
-   */
-  private static void registerConfigurationResourceResolverDefaultImpl(SlingContextImpl context) {
-      context.registerInjectActivateService(new DefaultContextPathStrategy());
-      context.registerInjectActivateService(new DefaultConfigurationResourceResolvingStrategy());
-  }
-  
-  
-  /**
-   * Register all services for ConfigurationResolver (without the default implementations).
-   * @param context Sling context
-   */
-  private static void registerConfigurationResolver(SlingContextImpl context) {
-      context.registerInjectActivateService(new ConfigurationPersistenceStrategyMultiplexer());
-      context.registerInjectActivateService(new ConfigurationInheritanceStrategyMultiplexer());
-      context.registerInjectActivateService(new ConfigurationOverrideManager());
-      context.registerInjectActivateService(new ConfigurationResolverImpl());
-      context.registerInjectActivateService(new ConfigurationBuilderAdapterFactory());
-  }
+        context.registerInjectActivateService(new ConfigurationResolverImpl());
+        context.registerInjectActivateService(new ConfigurationBuilderAdapterFactory());
+    }
 
-  /**
-   * Register default implementations for for ConfigurationResolver.
-   * @param context Sling context
-   */
-  private static void registerConfigurationResolverDefaultImpl(SlingContextImpl context) {
-      context.registerInjectActivateService(new DefaultConfigurationPersistenceStrategy());
-      context.registerInjectActivateService(new DefaultConfigurationInheritanceStrategy());
-  }
-  
-  private static void registerConfigurationManagement(SlingContextImpl context) {
-      context.registerInjectActivateService(new ConfigurationMetadataProviderMultiplexer());
-      context.registerInjectActivateService(new ConfigurationManagerImpl());
-      context.registerInjectActivateService(new AnnotationClassConfigurationMetadataProvider());
-  }
-  
+    /**
+     * Register default implementations for for ConfigurationResolver.
+     * @param context Sling context
+     */
+    private static void registerConfigurationResolverDefaultImpl(SlingContextImpl context) {
+        context.registerInjectActivateService(new DefaultConfigurationPersistenceStrategy());
+
+        // only required for impl 1.2+
+        registerByClassName(context,"org.apache.sling.caconfig.impl.def.DefaultConfigurationInheritanceStrategy");
+    }
+
+    private static void registerConfigurationManagement(SlingContextImpl context) {
+        context.registerInjectActivateService(new ConfigurationMetadataProviderMultiplexer());
+        context.registerInjectActivateService(new ConfigurationManagerImpl());
+        context.registerInjectActivateService(new AnnotationClassConfigurationMetadataProvider());
+    }
+
+    private static boolean registerByClassName(SlingContextImpl context, String className) {
+        try {
+            Class<?> clazz = Class.forName(className);
+            context.registerInjectActivateService(clazz.newInstance());
+            return true;
+        }
+        catch (ClassNotFoundException ex) {
+            return false;
+        }
+        catch (InstantiationException ex) {
+            throw new RuntimeException(ex);
+        }
+        catch (IllegalAccessException ex) {
+            throw new RuntimeException(ex);
+        }
+
+    }
+
 }
