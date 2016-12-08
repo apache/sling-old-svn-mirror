35127dbfab50510280e2ca98c766a866bf61d1f4
Julian Sedding <jsedding@apache.org>
2016-12-08 18:10:36 +0000

    exclude ubuntu-4 slave until INFRA-13063 is fixed
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk@1773276 13f79535-47bb-0310-9956-ffa450edef68

--- tooling/jenkins/create_jobs.groovy
+++ tooling/jenkins/create_jobs.groovy
@@ -824,7 +824,7 @@ def modules = [
 // deployed for maximum compatibility
 def defaultJdks = ["1.7", "1.8"]
 def defaultMvn = "Maven 3.3.9"
-def defaultSlave = "ubuntu"
+def defaultSlave = "ubuntu&&!ubuntu4" // exclude ubuntu-4 until INFRA-13063 is fixed
 
 def jdkMapping = [
     "1.7": "JDK 1.7 (latest)",
