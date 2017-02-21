abdf616380828bd29ecf1fa3c4c9ca1b03fe5352
Konrad Windszus <kwin@apache.org>
2017-02-21 11:46:45 +0000

    migrate to OSGi version annotations
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk@1783870 13f79535-47bb-0310-9956-ffa450edef68

--- bundles/extensions/validation/api/src/main/java/org/apache/sling/validation/ValidationFailure.java
+++ bundles/extensions/validation/api/src/main/java/org/apache/sling/validation/ValidationFailure.java
@@ -22,7 +22,7 @@ import java.util.ResourceBundle;
 
 import javax.annotation.Nonnull;
 
-import aQute.bnd.annotation.ProviderType;
+import org.osgi.annotation.versioning.ProviderType;
 
 @ProviderType
 public interface ValidationFailure {
