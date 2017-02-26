c860e3d079ab37d7d1a9cab5eb5c2bc390bf9d7c
Oliver Lietz <olli@apache.org>
2017-02-26 18:52:27 +0000

    use OSGi R6 annotations and add resource presence (wip)
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk@1784469 13f79535-47bb-0310-9956-ffa450edef68

--- bundles/extensions/validation/test-services/src/main/java/org/apache/sling/validation/testservices/internal/ValidationPostResponseCreator.java
+++ bundles/extensions/validation/test-services/src/main/java/org/apache/sling/validation/testservices/internal/ValidationPostResponseCreator.java
@@ -16,26 +16,33 @@
  * specific language governing permissions and limitations
  * under the License.
  */
- package org.apache.sling.validation.testservices;
+ package org.apache.sling.validation.testservices.internal;
 
 import java.util.Locale;
+import java.util.ResourceBundle;
 
-import org.apache.felix.scr.annotations.Component;
-import org.apache.felix.scr.annotations.Service;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.servlets.post.PostResponse;
 import org.apache.sling.servlets.post.PostResponseCreator;
 import org.apache.sling.servlets.post.SlingPostConstants;
+import org.osgi.service.component.annotations.Component;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-@Component()
-@Service(PostResponseCreator.class)
+@Component(
+    service = PostResponseCreator.class
+)
 public class ValidationPostResponseCreator implements PostResponseCreator {
+
+    private final Logger logger = LoggerFactory.getLogger(ValidationPostResponseCreator.class);
 
     @Override
     public PostResponse createPostResponse(SlingHttpServletRequest request) {
         String operation = request.getParameter(SlingPostConstants.RP_OPERATION);
         if (operation != null && "validation".equals(operation)) {
-            return new ValidationPostResponse(request.getResourceBundle(Locale.US));
+            final ResourceBundle resourceBundle = request.getResourceBundle(Locale.US);
+            logger.debug("resource bundle: {}", resourceBundle);
+            return new ValidationPostResponse(resourceBundle);
         }
         return null;
     }
