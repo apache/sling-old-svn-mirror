081596f751b5ef0474978e02d2c7d42bc3f68eeb
Stefan Seifert <sseifert@apache.org>
2017-03-02 21:37:31 +0000

    SLING-6592 use DocViewProperty from vault-core to parse jcr xml property values fix artifactid
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk/bundles/commons/fscontentparser@1785194 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/sling/fscontentparser/impl/JcrXmlValueConverterTest.java
+++ src/test/java/org/apache/sling/fscontentparser/impl/JcrXmlValueConverterTest.java
@@ -23,75 +23,79 @@ import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 
+import java.net.URI;
 import java.util.Calendar;
+import java.util.UUID;
 
 import org.junit.Test;
 
 public class JcrXmlValueConverterTest {
+    
+    private static final String NAME = "prop1";
 
     @Test
     public void testNull() {
-        assertNull(parseValue(null));
+        assertNull(parseValue(NAME, null));
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void testInvalid() {
-        parseValue("{InvalidType}xyz");
+        parseValue(NAME, "{InvalidType}xyz");
     }
 
     @Test
     public void testString() {
-        assertEquals("myString", parseValue("myString"));
-        assertEquals("prop", "myString [ ] { } \\ ,", parseValue("myString [ ] { } \\\\ ,"));
-        assertEquals("{myString}", parseValue("\\{myString}"));
-        assertEquals("aaa{myString}", parseValue("aaa{myString}"));
-        assertEquals("[myString]", parseValue("\\[myString]"));
-        assertEquals("aaa[myString]", parseValue("aaa[myString]"));
+        assertEquals("myString", parseValue(NAME, "myString"));
+        assertEquals("prop", "myString [ ] { } \\ ,", parseValue(NAME, "myString [ ] { } \\\\ ,"));
+        assertEquals("{myString}", parseValue(NAME, "\\{myString}"));
+        assertEquals("aaa{myString}", parseValue(NAME, "aaa{myString}"));
+        assertEquals("[myString]", parseValue(NAME, "\\[myString]"));
+        assertEquals("aaa[myString]", parseValue(NAME, "aaa[myString]"));
     }
 
     @Test
     public void testStringArray() {
-        assertArrayEquals(new Object[] { "myString1", "myString2" }, (Object[]) parseValue("[myString1,myString2]"));
+        assertArrayEquals(new Object[] { "myString1", "myString2" }, (Object[]) parseValue(NAME, "[myString1,myString2]"));
         assertArrayEquals(new Object[] { "myString1,[]\\äöüß€", "myString2", "myString3 [ ] { } \\ ,", "", "[myString5]", "{myString6}" },
-                (Object[]) parseValue("[myString1\\,[]\\\\äöüß€,myString2,myString3 [ ] { } \\\\ \\,,,[myString5],{myString6}]"));
+                (Object[]) parseValue(NAME, "[myString1\\,[]\\\\äöüß€,myString2,myString3 [ ] { } \\\\ \\,,,[myString5],{myString6}]"));
     }
 
     @Test
     public void testBoolean() {
-        assertEquals(true, parseValue("{Boolean}true"));
-        assertEquals(false, parseValue("{Boolean}false"));
+        assertEquals(true, parseValue(NAME, "{Boolean}true"));
+        assertEquals(false, parseValue(NAME, "{Boolean}false"));
     }
 
     @Test
     public void testBooleanArray() {
-        assertArrayEquals(new Object[] { true, false }, (Object[]) parseValue("{Boolean}[true,false]"));
+        assertArrayEquals(new Object[] { true, false }, (Object[]) parseValue(NAME, "{Boolean}[true,false]"));
     }
 
     @Test
     public void testLong() {
-        assertEquals(1L, parseValue("{Long}1"));
-        assertEquals(10000000000L, parseValue("{Long}10000000000"));
+        assertEquals(1L, parseValue(NAME, "{Long}1"));
+        assertEquals(10000000000L, parseValue(NAME, "{Long}10000000000"));
     }
 
     @Test
     public void testLongArray() {
-        assertArrayEquals(new Object[] { 1L, 2L }, (Object[]) parseValue("{Long}[1,2]"));
-        assertArrayEquals(new Object[] { 10000000000L, 20000000000L }, (Object[]) parseValue("{Long}[10000000000,20000000000]"));
+        assertArrayEquals(new Object[] { 1L, 2L }, (Object[]) parseValue(NAME, "{Long}[1,2]"));
+        assertArrayEquals(new Object[] { 10000000000L, 20000000000L }, (Object[]) parseValue(NAME, "{Long}[10000000000,20000000000]"));
     }
 
     @Test
     public void testDouble() {
-        assertEquals(1.234d, parseValue("{Decimal}1.234"));
+        assertEquals(1.234d, parseValue(NAME, "{Decimal}1.234"));
     }
 
     @Test
     public void testDoubleArray() {
-        assertArrayEquals(new Object[] { 1.234d, 2.345d }, (Object[]) parseValue("{Decimal}[1.234,2.345]"));
+        assertArrayEquals(new Object[] { 1.234d, 2.345d }, (Object[]) parseValue(NAME, "{Decimal}[1.234,2.345]"));
     }
 
     @Test
     public void testCalendar() {
-        Calendar value = (Calendar)parseValue("{Date}2010-09-05T15:10:20.000Z");
+        Calendar value = (Calendar)parseValue(NAME, "{Date}2010-09-05T15:10:20.000Z");
         assertEquals(2010, value.get(Calendar.YEAR));
         assertEquals(8, value.get(Calendar.MONTH));
         assertEquals(5, value.get(Calendar.DAY_OF_MONTH));
@@ -99,7 +103,20 @@ public class JcrXmlValueConverterTest {
 
     @Test
     public void testStringArrayRepPrivileges() {
-        assertArrayEquals(new Object[] { "rep:write", "crx:replicate", "jcr:read" }, (Object[]) parseValue("{Name}[rep:write,crx:replicate,jcr:read]"));
+        assertArrayEquals(new Object[] { "rep:write", "crx:replicate", "jcr:read" }, (Object[]) parseValue(NAME, "{Name}[rep:write,crx:replicate,jcr:read]"));
+    }
+
+    @Test
+    public void testReference() {
+        UUID uuid = UUID.randomUUID();
+        UUID value = (UUID)parseValue(NAME, "{Reference}" + uuid.toString());
+        assertEquals(uuid, value);
+    }
+
+    @Test
+    public void testURI() {
+        URI value = (URI)parseValue(NAME, "{URI}http://www.jodelkaiser.de/");
+        assertEquals("http://www.jodelkaiser.de/", value.toString());
     }
 
 }
