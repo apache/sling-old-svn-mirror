62f1b363cde6a528eb4a97f00e7dd41cebf41a4a
Radu Cotescu <radu@apache.org>
2017-03-31 15:10:37 +0000

    SLING-6639 - Avoid split packages in the HTL Engine and HTL Java Compiler bundles
    * increased exported package version to prevent incorrect OSGi wiring
    
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk@1789701 13f79535-47bb-0310-9956-ffa450edef68

--- bundles/scripting/sightly/java-compiler/src/main/java/org/apache/sling/scripting/sightly/package-info.java
+++ bundles/scripting/sightly/java-compiler/src/main/java/org/apache/sling/scripting/sightly/package-info.java
@@ -15,7 +15,7 @@
  * limitations under the License.
  ******************************************************************************/
 
-@Version("1.1.0")
+@Version("2.0.0")
 package org.apache.sling.scripting.sightly;
 
 import org.osgi.annotation.versioning.Version;
