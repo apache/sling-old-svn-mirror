24fe854313d94ddd2287d303f0a4e6a7759ee78c
Justin Edelson <justin@apache.org>
2017-05-03 14:30:28 +0000

    SLING-6806 - move tags in HTML output to part of the first column
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk@1793654 13f79535-47bb-0310-9956-ffa450edef68

--- bundles/extensions/healthcheck/core/src/main/java/org/apache/sling/hc/core/impl/servlet/ResultHtmlSerializer.java
+++ bundles/extensions/healthcheck/core/src/main/java/org/apache/sling/hc/core/impl/servlet/ResultHtmlSerializer.java
@@ -81,14 +81,22 @@ public class ResultHtmlSerializer {
         final DateFormat dfLong = new SimpleDateFormat("yyyy-MM-dd HH:mm");
 
         writer.println("<table id=\"healthCheckResults\" cellspacing=\"0\">");
-        writer.println("<thead><tr><th>Health Check</th><th>Tags</th><th>Status</th><th>Log</th><th>Finished At</th><th>Execution Time</th></tr></thead>");
+        writer.println("<thead><tr><th>Health Check</th><th>Status</th><th>Log</th><th>Finished At</th><th>Execution Time</th></tr></thead>");
         for (HealthCheckExecutionResult executionResult : executionResults) {
             Result result = executionResult.getHealthCheckResult();
-            writer.println("<tr class=\"" + getClassForStatus(result.getStatus()) + "\" "
-                    + "title=\"Tags: " + StringEscapeUtils.escapeHtml(StringUtils.join(executionResult.getHealthCheckMetadata().getTags(), ",")) + "\">");
-            writer.println("<td><span title=\"" + StringEscapeUtils.escapeHtml(executionResult.getHealthCheckMetadata().getName()) + "\">"
-                    + StringEscapeUtils.escapeHtml(executionResult.getHealthCheckMetadata().getTitle()) + "</span></td>");
-            writer.println("<td style='font-weight:bold;'>" + StringEscapeUtils.escapeHtml(StringUtils.join(executionResult.getHealthCheckMetadata().getTags(), ", ")) + "</td>");
+            List<String> tags = executionResult.getHealthCheckMetadata().getTags();
+            boolean hasTags = tags != null && tags.size() > 0 && StringUtils.isNotBlank(tags.get(0));
+            writer.print("<tr class=\"" + getClassForStatus(result.getStatus()) + "\" ");
+            if (hasTags) {
+                writer.print("title=\"Tags: " + StringEscapeUtils.escapeHtml(StringUtils.join(tags, ",")) + "\"");
+            }
+            writer.println(">");
+            writer.print("<td><p title=\"" + StringEscapeUtils.escapeHtml(executionResult.getHealthCheckMetadata().getName()) + "\">"
+                    + StringEscapeUtils.escapeHtml(executionResult.getHealthCheckMetadata().getTitle()) + "</p>");
+            if (hasTags) {
+                writer.println("<span>Tags: " + StringEscapeUtils.escapeHtml(StringUtils.join(tags, ", ")) + "</span>");
+            }
+            writer.println("</td>");
             writer.println("<td style='font-weight:bold;'>" + StringEscapeUtils.escapeHtml(result.getStatus().toString()) + "</td>");
             writer.println("<td>");
             boolean isFirst = true;
