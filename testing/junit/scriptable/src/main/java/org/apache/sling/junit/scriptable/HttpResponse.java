905eab3d9f270edfd02eb6312ebd3fb78069f5b2
Carsten Ziegeler <cziegeler@apache.org>
2017-05-09 16:10:00 +0000

    Update to parent pom 30 and R6 OSGi annotations
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk@1794605 13f79535-47bb-0310-9956-ffa450edef68

--- testing/junit/scriptable/src/main/java/org/apache/sling/junit/scriptable/HttpResponse.java
+++ testing/junit/scriptable/src/main/java/org/apache/sling/junit/scriptable/HttpResponse.java
@@ -20,6 +20,7 @@ import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
+import java.util.Collection;
 import java.util.Locale;
 
 import javax.servlet.ServletOutputStream;
@@ -35,125 +36,157 @@ public class HttpResponse implements HttpServletResponse {
     private String contentType;
     private final TestServletOutputStream outputStream;
     private final PrintWriter writer;
-    
+
     HttpResponse() throws UnsupportedEncodingException {
         outputStream = new TestServletOutputStream();
         writer = new PrintWriter(new OutputStreamWriter(outputStream, encoding));
     }
-    
+
+    @Override
     public void addCookie(Cookie cookie) {
     }
 
+    @Override
     public void addDateHeader(String name, long date) {
     }
 
+    @Override
     public void addHeader(String name, String value) {
     }
 
+    @Override
     public void addIntHeader(String name, int value) {
     }
 
+    @Override
     public boolean containsHeader(String name) {
         return false;
     }
 
+    @Override
     public String encodeRedirectUrl(String url) {
         return null;
     }
 
+    @Override
     public String encodeRedirectURL(String url) {
         return null;
     }
 
+    @Override
     public String encodeUrl(String url) {
         return null;
     }
 
+    @Override
     public String encodeURL(String url) {
         return null;
     }
 
+    @Override
     public void sendError(int sc, String msg) throws IOException {
         status = sc;
         message = msg;
     }
 
+    @Override
     public void sendError(int sc) throws IOException {
         status = sc;
     }
 
+    @Override
     public void sendRedirect(String location) throws IOException {
     }
 
+    @Override
     public void setDateHeader(String name, long date) {
     }
 
+    @Override
     public void setHeader(String name, String value) {
     }
 
+    @Override
     public void setIntHeader(String name, int value) {
     }
 
+    @Override
     public void setStatus(int sc, String sm) {
         status = sc;
         message = sm;
     }
 
+    @Override
     public void setStatus(int sc) {
         status = sc;
     }
 
+    @Override
     public void flushBuffer() throws IOException {
     }
 
+    @Override
     public int getBufferSize() {
         return 0;
     }
 
+    @Override
     public String getCharacterEncoding() {
         return encoding;
     }
 
+    @Override
     public String getContentType() {
         return contentType;
     }
 
+    @Override
     public Locale getLocale() {
         return null;
     }
 
+    @Override
     public ServletOutputStream getOutputStream() throws IOException {
         return outputStream;
     }
 
+    @Override
     public PrintWriter getWriter() throws IOException {
         return writer;
     }
 
+    @Override
     public boolean isCommitted() {
         return false;
     }
 
+    @Override
     public void reset() {
     }
 
+    @Override
     public void resetBuffer() {
     }
 
+    @Override
     public void setBufferSize(int size) {
     }
 
+    @Override
     public void setCharacterEncoding(String charset) {
         encoding = charset;
     }
 
+    @Override
     public void setContentLength(int len) {
     }
 
+    @Override
     public void setContentType(String type) {
         contentType = type;
     }
 
+    @Override
     public void setLocale(Locale loc) {
     }
 
@@ -173,12 +206,32 @@ public class HttpResponse implements HttpServletResponse {
         this.encoding = encoding;
     }
 
+    @Override
     public int getStatus() {
         return status;
     }
-    
+
     public String getContent() {
         writer.flush();
         return outputStream.toString();
+    }
+
+    @Override
+    public void setContentLengthLong(long len) {
+    }
+
+    @Override
+    public String getHeader(String name) {
+        return null;
+    }
+
+    @Override
+    public Collection<String> getHeaders(String name) {
+        return null;
+    }
+
+    @Override
+    public Collection<String> getHeaderNames() {
+        return null;
     }
 }
--- testing/junit/scriptable/src/main/java/org/apache/sling/junit/scriptable/TestServletOutputStream.java
+++ testing/junit/scriptable/src/main/java/org/apache/sling/junit/scriptable/TestServletOutputStream.java
@@ -20,19 +20,29 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
 import javax.servlet.ServletOutputStream;
+import javax.servlet.WriteListener;
 
 /** Output stream for our fake HTTP response class */
 public class TestServletOutputStream extends ServletOutputStream {
 
     private final ByteArrayOutputStream bos = new ByteArrayOutputStream();
-    
+
     @Override
     public String toString() {
         return bos.toString();
     }
-    
+
     @Override
     public void write(int b) throws IOException {
         bos.write(b);
+    }
+
+    @Override
+    public boolean isReady() {
+        return true;
+    }
+
+    @Override
+    public void setWriteListener(WriteListener writeListener) {
     }
 }
