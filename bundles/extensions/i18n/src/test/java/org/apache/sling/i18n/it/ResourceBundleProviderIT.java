923f80a77c9c2d0de02cbe56665894630e2db08a
Karl Pauls <pauls@apache.org>
2017-06-02 11:18:36 +0000

    Remove the commons.json bundle from the i18n it tests
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk@1797372 13f79535-47bb-0310-9956-ffa450edef68

--- bundles/extensions/i18n/src/test/java/org/apache/sling/i18n/it/ResourceBundleProviderIT.java
+++ bundles/extensions/i18n/src/test/java/org/apache/sling/i18n/it/ResourceBundleProviderIT.java
@@ -181,6 +181,7 @@ public class ResourceBundleProviderIT {
                 mavenBundle("commons-collections", "commons-collections", "3.2.2"),
                 mavenBundle("commons-codec", "commons-codec", "1.10"),
                 mavenBundle("commons-lang", "commons-lang", "2.6"),
+                mavenBundle("org.apache.commons", "commons-lang3", "3.5"),
                 mavenBundle("commons-pool", "commons-pool", "1.6"),
 
                 mavenBundle("org.apache.servicemix.bundles", "org.apache.servicemix.bundles.concurrent", "1.3.4_1"),
@@ -201,7 +202,6 @@ public class ResourceBundleProviderIT {
                 // sling
                 mavenBundle("org.apache.sling", "org.apache.sling.settings", "1.3.8"),
                 mavenBundle("org.apache.sling", "org.apache.sling.commons.osgi", "2.3.0"),
-                mavenBundle("org.apache.sling", "org.apache.sling.commons.json", "2.0.16"),
                 mavenBundle("org.apache.sling", "org.apache.sling.commons.mime", "2.1.8"),
                 mavenBundle("org.apache.sling", "org.apache.sling.commons.classloader", "1.3.2"),
                 mavenBundle("org.apache.sling", "org.apache.sling.commons.scheduler", "2.4.14"),
@@ -209,17 +209,17 @@ public class ResourceBundleProviderIT {
 
                 mavenBundle("org.apache.sling", "org.apache.sling.auth.core", "1.3.12"),
                 mavenBundle("org.apache.sling", "org.apache.sling.discovery.api", "1.0.2"),
-                mavenBundle("org.apache.sling", "org.apache.sling.discovery.commons", "1.0.12"),
+                mavenBundle("org.apache.sling", "org.apache.sling.discovery.commons", "1.0.20"),
                 mavenBundle("org.apache.sling", "org.apache.sling.discovery.standalone", "1.0.2"),
 
                 mavenBundle("org.apache.sling", "org.apache.sling.api", "2.14.2"),
                 mavenBundle("org.apache.sling", "org.apache.sling.resourceresolver", "1.4.18"),
-                mavenBundle("org.apache.sling", "org.apache.sling.adapter", "2.1.2"),
+                mavenBundle("org.apache.sling", "org.apache.sling.adapter", "2.1.10"),
                 mavenBundle("org.apache.sling", "org.apache.sling.jcr.resource", "2.8.0"),
                 mavenBundle("org.apache.sling", "org.apache.sling.jcr.classloader", "3.2.2"),
-                mavenBundle("org.apache.sling", "org.apache.sling.jcr.contentloader", "2.1.8"),
+                mavenBundle("org.apache.sling", "org.apache.sling.jcr.contentloader", "2.2.4"),
                 mavenBundle("org.apache.sling", "org.apache.sling.engine", "2.6.2"),
-                mavenBundle("org.apache.sling", "org.apache.sling.serviceusermapper", "1.2.2"),
+                mavenBundle("org.apache.sling", "org.apache.sling.serviceusermapper", "1.3.2"),
 
                 mavenBundle("org.apache.sling", "org.apache.sling.jcr.jcr-wrapper", "2.0.0"),
                 mavenBundle("org.apache.sling", "org.apache.sling.jcr.api", "2.4.0"),
@@ -247,7 +247,8 @@ public class ResourceBundleProviderIT {
 
                 mavenBundle("org.apache.sling", "org.apache.sling.jcr.oak.server", "1.1.0"),
 
-                mavenBundle("org.apache.sling", "org.apache.sling.testing.tools", "1.0.6"),
+                mavenBundle("org.apache.sling", "org.apache.sling.testing.tools", "1.0.17-SNAPSHOT"),
+                mavenBundle("org.apache.sling", "org.apache.sling.commons.johnzon", "1.0.0"),
                 mavenBundle("org.apache.httpcomponents", "httpcore-osgi", "4.1.2"),
                 mavenBundle("org.apache.httpcomponents", "httpclient-osgi", "4.1.2"),
 
