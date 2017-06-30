1363007889d0db28cff86ee12e9a7d56df1bac72
Tommaso Teofili <tommaso@apache.org>
2017-06-30 12:26:46 +0000

    SLING-6977 - applied patch from Simone Tripodi
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk@1800389 13f79535-47bb-0310-9956-ffa450edef68

--- contrib/extensions/distribution/core/src/main/java/org/apache/sling/distribution/component/impl/DefaultDistributionComponentProvider.java
+++ contrib/extensions/distribution/core/src/main/java/org/apache/sling/distribution/component/impl/DefaultDistributionComponentProvider.java
@@ -59,11 +59,17 @@ public class DefaultDistributionComponentProvider implements DistributionCompone
     private static final String NAME = DistributionComponentConstants.PN_NAME;
 
     private final Map<String, DistributionComponent<DistributionAgent>> distributionAgentMap = new ConcurrentHashMap<String, DistributionComponent<DistributionAgent>>();
+
     private final Map<String, DistributionComponent<DistributionQueueProvider>> distributionQueueProviderMap = new ConcurrentHashMap<String, DistributionComponent<DistributionQueueProvider>>();
+
     private final Map<String, DistributionComponent<DistributionQueueDispatchingStrategy>> distributionQueueDistributionStrategyMap = new ConcurrentHashMap<String, DistributionComponent<DistributionQueueDispatchingStrategy>>();
+
     private final Map<String, DistributionComponent<DistributionTransportSecretProvider>> distributionTransportSecretProviderMap = new ConcurrentHashMap<String, DistributionComponent<DistributionTransportSecretProvider>>();
+
     private final Map<String, DistributionComponent<DistributionPackageImporter>> distributionPackageImporterMap = new ConcurrentHashMap<String, DistributionComponent<DistributionPackageImporter>>();
+
     private final Map<String, DistributionComponent<DistributionPackageExporter>> distributionPackageExporterMap = new ConcurrentHashMap<String, DistributionComponent<DistributionPackageExporter>>();
+
     private final Map<String, DistributionComponent<DistributionPackageBuilder>> distributionPackageBuilderMap = new ConcurrentHashMap<String, DistributionComponent<DistributionPackageBuilder>>();
 
     public DistributionComponent<?> getComponent(DistributionComponentKind kind, String componentName) {
@@ -92,7 +98,6 @@ public class DefaultDistributionComponentProvider implements DistributionCompone
         return type.cast(component.getService());
     }
 
-
     @SuppressWarnings({ "unchecked", "rawtypes" })
     private Map<String, DistributionComponent<?>> getComponentMap(Class<?> type) {
         if (type.isAssignableFrom(DistributionAgent.class)) {
@@ -114,82 +119,65 @@ public class DefaultDistributionComponentProvider implements DistributionCompone
         return null;
     }
 
-    // TODO are these methods still needed?!?
+    // (un)binding methods
 
-    private void bindDistributionQueueProvider(DistributionQueueProvider distributionQueueProvider, Map<String, Object> config) {
-
+    public void bindDistributionQueueProvider(DistributionQueueProvider distributionQueueProvider, Map<String, Object> config) {
         put(DistributionQueueProvider.class, distributionQueueProvider, config);
     }
 
-    private void unbindDistributionQueueProvider(DistributionQueueProvider distributionQueueProvider, Map<String, Object> config) {
-
+    public void unbindDistributionQueueProvider(DistributionQueueProvider distributionQueueProvider, Map<String, Object> config) {
         remove(DistributionQueueProvider.class, distributionQueueProvider, config);
     }
 
-    private void bindDistributionQueueDistributionStrategy(DistributionQueueDispatchingStrategy distributionQueueDispatchingStrategy, Map<String, Object> config) {
-
+    public void bindDistributionQueueDistributionStrategy(DistributionQueueDispatchingStrategy distributionQueueDispatchingStrategy, Map<String, Object> config) {
         put(DistributionQueueDispatchingStrategy.class, distributionQueueDispatchingStrategy, config);
     }
 
-    private void unbindDistributionQueueDistributionStrategy(DistributionQueueDispatchingStrategy distributionQueueDispatchingStrategy, Map<String, Object> config) {
-
+    public void unbindDistributionQueueDistributionStrategy(DistributionQueueDispatchingStrategy distributionQueueDispatchingStrategy, Map<String, Object> config) {
         remove(DistributionQueueDispatchingStrategy.class, distributionQueueDispatchingStrategy, config);
     }
 
-    private void bindDistributionTransportSecretProvider(DistributionTransportSecretProvider distributionTransportSecretProvider, Map<String, Object> config) {
-
+    public void bindDistributionTransportSecretProvider(DistributionTransportSecretProvider distributionTransportSecretProvider, Map<String, Object> config) {
         put(DistributionTransportSecretProvider.class, distributionTransportSecretProvider, config);
-
     }
 
-    private void unbindDistributionTransportSecretProvider(DistributionTransportSecretProvider distributionTransportSecretProvider, Map<String, Object> config) {
-
+    public void unbindDistributionTransportSecretProvider(DistributionTransportSecretProvider distributionTransportSecretProvider, Map<String, Object> config) {
         remove(DistributionTransportSecretProvider.class, distributionTransportSecretProvider, config);
     }
 
-    private void bindDistributionPackageImporter(DistributionPackageImporter distributionPackageImporter, Map<String, Object> config) {
-
+    public void bindDistributionPackageImporter(DistributionPackageImporter distributionPackageImporter, Map<String, Object> config) {
         put(DistributionPackageImporter.class, distributionPackageImporter, config);
     }
 
-    private void unbindDistributionPackageImporter(DistributionPackageImporter distributionPackageImporter, Map<String, Object> config) {
-
+    public void unbindDistributionPackageImporter(DistributionPackageImporter distributionPackageImporter, Map<String, Object> config) {
         remove(DistributionPackageImporter.class, distributionPackageImporter, config);
     }
 
-    private void bindDistributionPackageExporter(DistributionPackageExporter distributionPackageExporter, Map<String, Object> config) {
-
+    public void bindDistributionPackageExporter(DistributionPackageExporter distributionPackageExporter, Map<String, Object> config) {
         put(DistributionPackageExporter.class, distributionPackageExporter, config);
     }
 
-    private void unbindDistributionPackageExporter(DistributionPackageExporter distributionPackageExporter, Map<String, Object> config) {
-
+    public void unbindDistributionPackageExporter(DistributionPackageExporter distributionPackageExporter, Map<String, Object> config) {
         remove(DistributionPackageExporter.class, distributionPackageExporter, config);
-
     }
 
-    private void bindDistributionAgent(DistributionAgent distributionAgent, Map<String, Object> config) {
-
+    public void bindDistributionAgent(DistributionAgent distributionAgent, Map<String, Object> config) {
         put(DistributionAgent.class, distributionAgent, config);
     }
 
-    private void unbindDistributionAgent(DistributionAgent distributionAgent, Map<String, Object> config) {
-
+    public void unbindDistributionAgent(DistributionAgent distributionAgent, Map<String, Object> config) {
         remove(DistributionAgent.class, distributionAgent, config);
-
     }
 
-
-    private void bindDistributionPackageBuilder(DistributionPackageBuilder distributionPackageBuilder, Map<String, Object> config) {
-
+    public void bindDistributionPackageBuilder(DistributionPackageBuilder distributionPackageBuilder, Map<String, Object> config) {
         put(DistributionPackageBuilder.class, distributionPackageBuilder, config);
     }
 
-    private void unbindDistributionPackageBuilder(DistributionPackageBuilder distributionPackageBuilder, Map<String, Object> config) {
-
+    public void unbindDistributionPackageBuilder(DistributionPackageBuilder distributionPackageBuilder, Map<String, Object> config) {
         remove(DistributionPackageBuilder.class, distributionPackageBuilder, config);
-
     }
+
+    // internals
 
     private <ComponentType> void put(Class<ComponentType> typeClass, ComponentType service, Map<String, Object> config) {
         Map<String, DistributionComponent<?>> componentMap = getComponentMap(typeClass);
@@ -199,19 +187,15 @@ public class DefaultDistributionComponentProvider implements DistributionCompone
         if (name != null && kind != null) {
             componentMap.put(name, new DistributionComponent<ComponentType>(kind, name, service, config));
         }
-
     }
 
     private <ComponentType> void remove(Class<ComponentType> typeClass, ComponentType service, Map<String, Object> config) {
-
         Map<String, DistributionComponent<?>> componentMap = getComponentMap(typeClass);
 
         String name = PropertiesUtil.toString(config.get(NAME), null);
         if (name != null) {
             componentMap.remove(name);
         }
-
     }
-
 
 }
