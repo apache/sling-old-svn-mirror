626062de6103bc46acf23d8d0c59e8d65a731726
Radu Cotescu <radu@apache.org>
2017-08-01 14:07:48 +0000

    SLING-7025 - htl-maven-plugin: Correctly state default values for include parameter
    * applied slightly modified patch from Konrad Windszus
    
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk@1803644 13f79535-47bb-0310-9956-ffa450edef68

--- tooling/maven/htl-maven-plugin/src/test/java/org/apache/sling/maven/htl/ValidateMojoTest.java
+++ tooling/maven/htl-maven-plugin/src/test/java/org/apache/sling/maven/htl/ValidateMojoTest.java
@@ -19,10 +19,18 @@ package org.apache.sling.maven.htl;
 import java.io.File;
 import java.util.List;
 
+import org.apache.maven.execution.DefaultMavenExecutionRequest;
+import org.apache.maven.execution.MavenExecutionRequest;
+import org.apache.maven.execution.MavenSession;
+import org.apache.maven.plugin.MojoExecution;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugin.testing.MojoRule;
 import org.apache.maven.plugin.testing.SilentLog;
 import org.apache.maven.project.MavenProject;
+import org.apache.maven.project.ProjectBuilder;
+import org.apache.maven.project.ProjectBuildingRequest;
+import org.eclipse.aether.DefaultRepositorySystemSession;
+import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
 import org.sonatype.plexus.build.incremental.DefaultBuildContext;
@@ -33,20 +41,22 @@ import static org.junit.Assert.assertTrue;
 
 public class ValidateMojoTest {
 
-    public static final String ERROR_SLY = "src/main/resources/apps/projects/error.sly";
-    public static final String WARNING_SLY = "src/main/resources/apps/projects/warning.sly";
-    public static final String SCRIPT_HTML = "src/main/resources/apps/projects/script.html";
-    public static final String TEST_PROJECT = "test-project";
-    public static final String EXPLICIT_INCLUDES_POM = "explicit-includes.pom.xml";
-    public static final String EXPLICIT_EXCLUDES_POM = "explicit-excludes.pom.xml";
-    public static final String FAIL_ON_WARNINGS_POM = "fail-on-warnings.pom.xml";
+    private static final String ERROR_SLY = "src/main/resources/apps/projects/error.sly";
+    private static final String WARNING_SLY = "src/main/resources/apps/projects/warning.sly";
+    private static final String SCRIPT_HTML = "src/main/resources/apps/projects/script.html";
+    private static final String EXCLUDE_HTML = "src/main/resources/apps/projects/exclude.html";
+    private static final String TEST_PROJECT = "test-project";
+    private static final String EXPLICIT_INCLUDES_POM = "explicit-includes.pom.xml";
+    private static final String EXPLICIT_EXCLUDES_POM = "explicit-excludes.pom.xml";
+    private static final String FAIL_ON_WARNINGS_POM = "fail-on-warnings.pom.xml";
+    private static final String DEFAULT_INCLUDES_POM = "default-includes.pom.xml";
 
     @Rule
     public MojoRule mojoRule = new MojoRule() {
         @Override
         protected void before() throws Throwable {
             super.before();
-            /**
+            /*
              * Make sure the base directory is initialised properly for this test
              */
             System.setProperty("basedir", new File("src" + File.separator + "test" + File.separator + "resources" + File
@@ -99,20 +109,51 @@ public class ValidateMojoTest {
         assertEquals("Expected compilation warnings.", true, validateMojo.hasWarnings());
     }
 
+    @Test
+    public void testDefaultIncludes() throws Exception {
+        File baseDir = new File(System.getProperty("basedir"));
+        ValidateMojo validateMojo = getMojo(baseDir, DEFAULT_INCLUDES_POM);
+        Exception exception = null;
+        try {
+            validateMojo.execute();
+        } catch (MojoFailureException e) {
+            exception = e;
+        }
+        List<File> processedFiles = validateMojo.getProcessedFiles();
+        assertNotNull("Expected a MojoFailureException.", exception);
+        assertEquals("Expected 2 files to process.", 2, processedFiles.size());
+        assertTrue("Expected exclude.html to be one of the processed files.", processedFiles.contains(new File(baseDir, EXCLUDE_HTML)));
+        assertTrue("Expected script.html to be one of the processed files.", processedFiles.contains(new File(baseDir, SCRIPT_HTML)));
+        assertEquals("Did not expect compilation warnings.", false, validateMojo.hasWarnings());
+    }
+
     private ValidateMojo getMojo(File baseDir, String pomFile) throws Exception {
         SilentLog log = new SilentLog();
         DefaultBuildContext buildContext = new DefaultBuildContext();
 
-        File pom = new File(baseDir, pomFile);
-        ValidateMojo validateMojo = new ValidateMojo();
-        mojoRule.configureMojo(validateMojo, mojoRule.extractPluginConfiguration("htl-maven-plugin", pom));
-        MavenProject mavenProject = new ProjectStub(pom);
-        mojoRule.setVariableValueToObject(validateMojo, "project", mavenProject);
+        // use lookupConfiguredMojo to also consider default values (https://issues.apache.org/jira/browse/MPLUGINTESTING-23)
+        // similar to MojoRule#lookupConfiguredMojo(File, String) but with custom pom file name
+        MavenProject project = readMavenProject(baseDir, pomFile);
+        MavenSession session = mojoRule.newMavenSession(project);
+        MojoExecution execution = mojoRule.newMojoExecution("validate");
+        ValidateMojo validateMojo = (ValidateMojo) mojoRule.lookupConfiguredMojo(session, execution);
         validateMojo.setLog(log);
         buildContext.enableLogging(log);
         validateMojo.setBuildContext(buildContext);
         return validateMojo;
     }
 
-
+    /**
+     * Copied from {@code org.apache.maven.plugin.testing.readMavenProject(...)} but customized to allow custom pom names
+     */
+    private MavenProject readMavenProject(File basedir, String pomFileName) throws Exception {
+        File pom = new File(basedir, pomFileName);
+        MavenExecutionRequest request = new DefaultMavenExecutionRequest();
+        request.setBaseDirectory(basedir);
+        ProjectBuildingRequest configuration = request.getProjectBuildingRequest();
+        configuration.setRepositorySession(new DefaultRepositorySystemSession());
+        MavenProject project = mojoRule.lookup(ProjectBuilder.class).build(pom, configuration).getProject();
+        Assert.assertNotNull(project);
+        return project;
+    }
 }
