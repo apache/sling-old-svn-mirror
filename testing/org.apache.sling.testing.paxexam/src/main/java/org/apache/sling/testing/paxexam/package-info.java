a802f7941d6023f2bd905be33d512b7b19b86528
Oliver Lietz <olli@apache.org>
2017-08-23 22:30:58 +0000

    SLING-7080 Update options and versions to latest features
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk@1805968 13f79535-47bb-0310-9956-ffa450edef68

--- testing/org.apache.sling.testing.paxexam/src/main/java/org/apache/sling/testing/paxexam/package-info.java
+++ testing/org.apache.sling.testing.paxexam/src/main/java/org/apache/sling/testing/paxexam/package-info.java
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-@Version("1.0.0")
+@Version("1.1.0")
 package org.apache.sling.testing.paxexam;
 
 import org.osgi.annotation.versioning.Version;
