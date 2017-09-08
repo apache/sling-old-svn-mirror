de4192d0aadfbee32ea08d4781b2b7fbbe8bdb5a
Robert Munteanu <rombert@apache.org>
2017-09-08 13:24:03 +0000

    SLING-3987 - move from Subversion to Git
    Add a script to generate repo candidates
    
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk@1807758 13f79535-47bb-0310-9956-ffa450edef68

---
+++ tooling/scm/scripts/gen-repo-candidates.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# this script is used to generate a list of Git repo candidates for migration
+# from SVN to Git
+
+if [ ! -f check_staged_release.sh ]; then
+    echo "Please run this script from the root of the Sling SVN repository"
+    exit 1
+fi
+
+for pom in $(find . -name pom.xml  \
+    | grep -v './tooling/ide' \
+    | grep -v '/target/' \
+    | grep -v '/archetype-resources/' \
+    | grep -v '/src/test/resources/' \
+    | sort); do
+
+    # remove reactor poms
+    grep -q modules $pom
+    if [ $? -eq 0 ]; then
+        continue;
+    fi
+    
+    pom=${pom%/pom.xml} # remove trailing '/pom.xml'
+    pom=${pom#./} # remove leading ./
+
+    echo $pom
+done
+
+echo "tooling/ide" # by exception, this will be a standalone module
