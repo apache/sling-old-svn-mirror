6dc421cad08a4ce1fb9ace537c2b2fb0bbd59c66
Carsten Ziegeler <cziegeler@apache.org>
2017-10-05 11:42:19 +0000

    Rename package
    git-svn-id: https://svn.apache.org/repos/asf/sling/trunk@1811188 13f79535-47bb-0310-9956-ffa450edef68

--- launchpad/starter.startup/src/main/java/org/apache/sling/starter/startup/impl/Activator.java
+++ launchpad/starter.startup/src/main/java/org/apache/sling/starter/startup/impl/Activator.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.sling.demo.startup.impl;
+package org.apache.sling.starter.startup.impl;
 
 import java.util.Timer;
 import java.util.TimerTask;
